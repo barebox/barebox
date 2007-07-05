0c2b8f687e51d1c199856bcd8e603297dd5d7986
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:01:31 +0200

    svn_rev_194

--- arch/arm/lib/arm.c
+++ arch/arm/lib/arm.c
@@ -1,10 +1,12 @@
+#include <common.h>
 #include <init.h>
 #include <mem_malloc.h>
 
-int arm_mem_alloc_init(void)
+int arm_mem_malloc_init(void)
 {
-	mem_alloc_init(_armboot_start - CFG_MALLOC_LEN,
+	mem_malloc_init(_armboot_start - CFG_MALLOC_LEN,
 			_armboot_start);
+	return 0;
 }
 
-core_initcall(arm_mem_alloc_init);
+core_initcall(arm_mem_malloc_init);

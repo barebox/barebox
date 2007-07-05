5bd6b136a922c926d66b9b7c90d816e8a7bd3c8b
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:01:51 +0200

    svn_rev_410

--- include/asm-linux/arch-linux/linux.h
+++ include/asm-linux/arch-linux/linux.h
@@ -8,5 +8,8 @@ int linux_read(int fd, void *buf, size_t count);
 int linux_read_nonblock(int fd, void *buf, size_t count);
 ssize_t linux_write(int fd, const void *buf, size_t count);
 off_t linux_lseek(int fildes, off_t offset);
+int linux_getc (void);
+void linux_putc (const char c);
+int linux_tstc(void);
 
 #endif /* __ASM_ARCH_LINUX_H */

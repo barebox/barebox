b2b81e2f634b31e394d50125530415764434ea7e
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:01:52 +0200

    svn_rev_424

--- common/hush.c
+++ common/hush.c
@@ -87,7 +87,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#define __U_BOOT__
 #ifdef __U_BOOT__
 #include <malloc.h>         /* malloc, free, realloc*/
 #include <xfuncs.h>

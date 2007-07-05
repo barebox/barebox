75a10b942c77c0720b2f81c971672673b944d922
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:02:19 +0200

    svn_rev_706
    add file headers

--- arch/linux/lib/common.c
+++ arch/linux/lib/common.c
@@ -1,4 +1,29 @@
+/*
+ * common.c - common wrapper functions between U-Boot and the host
+ *
+ * Copyright (c) 2007 Sascha Hauer <s.hauer@pengutronix.de>, Pengutronix
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
+/*
+ * These are host includes. Never include any U-Boot header
+ * files here...
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -12,11 +37,14 @@
 #include <string.h>
 #include <libgen.h>
 #include <sys/mman.h>
-#include <asm/arch/linux.h>
-#include <asm/arch/hostfile.h>
 #include <errno.h>
 #include <signal.h>
 #include <sys/select.h>
+/*
+ * ...except the ones needed to connect with U-Boot
+ */
+#include <asm/arch/linux.h>
+#include <asm/arch/hostfile.h>
 
 static struct termios term_orig, term_vi;
 static char erase_char;	/* the users erase character */
--- lib/readkey.c
+++ lib/readkey.c
@@ -1,3 +1,26 @@
+/*
+ * readkey.c - read keystrokes and decode standard escape sequences
+ *
+ * Partly based on busybox vi
+ *
+ * Copyright (c) 2007 Sascha Hauer <s.hauer@pengutronix.de>, Pengutronix
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <common.h>
 #include <linux/ctype.h>

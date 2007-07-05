31d3ca035475ae9933d4398e56a170384fb9693f
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:02:17 +0200

    svn_rev_685

---
+++ include/cache.h
@@ -0,0 +1,14 @@
+#ifndef __CACHE_H
+#define __CACHE_H
+
+void	flush_cache   (unsigned long, unsigned long);
+int	icache_status (void);
+void	icache_enable (void);
+void	icache_disable(void);
+int	dcache_status (void);
+void	dcache_enable (void);
+void	dcache_disable(void);
+int	checkicache   (void);
+int	checkdcache   (void);
+
+#endif /* __CACHE_H */

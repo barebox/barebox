4cfbef6dd6e7a4546d395180086ab410e1c938b5
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:01:40 +0200

    svn_rev_292
    remove flags we will never use, add flags for rwsize

--- include/fcntl.h
+++ include/fcntl.h
@@ -9,48 +9,19 @@
 #define O_RDONLY	00000000
 #define O_WRONLY	00000001
 #define O_RDWR		00000002
-#ifndef O_CREAT
 #define O_CREAT		00000100	/* not fcntl */
-#endif
-#ifndef O_EXCL
 #define O_EXCL		00000200	/* not fcntl */
-#endif
-#ifndef O_NOCTTY
-#define O_NOCTTY	00000400	/* not fcntl */
-#endif
-#ifndef O_TRUNC
 #define O_TRUNC		00001000	/* not fcntl */
-#endif
-#ifndef O_APPEND
 #define O_APPEND	00002000
-#endif
-#ifndef O_NONBLOCK
-#define O_NONBLOCK	00004000
-#endif
-#ifndef O_SYNC
-#define O_SYNC		00010000
-#endif
-#ifndef FASYNC
-#define FASYNC		00020000	/* fcntl, for BSD compatibility */
-#endif
-#ifndef O_DIRECT
-#define O_DIRECT	00040000	/* direct disk access hint */
-#endif
-#ifndef O_LARGEFILE
-#define O_LARGEFILE	00100000
-#endif
-#ifndef O_DIRECTORY
 #define O_DIRECTORY	00200000	/* must be a directory */
-#endif
-#ifndef O_NOFOLLOW
 #define O_NOFOLLOW	00400000	/* don't follow links */
-#endif
-#ifndef O_NOATIME
-#define O_NOATIME	01000000
-#endif
-#ifndef O_NDELAY
-#define O_NDELAY	O_NONBLOCK
-#endif
+
+/* U-Boot additional flags */
+#define O_RWSIZE_MASK	00000070
+#define O_RWSIZE_SHIFT	3
+#define O_RWSIZE_1      00000010
+#define O_RWSIZE_2      00000020
+#define O_RWSIZE_4      00000040
 
 #define F_DUPFD		0	/* dup */
 #define F_GETFD		1	/* get close_on_exec */

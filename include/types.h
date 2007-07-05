ba737ad29ef76369e995fcc3261dcaaa5fd3aa28
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:02:01 +0200

    svn_rev_522

---
+++ include/types.h
@@ -0,0 +1,8 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+typedef volatile unsigned long	vu_long;
+typedef volatile unsigned short vu_short;
+typedef volatile unsigned char	vu_char;
+
+#endif /* __TYPES_H */

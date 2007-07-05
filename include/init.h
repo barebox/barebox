a12c581aa8189f67e13deefeea26a21ed08dd8da
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:01:54 +0200

    svn_rev_446
    add postcore_initcall

--- include/init.h
+++ include/init.h
@@ -9,7 +9,8 @@ typedef int (*initcall_t)(void);
 
 
 #define core_initcall(fn)		__define_initcall("0",fn,0)
-#define console_initcall(fn)		__define_initcall("1",fn,1)
+#define postcore_initcall(fn)		__define_initcall("1",fn,1)
+#define console_initcall(fn)		__define_initcall("2",fn,2)
 #define coredevice_initcall(fn)		__define_initcall("4",fn,4)
 #define device_initcall(fn)		__define_initcall("5",fn,5)
 #define late_initcall(fn)		__define_initcall("6",fn,6)

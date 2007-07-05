c5ba7e8aa3238c1d8e44a7a683091d35fb7fcbfb
Sascha Hauer <s.hauer@pengutronix.de>
2007-07-05 18:02:02 +0200

    svn_rev_534

--- include/mem_malloc.h
+++ include/mem_malloc.h
@@ -3,7 +3,7 @@
 
 #include <linux/types.h>
 
-void mem_malloc_init (void *start, void *end);
-void *sbrk (ptrdiff_t increment);
+void mem_malloc_init(void *start, void *end);
+void *sbrk_no_zero(ptrdiff_t increment);
 
 #endif

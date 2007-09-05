b39e410ca701f0d96b38a1347cc1d3aaddbe0164
Sascha Hauer <sha@octopus.labnet.pengutronix.de>
2007-09-05 12:57:00 +0200

    add Hilscher netx support part3 (header files)

---
+++ include/asm-arm/arch-netx/netx-eth.h
@@ -0,0 +1,9 @@
+#ifndef _ASM_ARCH_NETX_ETH_H
+#define _ASM_ARCH_NETX_ETH_H
+
+struct netx_eth_platform_data {
+	int xcno;
+};
+
+#endif /* _ASM_ARCH_NETX_ETH_H */
+
---
+++ include/asm-arm/arch-netx/netx-xc.h
@@ -0,0 +1,6 @@
+#ifndef __ASM_ARCH_NETX_XC_H
+#define __ASM_ARCH_NETX_XC_H
+
+int loadxc(int);
+
+#endif

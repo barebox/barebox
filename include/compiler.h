c41b41f9e778e48f3afe7956870c7c7f1e10d006
Sascha Hauer <s.hauer@pengutronix.de>
2007-10-01 10:13:13 +0200

    add __used attribute in compiler.h

--- include/compiler.h
+++ include/compiler.h
@@ -1,1 +1,8 @@
+#ifndef __COMPILER_H
+#define __COMPILER_H
+
 #define __maybe_unused			__attribute__((unused))
+#define __used                 __attribute__((__used__))
+
+#endif /* __COMPILER_H */
+

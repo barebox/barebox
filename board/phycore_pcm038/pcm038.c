514eb64b1a67971123addcccb206ef3c45b766db
Sascha Hauer <s.hauer@pengutronix.de>
2007-10-08 00:13:19 +0200

    beginning phycore pcm038 i.MX27 support

---
+++ board/phycore_pcm038/pcm038.c
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2007 Sascha Hauer, Pengutronix 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ *
+ */
+
+#include <common.h>
+#include <net.h>
+#include <cfi_flash.h>
+#include <init.h>
+#include <environment.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/gpio.h>
+#include <partition.h>
+#include <fs.h>
+#include <fcntl.h>
+
+static struct device_d cfi_dev = {
+	.name     = "cfi_flash",
+	.id       = "nor0",
+
+	.map_base = 0xC0000000,
+	.size     = 16 * 1024 * 1024, /* FIXME */
+};
+
+static struct device_d sdram_dev = {
+	.name     = "ram",
+	.id       = "ram0",
+
+	.map_base = 0xa0000000,
+	.size     = 16 * 1024 * 1024, /* FIXME */
+
+	.type     = DEVICE_TYPE_DRAM,
+};
+
+static struct device_d fec_dev = {
+	.name     = "fec_imx27",
+	.id       = "eth0",
+
+	.type     = DEVICE_TYPE_ETHER,
+};
+
+static int pcm038_devices_init(void)
+{
+	int i;
+	unsigned short mode[] = {
+		PD12_AOUT_FEC_RXD0,
+		PD5_AOUT_FEC_RXD1,
+		PD6_AOUT_FEC_RXD2,
+		PD7_AOUT_FEC_RXD3,
+		PD4_AOUT_FEC_RX_ER,
+		PD13_AOUT_FEC_RX_DV,
+		PD0_AIN_FEC_TXD0,
+		PD1_AIN_FEC_TXD1,
+		PD2_AIN_FEC_TXD2,
+		PD3_AIN_FEC_TXD3,
+		PD11_AOUT_FEC_TX_CLK,
+		PF23_AIN_FEC_TX_EN,
+		PD8_AF_FEC_MDIO,
+		PD9_AIN_FEC_MDC,
+		PD14_AOUT_FEC_CLR,
+		PD15_AOUT_FEC_COL,
+		PD16_AIN_FEC_TX_ER,
+		PE12_PF_UART1_TXD,
+		PE13_PF_UART1_RXD,
+		PE14_PF_UART1_CTS,
+		PE15_PF_UART1_RTS };
+
+	for (i = 0; i < sizeof(mode) / sizeof(short); i ++)
+		imx_gpio_mode(mode[i]);
+
+	register_device(&cfi_dev);
+	register_device(&sdram_dev);
+	register_device(&fec_dev);
+
+	dev_add_partition(&cfi_dev, 0x00000, 0x20000, "self");
+	dev_add_partition(&cfi_dev, 0x40000, 0x20000, "env");
+	dev_protect(&cfi_dev, 0x20000, 0, 1);
+
+	return 0;
+}
+
+device_initcall(pcm038_devices_init);
+
+static struct device_d pcm038_serial_device = {
+	.name     = "imx_serial",
+	.id       = "cs0",
+	.map_base = IMX_UART1_BASE,
+	.size     = 4096,
+	.type     = DEVICE_TYPE_CONSOLE,
+};
+
+static int pcm038_console_init(void)
+{
+	register_device(&pcm038_serial_device);
+	return 0;
+}
+
+console_initcall(pcm038_console_init);
+

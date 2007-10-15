a149f9949bbe732ecf84ee4e9ea8c59e7a39aef8
Sascha Hauer <sha@octopus.labnet.pengutronix.de>
2007-10-15 17:54:52 +0200

    add pcm038 config

---
+++ include/configs/phycore_pcm038.h
@@ -0,0 +1,6 @@
+/* nothing yet */
+#define CONFIG_ARCH_NUMBER 9999
+#define CONFIG_BOOT_PARAMS 0xdeadbeef
+#define CFG_MALLOC_LEN		(4096 << 10)
+#define CONFIG_STACKSIZE	(120<<10)      /* stack size		     */
+

barebox/barebox

commit 6574529bb5a233278e9886a93afeed70e9993b54

[sandbox] add error handling to read
This patch adds propper error handling to the linux' read functions.

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

arch/sandbox/lib/common.c
@@ -146,7 +146,27 @@ void disable_interrupt(void)
 
 int linux_read(int fd, void *buf, size_t count)
 {
-	return read(fd, buf, count);
+	ssize_t ret;
+
+	do {
+		ret = read(fd, buf, count);
+
+		if (ret == 0) {
+			printf("read on fd %d returned 0, device gone? - exiting\n", fd);
+			reset_cpu(0);
+		} else if (ret == -1) {
+			if (errno == EAGAIN)
+				usleep(100);
+			else if (errno == EINTR)
+				continue;
+			else {
+				printf("read on fd %d returned -1, errno %d - exiting\n", fd, errno);
+				reset_cpu(0);
+			}
+		}
+	} while (ret <= 0);
+
+	return (int)ret;
 }
 
 int linux_read_nonblock(int fd, void *buf, size_t count)
@@ -160,13 +180,10 @@ int linux_read_nonblock(int fd, void *buf, size_t count)
 	if (fcntl(fd, F_SETFL, oldflags | O_NONBLOCK) == -1)
 		goto err_out;
 
-	ret = read(fd, buf, count);
+	ret = linux_read(fd, buf, count);
 
 	if (fcntl(fd, F_SETFL, oldflags) == -1)
 		goto err_out;
-
-	if (ret == -1)
-		usleep(100);
 
 	return ret;
 

commit 5c2afd89e1e0d2cddbc634890cae377f4392990e

[sandbox] fix read for buffers of zero length
This patch fixes the read function for the linux sandbox if a buffer
of zero length should be read. The error handling introduced in
6574529bb5a233278e9886a93afeed70e9993b54 detects a false positive if the
length is zero.

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

arch/sandbox/lib/common.c
@@ -148,6 +148,9 @@ int linux_read(int fd, void *buf, size_t count)
 {
 	ssize_t ret;
 
+	if (count == 0)
+		return 0;
+
 	do {
 		ret = read(fd, buf, count);
 
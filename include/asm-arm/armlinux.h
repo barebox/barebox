1a8e0f589f55a0ac2625f7d885944451b41a2eed
Sascha Hauer <s.hauer@pengutronix.de>
2008-03-01 22:47:46 +0100

    add armlinux.h

---
+++ include/asm-arm/armlinux.h
@@ -0,0 +1,17 @@
+#ifndef __ARCH_ARMLINUX_H
+#define __ARCH_ARMLINUX_H
+
+#ifdef CONFIG_CMD_BOOTM
+void armlinux_set_bootparams(void *params);
+void armlinux_set_architecture(int architecture);
+#else
+static inline void armlinux_set_bootparams(void *params)
+{
+}
+ficken
+static inline void armlinux_set_architecture(int architecture)
+{
+}
+#endif
+
+#endif /* __ARCH_ARMLINUX_H */

9bf7a4f97f58951507904f1d2ce7a469060deaae
Sascha Hauer <s.hauer@pengutronix.de>
2008-04-07 11:37:25 +0200

    [net] cleanups:  - remove cdp support  - replace CONFIG_BOOTP_MASK with #ifdef CONFIG_...

--- net/bootp.h
+++ net/bootp.h
@@ -18,7 +18,7 @@
 /*
  *	BOOTP header.
  */
-#if (CONFIG_COMMANDS & CFG_CMD_DHCP)
+#ifdef CONFIG_NET_DHCP
 #define OPT_SIZE 312	/* Minimum DHCP Options size per RFC2131 - results in 576 byte pkt */
 #else
 #define OPT_SIZE 64

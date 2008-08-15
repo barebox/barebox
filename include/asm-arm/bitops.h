31f9cb365ee738ab77ca5dfe629e9691231803b4
Nishanth Menon <x0nishan@ti.com>
2008-08-14 20:28:44 -0500

    Bitops:Cleanup ARM bitops
    Cleanup of ARM bitops functions.
    Introduce the findbits.S which allows for
    optimized algo.
    
    Signed-off-by: Nishanth Menon <x0nishan@ti.com>

--- include/asm-arm/bitops.h
+++ include/asm-arm/bitops.h
@@ -15,31 +15,27 @@
 #ifndef __ASM_ARM_BITOPS_H
 #define __ASM_ARM_BITOPS_H
 
-/*
- * Function prototypes to keep gcc -Wall happy.
- */
-extern void set_bit(int nr, volatile void * addr);
+#ifndef _LINUX_BITOPS_H
+#error only <linux/bitops.h> can be included directly
+#endif
 
+/*
+ * Functions equivalent of ops.h
+ */
 static inline void __set_bit(int nr, volatile void *addr)
 {
 	((unsigned char *) addr)[nr >> 3] |= (1U << (nr & 7));
 }
-
-extern void clear_bit(int nr, volatile void * addr);
 
 static inline void __clear_bit(int nr, volatile void *addr)
 {
 	((unsigned char *) addr)[nr >> 3] &= ~(1U << (nr & 7));
 }
 
-extern void change_bit(int nr, volatile void * addr);
-
 static inline void __change_bit(int nr, volatile void *addr)
 {
 	((unsigned char *) addr)[nr >> 3] ^= (1U << (nr & 7));
 }
-
-extern int test_and_set_bit(int nr, volatile void * addr);
 
 static inline int __test_and_set_bit(int nr, volatile void *addr)
 {
@@ -51,8 +47,6 @@ static inline int __test_and_set_bit(int nr, volatile void *addr)
 	return oldval & mask;
 }
 
-extern int test_and_clear_bit(int nr, volatile void * addr);
-
 static inline int __test_and_clear_bit(int nr, volatile void *addr)
 {
 	unsigned int mask = 1 << (nr & 7);
@@ -62,8 +56,6 @@ static inline int __test_and_clear_bit(int nr, volatile void *addr)
 	((unsigned char *) addr)[nr >> 3] = oldval & ~mask;
 	return oldval & mask;
 }
-
-extern int test_and_change_bit(int nr, volatile void * addr);
 
 static inline int __test_and_change_bit(int nr, volatile void *addr)
 {
@@ -75,9 +67,6 @@ static inline int __test_and_change_bit(int nr, volatile void *addr)
 	return oldval & mask;
 }
 
-extern int find_first_zero_bit(void * addr, unsigned size);
-extern int find_next_zero_bit(void * addr, int size, int offset);
-
 /*
  * This routine doesn't need to be atomic.
  */
@@ -85,6 +74,48 @@ static inline int test_bit(int nr, const void * addr)
 {
     return ((unsigned char *) addr)[nr >> 3] & (1U << (nr & 7));
 }
+
+#define set_bit(x, y)			__set_bit(x, y)
+#define clear_bit(x, y)			__clear_bit(x, y)
+#define change_bit(x, y)		__change_bit(x, y)
+#define test_and_set_bit(x, y)		__test_and_set_bit(x, y)
+#define test_and_clear_bit(x, y)	__test_and_clear_bit(x, y)
+#define test_and_change_bit(x, y)	__test_and_change_bit(x, y)
+
+#ifndef __ARMEB__
+/*
+ * These are the little endian definitions.
+ */
+extern int _find_first_zero_bit_le(const void *p, unsigned size);
+extern int _find_next_zero_bit_le(const void *p, int size, int offset);
+extern int _find_first_bit_le(const unsigned long *p, unsigned size);
+extern int _find_next_bit_le(const unsigned long *p, int size, int offset);
+#define find_first_zero_bit(p, sz)	_find_first_zero_bit_le(p, sz)
+#define find_next_zero_bit(p, sz, off)	_find_next_zero_bit_le(p, sz, off)
+#define find_first_bit(p, sz)		_find_first_bit_le(p, sz)
+#define find_next_bit(p, sz, off)	_find_next_bit_le(p, sz, off)
+
+#define WORD_BITOFF_TO_LE(x)		((x))
+
+#else		/* ! __ARMEB__ */
+
+/*
+ * These are the big endian definitions.
+ */
+extern int _find_first_zero_bit_be(const void *p, unsigned size);
+extern int _find_next_zero_bit_be(const void *p, int size, int offset);
+extern int _find_first_bit_be(const unsigned long *p, unsigned size);
+extern int _find_next_bit_be(const unsigned long *p, int size, int offset);
+#define find_first_zero_bit(p, sz)	_find_first_zero_bit_be(p, sz)
+#define find_next_zero_bit(p, sz, off)	_find_next_zero_bit_be(p, sz, off)
+#define find_first_bit(p, sz)		_find_first_bit_be(p, sz)
+#define find_next_bit(p, sz, off)	_find_next_bit_be(p, sz, off)
+
+#define WORD_BITOFF_TO_LE(x)		((x) ^ 0x18)
+
+#endif		/* __ARMEB__ */
+
+#if defined(__LINUX_ARM_ARCH__) && (__LINUX_ARM_ARCH__ < 5)
 
 /*
  * ffz = Find First Zero in word. Undefined if no zero exists,
@@ -103,35 +134,53 @@ static inline unsigned long ffz(unsigned long word)
 	if (word & 0x40000000) { k -= 1; }
 	return k;
 }
+#include <asm-generic/bitops/__ffs.h>
+#include <asm-generic/bitops/ffs.h>
+#include <asm-generic/bitops/fls.h>
+#else		/* ! __ARM__USE_GENERIC_FF */
+
+static inline int constant_fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
 
 /*
- * ffs: find first bit set. This is defined the same way as
- * the libc and compiler builtin ffs routines, therefore
- * differs in spirit from the above ffz (man ffs).
+ * On ARMv5 and above those functions can be implemented around
+ * the clz instruction for much better code efficiency.
  */
+#define fls(x) \
+	(__builtin_constant_p(x) ? constant_fls(x) : \
+	({ int __r; asm("clz\t%0, %1" : "=r"(__r) : "r"(x) : "cc"); 32-__r; }))
+#define ffs(x) ({ unsigned long __t = (x); fls(__t &-__t); })
+#define __ffs(x) (ffs(x) - 1)
+#define ffz(x) __ffs(~(x))
+#endif		/* __ARM__USE_GENERIC_FF */
+#include <asm-generic/bitops/fls64.h>
 
-#define ffs(x) generic_ffs(x)
-
-/*
- * hweightN: returns the hamming weight (i.e. the number
- * of bits set) of a N-bit word
- */
-
-#define hweight32(x) generic_hweight32(x)
-#define hweight16(x) generic_hweight16(x)
-#define hweight8(x) generic_hweight8(x)
-
-#define ext2_set_bit			test_and_set_bit
-#define ext2_clear_bit			test_and_clear_bit
-#define ext2_test_bit			test_bit
-#define ext2_find_first_zero_bit	find_first_zero_bit
-#define ext2_find_next_zero_bit		find_next_zero_bit
-
-/* Bitmap functions for the minix filesystem. */
-#define minix_test_and_set_bit(nr,addr)	test_and_set_bit(nr,addr)
-#define minix_set_bit(nr,addr)		set_bit(nr,addr)
-#define minix_test_and_clear_bit(nr,addr)	test_and_clear_bit(nr,addr)
-#define minix_test_bit(nr,addr)		test_bit(nr,addr)
-#define minix_find_first_zero_bit(addr,size)	find_first_zero_bit(addr,size)
+#include <asm-generic/bitops/hweight.h>
 
 #endif /* _ARM_BITOPS_H */

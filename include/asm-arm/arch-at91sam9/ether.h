26a8987c46ff250ba3ecee9b4112fca07844800c
Marc Kleine-Budde <mkl@pengutronix.de>
2009-01-19 12:03:09 +0100

    [macb] add MII flag, improve code readability
    This patch adds the flag "AT91SAM_ETHER_MII" along to the existing
    "AT91SAM_ETHER_RMII". This improves code readability in baords which
    are using plain MII mode.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- include/asm-arm/arch-at91sam9/ether.h
+++ include/asm-arm/arch-at91sam9/ether.h
@@ -1,7 +1,8 @@
 #ifndef __AT91SAM_ETHER_H
 #define __AT91SAM_ETHER_H
 
-#define AT91SAM_ETHER_RMII	(1 << 0)
+#define AT91SAM_ETHER_MII		(0 << 0)
+#define AT91SAM_ETHER_RMII		(1 << 0)
 
 struct at91sam_ether_platform_data {
 	unsigned int flags;

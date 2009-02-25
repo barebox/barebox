f090bca2d80a1f64fcfe07bb6f4efe7d1a00d741
Sascha Hauer <s.hauer@pengutronix.de>
2009-02-26 00:53:43 +0100

    add __[lb]e types
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/types.h
+++ include/types.h
@@ -1,6 +1,8 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+#include <linux/types.h>
+
 typedef volatile unsigned long	vu_long;
 typedef volatile unsigned short vu_short;
 typedef volatile unsigned char	vu_char;

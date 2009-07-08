barebox/barebox

commit d5ec92129e1c67ac86e92c22d54bb090fb00d616

mx2: add gpio direction/value setting calls
Signed-off-by: Luotao Fu <l.fu@pengutronix.de>

arch/arm/mach-imx/gpio.c
@@ -86,3 +86,26 @@ void imx_gpio_mode(int gpio_mode)
 
 }
 
+void imx_gpio_set_value(unsigned gpio, int value)
+{
+	if(value)
+		DR(gpio >> GPIO_PORT_SHIFT) |= (1 << (gpio & GPIO_PIN_MASK));
+	else
+		DR(gpio >> GPIO_PORT_SHIFT) &= ~(1 << (gpio & GPIO_PIN_MASK));
+}
+
+int imx_gpio_direction_input(unsigned gpio)
+{
+	imx_gpio_mode(gpio | GPIO_IN | GPIO_GIUS | GPIO_DR);
+	return 0;
+}
+
+
+int imx_gpio_direction_output(unsigned gpio, int value)
+{
+	imx_gpio_set_value(gpio, value);
+	imx_gpio_mode(gpio | GPIO_OUT | GPIO_GIUS | GPIO_DR);
+	return 0;
+}
+
+

include/asm-arm/arch-imx/imx27-regs.h
@@ -299,4 +299,6 @@
 #define IMX_CS4_BASE	0xD4000000
 #define IMX_CS5_BASE	0xD6000000
 
+#define GPIO_DR                      0x1C
+
 #endif /* _IMX27_REGS_H */

include/asm-arm/arch-imx/gpio.h
@@ -2,6 +2,9 @@
 #define __ASM_ARCH_GPIO_H
 
 void imx_gpio_mode(int gpio_mode);
+void imx_gpio_set_value(unsigned gpio, int value);
+int imx_gpio_direction_output(unsigned gpio, int value);
+int imx_gpio_direction_input(unsigned gpio);
 
 #endif /* __ASM_ARCH_GPIO_H */
 

include/asm-arm/arch-imx/imx-regs.h
@@ -118,4 +118,6 @@
 #define GPIO_BOUT_0    (2 << GPIO_BOUT_SHIFT)
 #define GPIO_BOUT_1    (3 << GPIO_BOUT_SHIFT)
 
+#define GPIO_GIUS      (1<<16)
+
 #endif				/* _IMX_REGS_H */

commit 50cbe3e0fc8b664b6257e4d13d156bb741b60b08

IMX regression: fix GPIO_DR usage
GPIO_DR ist defined already as GPIO_GPIO in imx-regs.h. There's no need to
define archdependent GPIO_DR. This one fixes a regression brought in by
d5ec92129e1c67ac86e92c22d54bb090fb00d616

Signed-off-by: Luotao Fu <l.fu@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/gpio.c
@@ -96,7 +96,7 @@ void imx_gpio_set_value(unsigned gpio, int value)
 
 int imx_gpio_direction_input(unsigned gpio)
 {
-	imx_gpio_mode(gpio | GPIO_IN | GPIO_GIUS | GPIO_DR);
+	imx_gpio_mode(gpio | GPIO_IN | GPIO_GIUS | GPIO_GPIO);
 	return 0;
 }
 
@@ -104,7 +104,7 @@ int imx_gpio_direction_input(unsigned gpio)
 int imx_gpio_direction_output(unsigned gpio, int value)
 {
 	imx_gpio_set_value(gpio, value);
-	imx_gpio_mode(gpio | GPIO_OUT | GPIO_GIUS | GPIO_DR);
+	imx_gpio_mode(gpio | GPIO_OUT | GPIO_GIUS | GPIO_GPIO);
 	return 0;
 }
 

include/asm-arm/arch-imx/imx27-regs.h
@@ -299,6 +299,4 @@
 #define IMX_CS4_BASE	0xD4000000
 #define IMX_CS5_BASE	0xD6000000
 
-#define GPIO_DR                      0x1C
-
 #endif /* _IMX27_REGS_H */
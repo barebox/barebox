270c2673a1caf00bcc47f2ce571fbbb3bade1bb1
Sascha Hauer <s.hauer@pengutronix.de>
2009-08-18 12:24:00 +0200

    arm: implement optimized string functions
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/asm-arm/string.h
+++ include/asm-arm/string.h
@@ -1,9 +1,13 @@
 #ifndef __ASM_ARM_STRING_H
 #define __ASM_ARM_STRING_H
 
-/*
- * We don't do inline string functions, since the
- * optimised inline asm versions are not small.
- */
+#ifdef CONFIG_ARM_OPTIMZED_STRING_FUNCTIONS
+
+#define __HAVE_ARCH_MEMCPY
+extern void *memcpy(void *, const void *, __kernel_size_t);
+#define __HAVE_ARCH_MEMSET
+extern void *memset(void *, int, __kernel_size_t);
+
+#endif
 
 #endif

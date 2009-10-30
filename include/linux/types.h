318e743323beb6c965e24ce5c176dd6aea0347c3
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2009-10-30 22:55:06 +0100

    add bool types support
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/types.h
+++ include/linux/types.h
@@ -16,6 +16,7 @@ typedef __kernel_pid_t		pid_t;
 typedef __kernel_daddr_t	daddr_t;
 typedef __kernel_key_t		key_t;
 typedef __kernel_suseconds_t	suseconds_t;
+typedef _Bool			bool;
 
 #ifdef __KERNEL__
 typedef __kernel_uid32_t	uid_t;

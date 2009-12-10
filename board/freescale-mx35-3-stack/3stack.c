695539fd762b4c258a243d0a64a9f536df5c0b26
Marc Kleine-Budde <mkl@pengutronix.de>
2009-12-10 12:35:26 +0100

    i.MX35 3stack: increase env partition to hold splash image
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- board/freescale-mx35-3-stack/3stack.c
+++ board/freescale-mx35-3-stack/3stack.c
@@ -171,13 +171,13 @@ static int f3s_devices_init(void)
 	case 0x01:		/* NAND is the source */
 		devfs_add_partition("nand0", 0x00000, 0x40000, PARTITION_FIXED, "self_raw");
 		dev_add_bb_dev("self_raw", "self0");
-		devfs_add_partition("nand0", 0x40000, 0x20000, PARTITION_FIXED, "env_raw");
+		devfs_add_partition("nand0", 0x40000, 0x80000, PARTITION_FIXED, "env_raw");
 		dev_add_bb_dev("env_raw", "env0");
 		break;
 
 	case 0x00:		/* NOR is the source */
 		devfs_add_partition("nor0", 0x00000, 0x40000, PARTITION_FIXED, "self0");
-		devfs_add_partition("nor0", 0x40000, 0x20000, PARTITION_FIXED, "env0");
+		devfs_add_partition("nor0", 0x40000, 0x80000, PARTITION_FIXED, "env0");
 		protect_file("/dev/env0", 1);
 		break;
 	}

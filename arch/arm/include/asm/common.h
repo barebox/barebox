6b8e614bac8ef7e3a69ec597894c7fc6c217aff4
Sascha Hauer <s.hauer@pengutronix.de>
2010-02-22 09:49:52 +0100

    add arm helper function to determine the program counter
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/common.h
+++ arch/arm/include/asm/common.h
@@ -1,1 +1,19 @@
+#ifndef __ASM_ARM_COMMON_H
+#define __ASM_ARM_COMMON_H
+
 #define ARCH_SHUTDOWN
+
+static inline unsigned long get_pc(void)
+{
+	unsigned long pc;
+
+	__asm__ __volatile__(
+                "mov    %0, pc\n"
+                : "=r" (pc)
+                :
+                : "memory");
+
+	return pc;
+}
+
+#endif /* __ASM_ARM_COMMON_H */

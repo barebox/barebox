362d6375aabbe23239a63a2db86d369590697218
Sascha Hauer <s.hauer@pengutronix.de>
2010-03-29 10:55:17 +0200

    add snprintf function
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/stdio.h
+++ include/stdio.h
@@ -31,6 +31,7 @@ static inline void putchar(char c) {
 int	printf(const char *fmt, ...);
 int	vprintf(const char *fmt, va_list args);
 int	sprintf(char * buf, const char *fmt, ...);
+int	snprintf(char *buf, size_t size, const char *fmt, ...);
 int	vsprintf(char *buf, const char *fmt, va_list args);
 char	*asprintf(const char *fmt, ...);
 char	*vasprintf(const char *fmt, va_list ap);
--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -547,6 +547,18 @@ int sprintf(char * buf, const char *fmt, ...)
 }
 EXPORT_SYMBOL(sprintf);
 
+int snprintf(char * buf, size_t size, const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i = vsnprintf(buf, size, fmt, args);
+	va_end(args);
+	return i;
+}
+EXPORT_SYMBOL(sprintf);
+
 /* Simplified asprintf. */
 char *vasprintf(const char *fmt, va_list ap)
 {

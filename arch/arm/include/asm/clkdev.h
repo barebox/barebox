6186a855158afcd8d97002da973d88adc8637cdd
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2010-08-31 01:36:52 +0800

    arm: move clkdev to drivers/clk
    as refer in this patch "arm & sh: factorised duplicated clkdev.c"
    
    factorise some generic infrastructure to assist looking up struct clks
    for the ARM & SH architecture.
    
    as the code is identical at 99% in linux
    
    move it also as preparing for the SH adding
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- arch/arm/include/asm/clkdev.h
+++ arch/arm/include/asm/clkdev.h
@@ -12,19 +12,6 @@
 #ifndef __ASM_CLKDEV_H
 #define __ASM_CLKDEV_H
 
-struct clk;
-
-struct clk_lookup {
-	struct list_head	node;
-	const char		*dev_id;
-	const char		*con_id;
-	struct clk		*clk;
-};
-
-struct clk_lookup *clkdev_alloc(struct clk *clk, const char *con_id,
-	const char *dev_fmt, ...);
-
-void clkdev_add(struct clk_lookup *cl);
-void clkdev_drop(struct clk_lookup *cl);
+#include <mach/clkdev.h>
 
 #endif

f0e62e69a58e249e11fc33f993e4cdb95eac6ea3
Eric Bénard <eric@eukrea.com>
2010-11-01 20:51:02 +0100

    cpuimx35: fix warning
    this patch fix the following warning :
    arch/arm/boards/eukrea_cpuimx35/flash_header.c:30: warning: "DEST_BASE" redefined
    arch/arm/mach-imx/include/mach/imx-flash-header.h:29: note: this is the location of the previous definition
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/eukrea_cpuimx35/flash_header.c
+++ arch/arm/boards/eukrea_cpuimx35/flash_header.c
@@ -27,15 +27,15 @@ struct imx_dcd_entry __dcd_entry_section dcd_entry[] = {
 	{ .ptr_type = 4, .addr = 0xb8001010, .val = 0x00000004, },
 };
 
-#define DEST_BASE 0x80000000
+#define CPUIMX35_DEST_BASE 0x80000000
 struct imx_flash_header __flash_header_section flash_header = {
-	.app_code_jump_vector	= DEST_BASE + 0x1000,
+	.app_code_jump_vector	= CPUIMX35_DEST_BASE + 0x1000,
 	.app_code_barker	= APP_CODE_BARKER,
 	.app_code_csf		= 0,
 	.dcd_ptr_ptr		= FLASH_HEADER_BASE + offsetof(struct imx_flash_header, dcd),
 	.super_root_key		= 0,
 	.dcd			= FLASH_HEADER_BASE + offsetof(struct imx_flash_header, dcd_barker),
-	.app_dest		= DEST_BASE,
+	.app_dest		= CPUIMX35_DEST_BASE,
 	.dcd_barker		= DCD_BARKER,
 	.dcd_block_len		= sizeof(dcd_entry),
 };

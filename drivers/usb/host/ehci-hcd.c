203a5fd190315e454d1e61de46ecf79c4e2751f6
Sascha Hauer <s.hauer@pengutronix.de>
2011-01-19 13:57:46 +0100

    USB ehci: Set to host mode on tt capable controllers
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -919,6 +919,13 @@ static int ehci_probe(struct device_d *dev)
 	host->submit_control_msg = submit_control_msg;
 	host->submit_bulk_msg = submit_bulk_msg;
 
+	if (ehci->flags & EHCI_HAS_TT) {
+		/* Set to host mode */
+		reg = ehci_readl(ehci->hcor + USBMODE);
+		reg |= USBMODE_CM_HC;
+		writel(reg, ehci->hcor + USBMODE);
+	}
+
 	usb_register_host(host);
 
 	reg = HC_VERSION(ehci_readl(&ehci->hccr->cr_capbase));

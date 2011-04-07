99b9ca240ddccf63ab0e3eefbcf5e466220bb7c0
Sascha Hauer <s.hauer@pengutronix.de>
2011-04-07 17:18:33 +0200

    ata: make write support optional
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/ata/disk_drive.c
+++ drivers/ata/disk_drive.c
@@ -137,6 +137,7 @@ static int atablk_read(struct block_device *blk, void *buf, int block,
 	return atablk->intf->read(atablk->dev, block, num_blocks, buf);
 }
 
+#ifdef CONFIG_ATA_WRITE
 static int atablk_write(struct block_device *blk, const void *buf, int block,
 		int num_blocks)
 {
@@ -144,10 +145,13 @@ static int atablk_write(struct block_device *blk, const void *buf, int block,
 
 	return atablk->intf->write(atablk->dev, block, num_blocks, buf);
 }
+#endif
 
 static struct block_device_ops ataops = {
 	.read = atablk_read,
+#ifdef CONFIG_ATA_WRITE
 	.write = atablk_write,
+#endif
 };
 
 /**

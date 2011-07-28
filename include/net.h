c46b04542f57ad4d0429ae72f47775d027b12ac6
Sascha Hauer <s.hauer@pengutronix.de>
2011-07-28 11:36:49 +0200

    net: Add a possibility for boards to give network devices a MAC address
    MAC addresses are sometimes stored at unusual places. This
    patch makes it possible to give a MAC address to a ethernet
    device id. This is independent of the device actually being
    present.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/net.h
+++ include/net.h
@@ -55,6 +55,12 @@ int eth_send(void *packet, int length);	   /* Send a packet		*/
 int eth_rx(void);			/* Check for received packets	*/
 void eth_halt(void);			/* stop SCC			*/
 
+/* associate a MAC address to a ethernet device. Should be called by
+ * board code for boards which store their MAC address at some unusual
+ * place.
+ */
+void eth_register_ethaddr(int ethid, const char *ethaddr);
+
 /*
  *	Ethernet header
  */

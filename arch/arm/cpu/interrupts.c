20fbf2a0e2c98108b5830558c70382e8d3c65303
Sascha Hauer <s.hauer@pengutronix.de>
2011-08-07 19:28:02 +0200

    ARM: remove unused irq enable/disable functions
    We do not have irq support in barebox, so remove the unused interrupt
    functions.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/cpu/interrupts.c
+++ arch/arm/cpu/interrupts.c
@@ -29,37 +29,6 @@
 #include <asm/ptrace.h>
 #include <asm/unwind.h>
 
-#ifdef CONFIG_USE_IRQ
-/* enable IRQ interrupts */
-void enable_interrupts (void)
-{
-	unsigned long temp;
-	__asm__ __volatile__("mrs %0, cpsr\n"
-			     "bic %0, %0, #0x80\n"
-			     "msr cpsr_c, %0"
-			     : "=r" (temp)
-			     :
-			     : "memory");
-}
-
-
-/*
- * disable IRQ/FIQ interrupts
- * returns true if interrupts had been enabled before we disabled them
- */
-int disable_interrupts (void)
-{
-	unsigned long old,temp;
-	__asm__ __volatile__("mrs %0, cpsr\n"
-			     "orr %1, %0, #0xc0\n"
-			     "msr cpsr_c, %1"
-			     : "=r" (old), "=r" (temp)
-			     :
-			     : "memory");
-	return(old & 0x80) == 0;
-}
-#endif
-
 /**
  * FIXME
  */

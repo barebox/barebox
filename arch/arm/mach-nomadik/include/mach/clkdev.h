e18df20c902b83f7526fd62a27c0f6a2d67df3cb
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-08-08 23:17:15 +0200

    nomadik: switch to static allocation clkdev
    as we need to register it before the device
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-nomadik/include/mach/clkdev.h
+++ arch/arm/mach-nomadik/include/mach/clkdev.h
@@ -4,4 +4,10 @@
 #define __clk_get(clk) ({ 1; })
 #define __clk_put(clk) do { } while (0)
 
+#define CLKDEV_CON_ID(_id, _clk)			\
+	{						\
+		.con_id = _id,				\
+		.clk = _clk,				\
+	}
+
 #endif

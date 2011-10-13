30a124d0461e358a4e71d08f614b164eaa1822b4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-10-13 11:51:52 +0200

    arm: at91: fix environment location in flash for multiple boards
    The environment partition was overlapping the Barebox partition in
    those three Atmel boards. Saving the environment resulted in the
    Barebox being overwritten.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/at91sam9m10g45ek/init.c
+++ arch/arm/boards/at91sam9m10g45ek/init.c
@@ -142,7 +142,7 @@ static int at91sam9m10g45ek_devices_init(void)
 
 	devfs_add_partition("nand0", 0x00000, 0x80000, PARTITION_FIXED, "self_raw");
 	dev_add_bb_dev("self_raw", "self0");
-	devfs_add_partition("nand0", 0x40000, 0x40000, PARTITION_FIXED, "env_raw");
+	devfs_add_partition("nand0", 0x80000, 0x20000, PARTITION_FIXED, "env_raw");
 	dev_add_bb_dev("env_raw", "env0");
 
 	armlinux_set_bootparams((void *)(AT91_CHIPSELECT_6 + 0x100));

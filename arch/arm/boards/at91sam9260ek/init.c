e305a3dbe4a80adac8b174e04fa5ee39a39921f8
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-12-14 16:46:13 +0100

    at91sam9260/9g20-ek: update nand support
    by default boot from nand with the bootstrap as first stage
    update the smc config for 9g20-ek
    
    switch to defaultenv
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/at91sam9260ek/init.c
+++ arch/arm/boards/at91sam9260ek/init.c
@@ -31,6 +31,7 @@
 #include <io.h>
 #include <asm/hardware.h>
 #include <nand.h>
+#include <sizes.h>
 #include <linux/mtd/nand.h>
 #include <mach/board.h>
 #include <mach/at91sam9_smc.h>
@@ -72,7 +73,7 @@ static struct atmel_nand_data nand_pdata = {
 #endif
 };
 
-static struct sam9_smc_config ek_nand_smc_config = {
+static struct sam9_smc_config ek_9260_nand_smc_config = {
 	.ncs_read_setup		= 0,
 	.nrd_setup		= 1,
 	.ncs_write_setup	= 0,
@@ -90,16 +91,41 @@ static struct sam9_smc_config ek_nand_smc_config = {
 	.tdf_cycles		= 2,
 };
 
+static struct sam9_smc_config ek_9g20_nand_smc_config = {
+	.ncs_read_setup		= 0,
+	.nrd_setup		= 2,
+	.ncs_write_setup	= 0,
+	.nwe_setup		= 2,
+
+	.ncs_read_pulse		= 4,
+	.nrd_pulse		= 4,
+	.ncs_write_pulse	= 4,
+	.nwe_pulse		= 4,
+
+	.read_cycle		= 7,
+	.write_cycle		= 7,
+
+	.mode			= AT91_SMC_READMODE | AT91_SMC_WRITEMODE | AT91_SMC_EXNWMODE_DISABLE,
+	.tdf_cycles		= 3,
+};
+
 static void ek_add_device_nand(void)
 {
+	struct sam9_smc_config *smc;
+
+	if (machine_is_at91sam9g20ek())
+		smc = &ek_9g20_nand_smc_config;
+	else
+		smc = &ek_9260_nand_smc_config;
+
 	/* setup bus-width (8 or 16) */
 	if (nand_pdata.bus_width_16)
-		ek_nand_smc_config.mode |= AT91_SMC_DBW_16;
+		smc->mode |= AT91_SMC_DBW_16;
 	else
-		ek_nand_smc_config.mode |= AT91_SMC_DBW_8;
+		smc->mode |= AT91_SMC_DBW_8;
 
 	/* configure chip-select 3 (NAND) */
-	sam9_smc_configure(3, &ek_nand_smc_config);
+	sam9_smc_configure(3, smc);
 
 	at91_add_device_nand(&nand_pdata);
 }
@@ -156,10 +182,14 @@ static int at91sam9260ek_devices_init(void)
 	armlinux_set_bootparams((void *)(AT91_CHIPSELECT_1 + 0x100));
 	ek_set_board_type();
 
-	devfs_add_partition("nand0", 0x00000, 0x80000, PARTITION_FIXED, "self_raw");
+	devfs_add_partition("nand0", 0x00000, SZ_128K, PARTITION_FIXED, "at91bootstrap_raw");
+	dev_add_bb_dev("at91bootstrap_raw", "at91bootstrap");
+	devfs_add_partition("nand0", SZ_128K, SZ_256K, PARTITION_FIXED, "self_raw");
 	dev_add_bb_dev("self_raw", "self0");
-	devfs_add_partition("nand0", 0x80000, 0x20000, PARTITION_FIXED, "env_raw");
+	devfs_add_partition("nand0", SZ_256K + SZ_128K, SZ_128K, PARTITION_FIXED, "env_raw");
 	dev_add_bb_dev("env_raw", "env0");
+	devfs_add_partition("nand0", SZ_512K, SZ_128K, PARTITION_FIXED, "env_raw1");
+	dev_add_bb_dev("env_raw1", "env1");
 
 	return 0;
 }

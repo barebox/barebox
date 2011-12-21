8007b25bdfa353cbce71c849a41f474604648ebf
Alexander Aring <a.aring@phytec.de>
2011-12-21 08:50:27 +0100

    i2c-omap: add i2c omap4 support
    Added i2c support for omap4. Tested on pcm049.
    
    Driver based on linux kernel implementation.
    Also added a shift to access 16-bit registers
    to make support for OMAP730/850 possible.
    
    If accessing a non existing slave the bus will go into arbitration mode.
    It's unable to recover from it.
    
    Signed-off-by: Alexander Aring <a.aring@phytec.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-omap/include/mach/generic.h
+++ arch/arm/mach-omap/include/mach/generic.h
@@ -5,6 +5,7 @@
 /* I2C controller revisions present on specific hardware */
 #define OMAP_I2C_REV_ON_2430		0x36
 #define OMAP_I2C_REV_ON_3430		0x3C
+#define OMAP_I2C_REV_ON_4430		0x40
 
 #ifdef CONFIG_ARCH_OMAP
 #define cpu_is_omap2430()	(1)
@@ -16,4 +17,10 @@
 #define cpu_is_omap34xx()	(1)
 #else
 #define cpu_is_omap34xx()	(0)
+#endif
+
+#ifdef CONFIG_ARCH_OMAP4
+#define cpu_is_omap4xxx()	(1)
+#else
+#define cpu_is_omap4xxx()	(0)
 #endif

de8c4de5ef7d8962b36c396a75e17a655fc247b2
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-12-31 16:21:28 +0100

    ARM: import memcpy_from/toio and memset_io from linux
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/io.h
+++ arch/arm/include/asm/io.h
@@ -3,4 +3,11 @@
 
 #include <asm-generic/io.h>
 
+/*
+ * String version of IO memory access ops:
+ */
+extern void memcpy_fromio(void *, const volatile void __iomem *, size_t);
+extern void memcpy_toio(volatile void __iomem *, const void *, size_t);
+extern void memset_io(volatile void __iomem *, int, size_t);
+
 #endif	/* __ASM_ARM_IO_H */

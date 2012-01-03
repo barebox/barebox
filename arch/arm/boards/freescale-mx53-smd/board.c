72a538f10ac8a5ce8593095410d535543fc31e97
Sascha Hauer <s.hauer@pengutronix.de>
2012-01-03 11:12:46 +0100

    ARM i.MX5 loco/smd: fix compilation
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/freescale-mx53-smd/board.c
+++ arch/arm/boards/freescale-mx53-smd/board.c
@@ -160,7 +160,7 @@ static int smd_console_init(void)
 {
 	mxc_iomux_v3_setup_multiple_pads(smd_pads, ARRAY_SIZE(smd_pads));
 
-	mx53_init_lowlevel();
+	imx53_init_lowlevel();
 
 	imx53_add_uart0();
 	imx53_add_uart1();

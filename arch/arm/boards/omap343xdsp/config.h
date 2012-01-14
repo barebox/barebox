be07ecb5c50e007d71f75145938c66bd04960b43
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-01-14 15:51:08 +0100

    omap343xdsp: move to arch/arm/boards/omap343xdsp
    this will allow to create it's own env/config
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/omap343xdsp/config.h
+++ arch/arm/boards/omap343xdsp/config.h
@@ -1,17 +1,4 @@
 /**
- * @file
- * @brief provide a wrapper for standard malloc and stack size defines
- *
- * FileName: arch/arm/boards/omap/config.h
- *
- * Standard defines should be configurable for us to move Stack and malloc
- * areas around this defines some basics for that
- */
-/*
- * (C) Copyright 2006-2008
- * Texas Instruments, <www.ti.com>
- * Nishanth Menon <x0nishan@ti.com>
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -19,7 +6,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -27,7 +14,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
-#ifndef __MACH_OMAP_CONFIG_H
-#define __MACH_OMAP_CONFIG_H
 
-#endif /* __MACH_OMAP_CONFIG_H */
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#endif	/* __CONFIG_H */

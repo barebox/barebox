deadc3dd7e18a4d177aabcad6df9045f69e53f2b
Sascha Hauer <s.hauer@pengutronix.de>
2012-04-29 20:30:32 +0200

    bootm: fix initrd handling
    bootm_initrd tests for data->initrd, but this never becomes true because
    the setting of data->initrd is inside a if(bootm_initrd()). Fix this. Also,
    do not support the initrd options when initrd is disabled.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- commands/bootm.c
+++ commands/bootm.c
@@ -50,18 +50,6 @@
 
 static LIST_HEAD(handler_list);
 
-#ifdef CONFIG_CMD_BOOTM_INITRD
-static inline int bootm_initrd(struct image_data *data)
-{
-	return data->initrd ? 1 : 0;
-}
-#else
-static inline int bootm_initrd(struct image_data *data)
-{
-	return 0;
-}
-#endif
-
 int register_image_handler(struct image_handler *handler)
 {
 	list_add_tail(&handler->list, &handler_list);
@@ -252,6 +240,14 @@ static void bootm_image_name_and_no(char *name, int *no)
 	*no = simple_strtoul(at, NULL, 10);
 }
 
+#define BOOTM_OPTS_COMMON "ca:e:vo:f"
+
+#ifdef CONFIG_CMD_BOOTM_INITRD
+#define BOOTM_OPTS BOOTM_OPTS_COMMON "L:r:"
+#else
+#define BOOTM_OPTS BOOTM_OPTS_COMMON
+#endif
+
 static int do_bootm(int argc, char *argv[])
 {
 	int opt;
@@ -269,7 +265,7 @@ static int do_bootm(int argc, char *argv[])
 	data.verify = 0;
 	data.verbose = 0;
 
-	while ((opt = getopt(argc, argv, "cL:r:a:e:vo:f")) > 0) {
+	while ((opt = getopt(argc, argv, BOOTM_OPTS)) > 0) {
 		switch(opt) {
 		case 'c':
 			data.verify = 1;
@@ -330,7 +326,7 @@ static int do_bootm(int argc, char *argv[])
 		}
 	}
 
-	if (bootm_initrd(&data)) {
+	if (IS_ENABLED(CONFIG_CMD_BOOTM_INITRD) && data.initrd_file) {
 		bootm_image_name_and_no(data.initrd_file, &data.initrd_num);
 
 		initrd_type = file_name_detect_type(data.initrd_file);

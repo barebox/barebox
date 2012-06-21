af76fae0fe6e70eddb8cc6fb848eea79421a2934
Juergen Beisert <jbe@pengutronix.de>
2012-06-21 11:12:21 +0200

    ARM/MXS: add new way to reset the whole SoC
    Currently the watchdog is occupied for system reset. This usage collides with
    the dedicated usage of a watchdog. This patch change the behaviour of at least
    i.MX23/i.MX28 where the chipset supports a simple and powerful alternative
    to reset the whole SoC (including the PMIC).
    
    Signed-off-by: Juergen Beisert <jbe@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/mach-mxs/soc-imx23.c
@@ -0,0 +1,37 @@
+/*
+ * (c) 2012 Juergen Beisert <kernel@pengutronix.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Collection of some SoC specific functions
+ */
+
+#include <common.h>
+#include <init.h>
+#include <mach/imx-regs.h>
+#include <io.h>
+
+#define HW_CLKCTRL_RESET 0x120
+# define HW_CLKCTRL_RESET_CHIP (1 << 1)
+
+/* Reset the full i.MX23 SoC via a chipset feature */
+void __noreturn reset_cpu(unsigned long addr)
+{
+	u32 reg;
+
+	reg = readl(IMX_CCM_BASE + HW_CLKCTRL_RESET);
+	writel(reg | HW_CLKCTRL_RESET_CHIP, IMX_CCM_BASE + HW_CLKCTRL_RESET);
+
+	while (1)
+		;
+	/*NOTREACHED*/
+}
+EXPORT_SYMBOL(reset_cpu);

39e450ca1c1865f95d7a62fe858a4af3312a1dd0
Sascha Hauer <s.hauer@pengutronix.de>
2012-06-21 11:56:53 +0200

    blackfin: Fix build due to missing __raw_* io accessors
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/blackfin/include/asm/io.h
+++ arch/blackfin/include/asm/io.h
@@ -51,6 +51,13 @@ extern void cf_outb(unsigned char val, volatile unsigned char* addr);
 #define writew(b,addr)		{((*(volatile unsigned short *) (addr)) = (b)); asm("ssync;");}
 #define writel(b,addr)		{((*(volatile unsigned int *) (addr)) = (b)); asm("ssync;");}
 
+#define __raw_readb readb
+#define __raw_readw readw
+#define __raw_readl readl
+#define __raw_writeb writeb
+#define __raw_writew writew
+#define __raw_writel writel
+
 #define memset_io(a,b,c)	memset((void *)(a),(b),(c))
 #define memcpy_fromio(a,b,c)	memcpy((a),(void *)(b),(c))
 #define memcpy_toio(a,b,c)	memcpy((void *)(a),(b),(c))

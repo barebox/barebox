abbbe22b4878a31da7af590440c7d0e8c195e6bb
Sascha Hauer <s.hauer@pengutronix.de>
2012-06-25 11:28:06 +0200

    nios2: Add missing ffs and fls include
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/nios2/include/asm/bitops.h
+++ arch/nios2/include/asm/bitops.h
@@ -1,4 +1,9 @@
 #ifndef _ASM_BITOPS_H
 #define _ASM_BITOPS_H
 
+#include <asm-generic/bitops/__ffs.h>
+#include <asm-generic/bitops/__fls.h>
+#include <asm-generic/bitops/ffs.h>
+#include <asm-generic/bitops/fls.h>
+
 #endif /* _ASM_BITOPS_H */

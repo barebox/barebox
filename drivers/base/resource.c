1c136767a2ab140a302acea13f155702b96238b5
Sascha Hauer <s.hauer@pengutronix.de>
2012-07-02 10:10:26 +0200

    ehci: Fix resource size
    We now have a resource size for the ehci hccr register space. This
    was assumed to be 0x40 in size. On OMAP though it is only 0x10 and
    then the hccr resource conflicts with the hcor resource which results
    in a non working ehci port on beagle and panda boards. This patch
    adds a nonintrusive workaround, it limits the hccr resource to 0x10,
    which then also works on OMAP.
    
    Later we should drop the multiple resources for the ehci port and
    make the resource as specified in the datasheets.
    
    This is broken since:
    
    commit 08845e41fba2b16dd3050b6b910c615e1bb8e57c
    Author: Sascha Hauer <s.hauer@pengutronix.de>
    Date:   Wed May 23 12:54:24 2012 +0200
    
        usb ehci: Add resource sizes
    
        add_usb_ehci_device registers resources with size 0. Fix this.
    
        Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/base/resource.c
+++ drivers/base/resource.c
@@ -113,7 +113,7 @@ struct device_d *add_usb_ehci_device(int id, resource_size_t hccr,
 
 	res = xzalloc(sizeof(struct resource) * 2);
 	res[0].start = hccr;
-	res[0].size = 0x40;
+	res[0].size = 0x10;
 	res[0].flags = IORESOURCE_MEM;
 	res[1].start = hcor;
 	res[1].size = 0xc0;

65a338855a53030f20ab7eb34e86f2565dc855da
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2012-07-04 14:14:53 +0200

    ARM: tqma53: change uart to second uart
    The second UART is used on the mba53 baseboard. The first UART is only
    used on custom hardware. This patch changes the UART to the one used
    on the freely available baseboard.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- arch/arm/boards/tqma53/board.c
+++ arch/arm/boards/tqma53/board.c
@@ -254,7 +254,7 @@ static int tqma53_console_init(void)
 {
 	mxc_iomux_v3_setup_multiple_pads(tqma53_pads, ARRAY_SIZE(tqma53_pads));
 	imx53_init_lowlevel(800);
-	imx53_add_uart0();
+	imx53_add_uart1();
 
 	return 0;
 }

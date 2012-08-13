73918efe1a720f8837760fd99b6402e39bb8daf9
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-08-13 14:30:40 +0800

    bootm: do not load the initrd in the common code
    This need to be handled by the arch bootm as the address can be invalid and
    provided at arch level.
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- commands/bootm.c
+++ commands/bootm.c
@@ -126,14 +126,6 @@ static int bootm_open_initrd_uimage(struct image_data *data)
 	if (data->initrd_address == UIMAGE_SOME_ADDRESS)
 		data->initrd_address = data->initrd->header.ih_load;
 
-	if (data->initrd_address != UIMAGE_INVALID_ADDRESS) {
-		data->initrd_res = uimage_load_to_sdram(data->initrd,
-				data->initrd_num,
-				data->initrd_address);
-		if (!data->initrd_res)
-			return -ENOMEM;
-	}
-
 	return 0;
 }
 

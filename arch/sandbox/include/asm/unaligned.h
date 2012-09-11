af516a9a35f4863f0405d22348802fd29b839975
Alexander Aring <alex.aring@googlemail.com>
2012-09-11 07:30:43 +0200

    sandbox-unaligned: better usement of ifdef
    GCC versions below 4.6 don't set __BYTE_ORDER__
    with __ORDER_LITTLE_ENDIAN__. So it's better to use
    __BYTE_ORDER and __LITTLE_ENDIAN instead.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/sandbox/include/asm/unaligned.h
+++ arch/sandbox/include/asm/unaligned.h
@@ -8,7 +8,7 @@
 #include <linux/unaligned/access_ok.h>
 #include <linux/unaligned/generic.h>
 
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#if __BYTE_ORDER == __LITTLE_ENDIAN
 #define get_unaligned __get_unaligned_le
 #define put_unaligned __put_unaligned_le
 #else

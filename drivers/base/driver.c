b2a13798a6c68c03ef87fbfdf66f2a942edebabd
Sascha Hauer <s.hauer@pengutronix.de>
2012-09-12 08:34:19 +0200

    driver: add dev_get_drvdata function
    Drivers need to get their driver data either from devicetree or the platform
    information. It should matter for the driver where this data comes from, so
    introduce a common function for it.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/base/driver.c
+++ drivers/base/driver.c
@@ -376,6 +376,16 @@ static int do_devinfo_subtree(struct device_d *dev, int depth)
 	return 0;
 }
 
+int dev_get_drvdata(struct device_d *dev, unsigned long *data)
+{
+	if (dev->id_entry) {
+		*data = dev->id_entry->driver_data;
+		return 0;
+	}
+
+	return -ENODEV;
+}
+
 static int do_devinfo(int argc, char *argv[])
 {
 	struct device_d *dev;

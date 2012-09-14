dc159b414f24305032c32ca42cdb3929e25d5ac5
Jan Weitzel <j.weitzel@phytec.de>
2012-09-14 07:49:56 +0200

    pcm049 phycard l + xl2: update partion sizes
    Use 512k NAND Partion for barebox in enviroment and boards code
    pcm049: use 4MB for kernel
    
    Signed-off-by: Jan Weitzel <j.weitzel@phytec.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/pcm049/board.c
+++ arch/arm/boards/pcm049/board.c
@@ -118,9 +118,9 @@ static int pcm049_devices_init(void)
 #ifdef CONFIG_PARTITION
 	devfs_add_partition("nand0", 0x00000, SZ_128K, DEVFS_PARTITION_FIXED, "xload_raw");
 	dev_add_bb_dev("xload_raw", "xload");
-	devfs_add_partition("nand0", SZ_128K, SZ_256K, DEVFS_PARTITION_FIXED, "self_raw");
+	devfs_add_partition("nand0", SZ_128K, SZ_512K, DEVFS_PARTITION_FIXED, "self_raw");
 	dev_add_bb_dev("self_raw", "self0");
-	devfs_add_partition("nand0", SZ_128K + SZ_256K, SZ_128K, DEVFS_PARTITION_FIXED, "env_raw");
+	devfs_add_partition("nand0", SZ_128K + SZ_512K, SZ_128K, DEVFS_PARTITION_FIXED, "env_raw");
 	dev_add_bb_dev("env_raw", "env0");
 #endif
 

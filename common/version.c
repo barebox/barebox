386681e2948248083e73c70612e709db292b1419
Sascha Hauer <s.hauer@pengutronix.de>
2012-10-09 20:50:11 +0200

    of: Print model name in banner
    If we know the model name from the devicetree print this
    in the banner instead of the hardcoded board name.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/version.c
+++ common/version.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include <generated/compile.h>
 #include <generated/utsrelease.h>
+#include <of.h>
 
 const char version_string[] =
 	"barebox " UTS_RELEASE " " UTS_VERSION "\n";
@@ -8,7 +9,13 @@ EXPORT_SYMBOL(version_string);
 
 void barebox_banner (void)
 {
-	printf("\n\n%s\n\n", version_string);
-	printf("Board: " CONFIG_BOARDINFO "\n");
-}
+	const char *board;
 
+	board = of_get_model();
+
+	if (!board)
+		board = CONFIG_BOARDINFO;
+
+	printf("\n\n%s\n\n", version_string);
+	printf("Board: %s\n", board);
+}

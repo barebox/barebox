bedf7dfce5bcf728937b9569ac0d038267f588f3
Alexander Shiyan <shc_work@mail.ru>
2012-11-02 13:17:15 +0400

    ARM: clps711x: Add serial driver
    This patch adds a simple serial driver for CLPS711X architecture.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-clps711x/devices.c
+++ arch/arm/mach-clps711x/devices.c
@@ -35,3 +35,63 @@ void clps711x_setup_memcfg(int bank, u32 val)
 		break;
 	}
 }
+
+static struct resource uart0_resources[] = {
+	{
+		.start	= UBRLCR1,
+		.end	= UBRLCR1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= SYSCON1,
+		.end	= SYSCON1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= SYSFLG1,
+		.end	= SYSFLG1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= UARTDR1,
+		.end	= UARTDR1,
+		.flags	= IORESOURCE_MEM,
+	},
+};
+
+static struct resource uart1_resources[] = {
+	{
+		.start	= UBRLCR2,
+		.end	= UBRLCR2,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= SYSCON2,
+		.end	= SYSCON2,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= SYSFLG2,
+		.end	= SYSFLG2,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= UARTDR2,
+		.end	= UARTDR2,
+		.flags	= IORESOURCE_MEM,
+	},
+};
+
+void clps711x_add_uart(unsigned int id)
+{
+	switch (id) {
+	case 0:
+		add_generic_device_res("clps711x_serial", 0, uart0_resources,
+				       ARRAY_SIZE(uart0_resources), NULL);
+		break;
+	case 1:
+		add_generic_device_res("clps711x_serial", 1, uart1_resources,
+				       ARRAY_SIZE(uart1_resources), NULL);
+		break;
+	}
+}
--- arch/arm/mach-clps711x/include/mach/devices.h
+++ arch/arm/mach-clps711x/include/mach/devices.h
@@ -2,5 +2,6 @@
 #define __MACH_DEVICES_H
 
 void clps711x_setup_memcfg(int bank, u32 val);
+void clps711x_add_uart(unsigned int id);
 
 #endif

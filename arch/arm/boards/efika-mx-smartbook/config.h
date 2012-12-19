63aa5bd1646306654a6138e44b9339a358e0eb40
Sascha Hauer <s.hauer@pengutronix.de>
2012-12-19 21:24:05 +0100

    Arm Efika MX Smartbook: coding style fixes
    Some Coding style fixes as suggested by Peter Korsgaard. No
    functional change.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/efika-mx-smartbook/config.h
+++ arch/arm/boards/efika-mx-smartbook/config.h
@@ -1,7 +1,4 @@
 /**
- * @file
- * @brief Global defintions for the ARM i.MX51 based babbage board
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -12,10 +9,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
  */
 
 #ifndef __CONFIG_H

460730195125a31a9e97236a4ffeb0bd0d543781
Alexander Aring <alex.aring@gmail.com>
2013-01-13 18:42:17 +0100

    meminfo: fix display of allocated addresses
    Fix display to stdout of allocated addresses.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/meminfo.c
+++ common/meminfo.c
@@ -10,13 +10,13 @@ static int display_meminfo(void)
 	ulong mend   = mem_malloc_end();
 	ulong msize  = mend - mstart + 1;
 
-	debug("barebox code: 0x%p -> 0x%p\n", _stext, _etext);
-	debug("bss segment:  0x%p -> 0x%p\n", __bss_start, __bss_stop);
+	debug("barebox code: 0x%p -> 0x%p\n", _stext, _etext - 1);
+	debug("bss segment:  0x%p -> 0x%p\n", __bss_start, __bss_stop - 1);
 	printf("malloc space: 0x%08lx -> 0x%08lx (size %s)\n",
 		mstart, mend, size_human_readable(msize));
 #ifdef CONFIG_ARM
 	printf("stack space:  0x%08x -> 0x%08x (size %s)\n",
-		STACK_BASE, STACK_BASE + STACK_SIZE,
+		STACK_BASE, STACK_BASE + STACK_SIZE - 1,
 		size_human_readable(STACK_SIZE));
 #endif
 	return 0;

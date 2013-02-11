b0917935ddbf06a0b1e1a404fb394e6ab5e460ca
Hubert Feurstein <h.feurstein@gmail.com>
2013-02-11 09:55:05 +0100

    ARM i.MX6: Add support for SabreSD board
    Cc: Sascha Hauer <s.hauer@pengutronix.de>
    Cc: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Signed-off-by: Hubert Feurstein <h.feurstein@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/freescale-mx6-sabresd/lowlevel.c
@@ -0,0 +1,10 @@
+#include <common.h>
+#include <sizes.h>
+#include <asm/barebox-arm-head.h>
+#include <asm/barebox-arm.h>
+
+void __naked barebox_arm_reset_vector(void)
+{
+	arm_cpu_lowlevel_init();
+	barebox_arm_entry(0x10000000, SZ_1G, 0);
+}

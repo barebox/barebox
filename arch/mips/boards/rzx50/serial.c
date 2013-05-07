8b35646356f778287c11971cc8d464aa579f0421
Antony Pavlov <antonynpavlov@gmail.com>
2013-05-07 12:51:26 +0400

    MIPS: rzx50: switch to common mach-xburst serial code
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/boards/rzx50/serial.c
+++ arch/mips/boards/rzx50/serial.c
@@ -16,47 +16,14 @@
  */
 
 #include <common.h>
-#include <types.h>
-#include <driver.h>
 #include <init.h>
-#include <ns16550.h>
+#include <mach/devices.h>
 #include <mach/jz4750d_regs.h>
-#include <io.h>
-#include <asm/common.h>
-
-#define JZ4750D_UART_SHIFT	2
-
-#define ier		(1 << JZ4750D_UART_SHIFT)
-#define fcr		(2 << JZ4750D_UART_SHIFT)
-
-static void jz4750d_serial_reg_write(unsigned int val, unsigned long base,
-	unsigned char reg_offset)
-{
-	switch (reg_offset) {
-	case fcr:
-		val |= 0x10; /* Enable uart module */
-		break;
-	case ier:
-		val |= (val & 0x4) << 2;
-		break;
-	default:
-		break;
-	}
-
-	writeb(val & 0xff, (void *)(base + reg_offset));
-}
-
-static struct NS16550_plat serial_plat = {
-	.clock = 12000000,
-	.shift = JZ4750D_UART_SHIFT,
-	.reg_write = &jz4750d_serial_reg_write,
-};
 
 static int rzx50_console_init(void)
 {
 	/* Register the serial port */
-	add_ns16550_device(DEVICE_ID_DYNAMIC, UART1_BASE, 8 << JZ4750D_UART_SHIFT,
-			IORESOURCE_MEM_8BIT, &serial_plat);
+	jz_add_uart(DEVICE_ID_DYNAMIC, UART1_BASE, 12000000);
 
 	return 0;
 }

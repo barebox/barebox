bee7cd3da2cff9c66739bef0d5af56bef8f94c8b
Masaki Muranaka <monaka@monami-ya.com>
2013-06-01 23:05:22 +0900

    Avoid build error on "config SPI" is y
    In case he configures with make ARCH=x85 menuconfig,
    config SPI will be set to "y". And he will got an error on spi.h.
    
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/x86/include/asm/types.h
+++ arch/x86/include/asm/types.h
@@ -29,12 +29,16 @@ typedef unsigned int __u32;
 typedef __signed__ long long __s64;
 typedef unsigned long long __u64;
 
+typedef signed char s8;
 typedef unsigned char u8;
 
+typedef signed short s16;
 typedef unsigned short u16;
 
+typedef signed int s32;
 typedef unsigned int u32;
 
+typedef signed long long s64;
 typedef unsigned long long u64;
 
 #endif /* __ASSEMBLY__ */

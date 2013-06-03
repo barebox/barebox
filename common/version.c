0686fe0edf2ec37b109c9b787985858b4b6a6bb7
Sascha Hauer <s.hauer@pengutronix.de>
2013-06-03 15:04:21 +0200

    add function to get boardinfo string
    When using devicetrees the boardinfo (or model) can be obtained
    from the devicetree. Add a function to get the boardinfo so that
    we have a chance to add information from the devicetree.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/version.c
+++ common/version.c
@@ -1,7 +1,6 @@
 #include <common.h>
 #include <generated/compile.h>
 #include <generated/utsrelease.h>
-#include <of.h>
 
 const char version_string[] =
 	"barebox " UTS_RELEASE " " UTS_VERSION "\n";
@@ -9,13 +8,6 @@ EXPORT_SYMBOL(version_string);
 
 void barebox_banner (void)
 {
-	const char *board;
-
-	board = of_get_model();
-
-	if (!board)
-		board = CONFIG_BOARDINFO;
-
 	pr_info("\n\n%s\n\n", version_string);
-	pr_info("Board: %s\n", board);
+	pr_info("Board: %s\n", barebox_boardinfo());
 }

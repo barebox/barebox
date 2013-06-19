55e5cf4a02804973f2b8d04bd5fdb37d597a2b30
Teresa Gámez <t.gamez@phytec.de>
2013-06-19 09:09:45 +0200

    ARM: OMAP: pcm051: Add spi flash support
    Add support for w25q64 SPI NOR flash.
    
    Signed-off-by: Teresa Gámez <t.gamez@phytec.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/pcm051/board.c
+++ arch/arm/boards/pcm051/board.c
@@ -26,6 +26,8 @@
 #include <mach/am33xx-devices.h>
 #include <mach/am33xx-mux.h>
 #include <mach/am33xx-silicon.h>
+#include <spi/spi.h>
+#include <spi/flash.h>
 
 #include "mux.h"
 
@@ -52,11 +54,50 @@ static int pcm051_mem_init(void)
 }
 mem_initcall(pcm051_mem_init);
 
+static struct flash_platform_data pcm051_spi_flash = {
+	.name	= "nor",
+	.type	= "w25q64",
+};
+
+/*
+* SPI Flash works at 80Mhz however the SPI controller runs with 48MHz.
+* So setup Max speed to be less than the controller speed.
+*/
+static struct spi_board_info pcm051_spi_board_info[] = {
+	{
+		.name		= "m25p80",
+		.platform_data	= &pcm051_spi_flash,
+		.max_speed_hz	= 24000000,
+		.bus_num	= 0,
+		.chip_select	= 0,
+	},
+};
+
+static void pcm051_spi_init(void)
+{
+	int ret;
+
+	am33xx_enable_spi0_pin_mux();
+
+	ret = spi_register_board_info(pcm051_spi_board_info,
+					ARRAY_SIZE(pcm051_spi_board_info));
+	am33xx_add_spi0();
+}
+
 static int pcm051_devices_init(void)
 {
 	pcm051_enable_mmc0_pin_mux();
 
 	am33xx_add_mmc0(NULL);
+
+	pcm051_spi_init();
+
+	devfs_add_partition("nor0", 0x00000, SZ_128K,
+					DEVFS_PARTITION_FIXED, "xload");
+	devfs_add_partition("nor0", SZ_128K, SZ_512K,
+					DEVFS_PARTITION_FIXED, "self0");
+	devfs_add_partition("nor0", SZ_128K + SZ_512K, SZ_128K,
+					DEVFS_PARTITION_FIXED, "env0");
 
 	armlinux_set_bootparams((void *)(AM33XX_DRAM_ADDR_SPACE_START + 0x100));
 	armlinux_set_architecture(MACH_TYPE_PCM051);

fd2e121b5b910cc5197464121146e52ddcd0b1cb
Sascha Hauer <s.hauer@pengutronix.de>
2013-06-23 13:49:48 +0200

    ARM: i.MX51 babbage: switch to multi image support
    The image will be named:
    
    barebox-freescale-imx51-babbage.img
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/freescale-mx51-pdk/lowlevel.c
+++ arch/arm/boards/freescale-mx51-pdk/lowlevel.c
@@ -1,9 +1,19 @@
 #include <common.h>
 #include <mach/esdctl.h>
 #include <asm/barebox-arm-head.h>
+#include <asm/barebox-arm.h>
 
-void __naked barebox_arm_reset_vector(void)
+extern char __dtb_imx51_babbage_start[];
+
+ENTRY_FUNCTION(start_imx51_babbage)(void)
 {
+	uint32_t fdt;
+
+	__barebox_arm_head();
+
 	arm_cpu_lowlevel_init();
-	imx51_barebox_entry(0);
+
+	fdt = (uint32_t)__dtb_imx51_babbage_start - get_runtime_offset();
+
+	imx51_barebox_entry(fdt);
 }

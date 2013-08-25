c2ef4788732b680ee370b7197c9982ac0345e782
Sascha Hauer <s.hauer@pengutronix.de>
2013-08-25 15:49:49 +0200

    mci: omap: Allow to set mmc devname via platformdata
    To be able to get persistent names for the sd/mmc devices.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mci/omap_hsmmc.c
+++ drivers/mci/omap_hsmmc.c
@@ -611,10 +611,16 @@ static int omap_mmc_probe(struct device_d *dev)
 	hsmmc->mci.f_min = 400000;
 
 	pdata = (struct omap_hsmmc_platform_data *)dev->platform_data;
-	if (pdata && pdata->f_max)
-		hsmmc->mci.f_max = pdata->f_max;
-	else
-		hsmmc->mci.f_max = 52000000;
+	if (pdata) {
+		if (pdata->f_max)
+			hsmmc->mci.f_max = pdata->f_max;
+		else
+			hsmmc->mci.f_max = 52000000;
+
+		if (pdata->devname)
+			hsmmc->mci.devname = pdata->devname;
+	}
+
 
 	dev->priv = hsmmc;
 	dev->detect = omap_mmc_detect,

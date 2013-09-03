c9b62b2cb28edea986472f6799af82562d505717
Antony Pavlov <antonynpavlov@gmail.com>
2013-09-03 08:33:33 +0400

    MIPS: qemu-malta: switch to devicetree
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/boards/qemu-malta/init.c
+++ arch/mips/boards/qemu-malta/init.c
@@ -17,36 +17,11 @@
  */
 
 #include <common.h>
-#include <types.h>
-#include <driver.h>
 #include <init.h>
-#include <ns16550.h>
-#include <mach/hardware.h>
-#include <partition.h>
 #include <sizes.h>
 
-static int malta_devices_init(void)
+static int malta_core_init(void)
 {
-	add_cfi_flash_device(0, 0x1e000000, SZ_4M, 0);
-
-	devfs_add_partition("nor0", 0x0, SZ_512K, DEVFS_PARTITION_FIXED, "self");
-	devfs_add_partition("nor0", SZ_512K, SZ_64K, DEVFS_PARTITION_FIXED, "env0");
-
 	return 0;
 }
-device_initcall(malta_devices_init);
-
-static struct NS16550_plat serial_plat = {
-	.clock = 1843200, /* no matter for emulated port */
-	.shift = 0,
-};
-
-static int malta_console_init(void)
-{
-	/* Register the serial port */
-	add_ns16550_device(DEVICE_ID_DYNAMIC, MALTA_PIIX4_UART0, 8,
-			IORESOURCE_MEM_8BIT, &serial_plat);
-
-	return 0;
-}
-console_initcall(malta_console_init);
+core_initcall(malta_core_init);

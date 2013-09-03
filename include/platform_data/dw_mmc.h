c6c3afb3ebd37ab36926f660c081f3d5ee16ff81
Sascha Hauer <s.hauer@pengutronix.de>
2013-09-03 10:30:08 +0200

    mci: add designware mmc controller support
    Based on the U-Boot driver.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ include/platform_data/dw_mmc.h
@@ -0,0 +1,9 @@
+#ifndef __INCLUDE_PLATFORM_DATA_DW_MMC_H
+#define __INCLUDE_PLATFORM_DATA_DW_MMC_H
+
+struct dw_mmc_platform_data {
+	char *devname;
+	int ciu_div;
+};
+
+#endif /* __INCLUDE_PLATFORM_DATA_DW_MMC_H */

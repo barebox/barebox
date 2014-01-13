c06522e88ecbaceeb864c6895140fa8735580fd6
Lucas Stach <dev@lynxeye.de>
2014-01-13 01:17:28 +0100

    ARM: imx6: add support for DL variant of SabreLite Board
    Signed-off-by: Lucas Stach <dev@lynxeye.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/freescale-mx6-sabrelite/lowlevel.c
+++ arch/arm/boards/freescale-mx6-sabrelite/lowlevel.c
@@ -5,13 +5,26 @@
 
 extern char __dtb_imx6q_sabrelite_start[];
 
-ENTRY_FUNCTION(start_imx6_sabrelite, r0, r1, r2)
+ENTRY_FUNCTION(start_imx6q_sabrelite, r0, r1, r2)
 {
 	uint32_t fdt;
 
 	arm_cpu_lowlevel_init();
 
 	fdt = (uint32_t)__dtb_imx6q_sabrelite_start - get_runtime_offset();
+
+	barebox_arm_entry(0x10000000, SZ_1G, fdt);
+}
+
+extern char __dtb_imx6dl_sabrelite_start[];
+
+ENTRY_FUNCTION(start_imx6dl_sabrelite, r0, r1, r2)
+{
+	uint32_t fdt;
+
+	arm_cpu_lowlevel_init();
+
+	fdt = (uint32_t)__dtb_imx6dl_sabrelite_start - get_runtime_offset();
 
 	barebox_arm_entry(0x10000000, SZ_1G, fdt);
 }

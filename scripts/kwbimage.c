2bc655f5f5d2d485f545a4cd46dd8314ad6b55f1
Alexander Shiyan <shc_work@mail.ru>
2014-02-03 22:50:50 +0400

    scripts/kwbimage: Fix resource leak in image_create_payload()
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- scripts/kwbimage.c
+++ scripts/kwbimage.c
@@ -701,17 +701,17 @@ static int image_create_payload(void *payload_start, size_t payloadsz,
 	if (ret < 0) {
 		fprintf(stderr, "Cannot stat payload file %s\n",
 			payload_filename);
+		fclose(payload);
 		return ret;
 	}
 
 	ret = fread(payload_start, s.st_size, 1, payload);
+	fclose(payload);
 	if (ret != 1) {
 		fprintf(stderr, "Cannot read payload file %s\n",
 			payload_filename);
 		return -1;
 	}
-
-	fclose(payload);
 
 	*payload_checksum = image_checksum32(payload_start, payloadsz);
 	return 0;

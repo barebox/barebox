0a2a8f7059e6cb11e5d4eb882538b37f99d09ee3
Sascha Hauer <s.hauer@pengutronix.de>
2014-02-20 08:16:01 +0100

    environment: Add function to load envfs from buffer
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/envfs.h
+++ include/envfs.h
@@ -92,6 +92,7 @@ struct envfs_super {
 #define ENV_FLAG_NO_OVERWRITE	(1 << 0)
 int envfs_load(const char *filename, const char *dirname, unsigned flags);
 int envfs_save(const char *filename, const char *dirname);
+int envfs_load_from_buf(void *buf, int len, const char *dir, unsigned flags);
 
 /* defaults to /dev/env0 */
 #ifdef CONFIG_ENV_HANDLING

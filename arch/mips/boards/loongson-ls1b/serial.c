b7cc37eedf1663edd62179715ad1deb44421444a
Antony Pavlov <antonynpavlov@gmail.com>
2014-04-07 11:38:49 +0400

    MIPS: loongson-ls1b: switch to device tree
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/boards/loongson-ls1b/serial.c
+++ arch/mips/boards/loongson-ls1b/serial.c
@@ -1,21 +1,9 @@
 #include <common.h>
 #include <init.h>
-#include <ns16550.h>
-
-#include <mach/loongson1.h>
-
-static struct NS16550_plat serial_plat = {
-	.clock = 83000000,
-	.shift = 0,
-};
 
 static int console_init(void)
 {
-	barebox_set_model("Loongson Tech LS1B Demo Board");
 	barebox_set_hostname("ls1b");
-
-	add_ns16550_device(DEVICE_ID_DYNAMIC, KSEG1ADDR(LS1X_UART2_BASE),
-		8, IORESOURCE_MEM_8BIT, &serial_plat);
 
 	return 0;
 }

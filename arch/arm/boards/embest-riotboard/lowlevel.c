ccc4eac63225410c364eec8ee33a402788334b4d
Eric Bénard <eric@eukrea.com>
2014-04-24 22:17:07 +0200

    RiOTboard: add new board
    this board is produced by Embest/Element 14 and is based on i.MX6 Solo
    The following features are tested :
    - UART2 (console)
    - eMMC
    - SDCard
    - uSDCard
    - Ethernet
    - USB Host (through 4 ports hub)
    - I2C 1/2/3
    - 2 LEDs
    
    Boot on eMMC and through USB loader are tested.
    
    For more informations on this board : http://www.riotboard.org/
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/embest-riotboard/lowlevel.c
@@ -0,0 +1,43 @@
+#include <debug_ll.h>
+#include <common.h>
+#include <sizes.h>
+#include <io.h>
+#include <asm/barebox-arm-head.h>
+#include <asm/barebox-arm.h>
+#include <asm/sections.h>
+#include <asm/cache.h>
+#include <asm/mmu.h>
+#include <mach/imx6-mmdc.h>
+#include <mach/imx6.h>
+
+static inline void setup_uart(void)
+{
+	/* Enable UART for lowlevel debugging purposes */
+	writel(0x00000000, 0x021e8080);
+	writel(0x00004027, 0x021e8084);
+	writel(0x00000704, 0x021e8088);
+	writel(0x00000a81, 0x021e8090);
+	writel(0x0000002b, 0x021e809c);
+	writel(0x00013880, 0x021e80b0);
+	writel(0x0000047f, 0x021e80a4);
+	writel(0x0000c34f, 0x021e80a8);
+	writel(0x00000001, 0x021e8080);
+}
+
+extern char __dtb_imx6s_riotboard_start[];
+
+ENTRY_FUNCTION(start_imx6s_riotboard, r0, r1, r2)
+{
+	uint32_t fdt;
+
+	arm_cpu_lowlevel_init();
+
+	if (IS_ENABLED(CONFIG_DEBUG_LL)) {
+		writel(0x4, 0x020e016c);
+		setup_uart();
+		putc_ll('a');
+	}
+
+	fdt = (uint32_t)__dtb_imx6s_riotboard_start - get_runtime_offset();
+	barebox_arm_entry(0x10000000, SZ_1G, fdt);
+}

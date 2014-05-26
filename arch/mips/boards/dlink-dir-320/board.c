d77b1094c4ba478132b4520c11eb9c10873be8cf
Antony Pavlov <antonynpavlov@gmail.com>
2014-05-26 10:40:50 +0400

    MIPS: dlink-dir-320: switch to dt
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/boards/dlink-dir-320/board.c
+++ arch/mips/boards/dlink-dir-320/board.c
@@ -1,12 +1,27 @@
+/*
+ * Copyright (C) 2011, 2014 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include <common.h>
 #include <init.h>
-#include <sizes.h>
-#include <asm/memory.h>
 
-static int mem_init(void)
+static int dir320_console_init(void)
 {
-	mips_add_ram0(SZ_32M);
+	barebox_set_hostname("dir320");
 
 	return 0;
 }
-mem_initcall(mem_init);
+console_initcall(dir320_console_init);

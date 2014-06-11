8e89bc594a8fbc3337a08c2c4ac03519000e89b7
Antony Pavlov <antonynpavlov@gmail.com>
2014-06-11 08:20:20 +0400

    treewide: remove address of the Free Software Foundation
    The FSF address has changed; The FSF site says that
    address is
    
      Free Software Foundation
      51 Franklin Street, Fifth Floor
      Boston, MA 02110-1301
      USA
    
    (see http://www.fsf.org/about/contact/)
    
    Instead of updating it each time the address changes,
    just drop it completely treewide.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- crypto/crc16.c
+++ crypto/crc16.c
@@ -21,10 +21,6 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with eCos; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
  * As a special exception, if other files instantiate templates or use macros
  * or inline functions from this file, or you compile this file and link it
  * with other works to produce a work based on this file, this file does not
--- drivers/mtd/nand/nand_ecc.c
+++ drivers/mtd/nand/nand_ecc.c
@@ -21,10 +21,6 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this file; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
  * As a special exception, if other files instantiate templates or use
  * macros or inline functions from these files, or you compile these
  * files and link them with other works to produce a work based on these

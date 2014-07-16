227a20fe31c6a03795a202f95af95fb371951963
Sascha Hauer <s.hauer@pengutronix.de>
2014-07-16 09:40:30 +0200

    consistently use the same bitops.h file
    We have many variants of the same bitops.h file. Consistently
    use the same file for all architectures which completely use
    the generic bitops versions.
    
    Some architectures had static inline versions of functions
    identically to the generic versions, these are removed and
    the generic versions are used directly now.
    
    Also several architectures depend on the generic find_*_bit
    functions but didn't have the GENERIC_FIND_NEXT_BIT Kconfig
    option selected. This is added where needed.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/openrisc/include/asm/bitops.h
+++ arch/openrisc/include/asm/bitops.h
@@ -1,6 +1,4 @@
 /*
- * (C) Copyright 2011, Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -11,14 +9,27 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ *
  */
 
-#ifndef __ASM_OPENRISC_BITOPS_H
-#define __ASM_OPENRISC_BITOPS_H
+#ifndef _ASM_BITOPS_H_
+#define _ASM_BITOPS_H_
 
-#define PLATFORM_FLS
-#include <asm/bitops/fls.h>
-#define PLATFORM_FFS
-#include <asm/bitops/ffs.h>
+#include <asm-generic/bitops/__ffs.h>
+#include <asm-generic/bitops/__fls.h>
+#include <asm-generic/bitops/ffs.h>
+#include <asm-generic/bitops/fls.h>
+#include <asm-generic/bitops/ffz.h>
+#include <asm-generic/bitops/hweight.h>
+#include <asm-generic/bitops/fls64.h>
+#include <asm-generic/bitops/find.h>
+#include <asm-generic/bitops/ops.h>
 
-#endif /* __ASM_GENERIC_BITOPS_H */
+#define set_bit(x, y)			__set_bit(x, y)
+#define clear_bit(x, y)			__clear_bit(x, y)
+#define change_bit(x, y)		__change_bit(x, y)
+#define test_and_set_bit(x, y)		__test_and_set_bit(x, y)
+#define test_and_clear_bit(x, y)	__test_and_clear_bit(x, y)
+#define test_and_change_bit(x, y)	__test_and_change_bit(x, y)
+
+#endif /* _ASM_BITOPS_H_ */

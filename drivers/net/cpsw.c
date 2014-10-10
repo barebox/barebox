c1450014643fef9b170f070dc2fd594e02509955
Teresa Gámez <t.gamez@phytec.de>
2014-10-10 11:14:29 +0200

    net: cpsw: Fix probe for one port ethernet
    If only one port is pinned out, probe fails
    as the second port phy_id is not found.
    
    Signed-off-by: Teresa Gámez <t.gamez@phytec.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/net/cpsw.c
+++ drivers/net/cpsw.c
@@ -1070,7 +1070,7 @@ static int cpsw_probe_dt(struct cpsw_priv *priv)
 				return ret;
 		}
 
-		if (!strncmp(child->name, "slave", 5)) {
+		if (i < priv->num_slaves && !strncmp(child->name, "slave", 5)) {
 			struct cpsw_slave *slave = &priv->slaves[i];
 			uint32_t phy_id[2];
 
@@ -1087,7 +1087,7 @@ static int cpsw_probe_dt(struct cpsw_priv *priv)
 		}
 	}
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < priv->num_slaves; i++) {
 		struct cpsw_slave *slave = &priv->slaves[i];
 
 		cpsw_gmii_sel_am335x(slave);

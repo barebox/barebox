566c1630c3164e4e790ca1bfce462034a2639947
Lucas Stach <dev@lynxeye.de>
2014-11-03 23:52:23 +0100

    tegra: remove custom UART setup
    The config option doesn't make any sense anymore
    when building a multiimage barebox. With a proper
    DT built into the image we don't need the ODMdata
    mechanism to find the debug UART anymore.
    
    Signed-off-by: Lucas Stach <dev@lynxeye.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-tegra/tegra20.c
+++ arch/arm/mach-tegra/tegra20.c
@@ -22,47 +22,6 @@
 #include <mach/lowlevel.h>
 #include <mach/tegra114-sysctr.h>
 
-static struct NS16550_plat debug_uart = {
-	.shift = 2,
-};
-
-static int tegra_add_debug_console(void)
-{
-	unsigned long base = 0;
-
-	if (!of_machine_is_compatible("nvidia,tegra20") &&
-	    !of_machine_is_compatible("nvidia,tegra30") &&
-	    !of_machine_is_compatible("nvidia,tegra124"))
-		return 0;
-
-	/* figure out which UART to use */
-	if (IS_ENABLED(CONFIG_TEGRA_UART_NONE))
-		return 0;
-	if (IS_ENABLED(CONFIG_TEGRA_UART_ODMDATA))
-		base = tegra20_get_debuguart_base();
-	if (IS_ENABLED(CONFIG_TEGRA_UART_A))
-		base = TEGRA_UARTA_BASE;
-	if (IS_ENABLED(CONFIG_TEGRA_UART_B))
-		base = TEGRA_UARTB_BASE;
-	if (IS_ENABLED(CONFIG_TEGRA_UART_C))
-		base = TEGRA_UARTC_BASE;
-	if (IS_ENABLED(CONFIG_TEGRA_UART_D))
-		base = TEGRA_UARTD_BASE;
-	if (IS_ENABLED(CONFIG_TEGRA_UART_E))
-		base = TEGRA_UARTE_BASE;
-
-	if (!base)
-		return -ENODEV;
-
-	debug_uart.clock = tegra_get_pllp_rate();
-
-	add_ns16550_device(DEVICE_ID_DYNAMIC, base, 8 << debug_uart.shift,
-			   IORESOURCE_MEM | IORESOURCE_MEM_8BIT, &debug_uart);
-
-	return 0;
-}
-console_initcall(tegra_add_debug_console);
-
 static int tegra20_mem_init(void)
 {
 	if (!of_machine_is_compatible("nvidia,tegra20"))

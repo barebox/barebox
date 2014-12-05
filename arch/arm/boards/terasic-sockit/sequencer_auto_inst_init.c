e4fc177199fe2f918160b359d3c5824f5b386f7b
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2014-12-05 17:41:48 +0100

    ARM: socfpga: sockit: update handoff files to 14.0
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/terasic-sockit/sequencer_auto_inst_init.c
+++ arch/arm/boards/terasic-sockit/sequencer_auto_inst_init.c
@@ -1,7 +1,35 @@
-#include <common.h>
+/*
+Copyright (c) 2012, Altera Corporation
+All rights reserved.
 
-const uint32_t inst_rom_init_size = 128;
-const uint32_t inst_rom_init[128] =
+SPDX-License-Identifier:    BSD-3-Clause
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Altera Corporation nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ALTERA CORPORATION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <common.h>
+const uint32_t inst_rom_init_size = 127;
+const uint32_t inst_rom_init[127] =
 {
 	0x80000,
 	0x80680,
@@ -24,6 +52,7 @@ const uint32_t inst_rom_init[128] =
 	0x900,
 	0x80680,
 	0x980,
+	0xa680,
 	0x8680,
 	0x80680,
 	0xb68,
@@ -114,16 +143,14 @@ const uint32_t inst_rom_init[128] =
 	0xe680,
 	0x80680,
 	0x0,
-	0x0,
-	0xa000,
 	0x8000,
+	0xa000,
+	0xc000,
 	0x80000,
 	0x80,
-	0x80,
-	0x80,
-	0x80,
-	0xa080,
 	0x8080,
+	0xa080,
+	0xc080,
 	0x80080,
 	0x9180,
 	0x8680,

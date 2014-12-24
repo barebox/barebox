d6d28acee05db669c55b53095044c6a791127c14
Masahiro Yamada <yamada.m@jp.panasonic.com>
2014-12-24 13:23:27 +0900

    kbuild: move asm-offsets.h rule to ./Kbuild
    Currently, MIPS is the only architecture that needs
    include/generated/asm-offsets.h, but we have got ./Kbuild file now.
    
    It is a good reason to move asm-offsets.h rule from arch/mips/Makefile
    to ./Kbuild and add dummy asm-offsets.c for the other architectures.
    asm-offsets.h would be useful for all the architectures.
    
    This commit does not implement include/generated/bounds.h,
    but if necessary, it is easy to implement it.
    
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/openrisc/lib/asm-offsets.c
@@ -0,0 +1,18 @@
+/*
+ * OpenRISC asm-offsets.c
+ *
+ * This program is used to generate definitions needed by
+ * assembly language modules.
+ *
+ * We use the technique used in the OSF Mach kernel code:
+ * generate asm statements containing #defines,
+ * compile this file to assembler, and then extract the
+ * #defines from the assembly-language output.
+ */
+
+#include <linux/kbuild.h>
+
+int main(void)
+{
+	return 0;
+}

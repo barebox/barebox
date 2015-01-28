ea0a3b7da852b3e889d13d0cf95cdd4715c30c38
Eric Bénard <eric@eukrea.com>
2015-01-29 00:37:33 +0100

    dhcp: fix request packet
    we are not supposed to fill ciaddr, yiaddr and siaddr in the request packet.
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- net/dhcp.c
+++ net/dhcp.c
@@ -559,9 +559,7 @@ static void dhcp_send_request_packet(struct bootp *bp_offer)
 	bp->bp_hops = 0;
 	/* FIXME what is this? */
 //	bp->bp_secs = htons(get_timer(0) / CFG_HZ);
-	net_copy_ip(&bp->bp_ciaddr, &bp_offer->bp_ciaddr); /* both in network byte order */
-	net_copy_ip(&bp->bp_yiaddr, &bp_offer->bp_yiaddr);
-	net_copy_ip(&bp->bp_siaddr, &bp_offer->bp_siaddr);
+
 	/*
 	 * RFC3046 requires Relay Agents to discard packets with
 	 * nonzero and offered giaddr

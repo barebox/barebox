00e4fd727bff16992b781b013a096da7993d888e
Antony Pavlov <antonynpavlov@gmail.com>
2015-07-01 07:28:24 +0300

    sandbox: asm/byteorder.h: fix guard macro name
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/sandbox/include/asm/byteorder.h
+++ arch/sandbox/include/asm/byteorder.h
@@ -1,8 +1,8 @@
-#ifndef _I386_BYTEORDER_H
-#define _I386_BYTEORDER_H
+#ifndef _SANDBOX_BYTEORDER_H
+#define _SANDBOX_BYTEORDER_H
 
 #include <asm/types.h>
 
 #include <linux/byteorder/little_endian.h>
 
-#endif /* _I386_BYTEORDER_H */
+#endif /* _SANDBOX_BYTEORDER_H */

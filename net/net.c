566cac17c3b5cfd85eb5bb1764c6cd29347b390d
Sascha Hauer <s.hauer@pengutronix.de>
2015-07-02 08:36:08 +0200

    net: Move library functions to net/lib.c
    Some network related functions are also needed when networking
    is disabled. Move these to a separate file which is always compiled.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- net/net.c
+++ net/net.c
@@ -62,50 +62,6 @@ uint16_t net_checksum(unsigned char *ptr, int len)
 	return xsum & 0xffff;
 }
 
-char *ip_to_string (IPaddr_t x)
-{
-	static char s[sizeof("xxx.xxx.xxx.xxx")];
-
-	x = ntohl (x);
-	sprintf (s, "%d.%d.%d.%d",
-		 (int) ((x >> 24) & 0xff),
-		 (int) ((x >> 16) & 0xff),
-		 (int) ((x >> 8) & 0xff), (int) ((x >> 0) & 0xff)
-	);
-	return s;
-}
-
-int string_to_ip(const char *s, IPaddr_t *ip)
-{
-	IPaddr_t addr = 0;
-	char *e;
-	int i;
-
-	if (!s)
-		return -EINVAL;
-
-	for (i = 0; i < 4; i++) {
-		unsigned long val;
-
-		if (!isdigit(*s))
-			return -EINVAL;
-
-		val = simple_strtoul(s, &e, 10);
-		if (val > 255)
-			return -EINVAL;
-
-		addr = (addr << 8) | val;
-
-		if (*e != '.' && i != 3)
-			return -EINVAL;
-
-		s = e + 1;
-	}
-
-	*ip = htonl(addr);
-	return 0;
-}
-
 IPaddr_t getenv_ip(const char *name)
 {
 	IPaddr_t ip;
@@ -129,40 +85,6 @@ int setenv_ip(const char *name, IPaddr_t ip)
 	setenv(name, str);
 
 	return 0;
-}
-
-void print_IPaddr (IPaddr_t x)
-{
-	puts(ip_to_string(x));
-}
-
-int string_to_ethaddr(const char *str, u8 enetaddr[6])
-{
-	int reg;
-	char *e;
-
-        if (!str || strlen(str) != 17) {
-		memset(enetaddr, 0, 6);
-		return -EINVAL;
-	}
-
-        if (str[2] != ':' || str[5] != ':' || str[8] != ':' ||
-                        str[11] != ':' || str[14] != ':')
-                return -EINVAL;
-
-	for (reg = 0; reg < 6; ++reg) {
-		enetaddr[reg] = simple_strtoul (str, &e, 16);
-			str = e + 1;
-	}
-
-	return 0;
-}
-
-void ethaddr_to_string(const u8 enetaddr[6], char *str)
-{
-	sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
-		 enetaddr[0], enetaddr[1], enetaddr[2], enetaddr[3],
-		 enetaddr[4], enetaddr[5]);
 }
 
 static unsigned char *arp_ether;

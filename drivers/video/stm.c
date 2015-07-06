491dc73fe463bdd12e292b17d847381fda306ba0
Sascha Hauer <s.hauer@pengutronix.de>
2015-07-06 12:26:33 +0200

    video: stm: reserve framebuffer memory for Linux
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/video/stm.c
+++ drivers/video/stm.c
@@ -566,6 +566,8 @@ static int stmfb_probe(struct device_d *hw_dev)
 				&fbi.info.bits_per_pixel);
 	}
 
+	fb_of_reserve_add_fixup(&fbi.info);
+
 	ret = register_framebuffer(&fbi.info);
 	if (ret != 0) {
 		dev_err(hw_dev, "Failed to register framebuffer\n");

90c52fe07a1073826d009d2b5f1d4e5d8350580f
Juergen Borleis <jbe@pengutronix.de>
2015-07-31 11:03:51 +0200

    PPC/MPC5200: re-use the IMX I2C master driver for MPC5200
    This IP core is shared between many FSL SoCs. The MPC5200 provides this
    core as well.
    
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/ppc/mach-mpc5xxx/include/mach/clock.h
+++ arch/ppc/mach-mpc5xxx/include/mach/clock.h
@@ -6,5 +6,9 @@ unsigned long get_cpu_clock(void);
 unsigned long get_ipb_clock(void);
 unsigned long get_pci_clock(void);
 unsigned long get_timebase_clock(void);
+static inline unsigned long fsl_get_i2c_freq(void)
+{
+	return get_ipb_clock();
+}
 
 #endif /* __ASM_ARCH_CLOCKS_H */

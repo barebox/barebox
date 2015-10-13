barebox/barebox

commit e89f1a1f188c196eade0d2ad05b662326d870be3

detect_fs: use device instead of file
detect_fs would usually mount a device on a directory,
so, use a device-specific type detection.

Signed-off-by: Vicente Bergas <vicencb@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -1201,7 +1201,7 @@ EXPORT_SYMBOL(register_fs_driver);
 
 static const char *detect_fs(const char *filename)
 {
-	enum filetype type = file_name_detect_type(filename);
+	enum filetype type = cdev_detect_type(filename);
 	struct driver_d *drv;
 	struct fs_driver_d *fdrv;
 

common/filetype.c
@@ -361,3 +361,40 @@ err_out:
 
 	return type;
 }
+
+enum filetype cdev_detect_type(const char *name)
+{
+	enum filetype type = filetype_unknown;
+	int ret;
+	struct cdev *cdev;
+	void *buf;
+
+	cdev = cdev_by_name(name);
+	if (!cdev)
+		return type;
+	buf = xzalloc(FILE_TYPE_SAFE_BUFSIZE);
+	ret = cdev_read(cdev, buf, FILE_TYPE_SAFE_BUFSIZE, 0, 0);
+	if (ret < 0)
+		goto err_out;
+
+	type = file_detect_type(buf, ret);
+
+	if (type == filetype_mbr) {
+		unsigned long bootsec;
+		/*
+		 * Get the first partition start sector
+		 * and check for FAT in it
+		 */
+		is_fat_or_mbr(buf, &bootsec);
+
+		ret = cdev_read(cdev, buf, 512, bootsec * 512, 0);
+		if (ret < 0)
+			goto err_out;
+
+		type = is_fat_or_mbr((u8 *)buf, NULL);
+	}
+
+err_out:
+	free(buf);
+	return type;
+}

include/filetype.h
@@ -45,6 +45,7 @@ const char *file_type_to_short_string(enum filetype f);
 enum filetype file_detect_partition_table(const void *_buf, size_t bufsize);
 enum filetype file_detect_type(const void *_buf, size_t bufsize);
 enum filetype file_name_detect_type(const char *filename);
+enum filetype cdev_detect_type(const char *name);
 enum filetype is_fat_or_mbr(const unsigned char *sector, unsigned long *bootsec);
 int is_fat_boot_sector(const void *_buf);
 

commit a7bbbe0236cd45c35bf79b4bdc628177e183791e

fs: Fix filesystem detection with full path
When mount is given a full path (including /dev/) to the cdev file
we can no longer automatically detect the filesystem. This is broken
since:

e89f1a1 detect_fs: use device instead of file

Fix it by dropping the /dev/ component of the path.

Signed-off-by: Peter Mamonov <pmamonov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -1201,9 +1201,13 @@ EXPORT_SYMBOL(register_fs_driver);
 
 static const char *detect_fs(const char *filename)
 {
-	enum filetype type = cdev_detect_type(filename);
+	enum filetype type;
 	struct driver_d *drv;
 	struct fs_driver_d *fdrv;
+
+	if (!strncmp(filename, "/dev/", 5))
+		filename += 5;
+	type = cdev_detect_type(filename);
 
 	if (type == filetype_unknown)
 		return NULL;
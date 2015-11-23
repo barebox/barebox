97a92f0d86e74dfe153c30ecefc480e0c7e08569
Sascha Hauer <s.hauer@pengutronix.de>
2015-11-23 09:57:47 +0100

    reset: Remove unused *_optional functions
    Since we now have dummy reset controllers we no longer need the
    *_optional functions. These have been unused anyway, so just remove
    them.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/reset.h
+++ include/linux/reset.h
@@ -15,17 +15,6 @@ void reset_control_put(struct reset_control *rstc);
 
 int __must_check device_reset(struct device_d *dev);
 
-static inline int device_reset_optional(struct device_d *dev)
-{
-	return device_reset(dev);
-}
-
-static inline struct reset_control *reset_control_get_optional(
-					struct device_d *dev, const char *id)
-{
-	return reset_control_get(dev, id);
-}
-
 #else
 
 static inline int reset_control_reset(struct reset_control *rstc)
@@ -49,17 +38,6 @@ static inline int reset_control_deassert(struct reset_control *rstc)
 static inline void reset_control_put(struct reset_control *rstc)
 {
 	WARN_ON(1);
-}
-
-static inline int device_reset_optional(struct device_d *dev)
-{
-	return -ENOSYS;
-}
-
-static inline struct reset_control *reset_control_get_optional(
-					struct device_d *dev, const char *id)
-{
-	return ERR_PTR(-ENOSYS);
 }
 
 #endif /* CONFIG_RESET_CONTROLLER */

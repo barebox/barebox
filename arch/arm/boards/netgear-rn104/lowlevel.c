a03a3915d9e76372dd24a6f3c01eb3b5efb940ac
Uwe Kleine-König <uwe@kleine-koenig.org>
2016-01-20 08:47:22 +0100

    Add initial support for Netgear ReadyNAS 104
    Currently only second stage booting from the vendor U-Boot is tested. I
    don't want to flash barebox into NAND yet because UART-booting for
    recovery doesn't work for me.
    
    Working so far are:
    
     - UART
     - networking
     - nand flash
    
    Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/netgear-rn104/lowlevel.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014  Uwe Kleine-Koenig <uwe@kleine-koenig.org>
+ */
+
+#include <common.h>
+#include <asm/barebox-arm.h>
+#include <asm/barebox-arm-head.h>
+#include <mach/lowlevel.h>
+
+extern char __dtb_armada_370_rn104_bb_start[];
+
+ENTRY_FUNCTION(start_netgear_rn104, r0, r1, r2)
+{
+	void *fdt;
+
+	arm_cpu_lowlevel_init();
+
+	fdt = __dtb_armada_370_rn104_bb_start -
+		get_runtime_offset();
+
+	mvebu_barebox_entry(fdt);
+}

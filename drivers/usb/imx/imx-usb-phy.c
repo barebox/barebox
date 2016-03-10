0ee69c9501a077aa7f68b3c4a068992674a74045
Sascha Hauer <s.hauer@pengutronix.de>
2016-03-10 10:37:41 +0100

    USB: imx-usb-phy: Fix uninitialized use of variable
    ret may no be initialized in the error path when clk_get fails. Fix it.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/usb/imx/imx-usb-phy.c
+++ drivers/usb/imx/imx-usb-phy.c
@@ -79,6 +79,7 @@ static int imx_usbphy_probe(struct device_d *dev)
 	imxphy->clk = clk_get(dev, NULL);
 	if (IS_ERR(imxphy->clk)) {
 		dev_err(dev, "could not get clk: %s\n", strerrorp(imxphy->clk));
+		ret = PTR_ERR(imxphy->clk);
 		goto err_clk;
 	}
 

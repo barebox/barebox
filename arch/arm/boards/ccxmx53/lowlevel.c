b8efd857b046ddec108a0df3e20214d8ad19c19d
Jason Cobham <cobham.jason@gmail.com>
2016-04-19 21:41:04 -0700

    ARM: i.MX53: ccxmx53: Fix memory detection.
    This patch fixes memory detection on the Digi ccxmx53 board.
    Also updates dts to support nand.
    Cleaned up whitespace?
    
    Signed-off-by: Jason Cobham <cobham.jason@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/ccxmx53/lowlevel.c
+++ arch/arm/boards/ccxmx53/lowlevel.c
@@ -13,7 +13,10 @@
  *
  */
 
+#include <debug_ll.h>
 #include <common.h>
+#include <linux/sizes.h>
+#include <io.h>
 #include <mach/esdctl.h>
 #include <mach/generic.h>
 #include <image-metadata.h>
@@ -26,28 +29,32 @@
 BAREBOX_IMD_TAG_STRING(ccxmx53_memsize_SZ_512M, IMD_TYPE_PARAMETER, "memsize=512", 0);
 BAREBOX_IMD_TAG_STRING(ccxmx53_memsize_SZ_1G, IMD_TYPE_PARAMETER, "memsize=1024", 0);
 
-static void __noreturn start_imx53_ccxmx53_common(uint32_t size,
-						void *fdt_blob_fixed_offset)
+extern char __dtb_imx53_ccxmx53_start[];
+
+ENTRY_FUNCTION(start_ccxmx53_512mb, r0, r1, r2)
 {
 	void *fdt;
 
 	imx5_cpu_lowlevel_init();
 	arm_setup_stack(0xf8020000 - 8);
 
-	fdt = fdt_blob_fixed_offset - get_runtime_offset();
-	barebox_arm_entry(0x70000000, size, fdt);
+	IMD_USED(ccxmx53_memsize_SZ_512M);
+
+	fdt = __dtb_imx53_ccxmx53_start - get_runtime_offset();
+
+	imx53_barebox_entry(fdt);
 }
 
-#define CCMX53_ENTRY(name, fdt_name, memory_size)	\
-	ENTRY_FUNCTION(name, r0, r1, r2)				\
-	{								\
-		extern char __dtb_##fdt_name##_start[];			\
-									\
-		IMD_USED(ccxmx53_memsize_##memory_size);		\
-									\
-		start_imx53_ccxmx53_common(memory_size,  \
-					 __dtb_##fdt_name##_start);	\
-	}
+ENTRY_FUNCTION(start_ccxmx53_1gib, r0, r1, r2)
+{
+	void *fdt;
 
-CCMX53_ENTRY(start_ccxmx53_512mb, imx53_ccxmx53, SZ_512M);
-CCMX53_ENTRY(start_ccxmx53_1gib, imx53_ccxmx53, SZ_1G);
+	imx5_cpu_lowlevel_init();
+	arm_setup_stack(0xf8020000 - 8);
+
+	IMD_USED(ccxmx53_memsize_SZ_1G);
+
+	fdt = __dtb_imx53_ccxmx53_start - get_runtime_offset();
+
+	imx53_barebox_entry(fdt);
+}

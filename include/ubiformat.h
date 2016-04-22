893228f66c63a452ad46537657d7bf8c98197702
Sascha Hauer <s.hauer@pengutronix.de>
2016-04-22 08:47:15 +0200

    commands: ubiformat: move code to common/
    This is the final step to separate the ubiformat code from the
    command. With this the ubiformat code gains a C API.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ include/ubiformat.h
@@ -0,0 +1,23 @@
+#ifndef __UBIFORMAT_H
+#define __UBIFORMAT_H
+
+#include <linux/types.h>
+
+struct ubiformat_args {
+	unsigned int yes:1;
+	unsigned int quiet:1;
+	unsigned int verbose:1;
+	unsigned int override_ec:1;
+	unsigned int novtbl:1;
+	unsigned int manual_subpage;
+	int subpage_size;
+	int vid_hdr_offs;
+	int ubi_ver;
+	uint32_t image_seq;
+	long long ec;
+	const char *image;
+};
+
+int ubiformat(struct mtd_info *mtd, struct ubiformat_args *args);
+
+#endif /* __UBIFORMAT_H */

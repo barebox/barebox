barebox/barebox

commit b1374a6d97f1211f5c897641668371978eaa592e

usb: fastboot: drop CONFIG_COMMAND_SUPPORT dependency
fastboot can run without command support. In this case we cannot
execute oem commands.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/gadget/f_fastboot.c
@@ -872,6 +872,11 @@ static void cb_oem_exec(struct usb_ep *ep, struct usb_request *req, const char *
 	struct f_fastboot *f_fb = req->context;
 	int ret;
 
+	if (!IS_ENABLED(CONFIG_COMMAND)) {
+		fastboot_tx_print(f_fb, "FAILno command support available");
+		return;
+	}
+
 	ret = run_command(cmd);
 	if (ret < 0)
 		fastboot_tx_print(f_fb, "FAIL%s", strerror(-ret));

drivers/usb/gadget/Kconfig
@@ -44,7 +44,6 @@ config USB_GADGET_SERIAL
 config USB_GADGET_FASTBOOT
 	bool
 	select BANNER
-	depends on COMMAND_SUPPORT
 	prompt "Android Fastboot support"
 
 endif

commit ece204a8acea2ae5f4343fef28e9e987a133e9ea

usb: fastboot: Fix oem exec support
Fixes: b1374a6 usb: fastboot: drop CONFIG_COMMAND_SUPPORT dependency

The correct config option to depend on is CONFIG_COMMAND_SUPPORT,
not CONFIG_COMMAND.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/gadget/f_fastboot.c
@@ -873,7 +873,7 @@ static void cb_oem_exec(struct usb_ep *ep, struct usb_request *req, const char *
 	struct f_fastboot *f_fb = req->context;
 	int ret;
 
-	if (!IS_ENABLED(CONFIG_COMMAND)) {
+	if (!IS_ENABLED(CONFIG_COMMAND_SUPPORT)) {
 		fastboot_tx_print(f_fb, "FAILno command support available");
 		return;
 	}
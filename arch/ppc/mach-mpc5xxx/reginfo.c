ad955eab6dad763f17f32f4c8d05a7c9a7f2907d
Juergen Borleis <jbe@pengutronix.de>
2016-05-27 11:32:50 +0200

    PPC/MPC5XXX/reginfo: fix a bunch of compiler warnings
    This change fixes a bunch of compiler warnings of this type:
    
    arch/ppc/mach-mpc5xxx/reginfo.c: In function 'reginfo':
    arch/ppc/mach-mpc5xxx/reginfo.c:14:3: warning: format '%X' expects argument of type 'unsigned int', but argument 2 has type 'ulong' [-Wformat=]
       (*(volatile ulong*)MPC5XXX_ADDECR & 0x00010000) ? 1 : 0);
       ^
    
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/ppc/mach-mpc5xxx/reginfo.c
+++ arch/ppc/mach-mpc5xxx/reginfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <config.h>
 #include <mach/mpc5xxx.h>
+#include <asm/io.h>
 
 void reginfo(void)
 {
@@ -8,52 +9,52 @@ void reginfo(void)
 	printf ("MBAR=%08x\n", CFG_MBAR);
 	puts ("Memory map registers\n");
 	printf ("\tCS0: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS0_START,
-		*(volatile ulong*)MPC5XXX_CS0_STOP,
-		*(volatile ulong*)MPC5XXX_CS0_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00010000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS0_START),
+		in_be32((void*)MPC5XXX_CS0_STOP),
+		in_be32((void*)MPC5XXX_CS0_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00010000 ? 1 : 0);
 	printf ("\tCS1: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS1_START,
-		*(volatile ulong*)MPC5XXX_CS1_STOP,
-		*(volatile ulong*)MPC5XXX_CS1_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00020000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS1_START),
+		in_be32((void*)MPC5XXX_CS1_STOP),
+		in_be32((void*)MPC5XXX_CS1_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00020000 ? 1 : 0);
 	printf ("\tCS2: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS2_START,
-		*(volatile ulong*)MPC5XXX_CS2_STOP,
-		*(volatile ulong*)MPC5XXX_CS2_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00040000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS2_START),
+		in_be32((void*)MPC5XXX_CS2_STOP),
+		in_be32((void*)MPC5XXX_CS2_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00040000 ? 1 : 0);
 	printf ("\tCS3: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS3_START,
-		*(volatile ulong*)MPC5XXX_CS3_STOP,
-		*(volatile ulong*)MPC5XXX_CS3_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00080000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS3_START),
+		in_be32((void*)MPC5XXX_CS3_STOP),
+		in_be32((void*)MPC5XXX_CS3_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00080000 ? 1 : 0);
 	printf ("\tCS4: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS4_START,
-		*(volatile ulong*)MPC5XXX_CS4_STOP,
-		*(volatile ulong*)MPC5XXX_CS4_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00100000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS4_START),
+		in_be32((void*)MPC5XXX_CS4_STOP),
+		in_be32((void*)MPC5XXX_CS4_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00100000 ? 1 : 0);
 	printf ("\tCS5: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS5_START,
-		*(volatile ulong*)MPC5XXX_CS5_STOP,
-		*(volatile ulong*)MPC5XXX_CS5_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x00200000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS5_START),
+		in_be32((void*)MPC5XXX_CS5_STOP),
+		in_be32((void*)MPC5XXX_CS5_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x00200000 ? 1 : 0);
 	printf ("\tCS6: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS6_START,
-		*(volatile ulong*)MPC5XXX_CS6_STOP,
-		*(volatile ulong*)MPC5XXX_CS6_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x04000000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS6_START),
+		in_be32((void*)MPC5XXX_CS6_STOP),
+		in_be32((void*)MPC5XXX_CS6_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x04000000 ? 1 : 0);
 	printf ("\tCS7: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_CS7_START,
-		*(volatile ulong*)MPC5XXX_CS7_STOP,
-		*(volatile ulong*)MPC5XXX_CS7_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x08000000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_CS7_START),
+		in_be32((void*)MPC5XXX_CS7_STOP),
+		in_be32((void*)MPC5XXX_CS7_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x08000000 ? 1 : 0);
 	printf ("\tBOOTCS: start %08X\tstop %08X\tconfig %08X\ten %d\n",
-		*(volatile ulong*)MPC5XXX_BOOTCS_START,
-		*(volatile ulong*)MPC5XXX_BOOTCS_STOP,
-		*(volatile ulong*)MPC5XXX_BOOTCS_CFG,
-		(*(volatile ulong*)MPC5XXX_ADDECR & 0x02000000) ? 1 : 0);
+		in_be32((void*)MPC5XXX_BOOTCS_START),
+		in_be32((void*)MPC5XXX_BOOTCS_STOP),
+		in_be32((void*)MPC5XXX_BOOTCS_CFG),
+		in_be32((void*)MPC5XXX_ADDECR) & 0x02000000 ? 1 : 0);
 	printf ("\tSDRAMCS0: %08X\n",
-		*(volatile ulong*)MPC5XXX_SDRAM_CS0CFG);
+		in_be32((void*)MPC5XXX_SDRAM_CS0CFG));
 	printf ("\tSDRAMCS1: %08X\n",
-		*(volatile ulong*)MPC5XXX_SDRAM_CS1CFG);
+		in_be32((void*)MPC5XXX_SDRAM_CS1CFG));
 }

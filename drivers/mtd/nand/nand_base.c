6a175cb3ffe31480626ac7a2e30d82f21f20aed8
Sascha Hauer <s.hauer@pengutronix.de>
2016-06-24 10:23:58 +0200

    mtd: nand: Set ONFI function hooks earlier
    Make the ONFI function hooks available after nand_scan_ident() but
    before nand_scan_tail(). Based on the kernel commit:
    4204ccc mtd: set ONFI nand's default hooks in nand_set_defaults()
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -2891,6 +2891,13 @@ static void nand_set_defaults(struct nand_chip *chip, int busw)
 
 	if (!chip->select_chip)
 		chip->select_chip = nand_select_chip;
+
+	/* set for ONFI nand */
+	if (!chip->onfi_set_features)
+		chip->onfi_set_features = nand_onfi_set_features;
+	if (!chip->onfi_get_features)
+		chip->onfi_get_features = nand_onfi_get_features;
+
 	if (!chip->read_byte)
 		chip->read_byte = busw ? nand_read_byte16 : nand_read_byte;
 	if (!chip->read_word)
@@ -3545,12 +3552,6 @@ int nand_scan_tail(struct mtd_info *mtd)
 
 	if (!chip->write_page)
 		chip->write_page = nand_write_page;
-
-	/* set for ONFI nand */
-	if (!chip->onfi_set_features)
-		chip->onfi_set_features = nand_onfi_set_features;
-	if (!chip->onfi_get_features)
-		chip->onfi_get_features = nand_onfi_get_features;
 
 	/*
 	 * Check ECC mode, default to software if 3byte/512byte hardware ECC is

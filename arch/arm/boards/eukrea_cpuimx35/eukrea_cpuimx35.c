59a2582df555e511e931571169eca36cf2bf1baf
Alexander Kurz <akurz@blala.de>
2016-06-30 23:24:13 +0200

    eukrea-cpuimx35: Fix wrong clock gating for ESDHC1
    With commit 962d8b89d2ce ("imx35-regs: add defines for USB and SD") the
    shifter argument for the iMX35 ESDHC1 CGR instance has been erroneously
    assigned to CGR1 instead of CGR0. Fix this for the eukrea-cpuimx35 board.
    
    Signed-off-by: Alexander Kurz <akurz@blala.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/eukrea_cpuimx35/eukrea_cpuimx35.c
+++ arch/arm/boards/eukrea_cpuimx35/eukrea_cpuimx35.c
@@ -232,10 +232,12 @@ static int eukrea_cpuimx35_core_init(void)
 {
 	u32 reg;
 
-	/* enable clock for I2C1, SDHC1, USB and FEC */
+	/* enable clock for I2C1, ESDHC1, USB and FEC */
+	reg = readl(MX35_CCM_BASE_ADDR + MX35_CCM_CGR0);
+	reg |= 0x3 << MX35_CCM_CGR0_ESDHC1_SHIFT;
+	reg = writel(reg, MX35_CCM_BASE_ADDR + MX35_CCM_CGR0);
 	reg = readl(MX35_CCM_BASE_ADDR + MX35_CCM_CGR1);
 	reg |= 0x3 << MX35_CCM_CGR1_FEC_SHIFT;
-	reg |= 0x3 << MX35_CCM_CGR1_SDHC1_SHIFT;
 	reg |= 0x3 << MX35_CCM_CGR1_I2C1_SHIFT,
 	reg = writel(reg, MX35_CCM_BASE_ADDR + MX35_CCM_CGR1);
 	reg = readl(MX35_CCM_BASE_ADDR + MX35_CCM_CGR2);

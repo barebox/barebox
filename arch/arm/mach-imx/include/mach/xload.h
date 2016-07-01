509d2228f907652289928b6b3c07e17650a8c124
Sascha Hauer <s.hauer@pengutronix.de>
2016-07-01 13:20:56 +0200

    ARM: i.MX53: Implement NAND xload
    Some i.MX53 want to setup the SDRAM from C code rather than
    from DCD tables. The image size for these boards is limited
    to the internal SRAM size. To overcome this limitation for
    i.MX53 boards booting from NAND implement an xload mechanism
    to load only the PBL to SRAM and let barebox load the rest
    of the image itself after SDRAM has been initialized.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/include/mach/xload.h
+++ arch/arm/mach-imx/include/mach/xload.h
@@ -1,6 +1,7 @@
 #ifndef __MACH_XLOAD_H
 #define __MACH_XLOAD_H
 
+int imx53_nand_start_image(void);
 int imx6_spi_load_image(int instance, unsigned int flash_offset, void *buf, int len);
 int imx6_spi_start_image(int instance);
 int imx6_esdhc_load_image(int instance, void *buf, int len);

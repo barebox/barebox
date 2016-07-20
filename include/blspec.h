bf07b3411a023a031bea60140cc47668c7e1af2d
Sascha Hauer <s.hauer@pengutronix.de>
2016-07-20 13:58:20 +0200

    bootentries: Move struct bootentries to include/boot.h
    The boot function prototypes are declared in include/blspec.h. Move them
    to include/boot.h where they belong.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/blspec.h
+++ include/blspec.h
@@ -2,21 +2,7 @@
 #define __LOADER_H__
 
 #include <linux/list.h>
-#include <menu.h>
-
-struct bootentries {
-	struct list_head entries;
-	struct menu *menu;
-};
-
-struct bootentry {
-	struct list_head list;
-	struct menu_entry me;
-	char *title;
-	char *description;
-	int (*boot)(struct bootentry *entry, int verbose, int dryrun);
-	void (*release)(struct bootentry *entry);
-};
+#include <boot.h>
 
 struct blspec_entry {
 	struct bootentry entry;
@@ -36,10 +22,5 @@ int blspec_scan_devices(struct bootentries *bootentries);
 int blspec_scan_device(struct bootentries *bootentries, struct device_d *dev);
 int blspec_scan_devicename(struct bootentries *bootentries, const char *devname);
 int blspec_scan_directory(struct bootentries *bootentries, const char *root);
-
-int bootentries_add_entry(struct bootentries *entries, struct bootentry *entry);
-
-#define bootentries_for_each_entry(bootentries, entry) \
-	list_for_each_entry(entry, &bootentries->entries, list)
 
 #endif /* __LOADER_H__ */
--- include/boot.h
+++ include/boot.h
@@ -2,6 +2,7 @@
 #define __BOOT_H
 
 #include <of.h>
+#include <menu.h>
 #include <environment.h>
 
 #ifdef CONFIG_FLEXIBLE_BOOTARGS
@@ -18,5 +19,24 @@ static inline int linux_bootargs_overwrite(const char *bootargs)
 	return setenv("bootargs", bootargs);
 }
 #endif
+
+struct bootentries {
+	struct list_head entries;
+	struct menu *menu;
+};
+
+struct bootentry {
+	struct list_head list;
+	struct menu_entry me;
+	char *title;
+	char *description;
+	int (*boot)(struct bootentry *entry, int verbose, int dryrun);
+	void (*release)(struct bootentry *entry);
+};
+
+int bootentries_add_entry(struct bootentries *entries, struct bootentry *entry);
+
+#define bootentries_for_each_entry(bootentries, entry) \
+	list_for_each_entry(entry, &bootentries->entries, list)
 
 #endif /* __BOOT_H */

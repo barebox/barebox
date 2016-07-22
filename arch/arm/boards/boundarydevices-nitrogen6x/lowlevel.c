b82e9f62d98db90757949a53695216c08302bec2
Lucas Stach <l.stach@pengutronix.de>
2016-07-22 14:31:29 +0200

    ARM: add support for Nitrogen6MAX QP variant
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/boundarydevices-nitrogen6x/lowlevel.c
+++ arch/arm/boards/boundarydevices-nitrogen6x/lowlevel.c
@@ -50,3 +50,16 @@ ENTRY_FUNCTION(start_imx6dl_nitrogen6x_2g, r0, r1, r2)
 
 	imx6q_barebox_entry(fdt);
 }
+
+extern char __dtb_imx6qp_nitrogen6_max_start[];
+
+ENTRY_FUNCTION(start_imx6qp_nitrogen6_max, r0, r1, r2)
+{
+	void *fdt;
+
+	imx6_cpu_lowlevel_init();
+
+	fdt = __dtb_imx6qp_nitrogen6_max_start - get_runtime_offset();
+
+	imx6q_barebox_entry(fdt);
+}

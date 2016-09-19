b6ca0241b0ba2f166f12b9d58c608fb777ec3bd5
Enrico Jorns <ejo@pengutronix.de>
2016-09-19 18:03:49 +0200

    fs: nfs: pick up network interface bootargs parameter
    This adds the linux.bootarg device parameter from the network device of
    the current nfs connection and adds it to the nfs bootargs line.
    
    This allows booting from nfs without manually setting a ip=dhcp or
    ip=<ipaddr> option.
    
    Signed-off-by: Enrico Jorns <ejo@pengutronix.de>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- fs/nfs.c
+++ fs/nfs.c
@@ -1314,6 +1314,7 @@ static char *rootnfsopts;
 static void nfs_set_rootarg(struct nfs_priv *npriv, struct fs_device_d *fsdev)
 {
 	char *str, *tmp;
+	const char *bootargs;
 
 	str = basprintf("root=/dev/nfs nfsroot=%pI4:%s%s%s", &npriv->server, npriv->path,
 			  rootnfsopts[0] ? "," : "", rootnfsopts);
@@ -1327,6 +1328,13 @@ static void nfs_set_rootarg(struct nfs_priv *npriv, struct fs_device_d *fsdev)
 
 	if (npriv->manual_mount_port == 1) {
 		tmp = basprintf("%s,mountport=%hu", str, npriv->mount_port);
+		free(str);
+		str = tmp;
+	}
+
+	bootargs = dev_get_param(&npriv->con->edev->dev, "linux.bootargs");
+	if (bootargs) {
+		tmp = basprintf("%s %s", str, bootargs);
 		free(str);
 		str = tmp;
 	}

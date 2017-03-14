a8ef43b995fcd4c92edbf9492ac0378f8dc2441a
Sascha Hauer <s.hauer@pengutronix.de>
2017-03-14 08:08:36 +0100

    mtd: when creating bitflips the offset has to be page aligned
    mtd_peb_create_bitflips() assumes the offset is page aligned. Enforce
    this and in the nand_bitlfip command print a warning if it's not
    aligned.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mtd/peb.c
+++ drivers/mtd/peb.c
@@ -565,6 +565,8 @@ int mtd_peb_create_bitflips(struct mtd_info *mtd, int pnum, int offset,
 
 	if (offset < 0 || offset + len > mtd->erasesize)
 		return -EINVAL;
+	if (offset % mtd->writesize)
+		return -EINVAL;
 	if (len <= 0)
 		return -EINVAL;
 	if (num_bitflips <= 0)

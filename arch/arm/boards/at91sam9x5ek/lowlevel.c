759b50110a9eb36ae8be3fdc041d4c7f5bed50e3
Andrey Smirnov <andrew.smirnov@gmail.com>
2017-03-23 06:23:47 -0700

    at91sam9x5ek: Add preliminary device tree support
    Add basic code to build and pass board device tree blob to Barebox
    initialization code.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/at91sam9x5ek/lowlevel.c
+++ arch/arm/boards/at91sam9x5ek/lowlevel.c
@@ -6,11 +6,16 @@
 #include <io.h>
 #include <debug_ll.h>
 
+extern char __dtb_at91sam9x5ek_start[];
 
 ENTRY_FUNCTION(start_at91sam9x5ek, r0, r1, r2)
 {
+	void *fdt;
+
 	arm_cpu_lowlevel_init();
 	arm_setup_stack(AT91SAM9X5_SRAM_BASE + AT91SAM9X5_SRAM_SIZE - 16);
 
-	barebox_arm_entry(AT91_CHIPSELECT_1, at91sam9x5_get_ddram_size(), NULL);
+	fdt = __dtb_at91sam9x5ek_start - get_runtime_offset();
+
+	barebox_arm_entry(AT91_CHIPSELECT_1, at91sam9x5_get_ddram_size(), fdt);
 }

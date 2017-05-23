barebox/barebox

commit bbe0df90b8e2463dd8db651e9f1dd57702985d2d

usb: imx: Add clock support
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/imx/chipidea-imx.c
@@ -27,6 +27,7 @@
 #include <usb/fsl_usb2.h>
 #include <linux/err.h>
 #include <linux/phy/phy.h>
+#include <linux/clk.h>
 
 #define MXC_EHCI_PORTSC_MASK ((0xf << 28) | (1 << 25))
 
@@ -44,6 +45,7 @@ struct imx_chipidea {
 	struct regulator *vbus;
 	struct phy *phy;
 	struct usb_phy *usbphy;
+	struct clk *clk;
 };
 
 static int imx_chipidea_port_init(void *drvdata)
@@ -267,6 +269,17 @@ static int imx_chipidea_probe(struct device_d *dev)
 	ci->vbus = regulator_get(dev, "vbus");
 	if (IS_ERR(ci->vbus))
 		ci->vbus = NULL;
+
+	/*
+	 * Some devices have more than one clock, in this case they are enabled
+	 * by default in the clock driver. At least enable the main clock for
+	 * devices which have only one.
+	 */
+	ci->clk = clk_get(dev, NULL);
+	if (IS_ERR(ci->clk))
+		return PTR_ERR(ci->clk);
+
+	clk_enable(ci->clk);
 
 	if (of_property_read_bool(dev->device_node, "fsl,usbphy")) {
 		ci->phy = of_phy_get_by_phandle(dev, "fsl,usbphy", 0);

commit f6ee281578930123987c1d92722316adda35b927

usb: imx: Only use clock when it exists
Fixes: bbe0df90b8e2463dd8db651e9f1dd57702985d2d

I assumed all chipidea devices actually have a clock. This is true
when looking at the device tree, but in barebox not all i.MX SoCs
use the clocks provided by the device tree. Most instead use
clkdev_add_physbase() to attach a clocks to devices. For these the
chipidea device does not have a clock.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/imx/chipidea-imx.c
@@ -276,10 +276,8 @@ static int imx_chipidea_probe(struct device_d *dev)
 	 * devices which have only one.
 	 */
 	ci->clk = clk_get(dev, NULL);
-	if (IS_ERR(ci->clk))
-		return PTR_ERR(ci->clk);
-
-	clk_enable(ci->clk);
+	if (!IS_ERR(ci->clk))
+		clk_enable(ci->clk);
 
 	if (of_property_read_bool(dev->device_node, "fsl,usbphy")) {
 		ci->phy = of_phy_get_by_phandle(dev, "fsl,usbphy", 0);
a6f379599227835114e4e2d8bf53f1a1c65d7fec
Ian Abbott <abbotti@mev.co.uk>
2017-07-03 14:17:29 +0100

    readlink: Improve -f handling
    For `readlink -f FILE VARIABLE`, set VARIABLE to the absolute, canonical
    file name of FILE by following symbolic links.  All but the final
    component of FILE must exist.
    
    Prior to commit a602bebcf7e4 ("fs: Implement links to directories") the
    above worked in the limited case where FILE was an absolute path, the
    final component was a symbolic link, and all preceding components were
    directories.  If FILE was a relative path with the final component being
    a symbolic link, and all preceding paths being directories, the command
    sort of worked, but produced a relative path as the result.  Both of
    these cases were broken by the above commit.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- commands/readlink.c
+++ commands/readlink.c
@@ -44,17 +44,21 @@ static int do_readlink(int argc, char *argv[])
 	if (argc < optind + 2)
 		return COMMAND_ERROR_USAGE;
 
-	if (readlink(argv[optind], realname, PATH_MAX - 1) < 0)
-		goto err;
-
 	if (canonicalize) {
-		char *buf = canonicalize_path(realname);
+		char *buf = canonicalize_path(argv[optind]);
+		struct stat s;
 
 		if (!buf)
 			goto err;
+		if (stat(dirname(argv[optind]), &s) || !S_ISDIR(s.st_mode)) {
+			free(buf);
+			goto err;
+		}
 		setenv(argv[optind + 1], buf);
 		free(buf);
 	} else {
+		if (readlink(argv[optind], realname, PATH_MAX - 1) < 0)
+			goto err;
 		setenv(argv[optind + 1], realname);
 	}
 
@@ -65,15 +69,16 @@ err:
 }
 
 BAREBOX_CMD_HELP_START(readlink)
-BAREBOX_CMD_HELP_TEXT("Read value of a symbolic link and store it into VARIABLE.")
+BAREBOX_CMD_HELP_TEXT("Read value of a symbolic link or canonical file name and store it into VARIABLE.")
 BAREBOX_CMD_HELP_TEXT("")
 BAREBOX_CMD_HELP_TEXT("Options:")
-BAREBOX_CMD_HELP_OPT ("-f", "canonicalize by following first symlink");
+BAREBOX_CMD_HELP_OPT("-f", "canonicalize by following symlinks;")
+BAREBOX_CMD_HELP_OPT("",   "final component need not exist");
 BAREBOX_CMD_HELP_END
 
 BAREBOX_CMD_START(readlink)
 	.cmd		= do_readlink,
-	BAREBOX_CMD_DESC("read value of a symbolic link")
+	BAREBOX_CMD_DESC("read value of a symbolic link or canonical file name")
 	BAREBOX_CMD_OPTS("[-f] FILE VARIABLE")
 	BAREBOX_CMD_GROUP(CMD_GRP_FILE)
 	BAREBOX_CMD_HELP(cmd_readlink_help)

barebox/barebox

commit 28e8db159f25024dad5e11f03a0fabe055c2df81

ARM: i.MX: Add imx7_uart_setup_ll()
Add imx7_uart_setup_ll() for setting up UART selected for DEBUG_LL.

Acked-by: Sam Ravnborg <sam@ravnborg.org>
Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

arch/arm/mach-imx/include/mach/debug_ll.h
@@ -81,6 +81,13 @@ static inline void imx6_uart_setup_ll(void)
 	imx6_uart_setup(base);
 }
 
+static inline void imx7_uart_setup_ll(void)
+{
+	void *base = IOMEM(IMX_UART_BASE(IMX_DEBUG_SOC, CONFIG_DEBUG_IMX_UART_PORT));
+
+	imx7_uart_setup(base);
+}
+
 static inline void vf610_uart_setup_ll(void)
 {
 	void *base = IOMEM(IMX_UART_BASE(IMX_DEBUG_SOC, CONFIG_DEBUG_IMX_UART_PORT));

commit 1833f1130268c7ee768c92a3e1bfa02e8f60f954

ARM: imx: add stub for imx7_uart_setup_ll
Fixes: 28e8db159f25 (ARM: i.MX: Add imx7_uart_setup_ll())
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

arch/arm/mach-imx/include/mach/debug_ll.h
@@ -115,6 +115,7 @@ static inline void imx50_uart_setup_ll(void) {}
 static inline void imx51_uart_setup_ll(void) {}
 static inline void imx53_uart_setup_ll(void) {}
 static inline void imx6_uart_setup_ll(void)  {}
+static inline void imx7_uart_setup_ll(void)  {}
 static inline void vf610_uart_setup_ll(void) {}
 
 #endif /* CONFIG_DEBUG_LL */
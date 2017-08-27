c4531782266fafe767cd913baecc8c5d270fac76
Oleksij Rempel <linux@rempel-privat.de>
2017-08-27 08:52:00 +0200

    phy: don't crash if we have no parent
    current code will crash if DT has no PHY entry. With this patch
    we will test if parent exist, before testing the parent device node.
    
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -323,7 +323,7 @@ static struct phy_device *of_mdio_find_phy(struct eth_device *edev)
 	if (!IS_ENABLED(CONFIG_OFDEVICE))
 		return NULL;
 
-	if (!edev->parent->device_node)
+	if (!edev->parent || !edev->parent->device_node)
 		return NULL;
 
 	phy_node = of_parse_phandle(edev->parent->device_node, "phy-handle", 0);

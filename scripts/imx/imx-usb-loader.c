2096492765521d978968b4649da50f6ec56a143c
Sascha Hauer <s.hauer@pengutronix.de>
2017-09-22 11:08:02 +0200

    scripts: imx imx-usb-loader: Add support for i.MX6ull
    Same as other i.MX6 variants, just add the product id.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- scripts/imx/imx-usb-loader.c
+++ scripts/imx/imx-usb-loader.c
@@ -171,6 +171,13 @@ static const struct mach_id imx_ids[] = {
 		.max_transfer = 1024,
 	}, {
 		.vid = 0x15a2,
+		.pid = 0x0080,
+		.name = "i.MX6ull",
+		.header_type = HDR_MX53,
+		.mode = MODE_HID,
+		.max_transfer = 1024,
+	}, {
+		.vid = 0x15a2,
 		.pid = 0x0076,
 		.name = "i.MX7S",
 		.header_type = HDR_MX53,

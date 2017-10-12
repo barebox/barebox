6690cb957d6d45cae785b41f24502ddaf25efbb3
Michael Olbrich <m.olbrich@pengutronix.de>
2017-10-12 12:26:55 +0200

    vexpress: use device tree provided by QEMU if available
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/vexpress/lowlevel.c
+++ arch/arm/boards/vexpress/lowlevel.c
@@ -9,18 +9,34 @@
 #include <asm/barebox-arm-head.h>
 #include <asm/barebox-arm.h>
 #include <asm/system_info.h>
+#include <asm/unaligned.h>
 #include <linux/amba/sp804.h>
 
 static inline void start_vexpress_common(void *internal_dt)
 {
 	void *fdt = internal_dt - get_runtime_offset();
+	unsigned long membase, memsize = SZ_512M;
 
 	arm_cpu_lowlevel_init();
 
 	if (amba_is_arm_sp804(IOMEM(0x10011000)))
-		barebox_arm_entry(0x60000000, SZ_512M, fdt);
+		membase = 0x60000000;
 	else
-		barebox_arm_entry(0x80000000, SZ_512M, fdt);
+		membase = 0x80000000;
+
+	/* QEMU may put a DTB at the start of RAM */
+	if (IS_ENABLED(CONFIG_OFDEVICE) &&
+	    get_unaligned_be32((void*)membase) == FDT_MAGIC) {
+		fdt = (void*)membase;
+		/*
+		 * Need to move membase a bit as the PBL wants to relocate
+		 * to the start of RAM, which would overwrite the DTB.
+		 */
+		membase += SZ_4M;
+		memsize -= SZ_4M;
+	}
+
+	barebox_arm_entry(membase, memsize, fdt);
 }
 
 extern char __dtb_vexpress_v2p_ca9_start[];

barebox/barebox

commit 7c3f8d3663cbee7e406bf949a263ce82f6b34ae8

uimage: fix: add can_lseek_backward and use in uimage_open
Since commit ce0cc7fe we support forward seek on tftpfs. This feature
breaks the condition to check rather we open an uimage over tftp. Since
backward seeking is the problem here, we add the function
can_lseek_backward and check for it instead of the simple lseek.

Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/fs.h
@@ -100,6 +100,26 @@ struct fs_device_d {
 	char *linux_rootarg;
 };
 
+/*
+ * Some filesystems i.e. tftpfs only support lseek into one direction.
+ * To detect this limited functionality we add this extra function.
+ * Additionaly we also return 0 if we even can not seek forward.
+ */
+static inline int can_lseek_backward(int fd)
+{
+	int ret;
+
+	ret = lseek(fd, 1, SEEK_SET);
+	if (ret < 0)
+		return 0;
+
+	ret = lseek(fd, 0, SEEK_SET);
+	if (ret < 0)
+		return 0;
+
+	return ret;
+}
+
 #define drv_to_fs_driver(d) container_of(d, struct fs_driver_d, drv)
 
 int flush(int fd);

common/uimage.c
@@ -111,9 +111,9 @@ again:
 	/*
 	 * Hack around tftp fs. We need lseek for uImage support, but
 	 * this cannot be implemented in tftp fs, so we detect this
-	 * by doing a test lseek and copy the file to ram if it fails
+	 * and copy the file to ram if it fails
 	 */
-	if (IS_BUILTIN(CONFIG_FS_TFTP) && lseek(fd, 0, SEEK_SET)) {
+	if (IS_BUILTIN(CONFIG_FS_TFTP) && !can_lseek_backward(fd)) {
 		close(fd);
 		ret = copy_file(filename, uimage_tmp, 0);
 		if (ret)

fs/uimagefs.c
@@ -374,9 +374,9 @@ again:
 	/*
 	 * Hack around tftp fs. We need lseek for uImage support, but
 	 * this cannot be implemented in tftp fs, so we detect this
-	 * by doing a test lseek and copy the file to ram if it fails
+	 * and copy the file to ram if it fails
 	 */
-	if (IS_BUILTIN(CONFIG_FS_TFTP) && lseek(fd, 0, SEEK_SET)) {
+	if (IS_BUILTIN(CONFIG_FS_TFTP) && !can_lseek_backward(fd)) {
 		close(fd);
 		ret = copy_file(priv->filename, priv->tmp, 0);
 		if (ret)

commit 3f2df4e92a8b68760e8e4f3c481cce88bd0bd082

fs: Fix can_lseek_backward()
To quote corresponding man page:

"... Upon successful completion, lseek() returns the resulting offset
location as measured in bytes from the beginning of the file."

Which for lseek(fd, 0, SEEK_SET) would be 0, so returning 'ret' as
final step of the function would mean it'd never return anything but 0
as well. Change the code to explicitly return '1' to fix the problem.

Fixes: 7c3f8d366 ("uimage: fix: add can_lseek_backward and use in uimage_open")
Cc: Michael Grzeschik <m.grzeschik@pengutronix.de>
Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/fs.h
@@ -117,7 +117,7 @@ static inline int can_lseek_backward(int fd)
 	if (ret < 0)
 		return 0;
 
-	return ret;
+	return 1;
 }
 
 #define drv_to_fs_driver(d) container_of(d, struct fs_driver_d, drv)
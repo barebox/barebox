bd61482cba36abd315d14e4335eb8cf70fe99970
Oleksij Rempel <linux@rempel-privat.de>
2017-12-12 18:55:57 +0100

    MIPS: add support for DPTechnics DPT-Module v1
    This product can be found here:
    https://dptechnics.com/en/products/dpt-module-v1.html
    
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/mips/boards/dptechnics-dpt-module/include/board/board_pbl_start.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017 Oleksij Rempel <linux@rempel-privat.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ */
+
+#include <asm/pbl_macros.h>
+#include <mach/pbl_macros.h>
+#include <asm/pbl_nmon.h>
+
+	.macro	board_pbl_start
+	.set	push
+	.set	noreorder
+
+	ar9331_pbl_generic_start
+
+	.set	pop
+	.endm

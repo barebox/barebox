barebox/barebox

commit 1439cdf7c6fcee23b5734da8eb2b14d89309d00a

spi: imx: add timeout to xchg_single
If there is a problem STAT_RR might never be set which results in an endless loop.
Break out after 10 µs with -ETIMEOUT instead.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

drivers/spi/imx_spi.c
@@ -32,6 +32,9 @@
 #include <linux/err.h>
 #include <clock.h>
 
+/* time to wait for STAT_RR getting set */
+#define IMX_SPI_RR_TIMEOUT	10000 /* ns */
+
 struct imx_spi {
 	struct spi_master	master;
 	int			*cs_array;
@@ -89,6 +92,7 @@ static unsigned int imx_spi_maybe_reverse_bits(struct spi_device *spi, unsigned 
 static int cspi_0_0_xchg_single(struct imx_spi *imx, u32 txdata, u32 *rxdata)
 {
 	void __iomem *base = imx->regs;
+	int ret;
 
 	unsigned int cfg_reg = readl(base + CSPI_0_0_CTRL);
 
@@ -98,7 +102,12 @@ static int cspi_0_0_xchg_single(struct imx_spi *imx, u32 txdata, u32 *rxdata)
 
 	writel(cfg_reg, base + CSPI_0_0_CTRL);
 
-	while (!(readl(base + CSPI_0_0_INT) & CSPI_0_0_STAT_RR));
+	ret = wait_on_timeout(IMX_SPI_RR_TIMEOUT,
+			      readl(base + CSPI_0_0_INT) & CSPI_0_0_STAT_RR);
+	if (ret) {
+		dev_err(imx->master.dev, "Timeout waiting for received data\n");
+		return ret;
+	}
 
 	*rxdata = readl(base + CSPI_0_0_RXDATA);
 
@@ -157,6 +166,7 @@ static void cspi_0_0_init(struct imx_spi *imx)
 static int cspi_0_7_xchg_single(struct imx_spi *imx, u32 txdata, u32 *rxdata)
 {
 	void __iomem *base = imx->regs;
+	int ret;
 
 	unsigned int cfg_reg = readl(base + CSPI_0_7_CTRL);
 
@@ -166,8 +176,12 @@ static int cspi_0_7_xchg_single(struct imx_spi *imx, u32 txdata, u32 *rxdata)
 
 	writel(cfg_reg, base + CSPI_0_7_CTRL);
 
-	while (!(readl(base + CSPI_0_7_STAT) & CSPI_0_7_STAT_RR))
-		;
+	ret = wait_on_timeout(IMX_SPI_RR_TIMEOUT,
+			      readl(base + CSPI_0_7_STAT) & CSPI_0_7_STAT_RR);
+	if (ret) {
+		dev_err(imx->master.dev, "Timeout waiting for received data\n");
+		return ret;
+	}
 
 	*rxdata = readl(base + CSPI_0_7_RXDATA);
 	return 0;
@@ -248,10 +262,16 @@ static void cspi_0_7_init(struct imx_spi *imx)
 static int cspi_2_3_xchg_single(struct imx_spi *imx, u32 txdata, u32 *rxdata)
 {
 	void __iomem *base = imx->regs;
+	int ret;
 
 	writel(txdata, base + CSPI_2_3_TXDATA);
 
-	while (!(readl(base + CSPI_2_3_STAT) & CSPI_2_3_STAT_RR));
+	ret = wait_on_timeout(IMX_SPI_RR_TIMEOUT,
+			      readl(base + CSPI_2_3_STAT) & CSPI_2_3_STAT_RR);
+	if (ret) {
+		dev_err(imx->master.dev, "Timeout waiting for received data\n");
+		return ret;
+	}
 
 	*rxdata = readl(base + CSPI_2_3_RXDATA);
 

commit 1b23decf4f6f6a6b31e71397cfe29f001ebd0e4f

spi: imx: make xchg_single timeout value compatible for older boards
A call of xchg_single will issue the transfer of 24 data bits with a given
maximum frequency. Since there exist some i.MX based boards with
spi-max-frequency = <100000>, change the xchg_single timeout to still
support those boards.

Fixes: 1439cdf7c6fc ("spi: imx: add timeout to xchg_single")
Signed-off-by: Alexander Kurz <akurz@blala.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/spi/imx_spi.c
@@ -32,8 +32,11 @@
 #include <linux/err.h>
 #include <clock.h>
 
-/* time to wait for STAT_RR getting set */
-#define IMX_SPI_RR_TIMEOUT	10000 /* ns */
+/*
+ * time to wait for STAT_RR getting set, should allow the transfer
+ * of 24 bits at spi-max-frequency of 100kHz
+ */
+#define IMX_SPI_RR_TIMEOUT	250000 /* ns */
 
 struct imx_spi {
 	struct spi_master	master;
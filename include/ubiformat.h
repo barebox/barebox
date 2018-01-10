91e08222389da159944d918bbe6eee7510da8872
Sascha Hauer <s.hauer@pengutronix.de>
2018-01-10 14:20:57 +0100

    ubiformat: Add ubiformat write function
    The ubiformat C API expects an image file as argument. With upcoming
    Android fastboot sparse image support we can no longer provide a
    complete image anymore. With this patch we can write an image in
    multiple chunks after we've formatted a MTD device with ubiformat.
    
    ubiformat_write will skip the EC header in both the MTD device we
    write to and also the image we read from, so we can flash an image
    on a MTD device containing EC headers already.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/ubiformat.h
+++ include/ubiformat.h
@@ -20,4 +20,7 @@ struct ubiformat_args {
 
 int ubiformat(struct mtd_info *mtd, struct ubiformat_args *args);
 
+int ubiformat_write(struct mtd_info *mtd, const void *buf, size_t count,
+		    loff_t offset);
+
 #endif /* __UBIFORMAT_H */

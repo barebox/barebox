e1ac1912755a8999bc110f2e6cf6e17965bece3d
Antony Pavlov <antonynpavlov@gmail.com>
2018-01-15 00:22:50 +0300

    include/parseopt.h: add guard macro
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/parseopt.h
+++ include/parseopt.h
@@ -1,2 +1,7 @@
+#ifndef __PARSEOPT_H__
+#define __PARSEOPT_H__
+
 void parseopt_b(const char *options, const char *opt, bool *val);
 void parseopt_hu(const char *options, const char *opt, unsigned short *val);
+
+#endif /* __PARSEOPT_H__ */

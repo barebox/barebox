ee8e839569db30fc1d1a7fb712c4a81f6c0089b7
Antony Pavlov <antonynpavlov@gmail.com>
2018-01-15 00:22:51 +0300

    parseopt: introduce parseopt_u16() and parseopt_str()
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- lib/parseopt.c
+++ lib/parseopt.c
@@ -58,3 +58,67 @@ again:
 	if (*endp == ',' || *endp == '\0')
 		*val = v;
 }
+
+void parseopt_u16(const char *options, const char *opt, uint16_t *val)
+{
+	const char *start;
+	size_t optlen = strlen(opt);
+	ulong v;
+	char *endp;
+
+again:
+	start = strstr(options, opt);
+
+	if (!start)
+		return;
+
+	if (start > options && start[-1] != ',') {
+		options = start;
+		goto again;
+	}
+
+	if (start[optlen] != '=') {
+		options = start;
+		goto again;
+	}
+
+	v = simple_strtoul(start + optlen + 1, &endp, 0);
+	if (v > U16_MAX)
+		return;
+
+	if (*endp == ',' || *endp == '\0')
+		*val = v;
+}
+
+void parseopt_str(const char *options, const char *opt, char **val)
+{
+	const char *start;
+	size_t optlen = strlen(opt);
+	char *endp;
+	char *parsed;
+
+again:
+	start = strstr(options, opt);
+
+	if (!start)
+		return;
+
+	if (start > options && start[-1] != ',') {
+		options = start;
+		goto again;
+	}
+
+	if (start[optlen] != '=') {
+		options = start;
+		goto again;
+	}
+
+	parsed = (char *)start + optlen + 1;
+	endp = parsed;
+	while (*endp != '\0' && *endp != ',') {
+
+		endp++;
+	}
+
+	*val = xstrndup(parsed, endp - parsed);
+}

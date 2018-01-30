4078b040ef84295a264b644a9831f8c0988f0c74
Lucas Stach <l.stach@pengutronix.de>
2018-01-30 16:40:13 +0100

    reset: add missing stub for reset_control_get
    Fixes: 45b95b50cd0b (reset: add reset controller framework)
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/reset.h
+++ include/linux/reset.h
@@ -37,6 +37,13 @@ static inline int reset_control_deassert(struct reset_control *rstc)
 	return 0;
 }
 
+static inline struct reset_control *
+reset_control_get(struct device_d *dev, const char *id)
+{
+	WARN_ON(1);
+	return NULL;
+}
+
 static inline void reset_control_put(struct reset_control *rstc)
 {
 	WARN_ON(1);

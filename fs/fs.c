barebox/barebox

commit d79a81736f64eef5d19396ad04ee9391bc384a8e

fs: Don't bother filesystems without link support with additional stat() calls
In __canonicalize_path() we only call stat() to know if the path
is a link or not. When the filesystem doesn't support links we
already know that it's not a link, so we do not need to call stat().
This helps the tftp filesystem since the parent directories of
a file to be opened won't be stat()ed anymore, something tftp
does not support.

Fixes: a602bebc fs: Implement links to directories

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -145,6 +145,7 @@ char *normalise_path(const char *pathname)
 EXPORT_SYMBOL(normalise_path);
 
 static int __lstat(const char *filename, struct stat *s);
+static struct fs_device_d *get_fsdevice_by_path(const char *path);
 
 static char *__canonicalize_path(const char *_pathname, int level)
 {
@@ -167,6 +168,7 @@ static char *__canonicalize_path(const char *_pathname, int level)
 		char *p = strsep(&path, "/");
 		char *tmp;
 		char link[PATH_MAX] = {};
+		struct fs_device_d *fsdev;
 
 		if (!p)
 			break;
@@ -184,6 +186,14 @@ static char *__canonicalize_path(const char *_pathname, int level)
 		tmp = basprintf("%s/%s", outpath, p);
 		free(outpath);
 		outpath = tmp;
+
+		/*
+		 * Don't bother filesystems without link support
+		 * with an additional stat() call.
+		 */
+		fsdev = get_fsdevice_by_path(outpath);
+		if (!fsdev->driver->readlink)
+			continue;
 
 		ret = __lstat(outpath, &s);
 		if (ret)

commit ca880912a473ec6e92d78ddf0aa24857536e57da

fs: check pointer returned by get_fsdevice_by_path before dereferencing
In __canonicalize_path() we dereference the pointer returned by
get_fsdevice_by_path() without checking if the pointer is NULL or not.
When the pointer is NULL it leads to an Ooops.

	Ooops, address error on load or ifetch!

	$ 0   : 00000000 00000001 a0000026 a0811c10
	$ 4   : a0402e60 a0402e48 a0811c00 a0402e58
	$ 8   : 00000001 00000000 0000005a 00000023
	$12   : 00000000 00000002 00601021 00000000
	$16   : a0402e60 a0402e50 a0402e39 a0810000
	$20   : a0402e38 a0811420 a0811424 00000000
	$24   : 00000000 a080de10
	$28   : 87f87d40 a03ffa68 a0810000 a080ce30
	Hi    : 00000002
	Lo    : 00000000
	epc   : a080ce34
	ra    : a080ce30
	Status: 00000006
	Cause : 40008010
	Config: 80040483

	### ERROR ### Please RESET the board ###

Fixes: d79a81736 fs: Don't bother filesystems without link support with additional stat() calls

Signed-off-by: Gaël PORTAY <gael.portay@savoirfairelinux.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -191,7 +191,7 @@ static char *__canonicalize_path(const char *_pathname, int level)
 		 * with an additional stat() call.
 		 */
 		fsdev = get_fsdevice_by_path(outpath);
-		if (!fsdev->driver->readlink)
+		if (!fsdev || !fsdev->driver->readlink)
 			continue;
 
 		ret = __lstat(outpath, &s);
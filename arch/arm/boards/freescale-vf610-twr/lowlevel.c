567afbebb7936c093776a957189490710baee0ca
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-04-10 16:40:38 -0700

    i.MX: vf610-twr: Convert to use vf610_setup_pad()
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/freescale-vf610-twr/lowlevel.c
+++ arch/arm/boards/freescale-vf610-twr/lowlevel.c
@@ -13,13 +13,7 @@ static inline void setup_uart(void)
 	void __iomem *iomuxbase = IOMEM(VF610_IOMUXC_BASE_ADDR);
 
 	vf610_ungate_all_peripherals();
-
-	/*
-	 * VF610_PAD_PTB4__UART1_TX
-	 */
-	writel(VF610_UART_PAD_CTRL | (2 << 20), iomuxbase + 0x0068);
-	writel(0, iomuxbase + 0x0380);
-
+	vf610_setup_pad(iomuxbase, VF610_PAD_PTB4__UART1_TX);
 	vf610_uart_setup_ll();
 
 	putc_ll('>');

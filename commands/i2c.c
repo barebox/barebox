barebox/barebox

commit 34fadb685905530a15bb78d1c09bb281bc91458b

commands: i2c_write: enable raw write to address
Sometimes for communication with a simple I2C devices
(e.g. PCF8574 or TM1650) it's necessary to send only
one data byte into the I2C device.
Current i2c_write command makes this impossible because
you can't just pass 'device address' and 'register number'
(or 'device address' and 'one data byte') to the command.
You always have to pass all three parameters:
'device address', 'register number' and 'data'.

This commit fixes the problem.

Sample usage:

  barebox@barebox sandbox:/ i2c_write -a 0x24 0x01

Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

commands/i2c.c
@@ -115,7 +115,7 @@ static int do_i2c_write(int argc, char *argv[])
 
 	count = argc - optind;
 
-	if ((addr < 0) || (reg < 0) || (count == 0) || (addr > 0x7F))
+	if ((addr < 0) || (count == 0) || (addr > 0x7F))
 		return COMMAND_ERROR_USAGE;
 
 	adapter = i2c_get_adapter(bus);
@@ -131,7 +131,11 @@ static int do_i2c_write(int argc, char *argv[])
 	for (i = 0; i < count; i++)
 		*(buf + i) = (char) simple_strtol(argv[optind+i], NULL, 0);
 
-	ret = i2c_write_reg(&client, reg | wide, buf, count);
+	if (reg > 0) {
+		ret = i2c_write_reg(&client, reg | wide, buf, count);
+	} else {
+		ret = i2c_master_send(&client, buf, count);
+	}
 	if (ret != count) {
 		if (verbose)
 			printf("write aborted, count(%i) != writestatus(%i)\n",

commit aea31bf35f569aca4ec2091d7050488c0182926a

commands: i2c_write: enable reg == 0 too
This commit fixes the mistake introduced in the commit 34fadb685905
('commands: i2c_write: enable raw write to address').
The intended behaviour for i2c_write command is
"use raw i2c write only if reg address is not defined".
Before the commit 34fadb685905 reg address == 0
was acceptable. After the commit 34fadb685905 reg address == 0
became unacceptable ("undefined") and will lead to raw i2c write.

Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

commands/i2c.c
@@ -131,7 +131,7 @@ static int do_i2c_write(int argc, char *argv[])
 	for (i = 0; i < count; i++)
 		*(buf + i) = (char) simple_strtol(argv[optind+i], NULL, 0);
 
-	if (reg > 0) {
+	if (reg >= 0) {
 		ret = i2c_write_reg(&client, reg | wide, buf, count);
 	} else {
 		ret = i2c_master_send(&client, buf, count);
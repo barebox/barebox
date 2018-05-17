bb64685c7dc96a14d4ec5d7b2355e40343cf094a
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-05-17 14:29:15 -0700

    bootsource: Add bootsource alias name API
    Add API allowing to query and override the name of the alias pointing
    at DTB node representing current bootsource.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/bootsource.h
+++ include/bootsource.h
@@ -25,5 +25,7 @@ enum bootsource bootsource_get(void);
 int bootsource_get_instance(void);
 void bootsource_set(enum bootsource src);
 void bootsource_set_instance(int instance);
+void bootsource_set_alias_name(const char *name);
+char *bootsource_get_alias_name(void);
 
 #endif	/* __BOOTSOURCE_H__ */

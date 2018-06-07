32e8842c4005b38b643bc1215d0f1dfb9e288613
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-06-07 06:00:55 -0700

    ARM: lib64: Make string functions aware of MMU configuration
    Optimized version of memset() in memset.S if called as:
    
        memset(foo, 0, size)
    
    will try to explicitly zero out data cache with:
    
        dc	zva, dst
    
    which will result in Alignement Exception (DABT) if MMU is not
    enabled.
    
    For more info see:
    
        - C4.4.8 "DC ZVA, Data Cache Zero by VA"
        - D5.2.8 "The effects of disabling a stage of address translation"
    
    in "ARM Architecture Reference Manual. ARMv8, for ARMv8-A architecture
    profile"
    
    In similar vein, using optimized version of memcpy() could lead to a
    unaligned 16-byte write (using 'stp'), which is not allowed for
    Device-nGnRnE type of memory (see D5.2.8) and would liead to
    Alignement Exception.
    
    To fix both problems expose non-optimized and optimzied versions of
    the function and created a wrapper to dispatch the call to either one
    based on if MMU is enabled or not.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/string.h
+++ include/string.h
@@ -6,4 +6,7 @@
 void *memdup(const void *, size_t);
 int strtobool(const char *str, int *val);
 
+void *__default_memset(void *, int, __kernel_size_t);
+void *__default_memcpy(void * dest,const void *src,size_t count);
+
 #endif /* __STRING_H */

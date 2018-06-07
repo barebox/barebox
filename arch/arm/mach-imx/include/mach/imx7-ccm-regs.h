f171d4c8fb08a6cad1c757607ae0ff1daf24376f
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-06-07 06:00:31 -0700

    ARM: i.MX: Split shared CCM code into a separate file
    Both i.MX8 and i.MX7 have similar CCMs, so move any code that can be
    share into a separate file.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/include/mach/imx7-ccm-regs.h
+++ arch/arm/mach-imx/include/mach/imx7-ccm-regs.h
@@ -1,24 +1,10 @@
 #ifndef __MACH_IMX7_CCM_REGS_H__
 #define __MACH_IMX7_CCM_REGS_H__
 
-/* 0 <= n <= 190 */
-#define CCM_CCGRn_SET(n)	(0x4004 + 16 * (n))
-#define CCM_CCGRn_CLR(n)	(0x4008 + 16 * (n))
+#include "ccm.h"
 
 #define CCM_CCGR_UART1		148
 #define CCM_CCGR_UART2		149
-
-#define CCM_CCGR_SETTINGn(n, s)  ((s) << ((n) * 4))
-#define CCM_CCGR_SETTINGn_NOT_NEEDED(n)		CCM_CCGR_SETTINGn(n, 0b00)
-#define CCM_CCGR_SETTINGn_NEEDED_RUN(n)		CCM_CCGR_SETTINGn(n, 0b01)
-#define CCM_CCGR_SETTINGn_NEEDED_RUN_WAIT(n)	CCM_CCGR_SETTINGn(n, 0b10)
-#define CCM_CCGR_SETTINGn_NEEDED(n)		CCM_CCGR_SETTINGn(n, 0b11)
-
-/* 0 <= n <= 120 */
-#define CCM_TARGET_ROOTn(n)	(0x8000 + 128 * (n))
-
-#define CCM_TARGET_ROOTn_MUX(x)		((x) << 24)
-#define CCM_TARGET_ROOTn_ENABLE		BIT(28)
 
 #define CLOCK_ROOT_INDEX(x)	(((x) - 0x8000) / 128)
 

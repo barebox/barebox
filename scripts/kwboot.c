barebox/barebox

commit 39ebd7e73bec3a96f1563dcb89793b1dff187b13

kwboot: do a filetype check before sending the image
Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/kwboot.c
@@ -546,6 +546,63 @@ out:
 	return rc;
 }
 
+static int
+kwboot_check_image(const unsigned char *img, size_t size)
+{
+	size_t i;
+	size_t header_size, image_size;
+	unsigned char csum = 0;
+
+	if (size < 0x20) {
+		fprintf(stderr,
+			"Image too small to even contain a Main Header\n");
+		return 1;
+	}
+
+	switch (img[0x0]) {
+		case 0x5a: /* SPI/NOR */
+		case 0x69: /* UART0 */
+		case 0x78: /* SATA */
+		case 0x8b: /* NAND */
+		case 0x9c: /* PCIe */
+			break;
+		default:
+			fprintf(stderr,
+				"Unknown boot source: 0x%hhx\n", img[0x0]);
+			return 1;
+	}
+
+	if (img[0x8] != 1) {
+		fprintf(stderr, "Unknown version: 0x%hhx\n", img[0x8]);
+		return 1;
+	}
+
+	image_size = img[0x4] | (img[0x5] << 8) |
+		(img[0x6] << 16) | (img[0x7] << 24);
+
+	header_size = (img[0x9] << 16) | img[0xa] | (img[0xb] << 8);
+
+	if (header_size + image_size != size) {
+		fprintf(stderr, "Size mismatch (%zu + %zu != %zu)\n",
+			header_size, image_size, size);
+		return 1;
+	}
+
+	for (i = 0; i < header_size; ++i)
+		csum += img[i];
+
+	csum -= img[0x1f];
+
+	if (csum != img[0x1f]) {
+		fprintf(stderr,
+			"Checksum mismatch: specified: 0x%02hhx, calculated: 0x%02hhx\n",
+			img[0x1f], csum);
+		return 1;
+	}
+
+	return 0;
+}
+
 static void *
 kwboot_mmap_image(const char *path, size_t *size, int prot)
 {
@@ -608,7 +665,7 @@ int
 main(int argc, char **argv)
 {
 	const char *ttypath, *imgpath;
-	int rv, rc, tty, term;
+	int rv, rc, tty, term, force = 0;
 	void *bootmsg;
 	void *debugmsg;
 	void *img;
@@ -628,7 +685,7 @@ main(int argc, char **argv)
 	kwboot_verbose = isatty(STDOUT_FILENO);
 
 	do {
-		int c = getopt(argc, argv, "hb:dtB:D:");
+		int c = getopt(argc, argv, "b:dfhtB:D:");
 		if (c < 0)
 			break;
 
@@ -649,6 +706,10 @@ main(int argc, char **argv)
 
 		case 't':
 			term = 1;
+			break;
+
+		case 'f':
+			force = 1;
 			break;
 
 		case 'B':
@@ -682,6 +743,13 @@ main(int argc, char **argv)
 		img = kwboot_mmap_image(imgpath, &size, PROT_READ);
 		if (!img) {
 			perror(imgpath);
+			goto out;
+		}
+
+		rc = kwboot_check_image(img, size);
+		if (rc && !force) {
+			fprintf(stderr,
+				"Image check failed, restart with -f to ignore\n");
 			goto out;
 		}
 	}

commit ec4dca17bb6a0029466de80754a20fec13d0e111

scripts/kwboot: fix image check for v0 images
Since kwboot checks the contents of the file to send it only works for
v1 images (or with -f). Extend the check to know about v0 images, too.

Fixes: 39ebd7e73bec ("kwboot: do a filetype check before sending the image")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/kwboot.c
@@ -602,12 +602,24 @@ out:
 	return rc;
 }
 
+static unsigned char crc(const unsigned char *img, size_t len)
+{
+	unsigned char ret = 0;
+	size_t i;
+
+	for (i = 0; i < len; ++i)
+		ret += img[i];
+
+	return ret;
+}
+
 static int
 kwboot_check_image(unsigned char *img, size_t size)
 {
 	size_t i;
 	size_t header_size, image_size, image_offset;
 	unsigned char csum = 0;
+	unsigned char imgversion;
 
 	if (size < 0x20) {
 		fprintf(stderr,
@@ -636,7 +648,8 @@ kwboot_check_image(unsigned char *img, size_t size)
 			return 1;
 	}
 
-	if (img[0x8] != 1) {
+	imgversion = img[0x8];
+	if (imgversion > 1) {
 		fprintf(stderr, "Unknown version: 0x%hhx\n", img[0x8]);
 		return 1;
 	}
@@ -646,7 +659,15 @@ kwboot_check_image(unsigned char *img, size_t size)
 	image_offset = img[0xc] | (img[0xd] << 8) |
 		(img[0xe] << 16) | (img[0xf] << 24);
 
-	header_size = (img[0x9] << 16) | img[0xa] | (img[0xb] << 8);
+	if (imgversion == 0) {
+		/* Image format 0 */
+		header_size =
+			img[0x1e] * 0x200 /* header extensions */ +
+			img[0x1d] * 0x800 /* binary header extensions */;
+	} else {
+		/* Image format 1 */
+		header_size = (img[0x9] << 16) | img[0xa] | (img[0xb] << 8);
+	}
 
 	if (header_size > image_offset) {
 		fprintf(stderr, "Header (%zu) expands over image start (%zu)\n",
@@ -660,16 +681,38 @@ kwboot_check_image(unsigned char *img, size_t size)
 		return 1;
 	}
 
-	for (i = 0; i < header_size; ++i)
-		csum += img[i];
 
-	csum -= img[0x1f];
+	if (imgversion == 0) {
+		/* check Main Header */
+		csum = crc(img, 0x1f);
+		if (csum != img[0x1f]) {
+			fprintf(stderr,
+				"Main Header checksum mismatch: specified: 0x%02hhx, calculated: 0x%02hhx\n",
+				img[0x1f], csum);
+			return 1;
+		}
 
-	if (csum != img[0x1f]) {
-		fprintf(stderr,
-			"Checksum mismatch: specified: 0x%02hhx, calculated: 0x%02hhx\n",
-			img[0x1f], csum);
-		return 1;
+		/* check Header Extensions */
+		for (i = 0; i < img[0x1e]; ++i) {
+			csum = crc(img + 0x20 + i * 0x200, 0x1df);
+			if (csum != img[i * 0x200 + 0x1ff]) {
+				fprintf(stderr,
+					"Extension Header #%zu checksum mismatch: specified: 0x%02hhx, calculated: 0x%02hhx\n",
+					i, img[i * 0x200 + 0x1ff], csum);
+				return 1;
+			}
+		}
+	} else {
+		csum = crc(img, header_size);
+		csum -= img[0x1f];
+
+		if (csum != img[0x1f]) {
+			fprintf(stderr,
+				"Checksum mismatch: specified: 0x%02hhx, calculated: 0x%02hhx\n",
+				img[0x1f], csum);
+			return 1;
+		}
+
 	}
 
 	return 0;
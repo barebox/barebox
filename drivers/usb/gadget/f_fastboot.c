barebox/barebox

commit f4b5d3eeb6075bad56cddb154578a87fe1069ee0

usb: gadget: fastboot: Make sparse support optional
Sparse support is not always desired, make it optional.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/gadget/f_fastboot.c
@@ -322,8 +322,10 @@ static int fastboot_bind(struct usb_configuration *c, struct usb_function *f)
 	fb_setvar(var, "0.4");
 	var = fb_addvar(f_fb, "bootloader-version");
 	fb_setvar(var, release_string);
-	var = fb_addvar(f_fb, "max-download-size");
-	fb_setvar(var, "%u", fastboot_max_download_size);
+	if (IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE)) {
+		var = fb_addvar(f_fb, "max-download-size");
+		fb_setvar(var, "%u", fastboot_max_download_size);
+	}
 
 	if (IS_ENABLED(CONFIG_BAREBOX_UPDATE) && opts->export_bbu)
 		bbu_handlers_iterate(fastboot_add_bbu_variables, f_fb);
@@ -906,6 +908,11 @@ static void cb_flash(struct usb_ep *ep, struct usb_request *req, const char *cmd
 	filename = fentry->filename;
 
 	if (filetype == filetype_android_sparse) {
+		if (!IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE)) {
+			fastboot_tx_print(f_fb, "FAILsparse image not supported");
+			return;
+		}
+
 		ret = fastboot_handle_sparse(f_fb, fentry);
 		if (ret) {
 			fastboot_tx_print(f_fb, "FAILwriting sparse image: %s",
@@ -1172,7 +1179,8 @@ static void rx_handler_command(struct usb_ep *ep, struct usb_request *req)
 
 static int fastboot_globalvars_init(void)
 {
-	globalvar_add_simple_int("usbgadget.fastboot_max_download_size",
+	if (IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE))
+		globalvar_add_simple_int("usbgadget.fastboot_max_download_size",
 				 &fastboot_max_download_size, "%u");
 
 	return 0;

drivers/usb/gadget/Kconfig
@@ -57,7 +57,15 @@ config USB_GADGET_FASTBOOT
 	bool
 	select BANNER
 	select FILE_LIST
-	select IMAGE_SPARSE
 	prompt "Android Fastboot support"
+
+config USB_GADGET_FASTBOOT_SPARSE
+	bool
+	select IMAGE_SPARSE
+	prompt "Enable Fastboot sparse image support"
+	help
+	  Sparse images are a way for the fastboot protocol to write
+	  images that are bigger than the available memory. If unsure,
+	  say yes here.
 
 endif

commit 383ba5137a2ea4c2c9d5230bd51384aec187166d

usb: gadget: allow to actually enable sparse fastboot support
When sparse support was (intended to be) made optional it was
effectively unconditionally disabled because

	IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE)

always evaluates to 0. To actually make use of the introduced kconfig
symbol the CONFIG_ prefix must not be skipped.

Fixes: f4b5d3eeb607 ("usb: gadget: fastboot: Make sparse support optional")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Tested-by: Gavin Schenk <g.schenk@eckelmann.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/usb/gadget/f_fastboot.c
@@ -337,7 +337,7 @@ static int fastboot_bind(struct usb_configuration *c, struct usb_function *f)
 	fb_setvar(var, "0.4");
 	var = fb_addvar(f_fb, "bootloader-version");
 	fb_setvar(var, release_string);
-	if (IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE)) {
+	if (IS_ENABLED(CONFIG_USB_GADGET_FASTBOOT_SPARSE)) {
 		var = fb_addvar(f_fb, "max-download-size");
 		fb_setvar(var, "%u", fastboot_max_download_size);
 	}
@@ -947,7 +947,7 @@ static void cb_flash(struct f_fastboot *f_fb, const char *cmd)
 	filename = fentry->filename;
 
 	if (filetype == filetype_android_sparse) {
-		if (!IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE)) {
+		if (!IS_ENABLED(CONFIG_USB_GADGET_FASTBOOT_SPARSE)) {
 			fastboot_tx_print(f_fb, "FAILsparse image not supported");
 			ret = -EOPNOTSUPP;
 			goto out;
@@ -1233,7 +1233,7 @@ done:
 
 static int fastboot_globalvars_init(void)
 {
-	if (IS_ENABLED(USB_GADGET_FASTBOOT_SPARSE))
+	if (IS_ENABLED(CONFIG_USB_GADGET_FASTBOOT_SPARSE))
 		globalvar_add_simple_int("usbgadget.fastboot_max_download_size",
 				 &fastboot_max_download_size, "%u");
 
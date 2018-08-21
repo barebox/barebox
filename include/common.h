dfdac6015bb0e712d9f0f7111056ca857e93e65f
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-08-20 23:27:36 -0700

    include/common: Make use of ALIGN and ALIGN_DOWN
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/common.h
+++ include/common.h
@@ -109,8 +109,8 @@ void shutdown_barebox(void);
 
 #define PAGE_SIZE	4096
 #define PAGE_SHIFT	12
-#define PAGE_ALIGN(s) (((s) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
-#define PAGE_ALIGN_DOWN(x) ((x) & ~(PAGE_SIZE - 1))
+#define PAGE_ALIGN(s)	ALIGN(s, PAGE_SIZE)
+#define PAGE_ALIGN_DOWN(x) ALIGN_DOWN(x, PAGE_SIZE)
 
 int memory_display(const void *addr, loff_t offs, unsigned nbytes, int size, int swab);
 

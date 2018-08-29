e470a35c469029de65600e8b2edbe5986ebe733f
Lionel Debieve <lionel.debieve@st.com>
2018-08-29 14:20:11 +0200

    mtd: ubi: Update ubi-media.h to dual license
    Update license template using SPDX. Move the global layout
    of UBI headers to dual license helping UBI to be the standard
    solution for raw NAND management.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Artem Bityutskiy <dedekind1@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>
    [Fixed conflict]
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mtd/ubi/ubi-media.h
+++ drivers/mtd/ubi/ubi-media.h
@@ -1,24 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
 /*
- * Copyright (c) International Business Machines Corp., 2006
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU General Public License for more details.
- *
+ * Copyright (C) International Business Machines Corp., 2006
  * Authors: Artem Bityutskiy (Битюцкий Артём)
  *          Thomas Gleixner
  *          Frank Haverkamp
  *          Oliver Lohmann
  *          Andreas Arnez
- */
-
-/*
+ *
  * This file defines the layout of UBI headers and all the other UBI on-flash
  * data structures.
  */

39ff31c0dc010b95db9288ec4ccd5433e1215e9b
Marcin Niestroj <m.niestroj@grinn-global.com>
2018-09-03 13:24:01 +0200

    crypto: caam - clean-up in caam_init_rng()
    Pick commit f366af462aef1dcaeab0f68b031e5c4c4eb860e1 from Linux
    upstream.
    
        crypto: caam - clean-up in caam_init_rng()
    
        Clean up the code, as indicated by Coccinelle.
    
        Cc: Julia Lawall <julia.lawall@lip6.fr>
        Signed-off-by: Horia Geantă <horia.geanta@nxp.com>
        Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/crypto/caam/caamrng.c
+++ drivers/crypto/caam/caamrng.c
@@ -242,11 +242,7 @@ static int caam_init_rng(struct caam_rng_ctx *ctx, struct device_d *jrdev)
 	if (err)
 		return err;
 
-	err = caam_init_buf(ctx, 1);
-	if (err)
-		return err;
-
-	return 0;
+	return caam_init_buf(ctx, 1);
 }
 
 int caam_rng_probe(struct device_d *dev, struct device_d *jrdev)

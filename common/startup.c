barebox/barebox

commit b234a6da331fa9ec656f2d278adafdeea45f9073

environment: Do not use environment when overlapping with other partitions
Environment partitions are usually specified with their hardcoded offset
and size, either in the device tree or the board file. These partitions
potentially overlap with other partitions read from the partition table.
Overlapping partitions for sure have bad effects. Be more friendly to our
users and warn them when such a situation occurs and stop using that
partition for storing the environment.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/startup.c
@@ -73,16 +73,66 @@ fs_initcall(mount_root);
 #endif
 
 #ifdef CONFIG_ENV_HANDLING
+static int check_overlap(const char *path)
+{
+	struct cdev *cenv, *cdisk, *cpart;
+	const char *name;
+
+	name = devpath_to_name(path);
+
+	if (name == path)
+		/*
+		 * no /dev/ in front, so *path is some file. No need to
+		 * check further.
+		 */
+		return 0;
+
+	cenv = cdev_by_name(name);
+	if (!cenv)
+		return -EINVAL;
+
+	cdisk = cenv->master;
+
+	if (!cdisk)
+		return 0;
+
+	list_for_each_entry(cpart, &cdisk->partitions, partition_entry) {
+		if (cpart == cenv)
+			continue;
+
+		if (lregion_overlap(cpart->offset, cpart->size,
+				    cenv->offset, cenv->size))
+			goto conflict;
+	}
+
+	return 0;
+
+conflict:
+	pr_err("Environment partition (0x%08llx-0x%08llx) "
+		"overlaps with partition %s (0x%08llx-0x%08llx), not using it\n",
+		cenv->offset, cenv->offset + cenv->offset + cenv->size - 1,
+		cpart->name,
+		cpart->offset, cpart->offset + cpart->offset + cpart->size - 1);
+
+	return -EINVAL;
+}
+
 static int load_environment(void)
 {
 	const char *default_environment_path;
+	int ret;
 
 	default_environment_path = default_environment_path_get();
 
 	if (IS_ENABLED(CONFIG_DEFAULT_ENVIRONMENT))
 		defaultenv_load("/env", 0);
 
-	envfs_load(default_environment_path, "/env", 0);
+	ret = check_overlap(default_environment_path);
+	if (ret)
+		default_environment_path_set(NULL);
+	else
+		envfs_load(default_environment_path, "/env", 0);
+
 	nvvar_load();
 
 	return 0;

include/common.h
@@ -157,4 +157,14 @@ static inline bool region_overlap(unsigned long starta, unsigned long lena,
 	return 1;
 }
 
+static inline bool lregion_overlap(loff_t starta, loff_t lena,
+				   loff_t startb, loff_t lenb)
+{
+	if (starta + lena <= startb)
+		return 0;
+	if (startb + lenb <= starta)
+		return 0;
+	return 1;
+}
+
 #endif	/* __COMMON_H_ */

commit 1106af35ee9eaded222f9eebf41dde3318d42158

environment: Fix overlapping partitions error message
Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
Fixes: b234a6da331f ("environment: Do not use environment when
                      overlapping with other partitions")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/startup.c
@@ -110,9 +110,9 @@ static int check_overlap(const char *path)
 conflict:
 	pr_err("Environment partition (0x%08llx-0x%08llx) "
 		"overlaps with partition %s (0x%08llx-0x%08llx), not using it\n",
-		cenv->offset, cenv->offset + cenv->offset + cenv->size - 1,
+		cenv->offset, cenv->offset + cenv->size - 1,
 		cpart->name,
-		cpart->offset, cpart->offset + cpart->offset + cpart->size - 1);
+		cpart->offset, cpart->offset + cpart->size - 1);
 
 	return -EINVAL;
 }
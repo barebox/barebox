1649e1e874b60fa5fcbc66de131d55b8476c7d6c
Sascha Hauer <s.hauer@pengutronix.de>
2018-10-18 08:25:29 +0200

    dma: Add missing re-inclusion protector
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/dma-dir.h
+++ include/dma-dir.h
@@ -1,6 +1,11 @@
+#ifndef __DMA_DIR_H
+#define __DMA_DIR_H
+
 enum dma_data_direction {
 	DMA_BIDIRECTIONAL = 0,
 	DMA_TO_DEVICE = 1,
 	DMA_FROM_DEVICE = 2,
 	DMA_NONE = 3,
 };
+
+#endif /* __DMA_DIR_H */

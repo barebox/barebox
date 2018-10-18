ca73d78df079fff40f08f7c3b8ee729c26dfc7d6
Sascha Hauer <s.hauer@pengutronix.de>
2018-10-18 08:50:07 +0200

    ARM: unwind: Provide prototypes for exported functions
    __aeabi_unwind_cpp_prx are provided to make the linker happy. To fix
    warnings about missing prototypes just provide a prototype in the same
    file.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/lib32/unwind.c
+++ arch/arm/lib32/unwind.c
@@ -4,6 +4,10 @@
 #include <asm/unwind.h>
 #include <asm/sections.h>
 
+void __aeabi_unwind_cpp_pr0(void);
+void __aeabi_unwind_cpp_pr1(void);
+void __aeabi_unwind_cpp_pr2(void);
+
 /* Dummy functions to avoid linker complaints */
 void __aeabi_unwind_cpp_pr0(void)
 {

barebox/barebox

commit 8ca7053b3ffd12af20c2eff49082b6483b81d431

commands: mount: Make use of devpath_to_name()
Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

commands/mount.c
@@ -76,10 +76,7 @@ static int do_mount(int argc, char *argv[])
 		struct cdev *cdev;
 		const char *path;
 
-		if (!strncmp(devstr, "/dev/", 5))
-			devstr += 5;
-
-		device_detect_by_name(devstr);
+		device_detect_by_name(devpath_to_name(devstr));
 
 		cdev = cdev_by_name(devstr);
 		if (!cdev)

commit 1eb65685357a3c0df0b3965d4ce1d6c54c306c56

mount command: Fix "mount /dev/<devname>"
We used to put the device name without the "/dev/" component into
device_detect_by_name() and cdev_by_name(). Since 8ca7053b3ff we only
put the device name into device_detect_by_name(), but cdev_by_name()
still gets the full path. The result is that a "mount /dev/<devname>" no
longer works. Fix this.

Fixes: 8ca7053b3ff ("commands: mount: Make use of devpath_to_name()")

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
Cc: Andrey Smirnov <andrew.smirnov@gmail.com>

commands/mount.c
@@ -76,7 +76,9 @@ static int do_mount(int argc, char *argv[])
 		struct cdev *cdev;
 		const char *path;
 
-		device_detect_by_name(devpath_to_name(devstr));
+		devstr = devpath_to_name(devstr);
+
+		device_detect_by_name(devstr);
 
 		cdev = cdev_by_name(devstr);
 		if (!cdev)
9a36a66cb7216165bcb5155c9e2b23c1eb91a90c
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-12-16 21:19:18 -0800

    PCI: Add SPDX GPL-2.0 to replace GPL v2 boilerplate
    Port of a Linux commit 8cfab3cf63cfe5a53e2e566b3b86b30c187edf3a
    
      Add SPDX GPL-2.0 to all PCI files that specified the GPL version 2 license.
    
      Remove the boilerplate GPL version 2 language, relying on the assertion in
      b24413180f56 ("License cleanup: add SPDX GPL-2.0 license identifier to
      files with no license") that the SPDX identifier may be used instead of the
      full boilerplate text.
    
      Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
      Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/pci/pci-mvebu.h
+++ drivers/pci/pci-mvebu.h
@@ -1,11 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * PCIe include for Marvell MVEBU SoCs
  *
  * Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
- *
- * This file is licensed under the terms of the GNU General Public
- * License version 2.  This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 
 #ifndef __MVEBU_PCI_H

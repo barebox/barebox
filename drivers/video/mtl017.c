b02a0ed4ebc606b504898bce25c6531870d2aa0c
Sam Ravnborg <sam@ravnborg.org>
2018-12-31 09:07:25 +0100

    video: mtl017: fix driver name variable
    It look like a copy-paste bug that the twl_driver name
    was used.
    Rename to the more sensible mtl_driver
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/video/mtl017.c
+++ drivers/video/mtl017.c
@@ -268,8 +268,8 @@ static int mtl017_probe(struct device_d *dev)
 	return 0;
 }
 
-static struct driver_d twl_driver = {
+static struct driver_d mtl_driver = {
 	.name  = "mtl017",
 	.probe = mtl017_probe,
 };
-device_i2c_driver(twl_driver);
+device_i2c_driver(mtl_driver);

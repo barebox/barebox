a543300e5122ac079b3f44228d0e9a66fbaea447
Sascha Hauer <s.hauer@pengutronix.de>
2019-05-03 13:10:30 +0200

    ARM: Layerscape: TQMLS1046a: Fix pinmux setup for i2c4
    With this the I2C mux on i2c4 works properly.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/tqmls1046a/board.c
+++ arch/arm/boards/tqmls1046a/board.c
@@ -7,6 +7,7 @@
 #include <linux/sizes.h>
 #include <linux/clk.h>
 #include <linux/clkdev.h>
+#include <soc/fsl/immap_lsch2.h>
 
 static int tqmls1046a_mem_init(void)
 {
@@ -21,10 +22,15 @@ mem_initcall(tqmls1046a_mem_init);
 
 static int tqmls1046a_postcore_init(void)
 {
+	struct ccsr_scfg *scfg = IOMEM(LSCH2_SCFG_ADDR);
+
 	if (!of_machine_is_compatible("tqc,tqmls1046a"))
 		return 0;
 
 	defaultenv_append_directory(defaultenv_tqmls1046a);
+
+	/* Configure iomux for i2c4 */
+	out_be32(&scfg->rcwpmuxcr0, 0x3300);
 
 	return 0;
 }

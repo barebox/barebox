d3af2e6b8ece772fc0e7bd8482f7ff485e3c039b
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-05-27 11:57:35 +0200

    scripts: compiler.h: add endianness helpers for macOS
    These are taken from Mathias Panzenböck's portable_endian.h[1]
    which he had released it into the public domain.
    
    [1]: https://gist.github.com/panzi/6856583
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- scripts/compiler.h
+++ scripts/compiler.h
@@ -42,7 +42,23 @@
 # include <endian.h>
 # include <byteswap.h>
 #elif defined(__MACH__)
-# include <machine/endian.h>
+# ifdef __APPLE__
+#  include <libkern/OSByteOrder.h>
+#  define htobe16(x) OSSwapHostToBigInt16(x)
+#  define htole16(x) OSSwapHostToLittleInt16(x)
+#  define be16toh(x) OSSwapBigToHostInt16(x)
+#  define le16toh(x) OSSwapLittleToHostInt16(x)
+#  define htobe32(x) OSSwapHostToBigInt32(x)
+#  define htole32(x) OSSwapHostToLittleInt32(x)
+#  define be32toh(x) OSSwapBigToHostInt32(x)
+#  define le32toh(x) OSSwapLittleToHostInt32(x)
+#  define htobe64(x) OSSwapHostToBigInt64(x)
+#  define htole64(x) OSSwapHostToLittleInt64(x)
+#  define be64toh(x) OSSwapBigToHostInt64(x)
+#  define le64toh(x) OSSwapLittleToHostInt64(x)
+# else /* non apple __MACH__ */
+#  include <machine/endian.h>
+# endif /* __APPLE__ */
 # define __BYTE_ORDER    BYTE_ORDER
 # define __LITTLE_ENDIAN LITTLE_ENDIAN
 # define __BIG_ENDIAN    BIG_ENDIAN

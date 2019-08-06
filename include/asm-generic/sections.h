3c857568f1cc6805cd24200ee7598ecbf35d2632
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2019-08-06 07:10:57 +0200

    sections: fix macro for barebox_pbl_size
    On ARM64 this did not contain the expected barebox_pbl size. Rework this
    to be a substraction between __piggydata_start and __image_start, which
    should cover the whole PBL.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/asm-generic/sections.h
+++ include/asm-generic/sections.h
@@ -8,12 +8,12 @@ extern char __bare_init_start[], __bare_init_end[];
 extern char _end[];
 extern char __image_start[];
 extern char __image_end[];
+extern char __piggydata_start[];
 extern void *_barebox_image_size;
 extern void *_barebox_bare_init_size;
 extern void *_barebox_pbl_size;
 
 #define barebox_image_size	(__image_end - __image_start)
 #define barebox_bare_init_size	(unsigned int)&_barebox_bare_init_size
-#define barebox_pbl_size	(unsigned int)&_barebox_pbl_size
-
+#define barebox_pbl_size	(__piggydata_start - __image_start)
 #endif /* _ASM_GENERIC_SECTIONS_H_ */

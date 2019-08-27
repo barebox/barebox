e098835f87dc56f6fa4399d7000af8e1f413bf1d
Ahmad Fatoum <ahmad@a3f.at>
2019-08-27 17:09:16 +0200

    sandbox: use sanitizer unwind for dump_stack if available
    The sanitize unwind looks pretty and is just a function call away if we
    are compiling with KASAN or UBSAN. Use it if available.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/sandbox/lib/unwind.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2019 Ahmad Fatoum
+
+#include <common.h>
+
+void __sanitizer_print_stack_trace(void);
+
+void dump_stack(void)
+{
+		__sanitizer_print_stack_trace();
+}

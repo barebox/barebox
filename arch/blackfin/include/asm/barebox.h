6fa0642f6c7b897ac639cf3cd596f6c317147e1c
Ahmad Fatoum <ahmad@a3f.at>
2019-08-27 17:09:11 +0200

    blackfin: delete unused <asm/barebox.h> definitions
    The file currently defines struct bd_info, bd_t, bi_env_data and bi_env_crc,
    no of which is used anywhere. Delete them in preparation for getting rid
    of <asm/barebox.h> altogether.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/blackfin/include/asm/barebox.h
+++ arch/blackfin/include/asm/barebox.h
@@ -1,6 +1,4 @@
 /*
- * barebox - barebox.h Structure declarations for board specific data
- *
  * Copyright (c) 2005 blackfin.uclinux.org
  *
  * (C) Copyright 2000-2004
@@ -23,21 +21,5 @@
 
 #ifndef _BAREBOX_H_
 #define _BAREBOX_H_	1
-
-typedef struct bd_info {
-	int bi_baudrate;		/* serial console baudrate */
-	unsigned long bi_ip_addr;	/* IP Address */
-	unsigned char bi_enetaddr[6];	/* Ethernet adress */
-	unsigned long bi_arch_number;	/* unique id for this board */
-	unsigned long bi_boot_params;	/* where this board expects params */
-	unsigned long bi_memstart;	/* start of DRAM memory */
-	unsigned long bi_memsize;	/* size  of DRAM memory in bytes */
-	unsigned long bi_flashstart;	/* start of FLASH memory */
-	unsigned long bi_flashsize;	/* size  of FLASH memory */
-	unsigned long bi_flashoffset;	/* reserved area for startup monitor */
-} bd_t;
-
-#define bi_env_data bi_env->data
-#define bi_env_crc  bi_env->crc
 
 #endif	/* _BAREBOX_H_ */

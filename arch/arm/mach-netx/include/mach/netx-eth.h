687dbeca2cc33dcd296c4b8aa3f2c59703f16ee3
Sascha Hauer <s.hauer@pengutronix.de>
2019-09-11 09:42:36 +0200

    ARM: NETX: remove architecture
    NETX support has been removed from the Kernel, so there's no point in
    supporting it in barebox any longer. Remove the architecture.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-netx/include/mach/netx-eth.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ASM_ARCH_NETX_ETH_H
-#define _ASM_ARCH_NETX_ETH_H
-
-struct netx_eth_platform_data {
-	int xcno;
-};
-
-#endif /* _ASM_ARCH_NETX_ETH_H */
-
--- arch/arm/mach-netx/include/mach/netx-xc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ASM_ARCH_NETX_XC_H
-#define __ASM_ARCH_NETX_XC_H
-
-int loadxc(int);
-
-#endif

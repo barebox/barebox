1d9cc19aa742bff1aa848c48aad58e8d1166037d
Andrey Smirnov <andrew.smirnov@gmail.com>
2019-09-13 23:11:23 -0700

    ARM: aarch64: Re-implement most of barebox_arm_entry() in assembly
    GCC9 now produces the following warning:
    
    common.h:51:2: warning: listing the stack pointer register ‘sp’ in a clobber list is deprecated [-Wdeprecated]
       51 |  __asm__ __volatile__("mov sp, %0"
          |  ^~~~~~~
    common.h:51:2: note: the value of the stack pointer after  an ‘asm’ statement must be the same as it was before the statement
    
    Stack pointer was added to clobber list in commit f9fc8254b2 ("ARM:
    Mark SP as being clobbered in arm_setup_stack()") to prevent GCC from
    generating code that would corrupt 'boarddata' pointer by trying to
    restore it from invalid stack frame.
    
    Interestingly enough, seemingly unrelated change in commit
    64d95896cf ("ARM: aarch64: compile with general-regs-only") changed
    generated code such that adding SP to clobber list became no longer
    necessary.
    
    While the above can probably be a fix by itself, it seems a better and
    more future proof approach would be to address the problem at its root
    and re-implement offending startup sequence in assembly.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/cpu/entry.c
+++ arch/arm/cpu/entry.c
@@ -24,14 +24,19 @@
  * be fine.
  */
 
-void NAKED __noreturn barebox_arm_entry(unsigned long membase,
-					  unsigned long memsize, void *boarddata)
-{
-	arm_setup_stack(arm_mem_stack_top(membase, membase + memsize));
-	arm_early_mmu_cache_invalidate();
+/*
+ * It can be hard to convince GCC to not use old stack pointer after
+ * we modify it with arm_setup_stack() on ARM64, so we implement the
+ * low level details in assembly
+ */
+void __noreturn __barebox_arm_entry(unsigned long membase,
+				    unsigned long memsize,
+				    void *boarddata,
+				    unsigned long sp);
 
-	if (IS_ENABLED(CONFIG_PBL_IMAGE))
-		barebox_pbl_start(membase, memsize, boarddata);
-	else
-		barebox_non_pbl_start(membase, memsize, boarddata);
+void NAKED __noreturn barebox_arm_entry(unsigned long membase,
+					unsigned long memsize, void *boarddata)
+{
+	__barebox_arm_entry(membase, memsize, boarddata,
+			    arm_mem_stack_top(membase, membase + memsize));
 }

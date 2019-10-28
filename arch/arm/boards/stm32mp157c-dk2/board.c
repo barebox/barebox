859697993bc6849c072d6c9ebbf908316b7d644b
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-10-28 22:39:39 +0100

    ARM: stm32mp: dk2: add barebox SD-Card update handler
    Now with the SD/MMC controller supported, lets add a bbu handler, so we
    can use it to update the second stage boot loader partition.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/stm32mp157c-dk2/board.c
+++ arch/arm/boards/stm32mp157c-dk2/board.c
@@ -4,6 +4,7 @@
 #include <init.h>
 #include <asm/memory.h>
 #include <mach/stm32.h>
+#include <mach/bbu.h>
 
 static int dk2_mem_init(void)
 {
@@ -15,3 +16,15 @@ static int dk2_mem_init(void)
 	return 0;
 }
 mem_initcall(dk2_mem_init);
+
+static int dk2_postcore_init(void)
+{
+	if (!of_machine_is_compatible("st,stm32mp157c-dk2"))
+		return 0;
+
+	stm32mp_bbu_mmc_register_handler("sd", "/dev/mmc0.ssbl",
+					 BBU_HANDLER_FLAG_DEFAULT);
+
+	return 0;
+}
+postcore_initcall(dk2_postcore_init);

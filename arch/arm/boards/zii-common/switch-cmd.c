barebox/barebox

commit 493e2ee38f71bad6598ae2522898db5bdb69a06c

ARM: zii-common: add support for switch reset on RDU1
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/boards/zii-common/switch-cmd.c
@@ -11,18 +11,15 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#include <common.h>
 #include <command.h>
+#include <common.h>
 #include <i2c/i2c.h>
+#include <linux/mfd/rave-sp.h>
 
-static int do_rave_switch_reset(int argc, char *argv[])
+static int do_rdu2_switch_reset(void)
 {
 	struct i2c_client client;
 	u8 reg;
-
-	if (!of_machine_is_compatible("zii,imx6q-zii-rdu2") &&
-	    !of_machine_is_compatible("zii,imx6qp-zii-rdu2"))
-		return -ENODEV;
 
 	client.adapter = i2c_get_adapter(1);
 	if (!client.adapter)
@@ -42,8 +39,35 @@ static int do_rave_switch_reset(int argc, char *argv[])
 	return 0;
 }
 
+static int do_rdu1_switch_reset(void)
+{
+	struct device_d *sp_dev = get_device_by_name("sp");
+	struct rave_sp *sp = sp_dev->priv;
+	u8 cmd[] = {
+		[0] = RAVE_SP_CMD_RESET_ETH_SWITCH,
+		[1] = 0
+	};
+
+	if (IS_ENABLED(CONFIG_RAVE_SP_CORE))
+		return rave_sp_exec(sp, cmd, sizeof(cmd), NULL, 0);
+	else
+		return -ENODEV;
+}
+
+static int do_rave_switch_reset(int argc, char *argv[])
+{
+	if (of_machine_is_compatible("zii,imx6q-zii-rdu2") ||
+	    of_machine_is_compatible("zii,imx6qp-zii-rdu2"))
+		return do_rdu2_switch_reset();
+
+	if (of_machine_is_compatible("zii,imx51-rdu1"))
+		return do_rdu1_switch_reset();
+
+	return -ENODEV;
+}
+
 BAREBOX_CMD_START(rave_reset_switch)
 	.cmd		= do_rave_switch_reset,
-	BAREBOX_CMD_DESC("reset ethernet switch on RDU2")
+	BAREBOX_CMD_DESC("reset ethernet switch on RDU")
 	BAREBOX_CMD_GROUP(CMD_GRP_HWMANIP)
 BAREBOX_CMD_END

arch/arm/mach-imx/Kconfig
@@ -439,6 +439,7 @@ config MACH_ZII_RDU1
 	select ARCH_IMX51
 	select MACH_FREESCALE_MX51_PDK_POWER
 	select CRC8
+	select MACH_ZII_COMMON
 
 config MACH_ZII_RDU2
 	bool "ZII i.MX6Q(+) RDU2"

include/linux/mfd/rave-sp.h
@@ -25,6 +25,7 @@ enum rave_sp_command {
 	RAVE_SP_CMD_SET_BACKLIGHT		= 0xA6,
 	RAVE_SP_CMD_RESET			= 0xA7,
 	RAVE_SP_CMD_RESET_REASON		= 0xA8,
+	RAVE_SP_CMD_RESET_ETH_SWITCH		= 0xAF,
 
 	RAVE_SP_CMD_JUMP_TO_BOOTLOADER		= 0xB0,
 	RAVE_SP_CMD_BOOTLOADER			= 0xB1,

commit a9e96c1a5cef5fe975e898bff04867b7a697fb70

ARM: zii-commmon: Fix NULL pointer dereference in do_rdu1_switch_reset()
We need to go to parent's private data to get a pointer to struct
rave_sp. Fix that.

Fixes: 493e2ee38f ("ARM: zii-common: add support for switch reset on RDU1")
Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/boards/zii-common/switch-cmd.c
@@ -42,7 +42,7 @@ static int do_rdu2_switch_reset(void)
 static int do_rdu1_switch_reset(void)
 {
 	struct device_d *sp_dev = get_device_by_name("sp");
-	struct rave_sp *sp = sp_dev->priv;
+	struct rave_sp *sp = sp_dev->parent->priv;
 	u8 cmd[] = {
 		[0] = RAVE_SP_CMD_RESET_ETH_SWITCH,
 		[1] = 0
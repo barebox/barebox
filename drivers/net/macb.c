6a6278eb98783f7534e26d6be6045b4adc90c0e6
Lucas Stach <dev@lynxeye.de>
2019-11-09 15:28:27 +0100

    net: macb: add Zynq compatible
    Add compatible for the GEM on the Zynq 7000 device.
    
    Signed-off-by: Lucas Stach <dev@lynxeye.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/net/macb.c
+++ drivers/net/macb.c
@@ -792,6 +792,7 @@ static const struct of_device_id macb_dt_ids[] = {
 	{ .compatible = "cdns,at91sam9260-macb",},
 	{ .compatible = "atmel,sama5d2-gem",},
 	{ .compatible = "atmel,sama5d3-gem",},
+	{ .compatible = "cdns,zynq-gem",},
 	{ .compatible = "cdns,zynqmp-gem",},
 	{ /* sentinel */ }
 };

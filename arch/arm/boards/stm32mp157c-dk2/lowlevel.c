bffb9deb9030f26cb929c29a7b3acc2fba6f3f83
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-11-12 10:19:48 +0100

    ARM: stm32mp157c-dk2: add optional DEBUG_LL print to entry point
    The TF-A sets up pin muxing and clocking for the UART4 which is the UART
    suggested by ST for use as debug console.
    
    Eventually, we might want to do this ourselves to be sure, but for now
    lets just stick in a putc_ll('>'), so user selecting DEBUG_LL can see
    that barebox started.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/stm32mp157c-dk2/lowlevel.c
+++ arch/arm/boards/stm32mp157c-dk2/lowlevel.c
@@ -7,11 +7,20 @@
 
 extern char __dtb_z_stm32mp157c_dk2_start[];
 
+static void setup_uart(void)
+{
+	/* first stage has set up the UART, so nothing to do here */
+	putc_ll('>');
+}
+
 ENTRY_FUNCTION(start_stm32mp157c_dk2, r0, r1, r2)
 {
 	void *fdt;
 
 	arm_cpu_lowlevel_init();
+
+	if (IS_ENABLED(CONFIG_DEBUG_LL))
+		setup_uart();
 
 	fdt = __dtb_z_stm32mp157c_dk2_start + get_runtime_offset();
 

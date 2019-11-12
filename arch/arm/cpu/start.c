barebox/barebox

commit 19c24e2f0121742095698823034b525461aca1d4

ARM: start: Allow to pass machine type as boarddata
Allow to pass a machine type number as directly as boarddata. This makes
it easy for non device tree boards to pass a machine type and to
identify themselves during runtime.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/cpu/start.c
@@ -193,7 +193,19 @@ __noreturn void barebox_non_pbl_start(unsigned long membase,
 		uint32_t totalsize = 0;
 		const char *name;
 
-		if (blob_is_fdt(boarddata)) {
+		if ((unsigned long)boarddata < 8192) {
+			struct barebox_arm_boarddata *bd;
+			uint32_t machine_type = (unsigned long)boarddata;
+			unsigned long mem = arm_mem_boarddata(membase, endmem,
+							      sizeof(*bd));
+			pr_debug("found machine type %d in boarddata\n",
+				 machine_type);
+			bd = barebox_boarddata = (void *)mem;
+			barebox_boarddata_size = sizeof(*bd);
+			bd->magic = BAREBOX_ARM_BOARDDATA_MAGIC;
+			bd->machine = machine_type;
+			malloc_end = mem;
+		} else if (blob_is_fdt(boarddata)) {
 			totalsize = get_unaligned_be32(boarddata + 4);
 			name = "DTB";
 		} else if (blob_is_compressed_fdt(boarddata)) {

commit 390bc7834ffcee9fe9b94babf0f313bcc4d3b42d

ARM: start: check for machine type last
A valid DTB may reside low in the Barebox binary address map. If this
binary is started at a very low address, we might mistake the pointer
for a machine type. Make sure to check for all other possibilities first
before interpreting the boarddata as a raw machine type.

Fixes: 19c24e2f0121 (ARM: start: Allow to pass machine type as boarddata)
Signed-off-by: Lucas Stach <dev@lynxeye.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/cpu/start.c
@@ -193,7 +193,17 @@ __noreturn void barebox_non_pbl_start(unsigned long membase,
 		uint32_t totalsize = 0;
 		const char *name;
 
-		if ((unsigned long)boarddata < 8192) {
+		if (blob_is_fdt(boarddata)) {
+			totalsize = get_unaligned_be32(boarddata + 4);
+			name = "DTB";
+		} else if (blob_is_compressed_fdt(boarddata)) {
+			struct barebox_arm_boarddata_compressed_dtb *bd = boarddata;
+			totalsize = bd->datalen + sizeof(*bd);
+			name = "Compressed DTB";
+		} else if (blob_is_arm_boarddata(boarddata)) {
+			totalsize = sizeof(struct barebox_arm_boarddata);
+			name = "machine type";
+		} else if ((unsigned long)boarddata < 8192) {
 			struct barebox_arm_boarddata *bd;
 			uint32_t machine_type = (unsigned long)boarddata;
 			unsigned long mem = arm_mem_boarddata(membase, endmem,
@@ -205,16 +215,6 @@ __noreturn void barebox_non_pbl_start(unsigned long membase,
 			bd->magic = BAREBOX_ARM_BOARDDATA_MAGIC;
 			bd->machine = machine_type;
 			malloc_end = mem;
-		} else if (blob_is_fdt(boarddata)) {
-			totalsize = get_unaligned_be32(boarddata + 4);
-			name = "DTB";
-		} else if (blob_is_compressed_fdt(boarddata)) {
-			struct barebox_arm_boarddata_compressed_dtb *bd = boarddata;
-			totalsize = bd->datalen + sizeof(*bd);
-			name = "Compressed DTB";
-		} else if (blob_is_arm_boarddata(boarddata)) {
-			totalsize = sizeof(struct barebox_arm_boarddata);
-			name = "machine type";
 		}
 
 		if (totalsize) {
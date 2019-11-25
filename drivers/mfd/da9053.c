barebox/barebox

commit 06928c7d17586f3929de12fb774fa7ab3c555bfb

mfd: da9053: use new reset_source_set_device()
Make use of the just introduced reset_source_set_device() to save some
boilerplate.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mfd/da9053.c
@@ -214,7 +214,6 @@ static int da9053_set_timeout(struct watchdog *wd, unsigned timeout)
 
 static void da9053_detect_reset_source(struct da9053_priv *da9053)
 {
-	unsigned int priority;
 	enum reset_src_type type;
 	int ret;
 	u8 val;
@@ -236,9 +235,7 @@ static void da9053_detect_reset_source(struct da9053_priv *da9053)
 	else
 		return;
 
-	priority = of_get_reset_source_priority(da9053->dev->device_node);
-
-	reset_source_set_priority(type, priority);
+	reset_source_set_device(da9053->dev, ltype);
 
 	ret = da9053_reg_write(da9053, DA9053_FAULTLOG_REG, val);
 	if (ret < 0)

commit c7ec778bbe2527254c2a05f990bb632c72d94563

mfd: da9053: fix typo in variable name
Fixes: 06928c7d1758 (mfd: da9053: use new reset_source_set_device)
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mfd/da9053.c
@@ -235,7 +235,7 @@ static void da9053_detect_reset_source(struct da9053_priv *da9053)
 	else
 		return;
 
-	reset_source_set_device(da9053->dev, ltype);
+	reset_source_set_device(da9053->dev, type);
 
 	ret = da9053_reg_write(da9053, DA9053_FAULTLOG_REG, val);
 	if (ret < 0)
3755c616cc12aa98352762e5809616e71df056d9
Andrey Smirnov <andrew.smirnov@gmail.com>
2019-12-02 07:19:46 -0800

    mci: imx-esdhc-pbl: Don't setup DMA registers
    PBL code is PIO only, so setting up DMA related registers shouldn't be
    necessary.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mci/imx-esdhc-pbl.c
+++ drivers/mci/imx-esdhc-pbl.c
@@ -139,13 +139,7 @@ esdhc_send_cmd(struct esdhc *esdhc, struct mci_cmd *cmd, struct mci_data *data)
 	__udelay(1);
 
 	if (data) {
-		unsigned long dest = (unsigned long)data->dest;
-
-		if (dest > 0xffffffff)
-			return -EINVAL;
-
 		/* Set up for a data transfer if we have one */
-		esdhc_write32(esdhc, SDHCI_DMA_ADDRESS, (u32)dest);
 		esdhc_write32(esdhc, SDHCI_BLOCK_SIZE__BLOCK_COUNT, data->blocks << 16 | SECTOR_SIZE);
 	}
 

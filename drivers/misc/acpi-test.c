c86507a63c6139f70e97d94ffcded88796b87b59
Ahmad Fatoum <ahmad@a3f.at>
2019-12-04 15:07:21 +0100

    misc: add ACPI test driver
    We have a bus driver for ACPI, but no drivers yet. Add a simple (rather
    useless) driver that exercises the API, which future drivers can be
    based on. As soon as we have proper ACPI drivers, this driver could be
    removed again.
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ drivers/misc/acpi-test.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 Ahmad Fatoum
+ */
+
+#include <common.h>
+#include <init.h>
+#include <acpi.h>
+
+static const char *profiles[] = {
+	"Unspecified",
+	"Desktop",
+	"Mobile",
+	"Workstation",
+	"Enterprise Server",
+	"SOHO Server",
+	"Applicance PC",
+	"Performance Server",
+	"Tablet",
+};
+
+static int acpi_test_probe(struct device_d *dev)
+{
+	const char *profile = "reserved";
+	u8 *sdt;
+	u8 profileno;
+
+	dev_dbg(dev, "driver initializing...\n");
+
+	sdt = (u8 __force *)dev_request_mem_region_by_name(dev, "SDT");
+	if (IS_ERR(sdt)) {
+		dev_err(dev, "no SDT resource available: %s\n", strerrorp(sdt));
+		return PTR_ERR(sdt);
+	}
+
+	dev_dbg(dev, "SDT is at 0x%p\n", sdt);
+
+	profileno = sdt[45];
+
+	if (profileno < ARRAY_SIZE(profiles))
+		profile = profiles[profileno];
+
+	dev_info(dev, "PM profile is for '%s'\n", profile);
+
+	return 0;
+}
+
+static void acpi_test_remove(struct device_d *dev)
+{
+	dev_info(dev, "FADT driver removed\n");
+}
+
+static struct acpi_driver acpi_test_driver = {
+	.signature = "FACP",
+	.driver = {
+		.name = "acpi-test",
+		.probe = acpi_test_probe,
+		.remove = acpi_test_remove,
+	}
+};
+device_acpi_driver(acpi_test_driver);

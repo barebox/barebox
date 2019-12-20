barebox/barebox

commit c5d38e92010174f58d733274430ee91f340553b5

lds: Add and use RO_DATA_SECTION macro
We have many different pointer arrays which we put into linker sections
and each time there's one added we have to adjust all linker scripts.
This adds a common RO_DATA_SECTION define and uses it for all
architectures. This makes it easier to add a new linker array.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/asm-generic/barebox.lds.h
@@ -98,6 +98,17 @@
 #define BAREBOX_PCI_FIXUP
 #endif
 
+#define RO_DATA_SECTION				\
+	BAREBOX_INITCALLS			\
+	BAREBOX_EXITCALLS			\
+	BAREBOX_CMDS				\
+	BAREBOX_RATP_CMDS			\
+	BAREBOX_SYMS				\
+	BAREBOX_MAGICVARS			\
+	BAREBOX_CLK_TABLE			\
+	BAREBOX_DTB				\
+	BAREBOX_PCI_FIXUP
+
 #if defined(CONFIG_ARCH_BAREBOX_MAX_BARE_INIT_SIZE) && \
 CONFIG_ARCH_BAREBOX_MAX_BARE_INIT_SIZE < CONFIG_BAREBOX_MAX_BARE_INIT_SIZE
 #define MAX_BARE_INIT_SIZE CONFIG_ARCH_BAREBOX_MAX_BARE_INIT_SIZE

arch/arm/lib32/barebox.lds.S
@@ -55,7 +55,10 @@ SECTIONS
 	BAREBOX_BARE_INIT_SIZE
 
 	. = ALIGN(4);
-	.rodata : { *(.rodata*) }
+	.rodata : {
+		*(.rodata*)
+		RO_DATA_SECTION
+	}
 
 #ifdef CONFIG_ARM_UNWIND
 	/*
@@ -82,15 +85,6 @@ SECTIONS
 	.barebox_imd : { BAREBOX_IMD }
 
 	. = .;
-	.barebox_cmd : { BAREBOX_CMDS }
-	.barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-	.barebox_magicvar : { BAREBOX_MAGICVARS }
-	.barebox_initcalls : { BAREBOX_INITCALLS }
-	.barebox_exitcalls : { BAREBOX_EXITCALLS }
-	__usymtab : { BAREBOX_SYMS }
-	.pci_fixup : { BAREBOX_PCI_FIXUP }
-	.oftables : { BAREBOX_CLK_TABLE }
-	.dtb : { BAREBOX_DTB }
 
 	.rel_dyn_start : { *(.__rel_dyn_start) }
 	.rel.dyn : { *(.rel*) }

arch/arm/lib64/barebox.lds.S
@@ -53,7 +53,10 @@ SECTIONS
 	BAREBOX_BARE_INIT_SIZE
 
 	. = ALIGN(4);
-	.rodata : { *(.rodata*) }
+	.rodata : {
+		*(.rodata*)
+		RO_DATA_SECTION
+	}
 
 	_etext = .;			/* End of text and rodata section */
 	_sdata = .;
@@ -62,17 +65,6 @@ SECTIONS
 	.data : { *(.data*) }
 
 	.barebox_imd : { BAREBOX_IMD }
-
-	. = .;
-	.barebox_cmd : { BAREBOX_CMDS }
-	.barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-	.barebox_magicvar : { BAREBOX_MAGICVARS }
-	.barebox_initcalls : { BAREBOX_INITCALLS }
-	.barebox_exitcalls : { BAREBOX_EXITCALLS }
-	__usymtab : { BAREBOX_SYMS }
-	.pci_fixup : { BAREBOX_PCI_FIXUP }
-	.oftables : { BAREBOX_CLK_TABLE }
-	.dtb : { BAREBOX_DTB }
 
 	.rel_dyn_start : { *(.__rel_dyn_start) }
 	.rela.dyn : { *(.rela*) }

arch/mips/lib/barebox.lds.S
@@ -30,7 +30,10 @@ SECTIONS
 	PRE_IMAGE
 
 	. = ALIGN(4);
-	.rodata : { *(.rodata*) }
+	.rodata : {
+		*(.rodata*)
+		RO_DATA_SECTION
+	}
 
 	_etext = .;			/* End of text and rodata section */
 	_sdata = .;
@@ -39,17 +42,6 @@ SECTIONS
 	.data : { *(.data*) }
 
 	.barebox_imd : { BAREBOX_IMD }
-
-	. = .;
-	.barebox_cmd : { BAREBOX_CMDS }
-	.barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-	.barebox_magicvar : { BAREBOX_MAGICVARS }
-	.barebox_initcalls : { BAREBOX_INITCALLS }
-	.barebox_exitcalls : { BAREBOX_EXITCALLS }
-	__usymtab : { BAREBOX_SYMS }
-	.pci_fixup : { BAREBOX_PCI_FIXUP }
-	.oftables : { BAREBOX_CLK_TABLE }
-	.dtb : { BAREBOX_DTB }
 
 	_edata = .;
 	.image_end : { *(.__image_end) }

arch/nios2/cpu/barebox.lds.S
@@ -48,15 +48,11 @@ SECTIONS
 	BAREBOX_BARE_INIT_SIZE
 
 	. = ALIGN(4);
-	.rodata : { *(.rodata) }
 
-	. = .;
-	.barebox_cmd : { BAREBOX_CMDS }
-	.barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-	.barebox_magicvar : { BAREBOX_MAGICVARS }
-	.barebox_initcalls : { BAREBOX_INITCALLS }
-	.barebox_exitcalls : { BAREBOX_EXITCALLS }
-	__usymtab : { BAREBOX_SYMS }
+	.rodata : {
+		*(.rodata*)
+		RO_DATA_SECTION
+	}
 
 	_etext = .; /* End of text and rodata section */
 

arch/openrisc/cpu/barebox.lds.S
@@ -49,17 +49,8 @@ SECTIONS
 		*(.rodata);
 		*(.rodata.*)
 		*(.bbenv.rodata.*)
+		RO_DATA_SECTION
 	} > ram
-
-	. = ALIGN(4);
-	. = .;
-	.barebox_cmd : { BAREBOX_CMDS } > ram
-	.barebox_ratp_cmd : { BAREBOX_RATP_CMDS } > ram
-	.barebox_magicvar : { BAREBOX_MAGICVARS } > ram
-	.barebox_initcalls : { BAREBOX_INITCALLS } > ram
-	.barebox_exitcalls : { BAREBOX_EXITCALLS } > ram
-	__usymtab : { BAREBOX_SYMS } > ram
-	.dtb : { BAREBOX_DTB } > ram
 
 	__etext = .; /* End of text and rodata section */
 

arch/ppc/boards/pcm030/barebox.lds.S
@@ -38,6 +38,7 @@ SECTIONS
     *(.rodata*)
     *(.rodata1*)
     *(.rodata.str1.4)
+    RO_DATA_SECTION
   }
 
   /* Read-only sections, merged into text segment: */
@@ -98,14 +99,6 @@ SECTIONS
   }
   _edata  =  .;
   PROVIDE (edata = .);
-
-  . = .;
-  .barebox_cmd : { BAREBOX_CMDS }
-  .barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-  .barebox_magicvar : { BAREBOX_MAGICVARS }
-  .barebox_initcalls : { BAREBOX_INITCALLS }
-  .barebox_exitcalls : { BAREBOX_EXITCALLS }
-  __usymtab : { BAREBOX_SYMS }
 
   __start___ex_table = .;
   __ex_table : { *(__ex_table) }

arch/ppc/mach-mpc85xx/barebox.lds.S
@@ -71,6 +71,7 @@ SECTIONS
    .rodata    :
    {
     *(SORT_BY_ALIGNMENT(SORT_BY_NAME(.rodata*)))
+    RO_DATA_SECTION
   } :text
 
   /* Read-write section, merged into data segment: */
@@ -99,13 +100,6 @@ SECTIONS
     *(.dynamic*)
     CONSTRUCTORS
   }
-
-  . = .;
-  .barebox_cmd : { BAREBOX_CMDS }
-  .barebox_ratp_cmd : { BAREBOX_RATP_CMDS }
-  .barebox_initcalls : { BAREBOX_INITCALLS }
-  .barebox_exitcalls : { BAREBOX_EXITCALLS }
-  __usymtab : { BAREBOX_SYMS }
 
   . = .;
   __start___ex_table = .;

commit d5f8bd9c503e0a17ef071eec50c537cc82a7956c

lds: align RO_DATA_SECTION members
When using the RO_DATA_SECTION macro, all the individual components
are put into the rodata section. As they now end up in the same
section there is no alignment enforced between them anymore. This
leads to unaligned start pointers when any of the components have a
unaligned size. Enforce some alignment between them.

Fixes: c5d38e920101 (lds: Add and use RO_DATA_SECTION macro)
Signed-off-by: Lucas Stach <dev@lynxeye.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/asm-generic/barebox.lds.h
@@ -16,6 +16,7 @@
 #endif
 
 #define BAREBOX_INITCALLS			\
+	STRUCT_ALIGN();				\
 	__barebox_initcalls_start = .;		\
 	KEEP(*(.initcall.0))			\
 	KEEP(*(.initcall.1))			\
@@ -35,6 +36,7 @@
 	__barebox_initcalls_end = .;
 
 #define BAREBOX_EXITCALLS			\
+	STRUCT_ALIGN();				\
 	__barebox_exitcalls_start = .;		\
 	KEEP(*(.exitcall.0))			\
 	KEEP(*(.exitcall.1))			\
@@ -46,37 +48,44 @@
 	__barebox_exitcalls_end = .;
 
 #define BAREBOX_CMDS				\
+	STRUCT_ALIGN();				\
 	__barebox_cmd_start = .;		\
 	KEEP(*(SORT_BY_NAME(.barebox_cmd*)))	\
 	__barebox_cmd_end = .;
 
 #define BAREBOX_RATP_CMDS			\
+	STRUCT_ALIGN();				\
 	__barebox_ratp_cmd_start = .;		\
 	KEEP(*(SORT_BY_NAME(.barebox_ratp_cmd*)))	\
 	__barebox_ratp_cmd_end = .;
 
 #define BAREBOX_SYMS				\
+	STRUCT_ALIGN();				\
 	__usymtab_start = .;			\
 	KEEP(*(__usymtab))			\
 	__usymtab_end = .;
 
 #define BAREBOX_MAGICVARS			\
+	STRUCT_ALIGN();				\
 	__barebox_magicvar_start = .;		\
 	KEEP(*(SORT_BY_NAME(.barebox_magicvar*)))	\
 	__barebox_magicvar_end = .;
 
 #define BAREBOX_CLK_TABLE			\
+	STRUCT_ALIGN();				\
 	__clk_of_table_start = .;		\
 	KEEP(*(.__clk_of_table));		\
 	KEEP(*(.__clk_of_table_end));		\
 	__clk_of_table_end = .;
 
 #define BAREBOX_DTB				\
+	STRUCT_ALIGN();				\
 	__dtb_start = .;			\
 	KEEP(*(.dtb.rodata.*));			\
 	__dtb_end = .;
 
 #define BAREBOX_IMD				\
+	STRUCT_ALIGN();				\
 	KEEP(*(.barebox_imd_start))		\
 	KEEP(*(.barebox_imd_1*))		\
 	*(.barebox_imd_0*)			\
@@ -84,6 +93,7 @@
 
 #ifdef CONFIG_PCI
 #define BAREBOX_PCI_FIXUP			\
+	STRUCT_ALIGN();				\
 	__start_pci_fixups_early = .;		\
 	KEEP(*(.pci_fixup_early))		\
 	__end_pci_fixups_early = .;		\
@@ -98,6 +108,7 @@
 #endif
 
 #define BAREBOX_RSA_KEYS			\
+	STRUCT_ALIGN();				\
 	__rsa_keys_start = .;			\
 	KEEP(*(.rsa_keys.rodata.*));		\
 	__rsa_keys_end = .;			\
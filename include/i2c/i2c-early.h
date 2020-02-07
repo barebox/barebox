d19adc55f607fff299fb54d29aef3396cbdf0baa
Sascha Hauer <s.hauer@pengutronix.de>
2020-02-07 16:23:26 +0100

    I2C: i.MX: Add early i2c support for i.MX8M
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/i2c/i2c-early.h
+++ include/i2c/i2c-early.h
@@ -5,6 +5,7 @@
 
 int i2c_fsl_xfer(void *ctx, struct i2c_msg *msgs, int num);
 
+void *imx8m_i2c_early_init(void __iomem *regs);
 void *ls1046_i2c_init(void __iomem *regs);
 
 #endif /* __I2C_EARLY_H */

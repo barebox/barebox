075179763a91b6fea4a4309227d73c75d760c7bd
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-04-15 11:29:16 +0200

    mci: sdhci: add Atmel SDHCI (sama5d2, sam9x60) support
    We already support Ethernet and QSPI on the SAMA5D2, but MMC was so far
    missing. Port over the at91bootstrap driver to barebox. Some parts are
    based on the U-Boot and Linux implementations.
    
    To support future use in PBL, the driver model and driver implementation
    parts are split into separate files with the latter being compilable for PBL
    as well.
    
    Registers, which are found in the common Linux sdhci.h have been added
    to the barebox sdhci.h. Registers which appear to be Atmel specific have
    been added to atmel-sdhci-common.c instead.
    
    The driver still misses some parts, which will follow later:
    
      - ADMA is unsupported, PIO only for now
      - Only the SD/MMC controller already enabled by the first stage
        bootloader is supported. Further clocking changes appear
        to be necessary to enable another
    
    Both can be retrofitted later on and don't hold us back from booting
    from MMC.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ drivers/mci/atmel-sdhci.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2020 Ahmad Fatoum, Pengutronix
+
+#ifndef ATMEL_SDHCI_H_
+#define ATMEL_SDHCI_H_
+
+#include <linux/types.h>
+#include <mci.h>
+
+#include "sdhci.h"
+
+struct at91_sdhci {
+	struct sdhci	sdhci;
+	void __iomem	*base;
+	u32		caps_max_clock;
+};
+
+int at91_sdhci_init(struct at91_sdhci *host, u32 maxclk,
+		    bool force_cd, bool cal_always_on);
+void at91_sdhci_mmio_init(struct at91_sdhci *host, void __iomem *base);
+int at91_sdhci_send_command(struct at91_sdhci *host, struct mci_cmd *sd_cmd,
+			    struct mci_data *data);
+bool at91_sdhci_is_card_inserted(struct at91_sdhci *host);
+void at91_sdhci_host_capability(struct at91_sdhci *host,
+				unsigned int *voltages);
+int at91_sdhci_set_ios(struct at91_sdhci *host, struct mci_ios *ios);
+
+#endif

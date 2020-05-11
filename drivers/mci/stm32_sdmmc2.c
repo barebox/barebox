f27f4b817737c5c3c0fe33ac629fd6aff765b1cb
Lucas Stach <l.stach@pengutronix.de>
2020-05-11 18:49:56 +0200

    mci: stm32_sdmmc2: don't retry commands
    No other MCI does retry failing commands on its own (if at all
    this should be done by the MCI core). Remove the retry, as we
    don't want to paper over board level issues.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/mci/stm32_sdmmc2.c
+++ drivers/mci/stm32_sdmmc2.c
@@ -481,11 +481,8 @@ static int stm32_sdmmc2_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
 {
 	struct stm32_sdmmc2_priv *priv = to_mci_host(mci);
 	u32 cmdat = data ? SDMMC_CMD_CMDTRANS : 0;
-	u32 data_length;
-	int ret, retry = 3;
-
-retry_cmd:
-	data_length = 0;
+	u32 data_length = 0;
+	int ret;
 
 	if (data) {
 		data_length = data->blocks * data->blocksize;
@@ -528,15 +525,6 @@ retry_cmd:
 		ret = stm32_sdmmc2_end_cmd(priv, &stop_cmd);
 
 		writel(SDMMC_ICR_STATIC_FLAGS, priv->base + SDMMC_ICR);
-	}
-
-	if (ret && retry) {
-		dev_warn(priv->dev, "%s: cmd %d failed, retrying ...\n",
-			 __func__, cmd->cmdidx);
-
-		retry--;
-
-		goto retry_cmd;
 	}
 
 	dev_dbg(priv->dev, "%s: end for CMD %d, ret = %d\n", __func__,

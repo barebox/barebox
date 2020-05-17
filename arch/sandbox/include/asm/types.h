c251736e8f94442ec3bb04d437404229900a66d7
Masahiro Yamada <masahiroy@kernel.org>
2020-05-17 21:14:39 +0900

    arch: include <asm-generic/int-ll64.h> from <asm/types.h>
    Many architectures duplicate the same fixed type definitions
    as in <asm-generic/int-ll64.h>.
    
    Include <asm-generic/int-ll64.h> from <asm/types.h>, and remove
    the duplicated code.
    
    Note:
    Linux commit 0c79a8e29b5f ("asm/types.h: Remove
    include/asm-generic/int-l64.h") unified the kernel space
    definition into int-ll64.h. Barebox also adopts int-ll64.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/sandbox/include/asm/types.h
+++ arch/sandbox/include/asm/types.h
@@ -1,6 +1,8 @@
 #ifndef __ASM_I386_TYPES_H
 #define __ASM_I386_TYPES_H
 
+#include <asm-generic/int-ll64.h>
+
 #ifdef __x86_64__
 /*
  * This is used in dlmalloc. On X86_64 we need it to be
@@ -15,45 +17,5 @@
 #define CONFIG_PHYS_ADDR_T_64BIT
 
 #endif
-
-/*
- * __xx is ok: it doesn't pollute the POSIX namespace. Use these in the
- * header files exported to user space
- */
-
-typedef __signed__ char __s8;
-typedef unsigned char __u8;
-
-typedef __signed__ short __s16;
-typedef unsigned short __u16;
-
-typedef __signed__ int __s32;
-typedef unsigned int __u32;
-
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
-typedef __signed__ long long __s64;
-typedef unsigned long long __u64;
-#endif
-
-/*
- * These aren't exported outside the kernel to avoid name space clashes
- */
-#ifdef __KERNEL__
-
-typedef signed char s8;
-typedef unsigned char u8;
-
-typedef signed short s16;
-typedef unsigned short u16;
-
-typedef signed int s32;
-typedef unsigned int u32;
-
-typedef signed long long s64;
-typedef unsigned long long u64;
-
-#include <asm/bitsperlong.h>
-
-#endif /* __KERNEL__ */
 
 #endif

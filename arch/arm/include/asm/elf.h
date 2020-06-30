4b7b82810f23599460a516df47f2008858716565
David Dgien <dgienda125@gmail.com>
2020-06-29 20:38:37 -0400

    arm: elf: Add THM relocation types
    Signed-off-by: David Dgien <dgienda125@gmail.com>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/elf.h
+++ arch/arm/include/asm/elf.h
@@ -30,6 +30,9 @@ typedef struct user_fp elf_fpregset_t;
 #define R_ARM_CALL	28
 #define R_ARM_JUMP24	29
 
+#define R_ARM_THM_CALL		10
+#define R_ARM_THM_JUMP24	30
+
 /*
  * These are used to set parameters in the core dumps.
  */

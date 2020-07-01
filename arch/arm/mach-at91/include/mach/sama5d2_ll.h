666643cb2459390dfd0ed8f9fe5565d10b32806f
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-07-01 11:11:08 +0200

    ARM: at91: extend low level PMC driver for generic clk support
    The SAMA5D2 and SAM9X60 both feature generic clocks, which we already
    support in barebox proper, but not in PBL. Add PBL support for setting
    the parent and enabling them, so we may use it to enable the SDMMC
    peripherals in first stage.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-at91/include/mach/sama5d2_ll.h
+++ arch/arm/mach-at91/include/mach/sama5d2_ll.h
@@ -8,12 +8,12 @@
 
 #include <common.h>
 
+void sama5d2_lowlevel_init(void);
+
 static inline void sama5d2_pmc_enable_periph_clock(int clk)
 {
 	at91_pmc_sam9x5_enable_periph_clock(SAMA5D2_BASE_PMC, clk);
 }
-
-void sama5d2_lowlevel_init(void);
 
 /* requires relocation */
 static inline void sama5d2_udelay_init(unsigned int msc)
@@ -22,6 +22,17 @@ static inline void sama5d2_udelay_init(unsigned int msc)
 			  SAMA5D2_ID_PIT, msc, AT91_PMC_LL_SAMA5D2);
 }
 
+
 void sama5d2_ddr2_init(struct at91_ddramc_register *ddramc_reg_config);
+
+static inline int sama5d2_pmc_enable_generic_clock(unsigned int periph_id,
+						   unsigned int clk_source,
+						   unsigned int div)
+{
+	return at91_pmc_enable_generic_clock(SAMA5D2_BASE_PMC,
+					     SAMA5D2_BASE_SFR,
+					     periph_id, clk_source, div,
+					     AT91_PMC_LL_SAMA5D2);
+}
 
 #endif

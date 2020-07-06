df694a4bc4e7c030a976f4d7b704d621a00e4dd8
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-07-06 08:28:03 +0200

    sandbox: include: <asm/types.h>: don't define INTERNAL_SIZE_T
    The INTERNAL_SIZE_T default is size_t, which already is 64-bit on 64-bit
    platforms and 32-bit on 32-bit ones. We can thus drop the #define and go
    back to the minimal <asm/types.h>.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/sandbox/include/asm/types.h
+++ arch/sandbox/include/asm/types.h
@@ -1,15 +1,6 @@
-#ifndef __ASM_I386_TYPES_H
-#define __ASM_I386_TYPES_H
+#ifndef __ASM_SANDBOX_TYPES_H
+#define __ASM_SANDBOX_TYPES_H
 
 #include <asm-generic/int-ll64.h>
-
-#ifdef __x86_64__
-/*
- * This is used in dlmalloc. On X86_64 we need it to be
- * 64 bit
- */
-#define INTERNAL_SIZE_T unsigned long
-
-#endif
 
 #endif

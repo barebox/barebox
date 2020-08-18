barebox/barebox

commit a5f73a6dcd5296abb76c50615cb7c1ddfb227708

libfile: copy_file: explicitly truncate to final size
When possible truncate the destination file to the final size
explicitly. This allows for example ramfs to put the resulting
file contiguously into memory.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

lib/libfile.c
@@ -353,10 +353,6 @@ int copy_file(const char *src, const char *dst, int verbose)
 		goto out;
 	}
 
-	/* Set O_TRUNC only if file exist and is a regular file */
-	if (!s && S_ISREG(dststat.st_mode))
-		mode |= O_TRUNC;
-
 	dstfd = open(dst, mode);
 	if (dstfd < 0) {
 		printf("could not open %s: %s\n", dst, errno_str());
@@ -364,12 +360,22 @@ int copy_file(const char *src, const char *dst, int verbose)
 		goto out;
 	}
 
+	ret = ftruncate(dstfd, 0);
+	if (ret)
+		goto out;
+
 	ret = stat(src, &srcstat);
 	if (ret)
 		goto out;
 
-	if (srcstat.st_size != FILESIZE_MAX)
+	if (srcstat.st_size != FILESIZE_MAX) {
 		discard_range(dstfd, srcstat.st_size, 0);
+		if (S_ISREG(dststat.st_mode)) {
+			ret = ftruncate(dstfd, srcstat.st_size);
+			if (ret)
+				goto out;
+		}
+	}
 
 	if (verbose)
 		init_progression_bar(srcstat.st_size);

commit cdf3e7ed0d6624108a2a4722f721032d576e5516

libfile: copy_file: Fix ftruncate called on device files
ftruncate may only be called on regular files. This became broken in
a5f73a6dcd. Revert to the previous behaviour and pass O_TRUNC to the
initial open() call instead of truncating it with ftruncate().

Also we can call ftruncate() to the final size also when the file
previously didn't exist.

Fixes: a5f73a6dcd ("libfile: copy_file: explicitly truncate to final size")
Reported-by: Bastian Krause <bst@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

lib/libfile.c
@@ -353,6 +353,10 @@ int copy_file(const char *src, const char *dst, int verbose)
 		goto out;
 	}
 
+	/* Set O_TRUNC only if file exists and is a regular file */
+	if (!s && S_ISREG(dststat.st_mode))
+		mode |= O_TRUNC;
+
 	dstfd = open(dst, mode);
 	if (dstfd < 0) {
 		printf("could not open %s: %s\n", dst, errno_str());
@@ -360,17 +364,13 @@ int copy_file(const char *src, const char *dst, int verbose)
 		goto out;
 	}
 
-	ret = ftruncate(dstfd, 0);
-	if (ret)
-		goto out;
-
 	ret = stat(src, &srcstat);
 	if (ret)
 		goto out;
 
 	if (srcstat.st_size != FILESIZE_MAX) {
 		discard_range(dstfd, srcstat.st_size, 0);
-		if (S_ISREG(dststat.st_mode)) {
+		if (s || S_ISREG(dststat.st_mode)) {
 			ret = ftruncate(dstfd, srcstat.st_size);
 			if (ret)
 				goto out;
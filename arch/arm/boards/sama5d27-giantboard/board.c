bef194fd8581e8ad83744b4c6c33c4a4da84be09
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-09-02 15:13:07 +0200

    ARM: at91: sama5d27-giantboard: fix multi-image compatibility
    For multi-image compatibility, board code needs to be guarded by a check
    against the compatible. Retrofit the check.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/sama5d27-giantboard/board.c
+++ arch/arm/boards/sama5d27-giantboard/board.c
@@ -3,9 +3,13 @@
 #include <init.h>
 #include <envfs.h>
 #include <bbu.h>
+#include <of.h>
 
 static int giantboard_device_init(void)
 {
+	if (!of_machine_is_compatible("groboards,sama5d27-giantboard"))
+		return 0;
+
 	bbu_register_std_file_update("microSD", BBU_HANDLER_FLAG_DEFAULT,
 				     "/mnt/mmc1.0/barebox.bin",
 				     filetype_arm_barebox);

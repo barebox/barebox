c7dcac9de597f006fdc8c2680eb25e92ff62df7d
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2020-09-23 12:34:16 +0200

    common: print buildsystem version in barebox banner
    When the barebox banner is enabled and printed during startup, also show
    information about the buildsystem version: the exact state of the
    barebox binary and its config.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/version.c
+++ common/version.c
@@ -10,11 +10,17 @@ const char release_string[] =
 	"barebox-" UTS_RELEASE;
 EXPORT_SYMBOL(release_string);
 
+const char buildsystem_version_string[] =
+	BUILDSYSTEM_VERSION;
+EXPORT_SYMBOL(buildsystem_version_string);
+
 #ifdef CONFIG_BANNER
 void barebox_banner (void)
 {
 	printf("\n\n");
 	pr_info("%s", version_string);
+	if (strlen(buildsystem_version_string) > 0)
+		pr_info("Buildsystem version: %s", buildsystem_version_string);
 	printf("\n\n");
 	pr_info("Board: %s\n", barebox_get_model());
 }

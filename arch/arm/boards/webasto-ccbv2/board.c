f91b28d3113bc21048b74d4517c77072ed9c6cdf
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2020-10-14 08:18:58 +0200

    ARM: imx: Add Support for Webasto ccbv2
    Add support for the Webasto Common Communication Board Version 2. The
    device tree included with barebox can eventually be replaced with the
    required barebox changes when the ccbv2 device tree is upstream.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/webasto-ccbv2/board.c
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2019 Rouven Czerwinski, Pengutronix
+ */
+
+#include <common.h>
+#include <init.h>
+#include <mach/generic.h>
+#include <mach/bbu.h>
+#include <of.h>
+#include <string.h>
+
+#include "ccbv2.h"
+
+static int ccbv2_probe(struct device_d *dev)
+{
+	struct device_node *overlay;
+	struct fdt_header *fdt;
+	int ret;
+
+	/* the bootloader is stored in one of the two boot partitions */
+	imx6_bbu_internal_mmcboot_register_handler("emmc", "/dev/mmc1",
+			BBU_HANDLER_FLAG_DEFAULT);
+
+	barebox_set_hostname("weabsto-ccbv2");
+
+	if(!IS_ENABLED(CONFIG_FIRMWARE_CCBV2_OPTEE))
+		return 0;
+
+	fdt = (void*)OPTEE_OVERLAY_LOCATION;
+	overlay = of_unflatten_dtb(fdt);
+
+	if (IS_ERR(overlay))
+		return PTR_ERR(overlay);
+
+	ret = of_register_overlay(overlay);
+	if (ret) {
+		printf("cannot apply oftree overlay: %s\n", strerror(-ret));
+		goto err;
+	}
+
+	return 0;
+err:
+	of_delete_node(overlay);
+	return ret;
+
+}
+
+static const struct of_device_id ccbv2_of_match[] = {
+	{ .compatible = "webasto,imx6ul-ccbv2" },
+	{ /* sentinel */ },
+};
+
+static struct driver_d ccbv2_board_driver = {
+	.name = "board-imx6ul-ccbv2",
+	.probe = ccbv2_probe,
+	.of_compatible = ccbv2_of_match,
+};
+postcore_platform_driver(ccbv2_board_driver);
---
+++ arch/arm/boards/webasto-ccbv2/ccbv2.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * ccbv2.h - common defines between OP-TEE and barebox
+ *
+ * Copyright (c) 2019 Rouven Czerwinski <r.czerwinski@pengutronix.de>, Pengutronix
+ *
+ */
+#ifndef __CCBV2_H_
+#define __CCBV2_H_
+
+/* MX6UL_MMDC_PORT0_BASE_ADDR + SZ_64M */
+#define OPTEE_OVERLAY_LOCATION 	0x84000000
+
+
+#endif // __CCBV2_H_

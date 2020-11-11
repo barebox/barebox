barebox/barebox

commit b6bcd96de5a75bdc5d06a06f2efffc2d89e346ec

mtd: nand: Update to Linux-5.9
This updates the barebox NAND layer and parts of the mtd layer to
Linux-5.9.

This patch is huge, but the barebox NAND layer is so far away from the
Linux NAND layer that a step by step update would have taken ages.

Unlike Linux barebox has functions to mark a block as good. This feature
has been preserved. Also barebox used to make NAND write support
optional, this feature is lost during the update for the sake of better
compatibility to the Linux NAND layer.

This patch has been tested:

- GPMI aka nand_mxs on i.MX6
- nand_imx on i.MX25
- nand_omap_gpmc on AM335x
- atmel_nand on Atmel sama5d3
- nand_denali on SoCFPGA

Currently untested:

- nand_orion
- nand_mrvl_nfc
- nand_s3c24xx

The nand_denali driver is tested with the update of that driver to
Linux-5.9 following in the next patch.

I could only test the drivers with the NAND chips found on my boards, so
there's still enough room for regressions, especially given that the
NAND drivers themselves are mostly not updated. With the NAND layer
being up-to-date with Linux it should hopefully be easy to update
drivers to their Linux counterpart as well if necessary.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/Kconfig
@@ -1,6 +1,5 @@
 menuconfig NAND
 	bool "NAND support"
-	select MTD_NAND_IDS
 	help
 	  This enables support for accessing all type of NAND flash
 	  devices. For further information see
@@ -8,54 +7,10 @@ menuconfig NAND
 
 if NAND
 
-config NAND_ECC_SOFT
-	bool
-	default y
-	prompt "Support software ecc"
-
-config NAND_ECC_BCH
+config MTD_NAND_ECC_SW_BCH
 	select BCH
 	bool
 	prompt "Support software BCH ecc"
-
-config NAND_ECC_HW
-	bool
-	default y
-	prompt "Support hardware ecc"
-
-config NAND_ECC_HW_OOB_FIRST
-	bool
-	prompt "Support hardware ecc (oob first)"
-
-config NAND_ECC_HW_SYNDROME
-	bool
-	default y
-	prompt "Support syndrome hardware ecc controllers"
-
-config NAND_ECC_HW_NONE
-	bool
-	default y
-	prompt "Support skipping ecc support"
-
-config NAND_INFO
-	bool
-	default y
-	prompt "Nand vendor/size information"
-	help
-	  Show informational strings about the vendor and nand flash type
-	  during startup
-
-config NAND_READ_OOB
-	bool
-
-config NAND_BBT
-	bool
-	select NAND_READ_OOB
-	default y
-	prompt "support bad block tables"
-	help
-	  Say y here to include support for bad block tables. This speeds
-	  up the process of checking for bad blocks
 
 config NAND_ALLOW_ERASE_BAD
 	bool
@@ -119,7 +74,6 @@ config NAND_ATMEL_PMECC
 	bool
 	prompt "PMECC support"
 	depends on NAND_ATMEL || COMPILE_TEST
-	select NAND_ECC_HW
 	help
 	  Support for PMECC present on the SoC sam9x5 and sam9n12
 
@@ -130,15 +84,12 @@ config NAND_S3C24XX
 	help
 	  Add support for processor's NAND device controller.
 
-config MTD_NAND_ECC_SMC
+config MTD_NAND_ECC_SW_HAMMING_SMC
 	bool "NAND ECC Smart Media byte order"
 	default n
 	help
 	  Software ECC according to the Smart Media Specification.
 	  The original Linux implementation had byte 0 and 1 swapped.
-
-config MTD_NAND_IDS
-	tristate
 
 config MTD_NAND_NOMADIK
 	tristate "ST Nomadik 8815 NAND support"

drivers/mtd/Kconfig
@@ -10,13 +10,11 @@ config MTD_WRITE
 
 config MTD_OOB_DEVICE
 	bool
-	select NAND_READ_OOB if NAND
 	default y
 	prompt "Create a device for reading the OOB data"
 
 config MTD_RAW_DEVICE
 	bool
-	select NAND_READ_OOB if NAND
 	default n
 	prompt "mtdraw device to read/write both data+oob"
 

commit ebb9d20927cbe4c25f2f30334aec795c93fd0521

mtd: nand: Fix dependeny on CONFIG_NAND_BBT
CONFIG_NAND_BBT no longer exists, so remove the remaining occurences.

Fixes: b6bcd96de5 ("mtd: nand: Update to Linux-5.9")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/Kconfig
@@ -28,7 +28,6 @@ config NAND_IMX
 
 config NAND_MXS
 	bool
-	select NAND_BBT
 	select STMP_DEVICE
 	prompt "i.MX23/28/6 NAND driver"
 	depends on MXS_APBH_DMA

drivers/mtd/nand/nand_base.c
@@ -688,7 +688,7 @@ static int nand_block_markgood_lowlevel(struct nand_chip *chip, loff_t ofs)
 	}
 
 	/* Mark block good in BBT */
-	if (IS_ENABLED(CONFIG_NAND_BBT) && chip->bbt) {
+	if (chip->bbt) {
 		ret = nand_markgood_bbt(chip, ofs);
 		if (ret)
 			return ret;
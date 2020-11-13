barebox/barebox

commit fa4c41ba60af6e8260307fadcd5fb68f8bdb7360

nvvar: when setting a nvvar to NULL just free the content
When a nvvar is about to be removed then it is set to NULL
by barebox. This means for the variable to free it's memory.
In this case also do not pass the value to the corresponding
globalvar, since that would set the globalvar to NULL aswell,
but we want to keep its current value.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/globalvar.c
@@ -167,8 +167,10 @@ static int nv_set(struct device_d *dev, struct param_d *p, const char *val)
 {
 	int ret;
 
-	if (!val)
-		val = "";
+	if (!val) {
+		free(p->value);
+		return 0;
+	}
 
 	ret = dev_set_param(&global_device, p->name, val);
 	if (ret)

commit c2fa7340a961d10f9552babad4785cf0deb75e2c

nv: fix use-after-free when clearing from shell
When we use hush to set the same nv.var twice to the empty string:

  $ nv.user=
  $ nv.user=

nv_set is called twice with a NULL val argument leading
to a double free and accompanied memory corruption.

Reorder the code, so p->value is freed just once.

Fixes: fa4c41ba60af ("nvvar: when setting a nvvar to NULL just free the content")
Cc: Holger Assmann <has@pengutronix.de>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/globalvar.c
@@ -179,15 +179,11 @@ static int nv_set(struct device_d *dev, struct param_d *p, const char *name, con
 {
 	int ret;
 
-	if (!val) {
-		if (p)
-			free(p->value);
-		return 0;
+	if (val) {
+		ret = dev_set_param(&global_device, name, val);
+		if (ret)
+			return ret;
 	}
-
-	ret = dev_set_param(&global_device, name, val);
-	if (ret)
-		return ret;
 
 	if (p) {
 		free(p->value);
barebox/barebox

commit 5a5c5178e7dc6a58a059fe4a1f4167a31809ccaf

usbgadget: autostart: support delayed usbgadget.autostart=1
So far, global.usbgadget.autostart=1 from the shell was without effect,
because the variable is only evaluated once at postenvironment_initcall.

Use the new globalvar_add_bool() to allow acting on the variable being
true at any time. This is necessary for scripts that want to enable
the usbgadget autostart functionality selectively without themselves
hardcoding the particularities of what is exported.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/usbgadget.c
@@ -101,7 +101,7 @@ int usbgadget_register(bool dfu, const char *dfu_opts,
 	return ret;
 }
 
-static int usbgadget_autostart(void)
+static int usbgadget_autostart_set(struct param_d *param, void *ctx)
 {
 	bool fastboot_bbu = get_fastboot_bbu();
 
@@ -110,12 +110,12 @@ static int usbgadget_autostart(void)
 
 	return usbgadget_register(true, NULL, true, NULL, acm, fastboot_bbu);
 }
-postenvironment_initcall(usbgadget_autostart);
 
 static int usbgadget_globalvars_init(void)
 {
 	if (IS_ENABLED(CONFIG_USB_GADGET_AUTOSTART)) {
-		globalvar_add_simple_bool("usbgadget.autostart", &autostart);
+		globalvar_add_bool("usbgadget.autostart", usbgadget_autostart_set,
+				   &autostart, NULL);
 		globalvar_add_simple_bool("usbgadget.acm", &acm);
 	}
 	globalvar_add_simple_string("usbgadget.dfu_function", &dfu_function);

commit 7cc491383b9de67abfd5bb9e1222287243cc3741

usbgadget: autostart: don't print error on repeated nv.usbgadget.autostart=1
nvvar_add results in two calls to dev_set_param:
 - once when the existing global variable is found and set
 - once more when setting the nv variable

This results in an annoying but ultimately harmless message on startup:

  ERROR: USB multi gadget already registered
  ERROR: failed to create nv variable usbgadget.autostart: Device or
  resource busy

Avoid this by ignoring usbgadget.autostart=1 after it succeeded once.

This issue should only affect $global.usbgadget.autostart, because all
other global variables are "simple" meaning that they have no setters
triggered.

Fixes: 5a5c5178e7dc ("usbgadget: autostart: support delayed usbgadget.autostart=1")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/usbgadget.c
@@ -102,12 +102,18 @@ int usbgadget_register(bool dfu, const char *dfu_opts,
 
 static int usbgadget_autostart_set(struct param_d *param, void *ctx)
 {
+	static bool started;
 	bool fastboot_bbu = get_fastboot_bbu();
+	int err;
 
-	if (!IS_ENABLED(CONFIG_USB_GADGET_AUTOSTART) || !autostart)
+	if (!IS_ENABLED(CONFIG_USB_GADGET_AUTOSTART) || !autostart || started)
 		return 0;
 
-	return usbgadget_register(true, NULL, true, NULL, acm, fastboot_bbu);
+	err = usbgadget_register(true, NULL, true, NULL, acm, fastboot_bbu);
+	if (!err)
+		started = true;
+
+	return err;
 }
 
 static int usbgadget_globalvars_init(void)
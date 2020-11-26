fb244def400853e5b41c327e5cb02d6d34be2397
Ahmad Fatoum <a.fatoum@pengutronix.de>
2020-11-26 18:55:53 +0100

    common: replace license statements with SPDX-License-Identifiers
    For all files in common/ that already have a license text:
      - Replace with appropriate SPDX-License-Identifier
      - Remove empty comment lines around replacement
      - remove comment completely if only thing remaining is name
        of file without description
    
    Reviewed-by: Roland Hieber <rhi@pengutronix.de>
    Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/menu.c
+++ common/menu.c
@@ -1,16 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * (C) Copyright 2009-2010 Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
  */
 
 #include <common.h>
--- common/uimage.c
+++ common/uimage.c
@@ -1,18 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * uimage.c - uimage handling code
  *
  * Copyright (c) 2011 Sascha Hauer <s.hauer@pengutronix.de>, Pengutronix
  *
  * partly based on U-Boot uImage code
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 #include <common.h>
 #include <image.h>

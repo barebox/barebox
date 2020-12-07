barebox/barebox

commit c4d9463d969b6234c86f4045656775212da08508

i.MX: Introduce imx_ocotp_read_uid()
UID information in OCOTP is located in the same place on at least
i.MX6, i.MX7 and i.MX8MQ. Create an SoC-agnostic helper to do all the
work and convert i.MX6 to use it.

Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Cc: Chris Healy <cphealy@gmail.com>
Cc: Lucas Stach <l.stach@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/include/mach/ocotp.h
@@ -26,10 +26,24 @@
 #define OCOTP_BIT(n)		FIELD_PREP(OCOTP_BIT_MASK, n)
 #define OCOTP_WIDTH(n)		FIELD_PREP(OCOTP_WIDTH_MASK, (n) - 1)
 
+#define OCOTP_OFFSET_CFG0	0x410
+#define OCOTP_OFFSET_CFG1	0x420
+
 
 int imx_ocotp_read_field(uint32_t field, unsigned *value);
 int imx_ocotp_write_field(uint32_t field, unsigned value);
 int imx_ocotp_permanent_write(int enable);
 bool imx_ocotp_sense_enable(bool enable);
+
+static inline u64 imx_ocotp_read_uid(void __iomem *ocotp)
+{
+	u64 uid;
+
+	uid  = readl(ocotp + OCOTP_OFFSET_CFG0);
+	uid <<= 32;
+	uid |= readl(ocotp + OCOTP_OFFSET_CFG1);
+
+	return uid;
+}
 
 #endif /* __MACH_IMX_OCOTP_H */

arch/arm/mach-imx/imx6.c
@@ -192,13 +192,7 @@ int imx6_cpu_revision(void)
 
 u64 imx6_uid(void)
 {
-	void __iomem *ocotpbase = IOMEM(MX6_OCOTP_BASE_ADDR);
-	u64 uid;
-
-	uid = ((u64)readl(ocotpbase + MX6_OCOTP_CFG0) << 32);
-	uid |= (u64)readl(ocotpbase + MX6_OCOTP_CFG1);
-
-	return uid;
+	return imx_ocotp_read_uid(IOMEM(MX6_OCOTP_BASE_ADDR));
 }
 
 int imx6_init(void)

commit b68c3dddf74e89d88a7f4a8b30b45f547368ec0f

ARM: i.MX: OCOTP: read serial number with correct endianness
The serial number is stored with the low bytes first, as can be seen in
Linux commit 8267ff89b713 ("ARM: imx: Add serial number support for
i.MX6/7 SoCs").  The same goes for i.MX8M.

Also renamed the macro to the more descriptive name from Linux.

Fixes: c4d9463d969b ("i.MX: Introduce imx_ocotp_read_uid()")
Signed-off-by: Robert Karszniewicz <r.karszniewicz@phytec.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/include/mach/ocotp.h
@@ -26,8 +26,8 @@
 #define OCOTP_BIT(n)		FIELD_PREP(OCOTP_BIT_MASK, n)
 #define OCOTP_WIDTH(n)		FIELD_PREP(OCOTP_WIDTH_MASK, (n) - 1)
 
-#define OCOTP_OFFSET_CFG0	0x410
-#define OCOTP_OFFSET_CFG1	0x420
+#define OCOTP_UID_L		0x410
+#define OCOTP_UID_H		0x420
 
 
 int imx_ocotp_read_field(uint32_t field, unsigned *value);
@@ -39,9 +39,9 @@ static inline u64 imx_ocotp_read_uid(void __iomem *ocotp)
 {
 	u64 uid;
 
-	uid  = readl(ocotp + OCOTP_OFFSET_CFG0);
+	uid  = readl(ocotp + OCOTP_UID_H);
 	uid <<= 32;
-	uid |= readl(ocotp + OCOTP_OFFSET_CFG1);
+	uid |= readl(ocotp + OCOTP_UID_L);
 
 	return uid;
 }
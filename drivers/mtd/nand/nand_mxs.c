barebox/barebox

commit 616d942c3eec713d8addf9cbc09a9349f004a135

mtd:nand_mxs:update get/set features legacy function call
update get/set features legacy function call to avoid NULL pointer crash.

Signed-off-by: zhengxiaojun <jameszxj at gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/nand_mxs.c
@@ -2055,14 +2055,12 @@ static int mxs_nand_enable_edo_mode(struct mxs_nand_info *info)
 		/* [1] send SET FEATURE commond to NAND */
 		feature[0] = mode;
 
-		ret = chip->legacy.set_features(chip,
-				ONFI_FEATURE_ADDR_TIMING_MODE, feature);
+		ret = nand_set_features(chip, ONFI_FEATURE_ADDR_TIMING_MODE, feature);
 		if (ret)
 			goto err_out;
 
 		/* [2] send GET FEATURE command to double-check the timing mode */
-		ret = chip->legacy.get_features(chip,
-				ONFI_FEATURE_ADDR_TIMING_MODE, feature);
+		ret = nand_get_features(chip, ONFI_FEATURE_ADDR_TIMING_MODE, feature);
 		if (ret || feature[0] != mode)
 			goto err_out;
 	}

commit a1f04e3240ed883ecf521f9f25526e96345d8024

mtd:nand_mxs: Add missing include
Add include missing in last commit.

Fixes: 616d942c3e mtd:nand_mxs:update get/set features legacy function call
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/nand_mxs.c
@@ -37,6 +37,8 @@
 #include <stmp-device.h>
 #include <mach/generic.h>
 
+#include "internals.h"
+
 #define	MX28_BLOCK_SFTRST				(1 << 31)
 #define	MX28_BLOCK_CLKGATE				(1 << 30)
 
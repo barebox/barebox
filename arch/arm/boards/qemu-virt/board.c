ff8b098a4ad26688a8f2a853a864903b47f240d6
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2021-01-13 08:57:57 +0100

    ARM: qemu: support for state & env via DT overlay
    Add support for state and barebox environment by applying an overlay on
    top of the qemu generated device tree. If qemu is used with a persistent
    flash partition, this allows usage of environments and state across qemu
    reboots.
    Simple testing can be performed by starting with
    
      qemu-system-arm -m 256M -M virt -kernel images/barebox-dt-2nd.img -nographic
    
    Storing data, changing into the qemu console via Ctrl-A Ctrl-C, issuing
    'system_reset' and Ctrl-A Ctrl-C to get back into the barebox console.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/qemu-virt/board.c
+++ arch/arm/boards/qemu-virt/board.c
@@ -5,7 +5,41 @@
  */
 #include <common.h>
 #include <init.h>
+#include <of.h>
 #include <asm/system_info.h>
+#include <asm/barebox-arm.h>
+
+extern char __dtb_overlay_of_flash_start[];
+
+static int replace_dtb(void) {
+	struct device_node *overlay;
+	void *fdt;
+	struct device_node *root;
+
+	fdt = barebox_arm_boot_dtb();
+	if (fdt)
+		pr_debug("using boarddata provided DTB\n");
+
+	if (!fdt) {
+		pr_debug("No DTB found\n");
+		return 0;
+	}
+
+	root = of_unflatten_dtb(fdt);
+
+	if (!of_device_is_compatible(root, "linux,dummy-virt")) {
+		of_delete_node(root);
+		return 0;
+	}
+
+	overlay = of_unflatten_dtb(__dtb_overlay_of_flash_start);
+	of_overlay_apply_tree(root, overlay);
+	barebox_register_of(root);
+
+	return 0;
+}
+
+pure_initcall(replace_dtb);
 
 static int virt_probe(struct device_d *dev)
 {

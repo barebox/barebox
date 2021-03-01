0863bda9ce6586908e176cdb4ea6ae4b6057a45f
Jules Maselbas <jmaselbas@kalray.eu>
2021-03-01 10:30:40 +0100

    usb: gadget: dfu: Simplify state handle
    Simplify the state handle, the idle state will only be entered
    from do_close.
    
    Signed-off-by: Jules Maselbas <jmaselbas@kalray.eu>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/usb/gadget/dfu.c
+++ drivers/usb/gadget/dfu.c
@@ -320,6 +320,8 @@ static void dfu_do_close(struct dfu_work *dw)
 
 	if (!stat(DFU_TEMPFILE, &s))
 		unlink(DFU_TEMPFILE);
+
+	dw->dfu->dfu_state = DFU_STATE_dfuIDLE;
 }
 
 static void dfu_do_copy(struct dfu_work *dw)
@@ -756,11 +758,8 @@ static int dfu_setup(struct usb_function *f, const struct usb_ctrlrequest *ctrl)
 	case DFU_STATE_dfuMANIFEST_SYNC:
 		switch (ctrl->bRequest) {
 		case USB_REQ_DFU_GETSTATUS:
+			dfu->dfu_state = DFU_STATE_dfuMANIFEST;
 			value = dfu_status(f, ctrl);
-			if (dfu_file_entry->flags & FILE_LIST_FLAG_SAFE)
-				dfu->dfu_state = DFU_STATE_dfuMANIFEST;
-			else
-				dfu->dfu_state = DFU_STATE_dfuIDLE;
 			value = min(value, w_length);
 			break;
 		case USB_REQ_DFU_GETSTATE:

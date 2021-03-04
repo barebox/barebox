186838e09de277ac766352b0f37ba8172ebc5f0e
Alexander Kurz <akurz@blala.de>
2021-03-04 06:32:58 +0000

    ARM: i.MX8M: bbu: fix variable name to used semantic
    Both flags handle BBU behavior and should be bbu_flags.
    
    Signed-off-by: Alexander Kurz <akurz@blala.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/nxp-imx8mp-evk/board.c
+++ arch/arm/boards/nxp-imx8mp-evk/board.c
@@ -17,7 +17,7 @@
 static int nxp_imx8mp_evk_init(void)
 {
 	int emmc_bbu_flag = 0;
-	int emmc_sd_flag = 0;
+	int sd_bbu_flag = 0;
 	u32 val;
 
 	if (!of_machine_is_compatible("fsl,imx8mp-evk"))
@@ -29,14 +29,14 @@ static int nxp_imx8mp_evk_init(void)
 			emmc_bbu_flag = BBU_HANDLER_FLAG_DEFAULT;
 		} else {
 			of_device_enable_path("/chosen/environment-sd");
-			emmc_sd_flag = BBU_HANDLER_FLAG_DEFAULT;
+			sd_bbu_flag = BBU_HANDLER_FLAG_DEFAULT;
 		}
 	} else {
 		of_device_enable_path("/chosen/environment-emmc");
 		emmc_bbu_flag = BBU_HANDLER_FLAG_DEFAULT;
 	}
 
-	imx8m_bbu_internal_mmc_register_handler("SD", "/dev/mmc1.barebox", emmc_sd_flag);
+	imx8m_bbu_internal_mmc_register_handler("SD", "/dev/mmc1.barebox", sd_bbu_flag);
 	imx8m_bbu_internal_mmc_register_handler("eMMC", "/dev/mmc2", emmc_bbu_flag);
 
 	val = readl(MX8MP_IOMUXC_GPR_BASE_ADDR + MX8MP_IOMUXC_GPR1);

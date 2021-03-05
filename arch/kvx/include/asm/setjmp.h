ed120cf6c2d92ffaeb2a75e5205ec68cd28123b5
Jules Maselbas <jmaselbas@kalray.eu>
2021-03-05 19:33:23 +0100

    kvx: Implement setjmp/longjmp/initjmp
    Signed-off-by: Jules Maselbas <jmaselbas@kalray.eu>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/kvx/include/asm/setjmp.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-FileCopyrightText: 2021 Jules Maselbas <jmaselbas@kalray.eu>, Kalray Inc. */
+
+#ifndef _ASM_KVX_SETJMP_H_
+#define _ASM_KVX_SETJMP_H_
+
+#include <linux/types.h>
+
+typedef u64 jmp_buf[22];
+
+int initjmp(jmp_buf jmp, void __attribute__((noreturn)) (*func)(void), void *stack_top);
+int setjmp(jmp_buf jmp) __attribute__((returns_twice));
+void longjmp(jmp_buf jmp, int ret) __attribute__((noreturn));
+
+#endif /* _ASM_KVX_SETJMP_H_ */

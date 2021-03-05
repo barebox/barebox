719995bfb8c289bc434c7413f341e4fd1cce6cd7
Sascha Hauer <s.hauer@pengutronix.de>
2021-03-05 09:40:45 +0100

    printk: fix inverted logic in print_hex_dump_debug()
    print_hex_dump_debug() shall print a hexdump when debugging is enabled,
    not when it's disabled.
    
    Fixes: 5825231429 ("printk: port over Linux print_hex_dump_bytes/print_hex_dump_debug")
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/printk.h
+++ include/printk.h
@@ -160,7 +160,7 @@ extern void print_hex_dump(const char *level, const char *prefix_str,
 			   int prefix_type, int rowsize, int groupsize,
 			   const void *buf, size_t len, bool ascii);
 
-#if LOGLEVEL <= MSG_DEBUG
+#if LOGLEVEL >= MSG_DEBUG
 #define print_hex_dump_debug(prefix_str, prefix_type, rowsize,		\
 			     groupsize, buf, len, ascii)		\
 	print_hex_dump(KERN_DEBUG, prefix_str, prefix_type, rowsize,	\

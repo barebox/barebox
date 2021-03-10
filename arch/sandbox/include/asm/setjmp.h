e74e330216baff384b844fde06720b022614168b
Ahmad Fatoum <a.fatoum@pengutronix.de>
2021-03-10 09:47:48 +0100

    sandbox: asm: implement setjmp/longjmp/initjmp
    To extend incoming bthread support to sandbox, implement setjmp, longjmp
    and initjmp. Unlike bare metal platforms, setjmp() and longjmp() are
    readily provided on standard-conforming hosted platforms.  initjmp() on
    the other hand requires us to be able to invoke a function with a
    user-supplied stack pointer, which isn't possible in standard C.
    
    For POSIX systems, there are two methods to portably achieve this though:
    
      - Use makecontext(2) to set up a new context. makecontext(2) was however
        removed in POSIX.1-2008 and at least GCC 10.2.1 ASan complains that it
        "doesn't fully support makecontext/swapcontext functions and may
        produce false positives in some cases!"
    
      - Use sigaltstack to set a new signal stack, raise the signal call,
        setjmp in the signal handler to store the new stack pointer, return
        regularly from signal handler and then longjmp back
    
    Both methods are implemented in QEMU. While QEMU uses the makecontext
    method by default, for the reasons described, import the second implementation
    and use it implement initjmp.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/sandbox/include/asm/setjmp.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __SETJMP_H_
+#define __SETJMP_H_
+
+struct jmp_buf_data {
+	unsigned char opaque[512] __aligned(16);
+};
+
+typedef struct jmp_buf_data jmp_buf[1];
+
+int setjmp(jmp_buf jmp) __attribute__((returns_twice));
+void longjmp(jmp_buf jmp, int ret) __attribute__((noreturn));
+
+int initjmp(jmp_buf jmp, void __attribute__((noreturn)) (*func)(void), void *stack_top);
+
+#endif

barebox/barebox

commit 77e74ddca1a30ea1ac4bb9de55917f6b69290b41

treewide: Use driver macro
We have several macros for a oneline driver registration. Add some
missing and use them consistently where possible througout the tree.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/gpio/gpio-pl061.c
@@ -147,11 +147,7 @@ static struct amba_driver pl061_gpio_driver = {
 	.probe		= pl061_probe,
 };
 
-static int __init pl061_gpio_init(void)
-{
-	return amba_driver_register(&pl061_gpio_driver);
-}
-coredevice_initcall(pl061_gpio_init);
+coredevice_platform_driver(pl061_gpio_driver);
 
 MODULE_AUTHOR("Baruch Siach <baruch@tkos.co.il>");
 MODULE_DESCRIPTION("PL061 GPIO driver");

commit 15c3aa4be369075e0683e5454d572c25af8c6925

gpio: pl061: Fix driver registration
77e74ddca1 changes drivers to use coredevice_platform_driver() instead
of open coded initcall/driver registration. This is wrong for the pl061
driver as this is an amba driver. Revert 77e74ddca1 for this file.

Fixes: 77e74ddca1 ("treewide: Use driver macro")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/gpio/gpio-pl061.c
@@ -147,7 +147,11 @@ static struct amba_driver pl061_gpio_driver = {
 	.probe		= pl061_probe,
 };
 
-coredevice_platform_driver(pl061_gpio_driver);
+static int __init pl061_gpio_init(void)
+{
+	return amba_driver_register(&pl061_gpio_driver);
+}
+coredevice_initcall(pl061_gpio_init);
 
 MODULE_AUTHOR("Baruch Siach <baruch@tkos.co.il>");
 MODULE_DESCRIPTION("PL061 GPIO driver");
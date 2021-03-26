barebox/barebox

commit 31300d79e82767b45ffd93a82af083980033a89e

ARM: zii-common: Add common environment directory
Establish a common environment directory between all ZII boards, so
that we could share common bits and pieces. As a first step, get rid
of board scecific net boot scripts and replace them with a single
shared one.

Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/boards/zii-imx7d-dev/board.c
@@ -55,6 +55,8 @@ static int zii_imx7d_dev_init(void)
 	else if (of_machine_is_compatible("zii,imx7d-rmu2"))
 		barebox_set_hostname("rmu2");
 
+	defaultenv_append_directory(defaultenv_zii_common);
+
 	return 0;
 }
 late_initcall(zii_imx7d_dev_init);

arch/arm/boards/zii-imx51-rdu1/board.c
@@ -50,6 +50,7 @@ static int zii_rdu1_init(void)
 		BBU_HANDLER_FLAG_DEFAULT |
 		IMX_BBU_FLAG_PARTITION_STARTS_AT_HEADER);
 
+	defaultenv_append_directory(defaultenv_zii_common);
 	defaultenv_append_directory(defaultenv_rdu1);
 
 	return 0;

arch/arm/boards/zii-imx6q-rdu2/board.c
@@ -155,6 +155,7 @@ static int rdu2_devices_init(void)
 
 	imx6_bbu_internal_mmcboot_register_handler("eMMC", "/dev/mmc3", 0);
 
+	defaultenv_append_directory(defaultenv_zii_common);
 	defaultenv_append_directory(defaultenv_rdu2);
 
 	return 0;

arch/arm/boards/zii-imx8mq-dev/board.c
@@ -39,6 +39,7 @@ static int zii_imx8mq_dev_init(void)
 	else
 		of_device_enable_path("/chosen/environment-sd");
 
+	defaultenv_append_directory(defaultenv_zii_common);
 	defaultenv_append_directory(defaultenv_imx8mq_zii_dev);
 
 	return 0;

arch/arm/boards/zii-vf610-dev/board.c
@@ -141,6 +141,7 @@ static int zii_vf610_dev_set_hostname(void)
 		}
 	}
 
+	defaultenv_append_directory(defaultenv_zii_common);
 	defaultenv_append_directory(defaultenv_zii_vf610_dev);
 	return 0;
 }

arch/arm/boards/zii-common/Makefile
@@ -1,1 +1,2 @@
 obj-y += board.o switch-cmd.o pn-fixup.o
+bbenv-y += defaultenv-zii-common

arch/arm/boards/zii-common/defaultenv-zii-common/boot/net
@@ -2,8 +2,13 @@
 
 path="/mnt/tftp"
 
-# clear seat network config
-global.linux.bootargs.rdu_network=
+#
+# Clear seat network config. Some boards don't have that config, so
+# check that it is set first
+#
+if [ -n ${global.linux.bootargs.rdu_network} ]; then
+    global.linux.bootargs.rdu_network=
+fi
 
 global.bootm.image="${path}/${global.user}-linux-${global.hostname}"
 

arch/arm/boards/zii-imx51-rdu1/defaultenv-rdu1/boot/net
@@ -1,24 +1,0 @@
-#!/bin/sh
-
-path="/mnt/tftp"
-
-# clear seat network config
-global.linux.bootargs.rdu_network=
-
-global.bootm.image="${path}/${global.user}-linux-${global.hostname}"
-
-oftree="${path}/${global.user}-oftree-${global.hostname}"
-if [ -f "${oftree}" ]; then
-	global.bootm.oftree="$oftree"
-fi
-
-nfsroot="/home/${global.user}/nfsroot/${global.hostname}"
-
-ip_route_get -b ${global.net.server} global.linux.bootargs.dyn.ip
-
-initramfs="${path}/${global.user}-initramfs-${global.hostname}"
-if [ -f "${initramfs}" ]; then
-	global.bootm.initrd="$initramfs"
-else
-	global.linux.bootargs.dyn.root="root=/dev/nfs nfsroot=$nfsroot,v3,tcp"
-fi

commit 532864c6676439d12dd159dc33aeb584f7fcedbc

ARM: i.MX: Fix overwriting defaultenv with zii board specific env
zii_imx7d_dev_init() lacks a protection against running on the wrong
hardware. Add it so that the defaultenv no longer gets overwritten with
a board specific variant.

Fixes: 31300d79e8 ("ARM: zii-common: Add common environment directory")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/boards/zii-imx7d-dev/board.c
@@ -54,6 +54,8 @@ static int zii_imx7d_dev_init(void)
 		barebox_set_hostname("rpu2");
 	else if (of_machine_is_compatible("zii,imx7d-rmu2"))
 		barebox_set_hostname("rmu2");
+	else
+		return 0;
 
 	defaultenv_append_directory(defaultenv_zii_common);
 
barebox/barebox

commit 47b432c697c273603038a0a78736f2aa531a12a6

clk: divider: export clk_div_mask() helper
commit e6d3cc7b1fac3d7f1313faf8ac9b23830113e3ec Linux upstream.

  clk: divider: export clk_div_mask() helper

  Export clk_div_mask() in clk-provider header so every clock providers
  derived from the generic clock divider may share the definition instead
  of redefining it.

  Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
  Signed-off-by: Michael Turquette <mturquette@baylibre.com>
  Signed-off-by: Stephen Boyd <sboyd@kernel.org>

Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/clk/clk-divider.c
@@ -22,8 +22,6 @@
 #include <linux/log2.h>
 #include <asm-generic/div64.h>
 
-#define div_mask(width)	((1 << (width)) - 1)
-
 static unsigned int _get_table_maxdiv(const struct clk_div_table *table)
 {
 	unsigned int maxdiv = 0;
@@ -39,12 +37,12 @@ static unsigned int _get_maxdiv(const struct clk_div_table *table, u8 width,
 				unsigned long flags)
 {
 	if (flags & CLK_DIVIDER_ONE_BASED)
-		return div_mask(width);
+		return clk_div_mask(width);
 	if (flags & CLK_DIVIDER_POWER_OF_TWO)
-		return 1 << div_mask(width);
+		return 1 << clk_div_mask(width);
 	if (table)
 		return _get_table_maxdiv(table);
-	return div_mask(width) + 1;
+	return clk_div_mask(width) + 1;
 }
 
 static unsigned int _get_table_div(const struct clk_div_table *table,
@@ -111,7 +109,7 @@ static unsigned long clk_divider_recalc_rate(struct clk *clk,
 	unsigned int val;
 
 	val = readl(divider->reg) >> divider->shift;
-	val &= div_mask(divider->width);
+	val &= clk_div_mask(divider->width);
 
 	return divider_recalc_rate(clk, parent_rate, val, divider->table,
 				   divider->flags, divider->width);
@@ -270,7 +268,7 @@ int divider_get_val(unsigned long rate, unsigned long parent_rate,
 
 	value = _get_val(table, div, flags);
 
-	return min_t(unsigned int, value, div_mask(width));
+	return min_t(unsigned int, value, clk_div_mask(width));
 }
 
 static int clk_divider_set_rate(struct clk *clk, unsigned long rate,
@@ -296,11 +294,11 @@ static int clk_divider_set_rate(struct clk *clk, unsigned long rate,
 				divider->width, divider->flags);
 
 	val = readl(divider->reg);
-	val &= ~(div_mask(divider->width) << divider->shift);
+	val &= ~(clk_div_mask(divider->width) << divider->shift);
 	val |= value << divider->shift;
 
 	if (clk->flags & CLK_DIVIDER_HIWORD_MASK)
-		val |= div_mask(divider->width) << (divider->shift + 16);
+		val |= clk_div_mask(divider->width) << (divider->shift + 16);
 
 	writel(val, divider->reg);
 

include/linux/clk.h
@@ -340,6 +340,8 @@ struct clk_divider {
 	int table_size;
 };
 
+#define clk_div_mask(width)	((1 << (width)) - 1)
+
 #define CLK_DIVIDER_POWER_OF_TWO	(1 << 1)
 #define CLK_DIVIDER_HIWORD_MASK		(1 << 3)
 #define CLK_DIVIDER_READ_ONLY		(1 << 5)

commit 0199332f6b772149e47303b4244af06867247344

clk: divider: fix divider setting with CLK_SET_RATE_PARENT
When CLK_SET_RATE_PARENT is set for a divider then clk_divider_bestdiv()
is called which returns the best parent rate. This best parent rate must
be fed into divider_get_val(), not the unchanged original parent rate.

Fixes: 47b432c697 ("clk: divider: export clk_div_mask() helper")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/clk/clk-divider.c
@@ -265,19 +265,17 @@ static int clk_divider_set_rate(struct clk *clk, unsigned long rate,
 		unsigned long parent_rate)
 {
 	struct clk_divider *divider = container_of(clk, struct clk_divider, clk);
-	unsigned int div, value;
+	unsigned int value;
 	u32 val;
 
 	if (divider->flags & CLK_DIVIDER_READ_ONLY)
 		return 0;
 
 	if (clk->flags & CLK_SET_RATE_PARENT) {
-		unsigned long best_parent_rate = parent_rate;
-		div = clk_divider_bestdiv(clk, rate, &best_parent_rate,
-				divider->table, divider->width, divider->flags);
-		clk_set_rate(clk_get_parent(clk), best_parent_rate);
-	} else {
-		div = DIV_ROUND_UP(parent_rate, rate);
+		clk_divider_bestdiv(clk, rate, &parent_rate,
+				    divider->table, divider->width,
+				    divider->flags);
+		clk_set_rate(clk_get_parent(clk), parent_rate);
 	}
 
 	value = divider_get_val(rate, parent_rate, divider->table,
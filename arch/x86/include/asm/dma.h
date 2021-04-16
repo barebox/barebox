ce65ca49fda3a849f80ddb7d8cafcf90ed765e2c
Ahmad Fatoum <ahmad@a3f.at>
2021-04-16 08:24:33 +0200

    x86: add DMA support
    Both interconnect and PCI are cache coherent on x86, so we shouldn't
    need any special CPU barriers for DMA. Indeed, Linux defined neither
    ARCH_HAS_SYNC_DMA_FOR_CPU nor ARCH_HAS_SYNC_DMA_FOR_DEVICE on x86.
    
    It thus seems that the only reordering we need to take care of is
    compiler-induced reordering. The Linux memory model that barebox adheres
    to as well demands that all accesses to shared data are volatile.
    
    volatile accesses are already guarnateed to not be reordered against
    each other, so we don't even need an explicit barrier(), which is
    already the case on other architectures that have a disabled MMU.
    
    Cc: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
    Link: https://lore.barebox.org/20210416062436.332665-2-ahmad@a3f.at
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/x86/include/asm/dma.h
+++ arch/x86/include/asm/dma.h
@@ -4,6 +4,40 @@
 #ifndef __ASM_DMA_H
 #define __ASM_DMA_H
 
-/* empty */
+#include <linux/string.h>
+#include <linux/compiler.h>
+#include <xfuncs.h>
+#include <malloc.h>
+
+/*
+ * x86 is cache coherent, so we need not do anything special here
+ */
+
+static inline void *dma_alloc_coherent(size_t size, dma_addr_t *dma_handle)
+{
+	void *ret = xmemalign(4096, size);
+	if (dma_handle)
+		*dma_handle = (dma_addr_t)ret;
+
+	memset(ret, 0, size);
+
+	return ret;
+}
+
+static inline void dma_free_coherent(void *mem, dma_addr_t dma_handle,
+				     size_t size)
+{
+	free(mem);
+}
+
+static inline void dma_sync_single_for_cpu(dma_addr_t address, size_t size,
+					   enum dma_data_direction dir)
+{
+}
+
+static inline void dma_sync_single_for_device(dma_addr_t address, size_t size,
+					      enum dma_data_direction dir)
+{
+}
 
 #endif /* __ASM_DMA_H */

963b55822f6db510c9aff6a6a75aa4d7a5efb460
Ahmad Fatoum <a.fatoum@pengutronix.de>
2021-06-19 06:50:41 +0200

    net: designware: fix non-1:1 mapped 64-bit systems
    drivers/net/designware.c handles the older Designware < 4.x MAC IPs,
    which do not support DMA beyond 32-bit. They are still being integrated
    into SoCs with 64-bit CPUs like the StarFive JH7100, which additionally
    needs a non 1:1 mapping for coherent DMA.
    
    Fix the driver to support such usage. The driver still has the assumption
    that barebox core will only pass it 32-bit pointers. This is now made
    explicit by returning error codes when the DMA mask is violated.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20210619045055.779-16-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/riscv/include/asm/io.h
+++ arch/riscv/include/asm/io.h
@@ -5,4 +5,14 @@
 
 #include <asm-generic/io.h>
 
+static inline void *phys_to_virt(unsigned long phys)
+{
+	return (void *)phys;
+}
+
+static inline unsigned long virt_to_phys(volatile void *mem)
+{
+	return (unsigned long)mem;
+}
+
 #endif /* __ASM_RISCV_IO_H */

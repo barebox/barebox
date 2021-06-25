33ea0fbcb1b9eb27322e1d5a112bbac7fa39f69f
Marco Felsch <m.felsch@pengutronix.de>
2021-06-25 09:25:35 +0200

    ARM: i.MX: esdctl: add deep-probe support
    In case of deep-probe we have to ensure that the memory device is
    available after the mem_initcall().
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    Link: https://lore.pengutronix.de/20201021115813.31645-9-m.felsch@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Link: https://lore.barebox.org/20210625072540.32717-15-s.hauer@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/esdctl.c
+++ arch/arm/mach-imx/esdctl.c
@@ -696,7 +696,17 @@ static struct driver_d imx_esdctl_driver = {
 	.of_compatible = DRV_OF_COMPAT(imx_esdctl_dt_ids),
 };
 
-mem_platform_driver(imx_esdctl_driver);
+static int imx_esdctl_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&imx_esdctl_driver);
+	if (ret)
+		return ret;
+
+	return of_devices_ensure_probed_by_dev_id(imx_esdctl_dt_ids);
+}
+mem_initcall(imx_esdctl_init);
 
 /*
  * The i.MX SoCs usually have two SDRAM chipselects. The following

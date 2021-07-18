barebox/barebox

commit 655e085e3a22d52eb040e7c3bbd2fa1cb6c249c1

crypto: caam - Always do rng selftest
The caam rng selftest is known to be broken in several i.MX
incarnations. To be on the safe side just unconditionally execute
it rather than trying to guess from HAB failure events if this is
necessary.
We can only do the selftest once per boot though, doing it a second time
yields an error:

rng_self_test: Job Error:
2101000.jr0@1000.of: 20001953: CCB: desc idx 25: RNG: Instantiate

so only do the test when rng is not yet initialized as tested with the
RDSTA_IFx status bits.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/hab/habv4.c
@@ -388,18 +388,6 @@ static void habv4_display_event(uint8_t *data, uint32_t len)
 	habv4_display_event_record((struct hab_event_record *)data);
 }
 
-/* Some chips with HAB >= 4.2.3 have an incorrect implementation of the RNG
- * self-test in ROM code. In this case, an HAB event is generated, and a
- * software self-test should be run. This variable is set to @c true by
- * habv4_get_status() when this occurs. */
-static bool habv4_need_rng_software_self_test;
-
-bool caam_need_rng_software_selftest(void)
-{
-	return IS_ENABLED(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST) &&
-		habv4_need_rng_software_self_test;
-}
-
 #define RNG_FAIL_EVENT_SIZE 36
 static uint8_t habv4_known_rng_fail_events[][RNG_FAIL_EVENT_SIZE] = {
 	{ 0xdb, 0x00, 0x24, 0x42,  0x69, 0x30, 0xe1, 0x1d,
@@ -457,7 +445,6 @@ static int habv4_get_status(const struct habv4_rvt *rvt)
 		if (IS_ENABLED(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST) &&
 		    is_known_rng_fail_event(data, len)) {
 			pr_debug("RNG self-test failure detected, will run software self-test\n");
-			habv4_need_rng_software_self_test = true;
 		} else {
 			pr_err("-------- HAB warning Event %d --------\n", index);
 			pr_err("event data:\n");

drivers/crypto/caam/Kconfig
@@ -34,26 +34,3 @@ config CRYPTO_DEV_FSL_CAAM_RNG
 	help
 	  Selecting this will register the SEC4 hardware rng.
 
-if CRYPTO_DEV_FSL_CAAM_RNG
-
-config CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST
-	bool "Run RNG software self-test on impacted chips"
-	depends on ARCH_IMX6
-	depends on HABV4
-	default y
-	help
-	  Some chips with HAB >= 4.2.3 have an incorrect implementation of the
-	  RNG self-test in ROM code. In this case, a software self-test should
-	  be run to ensure correctness of the RNG. By enabling this config
-	  option, the software self-test is run automatically when this case
-	  is detected.
-
-	  Currently known impacted chips:
-	  * i.MX6DQ+ silicon revision 1.1
-	  * i.MX6DQ silicon revision 1.6
-	  * i.MX6DLS silicon revision 1.4
-	  * i.MX6SX silicon revision 1.4
-	  * i.MX6UL silicon revision 1.2
-	  * i.MX67SD silicon revision 1.3
-
-endif

drivers/crypto/caam/Makefile
@@ -3,5 +3,5 @@
 #
 obj-$(CONFIG_CRYPTO_DEV_FSL_CAAM) += ctrl.o error.o jr.o
 obj-$(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG) += caamrng.o
-obj-$(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST) += rng_self_test.o
+obj-$(CONFIG_CRYPTO_DEV_FSL_CAAM) += rng_self_test.o
 obj-$(CONFIG_BLOBGEN) += caam-blobgen.o

drivers/crypto/caam/ctrl.c
@@ -572,8 +572,12 @@ static int caam_probe(struct device_d *dev)
 
 	cha_vid_ls = rd_reg32(&ctrl->perfmon.cha_id_ls);
 
-	/* habv4_need_rng_software_self_test is determined by habv4_get_status() */
-	if (caam_need_rng_software_selftest()) {
+	/*
+	 * Only do the rng self test when the state handles are not yet
+	 * instantiated as indicated by the RDSTA_IF1 and RDSTA_IF2 flags.
+	 * Otherwise the self test fails.
+	 */
+	if (!(rd_reg32(&ctrl->r4tst[0].rdsta) & RDSTA_IFMASK)) {
 		u8 caam_era;
 		u8 rngvid;
 		u8 rngrev;

drivers/crypto/caam/rng_self_test.c
@@ -129,6 +129,21 @@ static void rng_self_test_done(struct device_d *dev, u32 *desc, u32 err, void *a
 /*
  * caam_rng_self_test() - Perform RNG self test
  * Returns zero on success, and negative on error.
+ *
+ * Some chips with HAB >= 4.2.3 have an incorrect implementation of the
+ * RNG self-test in ROM code. In this case, a software self-test should
+ * be run to ensure correctness of the RNG. By enabling this config
+ * option, the software self-test is run automatically when this case
+ * is detected.
+ *
+ * Currently known impacted chips:
+ * * i.MX6DQ+ silicon revision 1.1
+ * * i.MX6DQ silicon revision 1.6
+ * * i.MX6DLS silicon revision 1.4
+ * * i.MX6SX silicon revision 1.4
+ * * i.MX6UL silicon revision 1.2
+ * * i.MX67SD silicon revision 1.3
+ *
  */
 int caam_rng_self_test(struct device_d *dev, const u8 caam_era, const u8 rngvid, const u8 rngrev)
 {

include/hab.h
@@ -23,7 +23,6 @@
 #ifdef CONFIG_HABV4
 int imx28_hab_get_status(void);
 int imx6_hab_get_status(void);
-bool caam_need_rng_software_selftest(void);
 #else
 static inline int imx28_hab_get_status(void)
 {
@@ -32,10 +31,6 @@ static inline int imx28_hab_get_status(void)
 static inline int imx6_hab_get_status(void)
 {
 	return -EPERM;
-}
-static inline bool caam_need_rng_software_selftest(void)
-{
-	return false;
 }
 #endif
 

commit fb253775910a08307b0a272b97dcf466067edf40

habv4: habv4_get_status(): do not print HAB error event in case of known RNG self test failure
In commit

| 655e085e3a22 ("crypto: caam - Always do rng selftest")

the kconfig option CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST was
removed, which resulted in the pr_debug() never being executed. Remove
the IS_ENABLED() to restore the original behavior, i.e. to not print
the HAB error Event if it is the known RNG self-test failure.

Fixes: 655e085e3a22 ("crypto: caam - Always do rng selftest")
Reported-by: Roland Hieber <rhi@pengutronix.de>
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
Reviewed-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Link: https://lore.barebox.org/20210716122110.4022409-1-mkl@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/hab/habv4.c
@@ -553,8 +553,7 @@ static int habv4_get_status(const struct habv4_rvt *rvt)
 			break;
 
 		/* suppress RNG self-test fail events if they can be handled in software */
-		if (IS_ENABLED(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST) &&
-		    is_known_rng_fail_event(data, len)) {
+		if (is_known_rng_fail_event(data, len)) {
 			pr_debug("RNG self-test failure detected, will run software self-test\n");
 		} else {
 			pr_err("-------- HAB Event %d --------\n", i);
b74a0ba0bc333d781a6a37d265c8494f488290f2
Ahmad Fatoum <a.fatoum@pengutronix.de>
2021-10-30 19:56:29 +0200

    drivers: caam: remove license text covered by SPDX-License-Identifier
    There is already a SPDX-License-Identifier, so drop the boilerplate.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20211030175632.2276077-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/crypto/caam/rng_self_test.h
+++ drivers/crypto/caam/rng_self_test.h
@@ -4,16 +4,6 @@
  * Copyright (C) 2018 Pengutronix, Roland Hieber <r.hieber@pengutronix.de>
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #ifndef RNG_SELF_TEST_H

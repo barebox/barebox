barebox/barebox

commit a3cf324593eada0065d63e8db9d95bf7ee788ba0

mci: dw_mmc: add optional reset line
The bindings describe a reset line for the MMC controller and we have
boards with device tree that contain a reset line that barebox has so
far never acted upon. For the StarFive, we need the reset, so have the
driver toggle it optionally if specified. This change wasn't tested
on existing users of the driver, like the SoCFPGA or the Rockchip
rk2928 and rk3288.

Cc: Steffen Trumtrar <s.trumtrar@pengutronix.de>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20210619045055.779-21-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/dw_mmc.c
@@ -17,6 +17,7 @@
 #include <io.h>
 #include <platform_data/dw_mmc.h>
 #include <linux/bitops.h>
+#include <linux/reset.h>
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <errno.h>
@@ -548,6 +549,7 @@ static int dwmci_init(struct mci_host *mci, struct device_d *dev)
 
 static int dw_mmc_probe(struct device_d *dev)
 {
+	struct reset_control	*rst;
 	struct resource *iores;
 	struct dwmci_host *host;
 	struct dw_mmc_platform_data *pdata = dev->platform_data;
@@ -567,6 +569,15 @@ static int dw_mmc_probe(struct device_d *dev)
 
 	clk_enable(host->clk_biu);
 	clk_enable(host->clk_ciu);
+
+	rst = reset_control_get(dev, "reset");
+	if (IS_ERR(rst)) {
+		dev_warn(dev, "error claiming reset: %pe\n", rst);
+	} else if (rst) {
+		reset_control_assert(rst);
+		udelay(10);
+		reset_control_deassert(rst);
+	}
 
 	iores = dev_request_mem_resource(dev, 0);
 	if (IS_ERR(iores))

commit 724512bbac14b869f165cdbef46bfdca5303a51a

mci: dw_mmc: make reset control optional again
As documented in 90bdf1e5d1e4 ("mci: dw_mmc: match against StarFive MMC
compatibles"), it was intended for the reset to remain optional as to
not break existing users. Unfortunately, my later a3cf324593ea
("mci: dw_mmc: add optional reset line") didn't heed that and made it
required, breaking SoCFPGA DW-MMC use as a result.

Revert that line to fix the regression.

Fixes: a3cf324593ea ("mci: dw_mmc: add optional reset line")
Reported-by: Ian Abbott <abbotti@mev.co.uk>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20211101175207.2160779-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/dw_mmc.c
@@ -572,7 +572,7 @@ static int dw_mmc_probe(struct device_d *dev)
 
 	rst = reset_control_get(dev, "reset");
 	if (IS_ERR(rst)) {
-		return PTR_ERR(rst);
+		dev_warn(dev, "error claiming reset: %pe\n", rst);
 	} else if (rst) {
 		reset_control_assert(rst);
 		udelay(10);
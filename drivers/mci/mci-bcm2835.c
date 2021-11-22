barebox/barebox

commit 28f4a6a4df76f0f1581d1fad86b4efbd6f2c9816

drivers: add missing SPDX-License-Identifier
This adds the suitable SPDX-License-Identifier to all files in drivers/
that previously lacked one.

To aid manual inspection, following heuristics can be used:

* No changes outside of comments/whitespace:

  git show -U0 HEAD | rg -v '^(@@|diff|index)|[-+]([-+]|//|#|[\s/]\*)'

* -or-later come in pairs:

  git show --inter-hunk-context=19 HEAD | \
    perl -0777 -F'/^@/gm' -ne 'for (@F) { @m = /later/g; print if @m & 1 }'

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20211030175632.2276077-4-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/mci-bcm2835.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Raspberry PI MCI driver
  *
@@ -9,21 +10,7 @@
  * timing workarounds) obviously extracted from the Linux kernel at:
  * https://github.com/raspberrypi/linux.git rpi-3.6.y
  *
- * The Linux kernel code has the following (c) and license, which is hence
- * propagated to here:
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * The Linux kernel code has the following (c)
  *
  * Author: Wilhelm Lundgren <wilhelm.lundgren@cybercom.com>
  */

commit 4cca29fcf6c92703b50f7883f0318faa87352d69

drivers: mci-bcm2835: remove useless comment
The comment serves no purpose anymore since the previously following
license blurb was migrated to an SPDX license identifier.

Fixes: 28f4a6a4df76f0f1581d (2021-10-30, "drivers: add missing SPDX-License-Identifier")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Link: https://lore.barebox.org/20211117113851.2022669-1-rhi@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/mci-bcm2835.c
@@ -10,8 +10,6 @@
  * timing workarounds) obviously extracted from the Linux kernel at:
  * https://github.com/raspberrypi/linux.git rpi-3.6.y
  *
- * The Linux kernel code has the following (c)
- *
  * Author: Wilhelm Lundgren <wilhelm.lundgren@cybercom.com>
  */
 
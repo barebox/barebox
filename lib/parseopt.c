dc8c0a9425a7be301e03e77489a25b14e20b383a
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-01-03 13:02:05 +0100

    parseopt: drop unused, duplicate, parseopt_u16
    We always have sizeof(unsigned short) == sizeof(uint16_t), so
    parseopt_u16() is exactly equivalent to parseopt_hu().
    
    The former is unused anyway, so just drop it.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20220103120205.1728778-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- lib/parseopt.c
+++ lib/parseopt.c
@@ -59,37 +59,6 @@ again:
 		*val = v;
 }
 
-void parseopt_u16(const char *options, const char *opt, uint16_t *val)
-{
-	const char *start;
-	size_t optlen = strlen(opt);
-	ulong v;
-	char *endp;
-
-again:
-	start = strstr(options, opt);
-
-	if (!start)
-		return;
-
-	if (start > options && start[-1] != ',') {
-		options = start;
-		goto again;
-	}
-
-	if (start[optlen] != '=') {
-		options = start;
-		goto again;
-	}
-
-	v = simple_strtoul(start + optlen + 1, &endp, 0);
-	if (v > U16_MAX)
-		return;
-
-	if (*endp == ',' || *endp == '\0')
-		*val = v;
-}
-
 void parseopt_str(const char *options, const char *opt, char **val)
 {
 	const char *start;

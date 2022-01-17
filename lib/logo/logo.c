cad82010e259d419c24679267c43990abd7b7482
Jules Maselbas <jmaselbas@kalray.eu>
2022-01-18 00:02:34 +0100

    logo: Enable image format selection for the logo
    Enables the selection of an image format to be used for the built-in
    barebox logo. The default image format is set to png, has it was the
    only choice before. This also adds bmp as the other option.
    
    Signed-off-by: Jules Maselbas <jmaselbas@kalray.eu>
    Link: https://lore.barebox.org/20220117230235.13549-5-jmaselbas@kalray.eu
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- lib/logo/logo.c
+++ lib/logo/logo.c
@@ -32,8 +32,14 @@ static void load_logo(int width, void *start, void *end)
 {
 	char *filename;
 	size_t size = end - start;
+	char *ext = "";
 
-	filename = basprintf("/logo/barebox-logo-%d.png", width);
+	if (IS_ENABLED(CONFIG_BAREBOX_LOGO_PNG))
+		ext = "png";
+	else if (IS_ENABLED(CONFIG_BAREBOX_LOGO_BMP))
+		ext = "bmp";
+
+	filename = basprintf("/logo/barebox-logo-%d.%s", width, ext);
 	write_file(filename, start, size);
 	free(filename);
 }

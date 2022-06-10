barebox/barebox

commit b5da6970bf235b1f89cbdcf77352ba8990931606

ata: ahci: add missing capability in ahci_print_info()
Signed-off-by: Denis Orlov <denorl2009@gmail.com>
Link: https://lore.barebox.org/20220504092553.27961-13-denorl2009@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/ata/ahci.c
@@ -532,6 +532,7 @@ void ahci_print_info(struct ahci_device *ahci)
 	       "%s%s%s%s%s%s\n",
 	       cap & HOST_CAP_64 ? "64bit " : "",
 	       cap & HOST_CAP_NCQ ? "ncq " : "",
+	       cap & HOST_CAP_SNTF ? "sntf " : "",
 	       cap & HOST_CAP_SMPS ? "ilck " : "",
 	       cap & HOST_CAP_SSS ? "stag " : "",
 	       cap & HOST_CAP_ALPM ? "pm " : "",

commit a7a5f6e5a2713ac59b0b714bdab63746c7240f14

ata: ahci: Fix format string
drivers/ata/ahci.c: In function 'ahci_print_info':
drivers/ata/ahci.c:517:9: warning: too many arguments for format [-Wformat-extra-args]

Fixes: b5da6970bf23 ("ata: ahci: add missing capability in ahci_print_info()")
Signed-off-by: Alexander Shiyan <eagle.alexander923@gmail.com>
Link: https://lore.barebox.org/20220610054051.4911-3-eagle.alexander923@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/ata/ahci.c
@@ -304,8 +304,8 @@ static int ahci_init_port(struct ahci_port *ahci_port)
 	ahci_port->cmd_slot = mem;
 	ahci_port->cmd_slot_dma = mem_dma;
 
-	ahci_port_debug(ahci_port, "cmd_slot = 0x%p (0x%pa)\n",
-			ahci_port->cmd_slot, ahci_port->cmd_slot_dma);
+	ahci_port_debug(ahci_port, "cmd_slot = 0x%p (0x%pad)\n",
+			ahci_port->cmd_slot, &ahci_port->cmd_slot_dma);
 
 	/*
 	 * Second item: Received-FIS area
@@ -320,8 +320,8 @@ static int ahci_init_port(struct ahci_port *ahci_port)
 	ahci_port->cmd_tbl = mem + AHCI_CMD_LIST_SZ + AHCI_RX_FIS_SZ;
 	ahci_port->cmd_tbl_dma = mem_dma + AHCI_CMD_LIST_SZ + AHCI_RX_FIS_SZ;
 
-	ahci_port_debug(ahci_port, "cmd_tbl = 0x%p (0x%pa)\n",
-			ahci_port->cmd_tbl, ahci_port->cmd_tbl_dma);
+	ahci_port_debug(ahci_port, "cmd_tbl = 0x%p (0x%pad)\n",
+			ahci_port->cmd_tbl, &ahci_port->cmd_tbl_dma);
 
 	ahci_port->cmd_tbl_sg = ahci_port->cmd_tbl + AHCI_CMD_TBL_HDR_SZ;
 
@@ -517,7 +517,7 @@ void ahci_print_info(struct ahci_device *ahci)
 	printf("flags: "
 	       "%s%s%s%s%s%s%s"
 	       "%s%s%s%s%s%s%s"
-	       "%s%s%s%s%s%s\n",
+	       "%s%s%s%s%s%s%s\n",
 	       cap & HOST_CAP_64 ? "64bit " : "",
 	       cap & HOST_CAP_NCQ ? "ncq " : "",
 	       cap & HOST_CAP_SNTF ? "sntf " : "",
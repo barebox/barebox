c094f3d012bf3e078082590522ff7b077933ee55
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-06-29 10:56:27 +0200

    watchdog: imxwd: Convert error messages in .probe to dev_err_probe()
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20220629085628.106086-1-u.kleine-koenig@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/watchdog/imxwd.c
+++ drivers/watchdog/imxwd.c
@@ -239,10 +239,9 @@ static int imx_wd_probe(struct device_d *dev)
 
 	priv = xzalloc(sizeof(struct imx_wd));
 	iores = dev_request_mem_resource(dev, 0);
-	if (IS_ERR(iores)) {
-		dev_err(dev, "could not get memory region\n");
-		return PTR_ERR(iores);
-	}
+	if (IS_ERR(iores))
+		return dev_err_probe(dev, PTR_ERR(iores),
+				     "could not get memory region\n");
 
 	clk = clk_get(dev, NULL);
 	if (IS_ERR(clk))
@@ -279,7 +278,8 @@ static int imx_wd_probe(struct device_d *dev)
 	if (priv->ops->init) {
 		ret = priv->ops->init(priv);
 		if (ret) {
-			dev_err(dev, "Failed to init watchdog device %d\n", ret);
+			dev_err_probe(dev, ret,
+				      "Failed to init watchdog device\n");
 			goto error_unregister;
 		}
 	}

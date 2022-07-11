barebox/barebox

commit 7f9f45b9bfef2ce8706f25c46dcbd109e4181164

devfs: Do not create overlapping partitions
Until now it has been possible to create overlapping partitions. Go away
from that and allow to create partitions only in unallocated areas of a
device. This lowers the risk of having inconsistent partitioning and
increases the chance that inconsistent partitioning is recognized by
the user.

We had explicit overlap checking for the environment partition which
becomes unnecessary with this change and is removed.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
Link: https://lore.barebox.org/20211011073025.4187545-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/devfs-core.c
@@ -301,6 +301,37 @@ int devfs_remove(struct cdev *cdev)
 	return 0;
 }
 
+static bool region_overlap(loff_t starta, loff_t lena,
+			   loff_t startb, loff_t lenb)
+{
+	if (starta + lena <= startb)
+		return 0;
+	if (startb + lenb <= starta)
+		return 0;
+	return 1;
+}
+
+static int check_overlap(struct cdev *cdev, const char *name, loff_t offset, loff_t size)
+{
+	struct cdev *cpart;
+
+	list_for_each_entry(cpart, &cdev->partitions, partition_entry) {
+		if (region_overlap(cpart->offset, cpart->size,
+				   offset, size))
+			goto conflict;
+	}
+
+	return 0;
+
+conflict:
+	pr_err("New partition %s (0x%08llx-0x%08llx) on %s "
+		"overlaps with partition %s (0x%08llx-0x%08llx), not creating it\n",
+		name, offset, offset + size - 1, cpart->name,
+		cpart->name, cpart->offset, cpart->offset + cpart->size - 1);
+
+	return -EINVAL;
+}
+
 static struct cdev *__devfs_add_partition(struct cdev *cdev,
 		const struct devfs_partition *partinfo, loff_t *end)
 {
@@ -335,6 +366,9 @@ static struct cdev *__devfs_add_partition(struct cdev *cdev,
 				partinfo->name, cdev->name);
 		return ERR_PTR(-EINVAL);
 	}
+
+	if (check_overlap(cdev, partinfo->name, offset, size))
+		return ERR_PTR(-EINVAL);
 
 	if (IS_ENABLED(CONFIG_MTD) && cdev->mtd) {
 		struct mtd_info *mtd;

common/startup.c
@@ -68,70 +68,6 @@ static int mount_root(void)
 fs_initcall(mount_root);
 #endif
 
-#ifdef CONFIG_ENV_HANDLING
-static bool region_overlap(loff_t starta, loff_t lena,
-			   loff_t startb, loff_t lenb)
-{
-	if (starta + lena <= startb)
-		return 0;
-	if (startb + lenb <= starta)
-		return 0;
-	return 1;
-}
-
-static int check_overlap(const char *path)
-{
-	struct cdev *cenv, *cdisk, *cpart;
-	const char *name;
-
-	name = devpath_to_name(path);
-
-	if (name == path)
-		/*
-		 * no /dev/ in front, so *path is some file. No need to
-		 * check further.
-		 */
-		return 0;
-
-	cenv = cdev_by_name(name);
-	if (!cenv)
-		return -EINVAL;
-
-	if (cenv->mtd)
-		return 0;
-
-	cdisk = cenv->master;
-
-	if (!cdisk)
-		return 0;
-
-	list_for_each_entry(cpart, &cdisk->partitions, partition_entry) {
-		if (cpart == cenv)
-			continue;
-
-		if (region_overlap(cpart->offset, cpart->size,
-				   cenv->offset, cenv->size))
-			goto conflict;
-	}
-
-	return 0;
-
-conflict:
-	pr_err("Environment partition (0x%08llx-0x%08llx) "
-		"overlaps with partition %s (0x%08llx-0x%08llx), not using it\n",
-		cenv->offset, cenv->offset + cenv->size - 1,
-		cpart->name,
-		cpart->offset, cpart->offset + cpart->size - 1);
-
-	return -EINVAL;
-}
-#else
-static int check_overlap(const char *path)
-{
-	return 0;
-}
-#endif
-
 static int load_environment(void)
 {
 	const char *default_environment_path;
@@ -143,11 +79,7 @@ static int load_environment(void)
 		defaultenv_load("/env", 0);
 
 	if (IS_ENABLED(CONFIG_ENV_HANDLING)) {
-		ret = check_overlap(default_environment_path);
-		if (ret)
-			default_environment_path_set(NULL);
-		else
-			envfs_load(default_environment_path, "/env", 0);
+		envfs_load(default_environment_path, "/env", 0);
 	} else {
 		if (IS_ENABLED(CONFIG_DEFAULT_ENVIRONMENT))
 			pr_notice("No support for persistent environment. Using default environment\n");

commit 9d42c77f126eb394fe369797a49ada67e4495faf

devfs: take into account for the partitions check that mtd is different
mtd partitions are separate devices. Their partition info is relative to
these subdevices and so offset is 0 for them. This needs to be taken
into account for the overlap check.

Fixes: 7f9f45b9bfef ("devfs: Do not create overlapping partitions")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Link: https://lore.barebox.org/20220711090915.1487105-1-u.kleine-koenig@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/devfs-core.c
@@ -395,9 +395,20 @@ static bool region_overlap(loff_t starta, loff_t lena,
 static int check_overlap(struct cdev *cdev, const char *name, loff_t offset, loff_t size)
 {
 	struct cdev *cpart;
+	loff_t cpart_offset;
 
 	list_for_each_entry(cpart, &cdev->partitions, partition_entry) {
-		if (region_overlap(cpart->offset, cpart->size,
+		cpart_offset = cpart->offset;
+
+		/*
+		 * An mtd partition is represented by a separate cdev and its
+		 * cpart is relative to this one. So its .offset is 0 and we
+		 * have to consult .master_offset to get its offset.
+		 */
+		if (cpart->mtd)
+			cpart_offset = cpart->mtd->master_offset;
+
+		if (region_overlap(cpart_offset, cpart->size,
 				   offset, size))
 			goto conflict;
 	}
@@ -408,7 +419,7 @@ conflict:
 	pr_err("New partition %s (0x%08llx-0x%08llx) on %s "
 		"overlaps with partition %s (0x%08llx-0x%08llx), not creating it\n",
 		name, offset, offset + size - 1, cpart->name,
-		cpart->name, cpart->offset, cpart->offset + cpart->size - 1);
+		cpart->name, cpart_offset, cpart_offset + cpart->size - 1);
 
 	return -EINVAL;
 }
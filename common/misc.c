barebox/barebox

commit 81dd24a0946c694f3d40dadc0e01254ce15a79bb

of: add generic of_prepend_machine_compatible()
Add generic function to extend/fixup machine compatible.

Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
Link: https://lore.barebox.org/20220503091220.3871612-4-o.rempel@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/misc.c
@@ -150,6 +150,7 @@ BAREBOX_MAGICVAR(global.model, "Product name of this hardware");
 
 static char *hostname;
 static char *serial_number;
+static char *of_machine_compatible;
 
 /*
  * The hostname is supposed to be the shortname of a board. It should
@@ -194,6 +195,28 @@ const char *barebox_get_serial_number(void)
 }
 
 BAREBOX_MAGICVAR(global.serial_number, "Board serial number");
+
+void barebox_set_of_machine_compatible(const char *__compatible)
+{
+	free(of_machine_compatible);
+	of_machine_compatible = xstrdup(__compatible);
+}
+
+const char *barebox_get_of_machine_compatible(void)
+{
+	return of_machine_compatible;
+}
+
+static int of_kernel_init(void)
+{
+	globalvar_add_simple_string("of.kernel.add_machine_compatible",
+				    &of_machine_compatible);
+
+	return 0;
+}
+device_initcall(of_kernel_init);
+
+BAREBOX_MAGICVAR(global.of.kernel.add_machine_compatible, "Additional machine/board compatible");
 
 void __noreturn panic(const char *fmt, ...)
 {

common/oftree.c
@@ -207,10 +207,15 @@ static int of_fixup_bootargs(struct device_node *root, void *unused)
 	int instance = reset_source_get_instance();
 	struct device_d *dev;
 	const char *serialno;
+	const char *compat;
 
 	serialno = barebox_get_serial_number();
 	if (serialno)
 		of_property_write_string(root, "serial-number", serialno);
+
+	compat = barebox_get_of_machine_compatible();
+	if (compat)
+		of_prepend_machine_compatible(root, compat);
 
 	node = of_create_node(root, "/chosen");
 	if (!node)
@@ -482,4 +487,35 @@ int of_autoenable_i2c_by_component(char *path)
 		printf("autoenabled i2c device %s\n", node->name);
 
 	return ret;
+}
+
+int of_prepend_machine_compatible(struct device_node *root, const char *compat)
+{
+	int cclen = 0, clen = strlen(compat) + 1;
+	const char *curcompat;
+	void *buf;
+
+	if (!root) {
+		root = of_get_root_node();
+		if (!root)
+			return -ENODEV;
+	}
+
+	if (of_device_is_compatible(root, compat))
+		return 0;
+
+	curcompat = of_get_property(root, "compatible", &cclen);
+
+	buf = xzalloc(cclen + clen);
+
+	memcpy(buf, compat, clen);
+
+	if (curcompat)
+		memcpy(buf + clen, curcompat, cclen);
+
+	of_set_property(root, "compatible", buf, cclen + clen, true);
+
+	free(buf);
+
+	return 0;
 }

include/common.h
@@ -129,4 +129,7 @@ void barebox_set_hostname_no_overwrite(const char *);
 const char *barebox_get_serial_number(void);
 void barebox_set_serial_number(const char *);
 
+void barebox_set_of_machine_compatible(const char *);
+const char *barebox_get_of_machine_compatible(void);
+
 #endif	/* __COMMON_H_ */

include/of.h
@@ -316,6 +316,7 @@ struct device_node *of_find_node_by_path_or_alias(struct device_node *root,
 		const char *str);
 int of_autoenable_device_by_path(char *path);
 int of_autoenable_i2c_by_component(char *path);
+int of_prepend_machine_compatible(struct device_node *root, const char *compat);
 #else
 static inline bool of_node_name_eq(const struct device_node *np, const char *name)
 {
@@ -834,6 +835,11 @@ static inline int of_autoenable_i2c_by_component(char *path)
 	return -ENODEV;
 }
 
+static int of_prepend_machine_compatible(struct device_node *root,
+					 const char *compat)
+{
+	return -ENODEV;
+}
 
 #endif
 

commit 1da26bfb9da2d0d0054169b15d90e9a2c85ae902

common: don't fixup empty serial/machine_compatible strings
There is no point in fixing up an empty string into the kernel device
tree, yet this can happen when globalvar_add_simple_string() is called
for the variable at least once as the function replaces NULL with an
allocated empty string. globalvar_add_simple_string() was called
unconditionally for of.kernel.add_machine_compatible, which in turn
led to always fixing up an empty string as the top-most compatible.

Resolve this by having barebox_get_(of_machine_compatible|serial_number)
return NULL for the empty string as well.

Fixes: 81dd24a0946c ("of: add generic of_prepend_machine_compatible()")
Fixes: f6756e9ce6f2 ("common: add $global.serial_number with device tree fixup")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20220718114824.2632364-2-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/misc.c
@@ -191,6 +191,8 @@ void barebox_set_serial_number(const char *__serial_number)
 
 const char *barebox_get_serial_number(void)
 {
+	if (!serial_number || *serial_number == '\0')
+		return NULL;
 	return serial_number;
 }
 
@@ -204,6 +206,8 @@ void barebox_set_of_machine_compatible(const char *__compatible)
 
 const char *barebox_get_of_machine_compatible(void)
 {
+	if (!of_machine_compatible || *of_machine_compatible == '\0')
+		return NULL;
 	return of_machine_compatible;
 }
 
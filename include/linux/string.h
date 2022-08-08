a6ae6ec12eb722efbb6a91a86ecbc2670c01c0c8
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-08-08 08:20:28 +0200

    string: implement str_has_prefix
    In addition to strstarts which we already have, Linux also defines
    str_has_prefix(), which returns the length of the prefix on success and
    zero otherwise. This allows writing straight-forward code to skip over
    a prefix in case it exists. Port it over.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20220808062028.391120-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/string.h
+++ include/linux/string.h
@@ -155,4 +155,25 @@ static inline bool strstarts(const char *str, const char *prefix)
 	return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
+/**
+ * str_has_prefix - Test if a string has a given prefix
+ * @str: The string to test
+ * @prefix: The string to see if @str starts with
+ *
+ * A common way to test a prefix of a string is to do:
+ *  strncmp(str, prefix, sizeof(prefix) - 1)
+ *
+ * But this can lead to bugs due to typos, or if prefix is a pointer
+ * and not a constant. Instead use str_has_prefix().
+ *
+ * Returns:
+ * * strlen(@prefix) if @str starts with @prefix
+ * * 0 if @str does not start with @prefix
+ */
+static __always_inline size_t str_has_prefix(const char *str, const char *prefix)
+{
+	size_t len = strlen(prefix);
+	return strncmp(str, prefix, len) == 0 ? len : 0;
+}
+
 #endif /* _LINUX_STRING_H_ */

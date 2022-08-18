eb86034040c255cc35b8493d47beec5cc3cb10c5
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-08-18 07:19:50 +0200

    soc: imx: add i.MX8M feature controller driver
    The tester4 fuse bank of the i.MX8M is a 32-bit collection of fuses,
    apaprently fused during test, which contains information about the
    available IPs: How many cores are available and whether a VPU and GPU
    is available an usable. Add a imx8m_feat_ctrl_init() function
    that initializes a bitmap of supported features using tester4's value
    and registers a feature controller with a check callback that just
    looks up the relevant bit.
    
    This function can then be called from a standalone driver or from the
    fuse bank (ocotp) driver itself.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20220818051955.2088238-6-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ include/soc/imx8m/featctrl.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-FileCopyrightText: 2022 Ahmad Fatoum, Pengutronix */
+
+#ifndef __IMX8M_FEATCTRL_H_
+#define __IMX8M_FEATCTRL_H_
+
+#include <linux/types.h>
+
+struct imx8m_featctrl_data {
+	u32 vpu_bitmask;
+	u32 gpu_bitmask;
+};
+
+#ifdef CONFIG_IMX8M_FEATCTRL
+int imx8m_feat_ctrl_init(struct device_d *dev, u32 tester4,
+			 const struct imx8m_featctrl_data *data);
+#else
+static inline int imx8m_feat_ctrl_init(struct device_d *dev, u32 tester4,
+				       const struct imx8m_featctrl_data *data)
+{
+	return -ENODEV;
+}
+#endif
+
+#endif

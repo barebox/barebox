13c7a12af0ab73adb5577adfd3d28731cd1b6190
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-09-30 17:38:19 +0200

    include: define SSIZE_MAX in <linux/limits.h>
    For symmetry with SIZE_MAX, which we already define, define SIZE_MAX as
    well. Definition taken from Linux.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20220930153819.744833-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/limits.h
+++ include/linux/limits.h
@@ -19,6 +19,7 @@
 #define SIZE_MAX	(~(size_t)0)
 #define PHYS_ADDR_MAX	(~(phys_addr_t)0)
 #define UINTPTR_MAX	ULONG_MAX
+#define SSIZE_MAX	((ssize_t)(SIZE_MAX >> 1))
 
 #define U8_MAX		((u8)~0U)
 #define S8_MAX		((s8)(U8_MAX >> 1))

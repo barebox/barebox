barebox/barebox

commit 726a802456bc200698cf921763cc27668eb3b649

common: bbu: only add available handlers
A board may have multiple handlers registered:

  registered update handlers:
    SD          -> /dev/mmc0.barebox
  * eMMC        -> /dev/mmc1

However when using the usbgadget with the -b command line argument,
fastboot will stat all devicefiles listed in the handlers and will fail
if i.e. the SD card is not available:

  usbgadget -A /dev/mmc1(root) -b
  udc0: registering UDC driver [g_multi]
  multi_bind: creating Fastboot function
  ERROR: g_multi udc0: failed to start g_multi: -2
  usbgadget: No such file or directory

To fix this, check the availability of handlers before adding them to
the list and skip those that are not available with an info level message.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Link: https://lore.barebox.org/20210618120557.2192098-1-r.czerwinski@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/bbu.c
@@ -32,12 +32,32 @@ static void append_bbu_entry(struct bbu_handler *handler, struct file_list *file
 	free(name);
 }
 
+static bool bbu_handler_is_available(struct bbu_handler *handler)
+{
+	struct stat s;
+	int ret;
+
+	device_detect_by_name(devpath_to_name(handler->devicefile));
+
+	ret = stat(handler->devicefile, &s);
+	if (ret)
+		return false;
+
+	return true;
+}
+
 void bbu_append_handlers_to_file_list(struct file_list *files)
 {
 	struct bbu_handler *handler;
 
-	list_for_each_entry(handler, &bbu_image_handlers, list)
-		append_bbu_entry(handler, files);
+	list_for_each_entry(handler, &bbu_image_handlers, list) {
+		if (bbu_handler_is_available(handler)) {
+			append_bbu_entry(handler, files);
+		} else {
+			pr_info("Skipping unavailable handler bbu-%s\n",
+				handler->name);
+		}
+	}
 }
 
 int bbu_force(struct bbu_data *data, const char *fmt, ...)

commit df22a22f84b668da75bf14095ff4ebc475dfe79a

bbu: fix exporting i.MX NAND bbu handler over fastboot
handler->devicefile for i.MX NAND is nand0.barebox, which lacks a /dev/
prefix. This is ok for detection, as device_detect_by_name expects the
cdev name and devpath_to_name() will strip a /dev/ if available.

For stat() however, we need to add back /dev/, otherwise the file can't
be found. Rework the code to do that.

This fixes an issue where usbgadget -A '' -b would not export a i.MX
NAND barebox update handler.

Fixes: 726a802456bc ("common: bbu: only add available handlers")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20220928085622.2677478-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/bbu.c
@@ -24,30 +24,18 @@
 
 static LIST_HEAD(bbu_image_handlers);
 
-static void append_bbu_entry(struct bbu_handler *handler, struct file_list *files)
+static void append_bbu_entry(const char *_name,
+			     const char *devicefile,
+			     struct file_list *files)
 {
 	char *name;
 
-	name = basprintf("bbu-%s", handler->name);
+	name = basprintf("bbu-%s", _name);
 
-	if (file_list_add_entry(files, name, handler->devicefile, 0))
+	if (file_list_add_entry(files, name, devicefile, 0))
 		pr_warn("duplicate partition name %s\n", name);
 
 	free(name);
-}
-
-static bool bbu_handler_is_available(struct bbu_handler *handler)
-{
-	struct stat s;
-	int ret;
-
-	device_detect_by_name(devpath_to_name(handler->devicefile));
-
-	ret = stat(handler->devicefile, &s);
-	if (ret)
-		return false;
-
-	return true;
 }
 
 void bbu_append_handlers_to_file_list(struct file_list *files)
@@ -55,12 +43,23 @@ void bbu_append_handlers_to_file_list(struct file_list *files)
 	struct bbu_handler *handler;
 
 	list_for_each_entry(handler, &bbu_image_handlers, list) {
-		if (bbu_handler_is_available(handler)) {
-			append_bbu_entry(handler, files);
+		const char *cdevname;
+		struct stat s;
+		char *devpath;
+
+		cdevname = devpath_to_name(handler->devicefile);
+		device_detect_by_name(cdevname);
+
+		devpath = basprintf("/dev/%s", cdevname);
+
+		if (stat(devpath, &s) == 0) {
+			append_bbu_entry(handler->name, devpath, files);
 		} else {
 			pr_info("Skipping unavailable handler bbu-%s\n",
 				handler->name);
 		}
+
+		free(devpath);
 	}
 }
 
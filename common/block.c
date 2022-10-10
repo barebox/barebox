barebox/barebox

commit 84173bf3d0ccc4036a16c4b074642aea59648063

block: set S_IFBLK for block devices instead of S_IFCHR
In barebox, block devices are a special case of character devices.
Nevertheless, differentiation can be useful to allow scripts iterating
over all block devices without accounting for naming, e.g.

  for dev in /dev/*; do
	test -b $dev && echo $dev: blockdevice
  done

Add the necessary support. This will break scripts that assume
test -c blockdevice to be true, but that's a quite improbable check.

Tested-by: Jules Maselbas <jmaselbas@kalray.eu>
Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
Link: https://lore.barebox.org/20220602195916.9061-2-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/block.c
@@ -361,6 +361,14 @@ static struct cdev_operations block_ops = {
 	.discard_range = block_op_discard_range,
 };
 
+struct block_device *cdev_get_block_device(struct cdev *cdev)
+{
+	if (!cdev || cdev->ops != &block_ops)
+		return NULL;
+
+	return container_of(cdev, struct block_device, cdev);
+}
+
 int blockdevice_register(struct block_device *blk)
 {
 	loff_t size = (loff_t)blk->num_blocks * BLOCKSIZE(blk);

commands/memtester/memtester.c
@@ -113,7 +113,7 @@ static int do_memtester(int argc, char **argv) {
                             strerror(errno));
                     return COMMAND_ERROR_USAGE;
                 } else {
-                    if (!S_ISCHR(statbuf.st_mode)) {
+                    if (!S_ISCHR(statbuf.st_mode) && !S_ISBLK(statbuf.st_mode)) {
                         printf("can not mmap non-char device %s\n",
                                 optarg);
                         return COMMAND_ERROR_USAGE;

fs/devfs.c
@@ -29,6 +29,7 @@
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/mtd-abi.h>
 #include <partition.h>
+#include <block.h>
 
 struct devfs_inode {
 	struct inode inode;
@@ -231,6 +232,7 @@ static struct inode *devfs_get_inode(struct super_block *sb, const struct inode 
 	default:
 		return NULL;
 	case S_IFCHR:
+	case S_IFBLK:
 		inode->i_op = &devfs_file_inode_operations;
 		inode->i_fop = &devfs_file_operations;
 		break;
@@ -250,12 +252,15 @@ static struct dentry *devfs_lookup(struct inode *dir, struct dentry *dentry,
 	struct devfs_inode *dinode;
 	struct inode *inode;
 	struct cdev *cdev;
+	umode_t mode;
 
 	cdev = cdev_by_name(dentry->name);
 	if (!cdev)
 		return ERR_PTR(-ENOENT);
 
-	inode = devfs_get_inode(dir->i_sb, dir, S_IFCHR);
+	mode = cdev_get_block_device(cdev) ? S_IFBLK : S_IFCHR;
+
+	inode = devfs_get_inode(dir->i_sb, dir, mode);
 	if (!inode)
 		return ERR_PTR(-ENOMEM);
 

fs/legacy.c
@@ -288,6 +288,7 @@ static struct inode *legacy_get_inode(struct super_block *sb, const struct inode
 		return NULL;
 	case S_IFREG:
 	case S_IFCHR:
+	case S_IFBLK:
 		inode->i_op = &legacy_file_inode_operations;
 		break;
 	case S_IFDIR:

include/block.h
@@ -49,4 +49,13 @@ static inline int block_flush(struct block_device *blk)
 	return cdev_flush(&blk->cdev);
 }
 
+#ifdef CONFIG_BLOCK
+struct block_device *cdev_get_block_device(struct cdev *cdev);
+#else
+static inline struct block_device *cdev_get_block_device(struct cdev *cdev)
+{
+	return NULL;
+}
+#endif
+
 #endif /* __BLOCK_H */

commit 0a21d8bbc3cd129d8f0ad2455417d4ce3f875a5d

block: fix calling cdev_get_block_device with partitions
For partitions, cdev->priv references the parent block device.
Doing the pointer arithmetic in container_of will result in an
out-of-bounds access. Do what the rest of the code does
and access cdev->priv instead.

Fixes: 84173bf3d0cc ("block: set S_IFBLK for block devices instead of S_IFCHR")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20221010063834.2110400-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/block.c
@@ -366,7 +366,7 @@ struct block_device *cdev_get_block_device(struct cdev *cdev)
 	if (!cdev || cdev->ops != &block_ops)
 		return NULL;
 
-	return container_of(cdev, struct block_device, cdev);
+	return cdev->priv;
 }
 
 int blockdevice_register(struct block_device *blk)
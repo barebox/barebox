8f97a2adf7ea7335f9b0c9813cd05cfe5a7b062d
Ahmad Fatoum <a.fatoum@pengutronix.de>
2022-10-10 08:11:19 +0200

    ARM: lds: define and use BAREBOX_RELOCATION_TABLE
    The structure of the relocation table is one of the main differences
    between different architectures' linker scripts. Hoist it out for ARM
    into <asm/barebox.lds.h>.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20221010061122.2084009-8-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/barebox.lds.h
+++ arch/arm/include/asm/barebox.lds.h
@@ -12,4 +12,19 @@
 #define BAREBOX_OUTPUT_ARCH	"aarch64"
 #endif
 
+#ifdef CONFIG_CPU_32
+#define BAREBOX_RELOCATION_TYPE	rel
+#else
+#define BAREBOX_RELOCATION_TYPE	rela
+#endif
+
+#define BAREBOX_RELOCATION_TABLE					\
+	.rel_dyn_start : { *(.__rel_dyn_start) }			\
+	.BAREBOX_RELOCATION_TYPE.dyn : { *(.BAREBOX_RELOCATION_TYPE*) }	\
+	.rel_dyn_end : { *(.__rel_dyn_end) }				\
+	.__dynsym_start :  { *(.__dynsym_start) }			\
+	.dynsym : { *(.dynsym) }					\
+	.__dynsym_end : { *(.__dynsym_end) }
+
+
 #include <asm-generic/barebox.lds.h>

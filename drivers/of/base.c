barebox/barebox

commit 90f70dbe29586b5fc98a9b96a41772cdd5ae251f

of: split part of of_get_stdoutpath into of_find_node_by_chosen
Follow-up commit will also lookup the value of a chosen property by full
path or alias, so factor this out into a helper function.

Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>
Link: https://lore.barebox.org/20230221070735.1130600-1-ahmad@a3f.at
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/of/base.c
@@ -2668,30 +2668,52 @@ void of_delete_node(struct device_node *node)
 	free(node);
 }
 
+/*
+ * of_find_node_by_chosen - Find a node given a chosen property pointing at it
+ * @propname:   the name of the property containing a path or alias
+ *              The function will lookup the first string in the property
+ *              value up to the first : character or till \0.
+ * @options     The Remainder (without : or \0 at the end) will be written
+ *              to *options if not NULL.
+ */
+struct device_node *of_find_node_by_chosen(const char *propname,
+					   const char **options)
+{
+	const char *value, *p;
+	char *buf = NULL;
+	struct device_node *dn;
+
+	value = of_get_property(of_chosen, propname, NULL);
+	if (!value)
+		return NULL;
+
+	p = strchrnul(value, ':');
+	if (*p)
+		buf = xstrndup(value, p - value);
+
+	dn = of_find_node_by_path_or_alias(NULL, buf);
+
+	free(buf);
+
+	if (options && *p)
+		*options = p + 1;
+
+	return dn;
+}
+
 struct device_node *of_get_stdoutpath(unsigned int *baudrate)
 {
+	const char *opts = NULL;
 	struct device_node *dn;
-	const char *name;
-	const char *p;
-	char *q;
 
-	name = of_get_property(of_chosen, "stdout-path", NULL);
-	if (!name)
-		name = of_get_property(of_chosen, "linux,stdout-path", NULL);
+	dn = of_find_node_by_chosen("stdout-path", &opts);
+	if (!dn)
+		dn = of_find_node_by_chosen("linux,stdout-path", &opts);
+	if (!dn)
+		return NULL;
 
-	if (!name)
-		return 0;
-
-	p = strchrnul(name, ':');
-
-	q = xstrndup(name, p - name);
-
-	dn = of_find_node_by_path_or_alias(NULL, q);
-
-	free(q);
-
-	if (baudrate && *p) {
-		unsigned rate = simple_strtoul(p + 1, NULL, 10);
+	if (baudrate && opts) {
+		unsigned rate = simple_strtoul(opts, NULL, 10);
 		if (rate)
 			*baudrate = rate;
 	}

include/of.h
@@ -314,6 +314,8 @@ struct cdev *of_parse_partition(struct cdev *cdev, struct device_node *node);
 int of_parse_partitions(struct cdev *cdev, struct device_node *node);
 int of_fixup_partitions(struct device_node *np, struct cdev *cdev);
 int of_partitions_register_fixup(struct cdev *cdev);
+struct device_node *of_find_node_by_chosen(const char *propname,
+					   const char **options);
 struct device_node *of_get_stdoutpath(unsigned int *);
 int of_device_is_stdout_path(struct device *dev, unsigned int *baudrate);
 const char *of_get_model(void);
@@ -367,6 +369,12 @@ static inline int of_fixup_partitions(struct device_node *np, struct cdev *cdev)
 static inline int of_partitions_register_fixup(struct cdev *cdev)
 {
 	return -ENOSYS;
+}
+
+static inline struct device_node *of_find_node_by_chosen(const char *propname,
+							 const char **options)
+{
+	return NULL;
 }
 
 static inline struct device_node *of_get_stdoutpath(unsigned int *rate)

commit b4a2ce5bd888c57422c2c3e95b68b094d4a63551

of: Fix of_get_stdoutpath() when property does not contain a colon
With 90f70dbe29 'buf' only gets initialized when the stdout-path
property contains a colon. It changes

	p = strchrnul(value, ':');
	buf = xstrndup(value, p - value);

to
	p = strchrnul(value, ':');
	if (*p)
		buf = xstrndup(value, p - value);

With the latter we end up calling of_find_node_by_path_or_alias() with
a NULL pointer, so no node can be found.

Fix this by restoring the original behaviour.

Fixes: 90f70dbe29 ("of: split part of of_get_stdoutpath into of_find_node_by_chosen")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/of/base.c
@@ -2680,7 +2680,7 @@ struct device_node *of_find_node_by_chosen(const char *propname,
 					   const char **options)
 {
 	const char *value, *p;
-	char *buf = NULL;
+	char *buf;
 	struct device_node *dn;
 
 	value = of_get_property(of_chosen, propname, NULL);
@@ -2688,8 +2688,7 @@ struct device_node *of_find_node_by_chosen(const char *propname,
 		return NULL;
 
 	p = strchrnul(value, ':');
-	if (*p)
-		buf = xstrndup(value, p - value);
+	buf = xstrndup(value, p - value);
 
 	dn = of_find_node_by_path_or_alias(NULL, buf);
 
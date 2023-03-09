5b8c627b62369b6f0f0d86d2de3ce9db082cc321
Sascha Hauer <s.hauer@pengutronix.de>
2023-03-09 15:59:11 +0100

    usb: gadget: dfu: Assign super speed descriptors
    DFU uses the same descriptors for full speed and high speed already,
    just use them for super speed (plus) as well.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/usb/gadget/function/dfu.c
+++ drivers/usb/gadget/function/dfu.c
@@ -427,7 +427,7 @@ dfu_bind(struct usb_configuration *c, struct usb_function *f)
 	header[i] = (struct usb_descriptor_header *) &usb_dfu_func;
 	header[i + 1] = NULL;
 
-	status = usb_assign_descriptors(f, header, header, NULL, NULL);
+	status = usb_assign_descriptors(f, header, header, header, header);
 
 	free(desc);
 	free(header);

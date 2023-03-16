31d537e314ea0737b69010389a72a5ad842fe061
Denis Orlov <denorl2009@gmail.com>
2023-03-16 10:36:50 +0300

    lib: string: remove duplicated function
    We have two functions that are doing the same thing: 'strncasecmp' and
    'strnicmp'. The only difference between them is that the latter is
    correctly handling the len argument of 0. So rename it into the former
    one ('strncasecmp', as it is the POSIX name for this function), deleting
    the other implementation. As no one is actually using 'strnicmp', no
    other code requires any fixes.
    
    This change is effectively forwarded from the Linux commits
    'lib/string.c: remove duplicated function'
    (hash cd514e727b18ff4d189b8e268db13729a4175091) and
    'lib/string.c: remove strnicmp()'
    (hash af3cd13501eb04ca61d017ff4406f1cbffafdc04).
    
    Signed-off-by: Denis Orlov <denorl2009@gmail.com>
    Link: https://lore.barebox.org/20230316073650.4170874-1-denorl2009@gmail.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/string.h
+++ include/linux/string.h
@@ -58,9 +58,6 @@ extern int strcmp(const char *,const char *);
 #ifndef __HAVE_ARCH_STRNCMP
 extern int strncmp(const char *,const char *,__kernel_size_t);
 #endif
-#ifndef __HAVE_ARCH_STRNICMP
-extern int strnicmp(const char *, const char *, __kernel_size_t);
-#endif
 #ifndef __HAVE_ARCH_STRCASECMP
 extern int strcasecmp(const char *s1, const char *s2);
 #endif

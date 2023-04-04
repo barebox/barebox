f292406f752407a9a66027a3dd0c867b3c2620c9
Ahmad Fatoum <a.fatoum@pengutronix.de>
2023-04-04 11:05:42 +0200

    hush: pacify clang -Wparentheses-equality warning
    Code is fine, but clang's -Wparentheses-equality warns when using double
    parentheses in conditionals that don't do assignment. This aligns with
    our code style as well, so change it to silence the warning.
    
    No functional change.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20230404090542.1820785-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/hush.c
+++ common/hush.c
@@ -1837,7 +1837,7 @@ static char **make_list_in(char **inp, char *name)
 		p3 = insert_var_value(inp[i]);
 		p1 = p3;
 		while (*p1) {
-			if ((*p1 == ' ')) {
+			if (*p1 == ' ') {
 				p1++;
 				continue;
 			}

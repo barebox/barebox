14b296d2a7e6088c8be9095a2d3a14ce0b4b4172
Ahmad Fatoum <a.fatoum@pengutronix.de>
2023-04-14 19:14:21 +0200

    arm: error out if __LINUX_ARM_ARCH__ is undefined
    With recent multiarch (really, multiplatform) rework, oldconfig will
    reask users what platforms they want to build for. The default is N,
    so holding return (or an olddefconfig) will just disable all boards and
    with it all CPUs leading to a cryptic torrent of:
    
      arch/arm/include/asm/swab.h:31:28: warning: "__LINUX_ARM_ARCH__" is not defined, evaluates to 0 [-Wundef]
         31 | #if defined(__KERNEL__) && __LINUX_ARM_ARCH__ >= 6
            |                            ^~~~~~~~~~~~~~~~~~
    
    Make user experience a bit better by printing an error message instead.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20230414171421.1745965-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/include/asm/arch-check.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ARM_ASM_ARCH_CHECK_H__
+#define __ARM_ASM_ARCH_CHECK_H__
+
+#ifndef __LINUX_ARM_ARCH__
+#error No boards/CPUs selected in Kconfig
+#endif
+
+#endif

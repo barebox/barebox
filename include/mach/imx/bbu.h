barebox/barebox

commit 6e0cb133bee8d16ecb7d47222261f3a90209c472

ARM: i.MX: bbu: add FlexSPI update handler
This adds the update handler support to flash barebox flexspi compatible
images to the QSPI.

Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
Link: https://lore.barebox.org/20230228-v2023-02-0-topic-flexspi-v2-19-3d33126d2434@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mach/imx/bbu.h
@@ -86,6 +86,10 @@ int imx8m_bbu_internal_mmcboot_register_handler(const char *name, const char *de
 int imx_bbu_external_nor_register_handler(const char *name, const char *devicefile,
 		unsigned long flags);
 
+int imx8m_bbu_internal_flexspi_nor_register_handler(const char *name,
+						    const char *devicefile,
+						    unsigned long flags);
+
 #else
 
 static inline int imx51_bbu_internal_mmc_register_handler(const char *name, const char *devicefile,
@@ -194,6 +198,13 @@ vf610_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefile,
 static inline int
 imx7_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefile,
 					   unsigned long flags)
+{
+	return -ENOSYS;
+}
+
+static inline int
+imx8m_bbu_internal_flexspi_nor_register_handler(const char *name, const char *devicefile,
+						unsigned long flags);
 {
 	return -ENOSYS;
 }

arch/arm/mach-imx/imx-bbu-internal.c
@@ -19,6 +19,7 @@
 #include <environment.h>
 #include <mach/imx/bbu.h>
 #include <mach/imx/generic.h>
+#include <mach/imx/imx-header.h>
 #include <libfile.h>
 
 struct imx_internal_bbu_handler {
@@ -655,3 +656,34 @@ int imx_bbu_external_nor_register_handler(const char *name,
 
 	return __register_handler(imx_handler);
 }
+
+static unsigned long imx_bbu_filetype_offset_flexspi(void)
+{
+	unsigned int sd_flash_header_gap = SZ_32K;
+
+	if (cpu_is_mx8mm())
+		return sd_flash_header_gap;
+
+	return sd_flash_header_gap + SZ_1K;
+}
+
+static int
+imx_bbu_internal_flexspi_nor_register_handler(const char *name,
+					      const char *devicefile,
+					      unsigned long flags)
+{
+	struct imx_internal_bbu_handler *imx_handler;
+
+	flags |= IMX_BBU_FLAG_ERASE | IMX_BBU_FLAG_PARTITION_STARTS_AT_HEADER;
+	imx_handler = __init_handler(name, devicefile, flags);
+	imx_handler->flash_header_offset = SZ_32K;
+	imx_handler->expected_type = filetype_nxp_fspi_image;
+	imx_handler->filetype_offset = imx_bbu_filetype_offset_flexspi();
+
+	return __register_handler(imx_handler);
+}
+
+int imx8m_bbu_internal_flexspi_nor_register_handler(const char *name,
+						    const char *devicefile,
+						    unsigned long flags)
+	__alias(imx_bbu_internal_flexspi_nor_register_handler);

commit 65e947456e0e45ae83cc935c136a2af8b99d220f

ARM: i.MX: bbu: fix compilation without CONFIG_BAREBOX_UPDATE
Fix
include/mach/imx/bbu.h:208:1: error: expected identifier or ‘(’ before ‘{’ token

Signed-off-by: Andreas Kemnade <andreas@kemnade.info>
Reviewed-by: Marco Felsch <m.felsch@pengutronix.de>
Fixes: 6e0cb133be ("ARM: i.MX: bbu: add FlexSPI update handler")
Link: https://lore.barebox.org/20230420195255.1153119-1-andreas@kemnade.info
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mach/imx/bbu.h
@@ -204,7 +204,7 @@ imx7_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefile,
 
 static inline int
 imx8m_bbu_internal_flexspi_nor_register_handler(const char *name, const char *devicefile,
-						unsigned long flags);
+						unsigned long flags)
 {
 	return -ENOSYS;
 }
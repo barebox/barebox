c1b50061f4b33482ae749f9d6d6c92aa5bf6b37a
Johannes Zink <j.zink@pengutronix.de>
2023-05-05 17:52:48 +0200

    imx-usb-loader: add commandline option for displaying version number
    For debugging purposes of the imx-usb-loader it can be helpful to
    display the version number as a commandline option.
    
    If built from outside the KBuild infrastructure, the version string
    cannot be fetched from the generated utsrelease.h header the version
    number needs to be defined by setting the UTS_VERSION macro. If the
    macro is not set on an out-of-KBuild build, the version number "unknown"
    is displayed.
    
    Signed-off-by: Johannes Zink <j.zink@pengutronix.de>
    Link: https://lore.barebox.org/20230505155248.2261867-4-j.zink@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- scripts/imx/imx-usb-loader.c
+++ scripts/imx/imx-usb-loader.c
@@ -46,6 +46,10 @@
 #define FT_DCD	0xee
 #define FT_LOAD_ONLY	0x00
 
+#ifndef UTS_RELEASE
+#define UTS_RELEASE "unknown"
+#endif
+
 /*
  * comment from libusb:
  * As per the USB 3.0 specs, the current maximum limit for the depth is 7.
@@ -1526,7 +1530,13 @@ static void usage(const char *prgname)
 		"-p <devpath> Specify device path: <bus>-<port>[.<port>]...\n"
 		"-s           skip DCD included in image\n"
 		"-v           verbose (give multiple times to increase)\n"
+		"--version    display version number\n"
 		"-h           this help\n", prgname);
+}
+
+static void version(const char *prgname)
+{
+	fprintf(stderr, "%s %s\n", prgname, UTS_RELEASE);
 }
 
 int main(int argc, char *argv[])
@@ -1544,10 +1554,20 @@ int main(int argc, char *argv[])
 	char *initfile = NULL;
 	char *devpath = NULL;
 	char *devtype = NULL;
+	int opt_version = 0;
+	struct option long_options[] = {
+		{"version", no_argument, &opt_version, 1},
+		{ }
+	};
 
 	w.do_dcd_once = 1;
 
-	while ((opt = getopt(argc, argv, "cvhd:i:p:s")) != -1) {
+	while ((opt = getopt_long(argc, argv, "cvhd:i:p:s", long_options, NULL)) != -1) {
+		if (opt_version) {
+			version(argv[0]);
+			exit(EXIT_SUCCESS);
+		}
+
 		switch (opt) {
 		case 'c':
 			verify = 1;

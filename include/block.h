01d7ea1247ed24c2a9e9ac26fb9a7d1a7d4d33fd
Ahmad Fatoum <a.fatoum@pengutronix.de>
2023-06-07 14:07:09 +0200

    block: add cdev_is_block_(device|partition|disk) helpers
    We look too much into struct cdev's guts. Let's add helpers to make
    operating on block device cdevs more concise.
    
    Reviewed-by: Marco Felsch <m.felsch@pengutronix.de>
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20230607120714.3083182-15-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/block.h
+++ include/block.h
@@ -58,4 +58,19 @@ static inline struct block_device *cdev_get_block_device(const struct cdev *cdev
 }
 #endif
 
+static inline bool cdev_is_block_device(const struct cdev *cdev)
+{
+	return cdev_get_block_device(cdev) != NULL;
+}
+
+static inline bool cdev_is_block_partition(const struct cdev *cdev)
+{
+	return cdev_is_block_device(cdev) && cdev_is_partition(cdev);
+}
+
+static inline bool cdev_is_block_disk(const struct cdev *cdev)
+{
+	return cdev_is_block_device(cdev) && !cdev_is_partition(cdev);
+}
+
 #endif /* __BLOCK_H */

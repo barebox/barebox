dca73e738f53e255e82661299931879c44bb1c8f
Denis Orlov <denorl2009@gmail.com>
2023-07-25 08:05:02 +0300

    MIPS: addrspace: rectify ksseg segment naming
    There was a slight confusion in macro definitions for various memory
    segments. In the 64-bit mode, there was CKSEG2ADDR defined without a
    corresponding CKSEG2 definition, which led to errors when trying to
    actually use it. Instead there was a CKSSEG definition that stands for
    exactly the same memory segment.
    
    Change the name of this memory segment macro into KSSEG/CKSSEG for the
    32/64-bit mode respectively and also rename the corresponding *ADDR
    macros as this is how this segment is actually named in the MIPS
    architecture specification. This makes the naming more consistent.
    
    Signed-off-by: Denis Orlov <denorl2009@gmail.com>
    Link: https://lore.barebox.org/20230725050618.3451-2-denorl2009@gmail.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/include/asm/addrspace.h
+++ arch/mips/include/asm/addrspace.h
@@ -67,7 +67,7 @@
 
 #define CKSEG0ADDR(a)		(CPHYSADDR(a) | CKSEG0)
 #define CKSEG1ADDR(a)		(CPHYSADDR(a) | CKSEG1)
-#define CKSEG2ADDR(a)		(CPHYSADDR(a) | CKSEG2)
+#define CKSSEGADDR(a)		(CPHYSADDR(a) | CKSSEG)
 #define CKSEG3ADDR(a)		(CPHYSADDR(a) | CKSEG3)
 
 /*
@@ -94,7 +94,7 @@
 
 #define CKSEG0ADDR(a)		(CPHYSADDR(a) | KSEG0)
 #define CKSEG1ADDR(a)		(CPHYSADDR(a) | KSEG1)
-#define CKSEG2ADDR(a)		(CPHYSADDR(a) | KSEG2)
+#define CKSSEGADDR(a)		(CPHYSADDR(a) | KSSEG)
 #define CKSEG3ADDR(a)		(CPHYSADDR(a) | KSEG3)
 
 /*
@@ -102,7 +102,7 @@
  */
 #define KSEG0ADDR(a)		(CPHYSADDR(a) | KSEG0)
 #define KSEG1ADDR(a)		(CPHYSADDR(a) | KSEG1)
-#define KSEG2ADDR(a)		(CPHYSADDR(a) | KSEG2)
+#define KSSEGADDR(a)		(CPHYSADDR(a) | KSSEG)
 #define KSEG3ADDR(a)		(CPHYSADDR(a) | KSEG3)
 
 /*
@@ -112,13 +112,13 @@
 #define KUSEG			0x00000000
 #define KSEG0			0x80000000
 #define KSEG1			0xa0000000
-#define KSEG2			0xc0000000
+#define KSSEG			0xc0000000
 #define KSEG3			0xe0000000
 
 #define CKUSEG			0x00000000
 #define CKSEG0			0x80000000
 #define CKSEG1			0xa0000000
-#define CKSEG2			0xc0000000
+#define CKSSEG			0xc0000000
 #define CKSEG3			0xe0000000
 
 #endif

adc36109d1532fe74622efafbe32c27b204eec0d
Marc Kleine-Budde <mkl@pengutronix.de>
2023-08-09 09:26:06 +0200

    ARM: i.MX8MM: add Phytec i.MX8MM SoM support
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Link: https://lore.barebox.org/20230809-phytec-som-imx8mm-v4-3-b9d4759582d8@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/phytec-som-imx8mm/lowlevel.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef PHYBOARD_POLIS_RDK_LOWLEVEL_H_
+#define PHYBOARD_POLIS_RDK_LOWLEVEL_H_
+
+extern struct dram_timing_info phyboard_polis_rdk_dram_timing;
+
+#endif

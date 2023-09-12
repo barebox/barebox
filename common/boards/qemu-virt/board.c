barebox/barebox

commit 484a0d281408504ce3fb7a9884048d468c9727e7

boards: qemu-virt: support passing in FIT public key
FIT public key is usually passed in via board DT. Usual way to use
barebox with QEMU Virt however is to use DT supplied by Qemu and apply
overlay to it. mkimage doesn't generate overlay DTB though. To make
barbebox Qemu Virt behave like other boards, let's define a dummy DT
that includes CONFIG_BOOTM_FITIMAGE_PUBKEY, which is merged with the
barebox live device tree.

Suggested-by: Jan Lübbe <jlu@pengutronix.de>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20230612125104.1082800-5-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/boards/qemu-virt/board.c
@@ -35,6 +35,7 @@ static inline void arm_virt_init(void) {}
 #endif
 
 extern char __dtbo_qemu_virt_flash_start[];
+extern char __dtb_fitimage_pubkey_start[];
 
 static const struct of_device_id virt_of_match[] = {
 	{ .compatible = "linux,dummy-virt", .data = arm_virt_init },
@@ -52,7 +53,7 @@ BAREBOX_DEEP_PROBE_ENABLE(virt_of_match);
 static int virt_board_driver_init(void)
 {
 	struct device_node *root = of_get_root_node();
-	struct device_node *overlay;
+	struct device_node *overlay, *pubkey;
 	const struct of_device_id *id;
 	void (*init)(void);
 
@@ -67,6 +68,9 @@ static int virt_board_driver_init(void)
 
 	overlay = of_unflatten_dtb(__dtbo_qemu_virt_flash_start, INT_MAX);
 	of_overlay_apply_tree(root, overlay);
+
+	pubkey = of_unflatten_dtb(__dtb_fitimage_pubkey_start, INT_MAX);
+	of_merge_nodes(root, pubkey);
 
 	/* of_probe() will happen later at of_populate_initcall */
 

common/boards/qemu-virt/Makefile
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 obj-y += board.o
-obj-y += qemu-virt-flash.dtbo.o
+obj-y += qemu-virt-flash.dtbo.o fitimage-pubkey.dtb.o
 ifeq ($(CONFIG_RISCV),y)
 DTC_CPP_FLAGS_qemu-virt-flash.dtbo := -DRISCV_VIRT=1
 endif

common/boards/qemu-virt/fitimage-pubkey.dts
@@ -1,0 +1,7 @@
+/dts-v1/;
+
+#ifdef CONFIG_BOOTM_FITIMAGE_PUBKEY
+#include CONFIG_BOOTM_FITIMAGE_PUBKEY
+#endif
+
+/{ };

commit 4b17ad98eb9655082962fa65dea6840a001c3a1c

boards: qemu-virt: rescan aliases after merging DT fragment
barebox now compiles an initially empty device tree that's combined with
the device tree passed by Qemu. That device tree can hold the FIT public
key or if CONFIG_EXTERNAL_DTS_FRAGMENTS is enabled, device tree nodes
passed at build time. If this device tree fragment adds an alias, it
won't be registered, which can be surprising. Therefore call
of_alias_scan after merging the device trees.

Fixes: 484a0d281408 ("boards: qemu-virt: support passing in FIT public key")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20230911162237.3797218-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/boards/qemu-virt/board.c
@@ -80,6 +80,9 @@ static int virt_board_driver_init(void)
 	pubkey = of_unflatten_dtb(__dtb_fitimage_pubkey_start, INT_MAX);
 	of_merge_nodes(root, pubkey);
 
+	/* fragment may have added aliases to the DT */
+	of_alias_scan();
+
 	/* of_probe() will happen later at of_populate_initcall */
 
 	return 0;
2fad25033492905c33bfd689ff8a4618e7e7623d
Ahmad Fatoum <a.fatoum@pengutronix.de>
2023-09-21 11:44:25 +0200

    include: linux/spinlock: add stubs for Linux _bh variants
    For symmetry with existing empty spin_lock/unlock stubs, add _bh
    suffixed versions as well.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20230921094425.270359-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/spinlock.h
+++ include/linux/spinlock.h
@@ -6,7 +6,9 @@
 typedef int   spinlock_t;
 #define spin_lock_init(...)
 #define spin_lock(...)
+#define spin_lock_bh spin_lock
 #define spin_unlock(...)
+#define spin_unlock_bh spin_unlock
 #define spin_lock_irqsave(lock, flags) do { flags = 0; } while (0)
 #define spin_unlock_irqrestore(lock, flags) do { flags = flags; } while (0)
 

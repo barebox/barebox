barebox/barebox

commit 8b35facbb9712d5cef97be3a56c0f52807cf8384

i.MX: HABv4: Improve HAB event printing
Instead of using a fixed sized buffer for the report_event function,
let's call it two times, once with a NULL pointer to get the size of the
event and a second time with a buffer of that size.
Also, instead of separating the events into warning and error type,
iterate over all events in one single loop. This helps to get the events
in the order they occured which probably helps the reader to make more
sense of them.
This also gets rid of the "ERROR: Recompile with larger event data buffer"
message which recently appeared everytime a warning or error was
reported.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/hab/habv4.c
@@ -500,11 +500,34 @@ static bool is_known_rng_fail_event(const uint8_t *data, size_t len)
 	return false;
 }
 
+static uint8_t *hab_get_event(const struct habv4_rvt *rvt, int index, int *len)
+{
+	enum hab_status err;
+	uint8_t *buf;
+
+	err = rvt->report_event(HAB_STATUS_ANY, index, NULL, len);
+	if (err != HAB_STATUS_SUCCESS)
+		return NULL;
+
+	buf = malloc(*len);
+	if (!buf)
+		return NULL;
+
+	err = rvt->report_event(HAB_STATUS_ANY, index, buf, len);
+	if (err != HAB_STATUS_SUCCESS) {
+		pr_err("Unexpected HAB return code\n");
+		free(buf);
+		return NULL;
+	}
+
+	return buf;
+}
+
 static int habv4_get_status(const struct habv4_rvt *rvt)
 {
-	uint8_t data[256];
+	uint8_t *data;
 	uint32_t len;
-	uint32_t index = 0;
+	int i;
 	enum hab_status status;
 	enum hab_config config = 0x0;
 	enum hab_state state = 0x0;
@@ -524,39 +547,23 @@ static int habv4_get_status(const struct habv4_rvt *rvt)
 		return 0;
 	}
 
-	len = sizeof(data);
-	while (rvt->report_event(HAB_STATUS_WARNING, index, data, &len) == HAB_STATUS_SUCCESS) {
+	for (i = 0;; i++) {
+		data = hab_get_event(rvt, i, &len);
+		if (!data)
+			break;
 
 		/* suppress RNG self-test fail events if they can be handled in software */
 		if (IS_ENABLED(CONFIG_CRYPTO_DEV_FSL_CAAM_RNG_SELF_TEST) &&
 		    is_known_rng_fail_event(data, len)) {
 			pr_debug("RNG self-test failure detected, will run software self-test\n");
 		} else {
-			pr_err("-------- HAB warning Event %d --------\n", index);
+			pr_err("-------- HAB Event %d --------\n", i);
 			pr_err("event data:\n");
 			habv4_display_event(data, len);
 		}
 
-		len = sizeof(data);
-		index++;
+		free(data);
 	}
-
-	len = sizeof(data);
-	index = 0;
-	while (rvt->report_event(HAB_STATUS_FAILURE, index, data, &len) == HAB_STATUS_SUCCESS) {
-		pr_err("-------- HAB failure Event %d --------\n", index);
-		pr_err("event data:\n");
-
-		habv4_display_event(data, len);
-		len = sizeof(data);
-		index++;
-	}
-
-	/* Check reason for stopping */
-	len = sizeof(data);
-	index = 0;
-	if (rvt->report_event(HAB_STATUS_ANY, index, NULL, &len) == HAB_STATUS_SUCCESS)
-		pr_err("ERROR: Recompile with larger event data buffer (at least %d bytes)\n\n", len);
 
 	return -EPERM;
 }

commit c54b984f00f4b44715f71845a600dcd3e6e0d3b1

i.MX: HABv4: fix event status comparison
Instead of only printing events belonging to a certain level, print all
events that have a higher status than the current event. This fixes
event printing for i.MX8M based boards.

Fixes: 8b35facbb971 ("i.MX: HABv4: Improve HAB event printing")

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Link: https://lore.barebox.org/20231017145131.3069283-3-m.felsch@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/hab/habv4.c
@@ -224,7 +224,7 @@ static enum hab_status imx8m_read_sram_events(enum hab_status status,
 		}
 	}
 	while (i < num_events) {
-		if (events[i]->status == status) {
+		if (events[i]->status >= status) {
 			if (internal_index == index) {
 				*bytes = sizeof(struct hab_event_record) +
 					be16_to_cpu(events[i]->hdr.len);
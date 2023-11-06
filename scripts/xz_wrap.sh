barebox/barebox

commit 02898c798d5d59f0657322a92dc1a30689173538

scripts: Select XZ --arm64 BCJ filter for 64-bit arm
This change will select the arm64 BCJ filter for aarch64 instead of
the arm filter which was always selected for arm based architectures.

The selected filter is also tested, by looking if it is present in
the long help message of xz, this is because the arm64 BCJ filter is
a recent addition to xz.

Signed-off-by: Jules Maselbas <jmaselbas@zdiv.net>
Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20230907152047.102747-2-jmaselbas@zdiv.net
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/xz_wrap.sh
@@ -16,8 +16,11 @@ case $SRCARCH in
 	x86)            BCJ=--x86 ;;
 	powerpc)        BCJ=--powerpc ;;
 	ia64)           BCJ=--ia64; LZMA2OPTS=pb=4 ;;
-	arm)            BCJ=--arm ;;
+	arm)            BCJ=--arm$S64 ;;
 	sparc)          BCJ=--sparc ;;
 esac
+
+# clear BCJ filter if unsupported
+xz -H | grep -q -- $BCJ || BCJ=
 
 exec xz --check=crc32 $BCJ --lzma2=$LZMA2OPTS,dict=32MiB

commit d7c005d43fbc0614b4184a598f1cc1e7402bdcf6

scripts: xz_wrap.sh: clear BCJ only when set
xz_wrap.sh parses the the xz help output to test if the selected BCJ
filter is actually supported. $BCJ might be empty though for some
architectures like Mips, so test the variable for being non-empty before
grepping for the BCJ filter to avoid warnings from grep:

Usage: grep [OPTION]... PATTERNS [FILE]...
Try 'grep --help' for more information.

Fixes: 02898c798d ("scripts: Select XZ --arm64 BCJ filter for 64-bit arm")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/xz_wrap.sh
@@ -25,6 +25,8 @@ if grep -q '^CONFIG_THUMB2_BAREBOX=y$' include/config/auto.conf; then
 fi
 
 # clear BCJ filter if unsupported
-xz -H | grep -q -- $BCJ || BCJ=
+if [ -n "${BCJ}" ]; then
+	xz -H | grep -q -- $BCJ || BCJ=
+fi
 
 exec xz --check=crc32 $BCJ --lzma2=$LZMA2OPTS,dict=32MiB
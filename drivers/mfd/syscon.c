barebox/barebox

commit b36b7b7271926f7ac37d95c389e5e8d712675142

mfd: syscon: clock peripheral if specified in device tree
Linux supports a clocks property in syscon nodes to indicate that access
should only occur with the clock active.

Attach the clock to the regmap if found.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mfd/syscon.c
@@ -19,6 +19,7 @@
 #include <xfuncs.h>
 #include <of_address.h>
 #include <linux/err.h>
+#include <linux/clk.h>
 
 #include <mfd/syscon.h>
 
@@ -37,7 +38,7 @@ static const struct regmap_config syscon_regmap_config = {
 	.reg_stride = 4,
 };
 
-static struct syscon *of_syscon_register(struct device_node *np)
+static struct syscon *of_syscon_register(struct device_node *np, bool check_clk)
 {
 	int ret;
 	struct syscon *syscon;
@@ -60,6 +61,21 @@ static struct syscon *of_syscon_register(struct device_node *np)
 
 	syscon->regmap = of_regmap_init_mmio_clk(np, NULL, syscon->base,
 					     &syscon_regmap_config);
+
+	if (check_clk) {
+		struct clk *clk = of_clk_get(np, 0);
+		if (IS_ERR(clk)) {
+			ret = PTR_ERR(clk);
+			/* clock is optional */
+			if (ret != -ENOENT)
+				goto err_map;
+		} else {
+			ret = regmap_mmio_attach_clk(syscon->regmap, clk);
+			if (ret)
+				goto err_map;
+		}
+	}
+
 	return syscon;
 
 err_map:
@@ -78,7 +94,7 @@ static struct syscon *node_to_syscon(struct device_node *np)
 		}
 
 	if (!syscon)
-		syscon = of_syscon_register(np);
+		syscon = of_syscon_register(np, true);
 
 	if (IS_ERR(syscon))
 		return ERR_CAST(syscon);

commit a1c19bd00cc2b9a44dcf32bd57b6f0bc83739d2a

mfd: syscon: don't register clocked syscon if geting clock fails
When of_syscon_register is called with check_clk=true, it will attempt
to attach the clock. If that fails, the newly allocated syscon is freed,
but a dangling pointer remained in the syscon_list.

Fix this by only registering the syscon in the successful case.

Fixes: b36b7b727192 ("mfd: syscon: clock peripheral if specified in device tree")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20231109115522.1370884-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mfd/syscon.c
@@ -74,8 +74,6 @@ static struct syscon *of_syscon_register(struct device_node *np, bool check_clk)
 	syscon_config.val_bits = reg_io_width * 8;
 	syscon_config.max_register = resource_size(&res) - reg_io_width;
 
-	list_add_tail(&syscon->list, &syscon_list);
-
 	syscon->regmap = regmap_init_mmio_clk(NULL, NULL, syscon->base,
 					      &syscon_config);
 
@@ -92,6 +90,8 @@ static struct syscon *of_syscon_register(struct device_node *np, bool check_clk)
 				goto err_map;
 		}
 	}
+
+	list_add_tail(&syscon->list, &syscon_list);
 
 	return syscon;
 
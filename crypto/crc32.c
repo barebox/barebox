d383f3f27b8dff4355505ec6587f9ba0501052a2
Sascha Hauer <s.hauer@pengutronix.de>
2023-12-13 15:00:04 +0100

    crypto: crc32: implement crc32() with crc32_no_comp()
    Link: https://lore.barebox.org/20231213140004.898488-1-s.hauer@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- crypto/crc32.c
+++ crypto/crc32.c
@@ -77,28 +77,6 @@ static void make_crc_table(void)
 #define DO4(buf)  DO2(buf); DO2(buf);
 #define DO8(buf)  DO4(buf); DO4(buf);
 
-STATIC uint32_t crc32(uint32_t crc, const void *_buf, unsigned int len)
-{
-	const unsigned char *buf = _buf;
-
-	make_crc_table();
-
-	crc = crc ^ 0xffffffffL;
-	while (len >= 8) {
-		DO8(buf);
-		len -= 8;
-	}
-	if (len)
-		do {
-			DO1(buf);
-		} while (--len);
-	return crc ^ 0xffffffffL;
-}
-
-#ifdef __BAREBOX__
-EXPORT_SYMBOL(crc32);
-#endif
-
 /* No ones complement version. JFFS2 (and other things ?)
  * don't use ones compliment in their CRC calculations.
  */
@@ -119,6 +97,15 @@ STATIC uint32_t crc32_no_comp(uint32_t crc, const void *_buf, unsigned int len)
 
 	return crc;
 }
+
+STATIC uint32_t crc32(uint32_t crc, const void *buf, unsigned int len)
+{
+	return ~crc32_no_comp(~crc, buf, len);
+}
+
+#ifdef __BAREBOX__
+EXPORT_SYMBOL(crc32);
+#endif
 
 STATIC uint32_t crc32_be(uint32_t crc, const void *_buf, unsigned int len)
 {

3b437626ed4777d8ef6de28bf282bbbb36bee69f
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-01-03 11:19:48 +0100

    lib: add optional linked list debugging
    When enabled, this outputs messages like:
    
      list_del corruption, 000000005fe4a9d0->next is LIST_POISON1 (0000000000000100)
    
    which can be useful when debugging.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20240103101948.2630473-2-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/linux/bug.h
+++ include/linux/bug.h
@@ -6,4 +6,21 @@
 #include <asm-generic/bug.h>
 #include <linux/build_bug.h>
 
+/*
+ * Since detected data corruption should stop operation on the affected
+ * structures. Return value must be checked and sanely acted on by caller.
+ */
+static inline __must_check bool check_data_corruption(bool v) { return v; }
+#define CHECK_DATA_CORRUPTION(condition, fmt, ...)			 \
+	check_data_corruption(({					 \
+		bool corruption = unlikely(condition);			 \
+		if (corruption) {					 \
+			if (IS_ENABLED(CONFIG_BUG_ON_DATA_CORRUPTION)) { \
+				panic(fmt, ##__VA_ARGS__);		 \
+			} else						 \
+				WARN(1, fmt, ##__VA_ARGS__);		 \
+		}							 \
+		corruption;						 \
+	}))
+
 #endif	/* _LINUX_BUG_H */

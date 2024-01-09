82e6403fb394ccca5e6256335079a463a08c873e
Sascha Hauer <s.hauer@pengutronix.de>
2024-01-09 17:15:26 +0100

    ARM: Layerscape: LS1028a: Add barebox update handler
    The LS1028a image can be written straight to SD/eMMC at offset 0x100.
    Add a barebox update handler for it.
    
    Link: https://lore.barebox.org/20240109161527.3237581-21-s.hauer@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/mach/layerscape/bbu.h
+++ include/mach/layerscape/bbu.h
@@ -5,6 +5,14 @@
 
 #include <bbu.h>
 
+static inline int ls1028a_bbu_mmc_register_handler(const char *name,
+						   const char *devicefile,
+						   unsigned long flags)
+{
+	return bbu_register_std_file_update(name, flags, devicefile,
+					    filetype_layerscape_image);
+}
+
 static inline int ls1046a_bbu_mmc_register_handler(const char *name,
 						   const char *devicefile,
 						   unsigned long flags)

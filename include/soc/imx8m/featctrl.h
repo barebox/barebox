e21b25c8210e2de0930947240ccb1d56ca2305f2
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-01-19 23:45:20 +0100

    soc: imx: imx8m-featctrl: make header self-contained
    struct device is the only undefined type in the header, so
    forward-declare it to make the header independent of include order.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20240119224522.1399213-2-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/soc/imx8m/featctrl.h
+++ include/soc/imx8m/featctrl.h
@@ -14,6 +14,8 @@ struct imx8m_featctrl_data {
 	bool check_cpus;
 };
 
+struct device;
+
 #ifdef CONFIG_IMX8M_FEATCTRL
 int imx8m_feat_ctrl_init(struct device *dev, u32 tester4,
 			 const struct imx8m_featctrl_data *data);

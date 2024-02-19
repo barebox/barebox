fb4e864c0726c123bf97d968e318f6c4edd47438
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-02-19 14:38:27 +0100

    usb: xhci: don't use xhci_writeq for normal SDRAM
    While this shouldn't matter, it's confusing to use cpu_to_le32
    for one struct field and writeq for another, so use cpu_to_leXX
    for both.
    
    No functional change.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20240219133835.3886399-8-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/usb/host/xhci-mem.c
+++ drivers/usb/host/xhci-mem.c
@@ -573,7 +573,7 @@ int xhci_mem_init(struct xhci_ctrl *ctrl, struct xhci_hccr *hccr,
 
 		trb_64 = 0;
 		trb_64 = (uintptr_t)seg->trbs;
-		xhci_writeq(&entry->seg_addr, trb_64);
+		entry->seg_addr = cpu_to_le64(trb_64);
 		entry->seg_size = cpu_to_le32(TRBS_PER_SEGMENT);
 		entry->rsvd = 0;
 		seg = seg->next;

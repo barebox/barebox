33860c392de78bc607d5447c17c33559a34dca78
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-03-04 19:59:00 +0100

    efi: payload: iomem: adjust types to avoid casting
    We make ample use of GNU C's support for void pointer arithmetic, so use
    it here as well to get rid of the helper variable.
    
    No functional change.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20240304190038.3486881-16-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- efi/payload/iomem.c
+++ efi/payload/iomem.c
@@ -142,8 +142,7 @@ out:
 
 static int efi_barebox_populate_mmap(void)
 {
-	void *desc;
-	u8 *mmap_buf = NULL;
+	void *mmap_buf = NULL, *desc;
 	efi_status_t efiret;
 	size_t mmap_size;
 	size_t mapkey;
@@ -169,7 +168,7 @@ static int efi_barebox_populate_mmap(void)
 		goto out;
 	}
 
-	for (desc = mmap_buf; (u8 *)desc < mmap_buf + mmap_size; desc += descsz)
+	for (desc = mmap_buf; desc < mmap_buf + mmap_size; desc += descsz)
 		efi_parse_mmap(desc, __is_defined(DEBUG));
 
 out:

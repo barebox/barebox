barebox/barebox

commit d27c50c131e7393f4640c20c711e2aad83543066

ARM: i.MX93: ele: start TRNG on i.MX93 rev a1
On i.MX93 a0 the TRNG seems to be started automatically. On rev a1 it's
not and OP-TEE panics with "Cannot retrieve random data from ELE". Start
the TRNG to let OP-TEE startup successfully.

Link: https://lore.barebox.org/20240311080624.1836184-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/ele.c
@@ -167,6 +167,23 @@ int ele_get_info(struct ele_get_info_data *info)
 	return ret;
 }
 
+static int ele_get_start_trng(void)
+{
+	struct ele_msg msg = {
+		.version = ELE_VERSION,
+		.tag = ELE_CMD_TAG,
+		.size = 1,
+		.command = ELE_START_RNG,
+	};
+	int ret;
+
+	ret = ele_call(&msg);
+	if (ret)
+		pr_err("Could not start TRNG, response 0x%x\n", msg.data[0]);
+
+	return ret;
+}
+
 int imx93_ele_load_fw(void *bl33)
 {
 	struct ele_get_info_data info = {};
@@ -205,6 +222,9 @@ int imx93_ele_load_fw(void *bl33)
 	if (ret)
 		pr_err("Could not start ELE firmware: ret %d, response 0x%x\n",
 			ret, msg.data[0]);
+
+	if (rev >= 0xa1)
+		ele_get_start_trng();
 
 	return 0;
 }

commit 6314692c53904c4d893535676fdd35d93adf166e

ARM: i.MX93 ele: fix compilation
ele_call takes two arguments. The second one is removed in next where
this patch was previously applied on. Add the second argument to fix
compilation in master.

Fixes: d27c50c131 ("ARM: i.MX93: ele: start TRNG on i.MX93 rev a1")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-imx/ele.c
@@ -177,7 +177,7 @@ static int ele_get_start_trng(void)
 	};
 	int ret;
 
-	ret = ele_call(&msg);
+	ret = ele_call(&msg, true);
 	if (ret)
 		pr_err("Could not start TRNG, response 0x%x\n", msg.data[0]);
 
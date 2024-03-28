63d38f891a9ca38ef95c12a01c0e50fa1ff65692
Denis Orlov <denorl2009@gmail.com>
2024-03-28 21:37:02 +0300

    mips: mipsregs: remove duplicate definitions for Config 1 bits
    Also reorder some of them to be more uniform.
    
    Signed-off-by: Denis Orlov <denorl2009@gmail.com>
    Link: https://lore.barebox.org/20240328183851.3842-4-denorl2009@gmail.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -406,18 +406,14 @@
 #define MIPS_CONF1_PC		(_ULCAST_(1) <<  4)
 #define MIPS_CONF1_MD		(_ULCAST_(1) <<  5)
 #define MIPS_CONF1_C2		(_ULCAST_(1) <<  6)
+#define MIPS_CONF1_DA		(_ULCAST_(7) <<  7)
 #define MIPS_CONF1_DA_SHF	7
-#define MIPS_CONF1_DA		(_ULCAST_(7) <<  7)
+#define MIPS_CONF1_DL		(_ULCAST_(7) << 10)
 #define MIPS_CONF1_DL_SHF	10
-#define MIPS_CONF1_DL		(_ULCAST_(7) << 10)
+#define MIPS_CONF1_DS		(_ULCAST_(7) << 13)
 #define MIPS_CONF1_DS_SHF	13
-#define MIPS_CONF1_DS		(_ULCAST_(7) << 13)
-#define MIPS_CONF1_IA_SHF	16
-
-#define MIPS_CONF1_DA		(_ULCAST_(7) <<  7)
-#define MIPS_CONF1_DL		(_ULCAST_(7) << 10)
-#define MIPS_CONF1_DS		(_ULCAST_(7) << 13)
 #define MIPS_CONF1_IA		(_ULCAST_(7) << 16)
+#define MIPS_CONF1_IA_SHF	16
 #define MIPS_CONF1_IL		(_ULCAST_(7) << 19)
 #define MIPS_CONF1_IS		(_ULCAST_(7) << 22)
 #define MIPS_CONF1_TLBS		(_ULCAST_(63)<< 25)

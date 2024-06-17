barebox/barebox

commit d1fe68f1b010b83ddd3559ffbfcb70dca68b2563

mtd: nand: make interleaved placement optional
Currently only one driver in the tree (denali.c) needs interleaved
layout support. Make support for it optional to safe a few bytes
of binary space when interleaved support is not needed.

Link: https://lore.barebox.org/20240614133603.2452891-4-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/raw/nand_base.c
@@ -5671,6 +5671,10 @@ static int nand_set_ecc_on_host_ops(struct nand_chip *chip)
 		fallthrough;
 
 	case NAND_ECC_PLACEMENT_INTERLEAVED:
+		if (!IS_ENABLED(CONFIG_NAND_NEED_ECC_PLACEMENT_INTERLEAVED)) {
+			WARN(1, "CONFIG_NAND_NEED_ECC_PLACEMENT_INTERLEAVED is disabled\n"
+			break;
+		}
 		if ((!ecc->calculate || !ecc->correct || !ecc->hwctl) &&
 		    (!ecc->read_page ||
 		     ecc->read_page == nand_read_page_hwecc ||

drivers/mtd/nand/raw/Kconfig
@@ -35,6 +35,9 @@ config NAND_ALLOW_ERASE_BAD
 config NAND_NEED_EXEC_OP
 	bool
 
+config NAND_NEED_ECC_PLACEMENT_INTERLEAVED
+	bool
+
 comment "Raw/parallel NAND flash controllers"
 
 config NAND_IMX
@@ -156,6 +159,7 @@ config MTD_NAND_DENALI
 	tristate "Support Denali NAND controller"
 	depends on HAS_DMA
 	select NAND_NEED_EXEC_OP
+	select NAND_NEED_ECC_PLACEMENT_INTERLEAVED
 	help
 	  Enable support for the Denali NAND controller.  This should be
 	  combined with either the PCI or platform drivers to provide device

commit ec3b8c646bdc8c2ea0a78923ccb745012eabc097

mtd: nand: fix compilation
Add missing braces and semicolon.

Fixes: d1fe68f1b0 ("mtd: nand: make interleaved placement optional")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mtd/nand/raw/nand_base.c
@@ -5672,7 +5672,7 @@ static int nand_set_ecc_on_host_ops(struct nand_chip *chip)
 
 	case NAND_ECC_PLACEMENT_INTERLEAVED:
 		if (!IS_ENABLED(CONFIG_NAND_NEED_ECC_PLACEMENT_INTERLEAVED)) {
-			WARN(1, "CONFIG_NAND_NEED_ECC_PLACEMENT_INTERLEAVED is disabled\n"
+			WARN(1, "CONFIG_NAND_NEED_ECC_PLACEMENT_INTERLEAVED is disabled\n");
 			break;
 		}
 		if ((!ecc->calculate || !ecc->correct || !ecc->hwctl) &&
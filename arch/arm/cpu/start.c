barebox/barebox

commit 3f7b6146669c8abafd64b804bd3dcf961d11e811

ARM: cpu: start: register barebox memory area
By registering the barebox area, we ensure that functions like
memory_bank_first_find_space() skip over the memory area barebox uses
for itself.

The OS can still reclaim this area once barebox has shutdown.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240517064511.3307462-4-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/cpu/start.c
@@ -219,6 +219,8 @@ __noreturn __prereloc void barebox_non_pbl_start(unsigned long membase,
 	pr_debug("initializing malloc pool at 0x%08lx (size 0x%08lx)\n",
 			malloc_start, malloc_end - malloc_start);
 
+	register_barebox_area(barebox_base, endmem - barebox_base);
+
 	kasan_init(membase, memsize, malloc_start - (memsize >> KASAN_SHADOW_SCALE_SHIFT));
 
 	mem_malloc_init((void *)malloc_start, (void *)malloc_end - 1);

commit dc7419a068d61ab9765cb85ca77f6e7f81a7a3b3

ARM: fix OP-TEE DT reservation clashing with barebox area at endmem
The barebox area reservation is meant to fix the issue of having gaps
between early barebox-related reservations that might be clobbered by
later calls to request_sdram_region().

The OP-TEE memory region, if it exists, is not a barebox-related
reservation and treating it as such will clash if the reservation is
also listed in the device-tree:

  WARNING: of-reserved-mem: couldn't request reserved sdram region 0xdd000000-0xdfffffff

This failed reservation can lead to a failure to boot, e.g. on
STM32MP13:

  WARNING: of-reserved-mem: couldn't request reserved sdram region 0xdd000000-0xdfffffff
  E/TC:0   tzc_it_handler:26 TZC permission failure
  E/TC:0   dump_fail_filter:420 Permission violation on filter 0
  E/TC:0   dump_fail_filter:425 Violation @0xde03c530, non-secure privileged read, AXI ID 5c0
  E/TC:0   Panic at core/arch/arm/plat-stm32mp1/plat_tzc400.c:30 <tzc_it_handler>
  E/TC:0   TEE load address @ 0xde000000
  E/TC:0   Call stack:

Fix this by defining the end of the barebox area to not include any
OP-TEE memory regions.

Fixes: 3f7b6146669c ("ARM: cpu: start: register barebox memory area")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240701100859.3876429-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/cpu/start.c
@@ -170,7 +170,8 @@ __noreturn __prereloc void barebox_non_pbl_start(unsigned long membase,
 	pr_debug("initializing malloc pool at 0x%08lx (size 0x%08lx)\n",
 			malloc_start, malloc_end - malloc_start);
 
-	register_barebox_area(barebox_base, endmem - barebox_base);
+	register_barebox_area(barebox_base,
+			      arm_mem_barebox_image_end(endmem) - barebox_base);
 
 	kasan_init(membase, memsize, malloc_start - (memsize >> KASAN_SHADOW_SCALE_SHIFT));
 

arch/arm/include/asm/barebox-arm.h
@@ -59,7 +59,7 @@ void *barebox_arm_boot_dtb(void);
  *                                   ↑
  *	        OPTEE_SIZE (depends on CONFIG_OPTEE_SIZE)
  *                                   ↓
- *  ------------------------- arm_mem_optee() -------------------------
+ *  ---------- arm_mem_optee() / arm_mem_barebox_image_end() ----------
  *                                   ↑
  *                                 SZ_32K
  *                                   ↓
@@ -95,6 +95,11 @@ void *barebox_arm_boot_dtb(void);
 static inline unsigned long arm_mem_optee(unsigned long endmem)
 {
 	return endmem - OPTEE_SIZE;
+}
+
+static inline unsigned long arm_mem_barebox_image_end(unsigned long endmem)
+{
+	return arm_mem_optee(endmem);
 }
 
 static inline unsigned long arm_mem_scratch(unsigned long endmem)
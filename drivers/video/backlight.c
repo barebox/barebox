barebox/barebox

commit e3ea90860e9991bb6097ed79d8f0d533a05ad56b

video: Add backlight support
This adds a small backlight layer. It provides a backlight device
on which the brightness parameter can be used to adjust the brightness.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/video/backlight.c
@@ -1,0 +1,90 @@
+#include <common.h>
+#include <driver.h>
+#include <linux/list.h>
+#include <video/backlight.h>
+
+static LIST_HEAD(backlights);
+
+int backlight_set_brightness(struct backlight_device *bl, int brightness)
+{
+	int ret, step, i, num_steps;
+
+	if (brightness > bl->brightness_max)
+		brightness = bl->brightness_max;
+
+	if (brightness == bl->brightness_cur)
+		return 0;
+
+	if (brightness > bl->brightness_cur)
+		step = 1;
+	else
+		step = -1;
+
+	i = bl->brightness_cur;
+
+	num_steps = abs(brightness - bl->brightness_cur);
+
+	while (1) {
+		i += step;
+
+		ret = bl->brightness_set(bl, i);
+		if (ret)
+			return ret;
+
+		if (i == brightness)
+			break;
+
+		udelay(100000 / num_steps);
+	}
+
+
+	bl->brightness_cur = bl->brightness = brightness;
+
+	return ret;
+}
+
+int backlight_set_brightness_default(struct backlight_device *bl)
+{
+	int ret;
+
+	ret = backlight_set_brightness(bl, bl->brightness_default);
+
+	return ret;
+}
+
+static int backlight_brightness_set(struct param_d *p, void *priv)
+{
+	struct backlight_device *bl = priv;
+
+	return backlight_set_brightness(bl, bl->brightness);
+}
+
+int backlight_register(struct backlight_device *bl)
+{
+	int ret;
+
+	sprintf(bl->dev.name, "backlight");
+	bl->dev.id = DEVICE_ID_DYNAMIC;
+
+	ret = register_device(&bl->dev);
+	if (ret)
+		return ret;
+
+	dev_add_param_int(&bl->dev, "brightness", backlight_brightness_set,
+			NULL, &bl->brightness, "%d", bl);
+
+	list_add_tail(&bl->list, &backlights);
+
+	return ret;
+}
+
+struct backlight_device *of_backlight_find(struct device_node *node)
+{
+	struct backlight_device *bl;
+
+	list_for_each_entry(bl, &backlights, list)
+		if (bl->node == node)
+			return bl;
+
+	return NULL;
+}

include/video/backlight.h
@@ -1,0 +1,20 @@
+#ifndef __VIDEO_BACKLIGHT_H
+#define __VIDEO_BACKLIGHT_H
+
+struct backlight_device {
+	int brightness;
+	int brightness_cur;
+	int brightness_max;
+	int brightness_default;
+	int (*brightness_set)(struct backlight_device *, int brightness);
+	struct list_head list;
+	struct device_d dev;
+	struct device_node *node;
+};
+
+int backlight_set_brightness(struct backlight_device *, int brightness);
+int backlight_set_brightness_default(struct backlight_device *);
+int backlight_register(struct backlight_device *);
+struct backlight_device *of_backlight_find(struct device_node *node);
+
+#endif /* __VIDEO_BACKLIGHT_H */

drivers/video/Kconfig
@@ -93,4 +93,8 @@ config DRIVER_VIDEO_EDID
 	  This enabled support for reading and parsing EDID data from an attached
 	  monitor.
 
+config DRIVER_VIDEO_BACKLIGHT
+	bool "Add backlight support"
+	help
+	  Enable this for backlight support.
 endif

drivers/video/Makefile
@@ -1,6 +1,7 @@
 obj-$(CONFIG_VIDEO) += fb.o
 obj-$(CONFIG_DRIVER_VIDEO_EDID) += edid.o
 obj-$(CONFIG_OFDEVICE) += of_display_timing.o
+obj-$(CONFIG_DRIVER_VIDEO_BACKLIGHT) += backlight.o
 
 obj-$(CONFIG_DRIVER_VIDEO_ATMEL) += atmel_lcdfb.o atmel_lcdfb_core.o
 obj-$(CONFIG_DRIVER_VIDEO_ATMEL_HLCD) += atmel_hlcdfb.o atmel_lcdfb_core.o

commit 7b4c629a16aebf9fdf3a8f1b4d12c6a76c544b80

video: backlight: make brightness and slew time parameters unsigned
Despite the backlight parameter being registered as uint32,
backlight_brightness_set took the brightness level as a signed value.

This can lead to wrap around for very large values of brightness
supplied as device parameter and if this results in a negative
brightness, the function appears to hang, because the slew rate loop
runs for a long time.

Fix this by making brightness level an unsigned entity.

Fixes: e3ea90860e99 ("video: Add backlight support")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240701072714.163239-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/video/backlight.c
@@ -6,7 +6,7 @@
 
 static LIST_HEAD(backlights);
 
-int backlight_set_brightness(struct backlight_device *bl, int brightness)
+int backlight_set_brightness(struct backlight_device *bl, unsigned brightness)
 {
 	int ret, step, i, num_steps;
 
@@ -80,9 +80,9 @@ int backlight_register(struct backlight_device *bl)
 		return ret;
 
 	dev_add_param_uint32(&bl->dev, "brightness", backlight_brightness_set,
-			NULL, &bl->brightness, "%d", bl);
+			NULL, &bl->brightness, "%u", bl);
 	dev_add_param_uint32(&bl->dev, "slew_time_ms", NULL, NULL,
-			     &bl->slew_time_ms, "%d", NULL);
+			     &bl->slew_time_ms, "%u", NULL);
 
 	list_add_tail(&bl->list, &backlights);
 

include/video/backlight.h
@@ -16,7 +16,7 @@ struct backlight_device {
 	struct device_node *node;
 };
 
-int backlight_set_brightness(struct backlight_device *, int brightness);
+int backlight_set_brightness(struct backlight_device *, unsigned brightness);
 int backlight_set_brightness_default(struct backlight_device *);
 int backlight_register(struct backlight_device *);
 struct backlight_device *of_backlight_find(struct device_node *node);
@@ -24,7 +24,7 @@ struct backlight_device *of_backlight_find(struct device_node *node);
 struct backlight_device ;
 
 static inline int
-backlight_set_brightness(struct backlight_device *dev, int brightness)
+backlight_set_brightness(struct backlight_device *dev, unsigned brightness)
 {
 	return 0;
 }
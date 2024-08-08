barebox/barebox

commit e1de5f16460f1c311976c3536e65b332b76c2338

malloc: implement malloc_usable_size()
malloc_usable_size() is a standard libc function. Implement it for the
barebox allocators, dlmalloc and tlsf_malloc.

Link: https://lore.barebox.org/20240806105429.2278660-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/dlmalloc.c
@@ -1432,6 +1432,18 @@ void free(void *mem)
 		frontlink(p, sz, idx, bck, fwd);
 }
 
+size_t malloc_usable_size(void *mem)
+{
+	mchunkptr p;
+	size_t size;
+
+	if (!mem)
+		return 0;
+
+	p = mem2chunk(mem);
+	return chunksize(p);
+}
+
 /*
   Realloc algorithm:
 

arch/sandbox/Makefile
@@ -23,6 +23,7 @@ lds-y   := $(BOARD)/barebox.lds
 TEXT_BASE = $(CONFIG_TEXT_BASE)
 KBUILD_CFLAGS += -Dmalloc=barebox_malloc -Dcalloc=barebox_calloc \
 		-Dmalloc_stats=barebox_malloc_stats -Dmemalign=barebox_memalign \
+		-Dmalloc_usable_size=barebox_malloc_usable_size \
 		-Dfree=barebox_free -Drealloc=barebox_realloc \
 		-Dread=barebox_read -Dwrite=barebox_write \
 		-Dopen=barebox_open -Dclose=barebox_close \

arch/sandbox/os/libc_malloc.c
@@ -32,6 +32,11 @@ void *barebox_malloc(size_t size)
 	return mem;
 }
 
+size_t barebox_malloc_usable_size(void *mem)
+{
+	return malloc_usable_size(mem);
+}
+
 void barebox_free(void *ptr)
 {
 	free(ptr);

common/dummy_malloc.c
@@ -30,6 +30,11 @@ void free(void *ptr)
 {
 }
 
+size_t malloc_usable_size(void *mem)
+{
+	BUG();
+}
+
 void *realloc(void *ptr, size_t size)
 {
 	BUG();

common/tlsf_malloc.c
@@ -38,6 +38,17 @@ void free(void *mem)
 }
 EXPORT_SYMBOL(free);
 
+size_t malloc_usable_size(void *mem)
+{
+	size_t size;
+
+	if (!mem)
+		return 0;
+
+	return tlsf_block_size(mem);
+}
+EXPORT_SYMBOL(malloc_usable_size);
+
 void *realloc(void *oldmem, size_t bytes)
 {
 	void *mem = tlsf_realloc(tlsf_mem_pool, oldmem, bytes);

include/malloc.h
@@ -6,6 +6,7 @@
 #include <types.h>
 
 void *malloc(size_t) __alloc_size(1);
+size_t malloc_usable_size(void *);
 void free(void *);
 void *realloc(void *, size_t) __realloc_size(2);
 void *memalign(size_t, size_t) __alloc_size(2);

commit c3233ea21cf7aef78c2bff368154cd0aabc82c0e

dlmalloc: remove unused variable
e1de5f1646 ("malloc: implement malloc_usable_size()") introduced
an unused variable. Remove it.

Fixes: e1de5f1646 ("malloc: implement malloc_usable_size()")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/dlmalloc.c
@@ -1435,7 +1435,6 @@ void free(void *mem)
 size_t malloc_usable_size(void *mem)
 {
 	mchunkptr p;
-	size_t size;
 
 	if (!mem)
 		return 0;
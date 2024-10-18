barebox/barebox

commit 5f2f1f5fc1a9fd987d7aa3d227e3606a082e608e

optee: add support to verify 64-bit headers as well
Guard the check by IS_ENABLED() to make it possible to verify 32-bit and
64-bit optee headers.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
Link: https://lore.barebox.org/20240116170738.209954-9-m.felsch@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/optee.c
@@ -14,8 +14,14 @@ int optee_verify_header(struct optee_header *hdr)
 		return -EINVAL;
 	}
 
-	if (hdr->arch != OPTEE_ARCH_ARM32 || hdr->init_load_addr_hi) {
-		pr_err("Only 32bit supported\n");
+	if (IS_ENABLED(CPU_V7) &&
+	    (hdr->arch != OPTEE_ARCH_ARM32 || hdr->init_load_addr_hi)) {
+		pr_err("Wrong OP-TEE Arch for ARM v7 CPU\n");
+		return -EINVAL;
+	}
+
+	if (IS_ENABLED(CPU_V8) && hdr->arch != OPTEE_ARCH_ARM64) {
+		pr_err("Wrong OP-TEE Arch for ARM v8 CPU\n");
 		return -EINVAL;
 	}
 

commit b450dc13b43ea709a2e363834a04a6abe3b094b7

optee: fix use of IS_ENABLED() on option without CONFIG_ prefix
IS_ENABLED() is meant to be used on options with their CONFIG_ prefix,
fix that and while at it use the recently added more descriptive
CONFIG_ARM32, CONFIG_ARM64 config symbols.

Fixes: 5f2f1f5fc1a9 ("optee: add support to verify 64-bit headers as well")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20241016085218.3961116-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/optee.c
@@ -25,13 +25,13 @@ int optee_verify_header(const struct optee_header *hdr)
 		return -EINVAL;
 	}
 
-	if (IS_ENABLED(CPU_V7) &&
+	if (IS_ENABLED(CONFIG_ARM32) &&
 	    (hdr->arch != OPTEE_ARCH_ARM32 || hdr->init_load_addr_hi)) {
 		pr_err("Wrong OP-TEE Arch for ARM v7 CPU\n");
 		return -EINVAL;
 	}
 
-	if (IS_ENABLED(CPU_V8) && hdr->arch != OPTEE_ARCH_ARM64) {
+	if (IS_ENABLED(CONFIG_ARM64) && hdr->arch != OPTEE_ARCH_ARM64) {
 		pr_err("Wrong OP-TEE Arch for ARM v8 CPU\n");
 		return -EINVAL;
 	}
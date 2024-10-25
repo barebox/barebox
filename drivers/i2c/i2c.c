b4746725454ca48d8e43a67ff4d66839ed625629
Sascha Hauer <s.hauer@pengutronix.de>
2024-10-25 09:30:25 +0200

    i2c: get adapter.nr from device tree
    In case we are instantiating a I2C bus driver from device tree let's
    try to get the bus number from the device tree instead of dynamically
    assigning a free bus number.
    
    Link: https://lore.kernel.org/r/20241025-i2c-busnum-v1-2-332d9257eafe@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/i2c/i2c.c
+++ drivers/i2c/i2c.c
@@ -705,6 +705,9 @@ int i2c_add_numbered_adapter(struct i2c_adapter *adapter)
 	struct device *hw_dev;
 	int ret;
 
+	if (adapter->nr < 0 && adapter->dev.parent && adapter->dev.parent->of_node)
+		adapter->nr = of_alias_get_id(adapter->dev.parent->of_node, "i2c");
+
 	if (adapter->nr < 0) {
 		int nr;
 

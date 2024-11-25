24ada94d67ae5c53152e53456c231b517a76bc03
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-11-25 16:29:22 +0100

    lds: implement is_barebox_rodata
    Linux defines is_kernel_rodata(), which is mainly used to implement
    kstrdup_const/kfree_const, which are useful to avoid duplication of
    strings that already have static storage duration.
    
    Implement the same for barebox on all architectures.
    
    Note that this is of limited use on sandbox, because we don't define all
    of the linker script ourselves, but this only means that we will have
    some unneeded allocations there, which is a non-issue.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.kernel.org/r/20241125152927.546493-3-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/asm-generic/sections.h
+++ include/asm-generic/sections.h
@@ -3,7 +3,10 @@
 #ifndef _ASM_GENERIC_SECTIONS_H_
 #define _ASM_GENERIC_SECTIONS_H_
 
+#include <linux/types.h>
+
 extern char _text[], _stext[], _etext[];
+extern char __start_rodata[], __end_rodata[];
 extern char __bss_start[], __bss_stop[];
 extern char _sdata[], _edata[];
 extern char __bare_init_start[], __bare_init_end[];
@@ -21,4 +24,19 @@ extern char __ctors_start[], __ctors_end[];
 #define barebox_image_size	(__image_end - __image_start)
 #define barebox_bare_init_size	(unsigned int)&_barebox_bare_init_size
 #define barebox_pbl_size	(__piggydata_start - __image_start)
+
+/**
+ * is_barebox_rodata - checks if the pointer address is located in the
+ *                    .rodata section
+ *
+ * @addr: address to check
+ *
+ * Returns: true if the address is located in .rodata, false otherwise.
+ */
+static inline bool is_barebox_rodata(unsigned long addr)
+{
+	return addr >= (unsigned long)__start_rodata &&
+	       addr < (unsigned long)__end_rodata;
+}
+
 #endif /* _ASM_GENERIC_SECTIONS_H_ */

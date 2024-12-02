72f3e82057f67bcb1c9009ad08a7f6a6eb004bf2
Ahmad Fatoum <a.fatoum@pengutronix.de>
2024-12-02 09:08:17 +0100

    crc: crc-itu-t: generate lookup table on first use
    Unlike CRC32, CRC16 enjoys only limited use in barebox:
    
      - RATP
      - DDR SPD EEPROM
      - TQ EEPROM
      - SD over SPI
      - XYMODEM
    
    Nevertheless, once one of these is enabled, a lookup table of 512 bytes
    is generated, which is, by definition, badly compressible.
    
    Let's do what CRC32 is doing and generate the lookup table anew on first
    access instead.
    
    This saves us 404 bytes for a XZ-compressed THUMB2 barebox.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.kernel.org/r/20241202080818.3471605-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/crc.h
+++ include/crc.h
@@ -10,30 +10,7 @@
  *   Poly  0x1021 (x^16 + x^12 + x^15 + 1)
  *   Init  0
  */
-extern u16 const crc_itu_t_table[256];
-
 extern u16 crc_itu_t(u16 crc, const u8 *buffer, size_t len);
-
-#ifdef __PBL__
-static inline u16 crc_itu_t_byte(u16 crc, const u8 data)
-{
-	int i;
-
-	crc = crc ^ data << 8;
-	for (i = 0; i < 8; ++i) {
-		if (crc & 0x8000)
-			crc = crc << 1 ^ 0x1021;
-		else
-			crc = crc << 1;
-	}
-	return crc;
-}
-#else
-static inline u16 crc_itu_t_byte(u16 crc, const u8 data)
-{
-	return (crc << 8) ^ crc_itu_t_table[((crc >> 8) ^ data) & 0xff];
-}
-#endif
 
 uint32_t crc32(uint32_t, const void *, unsigned int);
 uint32_t crc32_be(uint32_t, const void *, unsigned int);

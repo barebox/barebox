0e59188049f779fb1a9d5839d3f0688d107281f7
Alexander Shiyan <eagle.alexander923@gmail.com>
2024-12-05 09:16:41 +0300

    video: omap-fb: Remove orphan driver
    Since OMAP non-PBL boards are removed from the barebox,
    the omap-fb driver remains unused. Let's remove it.
    
    Fixes: b93f8a06c85f ("ARM: remove non PBL OMAP boards")
    Signed-off-by: Alexander Shiyan <eagle.alexander923@gmail.com>
    Link: https://lore.kernel.org/r/20241205061641.7688-1-eagle.alexander923@gmail.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-omap/omap_devices.c
+++ arch/arm/mach-omap/omap_devices.c
@@ -21,32 +21,3 @@ struct device *omap_add_uart(int id, unsigned long base)
 	return add_generic_device("omap-uart", id, NULL, base, 1024,
 				  IORESOURCE_MEM | IORESOURCE_MEM_8BIT, NULL);
 }
-
-#if defined(CONFIG_DRIVER_VIDEO_OMAP)
-static struct resource omapfb_resources[] = {
-	{
-		.name	= "omap4_dss",
-		.start	= 0x48040000,
-		.end	= 0x48040000 + 512 - 1,
-		.flags	= IORESOURCE_MEM | IORESOURCE_MEM_32BIT,
-	}, {
-		.name	= "omap4_dispc",
-		.start	= 0x48041000,
-		.end	= 0x48041000 + 3072 - 1,
-		.flags	= IORESOURCE_MEM | IORESOURCE_MEM_32BIT,
-	},
-};
-
-struct device *omap_add_display(struct omapfb_platform_data *o_pdata)
-{
-	return add_generic_device_res("omap_fb", -1,
-				      omapfb_resources,
-				      ARRAY_SIZE(omapfb_resources),
-				      o_pdata);
-}
-#else
-struct device *omap_add_display(struct omapfb_platform_data *o_pdata)
-{
-	return NULL;
-}
-#endif

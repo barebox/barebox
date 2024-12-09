b770e8ca7fdc919ddfceaeefd40ab75a38c4bf9f
Sascha Hauer <s.hauer@pengutronix.de>
2024-12-09 15:28:13 +0100

    ARM: beagleplay: put external device tree into handoff data
    Pass the dtb from 1st stage loader to barebox using handoff data.
    
    Link: https://lore.kernel.org/r/20241209-ext-dt-handoff-v1-3-e61052d5d437@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/beagleplay/lowlevel.c
+++ arch/arm/boards/beagleplay/lowlevel.c
@@ -5,21 +5,27 @@
 #include <asm/barebox-arm.h>
 #include <debug_ll.h>
 #include <pbl.h>
+#include <pbl/handoff-data.h>
+#include <compressed-dtb.h>
 
 /* Called from assembly */
-void beagleplay(void);
+void beagleplay(void *dtb);
 
-static noinline void beagleplay_continue(void)
+static noinline void beagleplay_continue(void *dtb)
 {
 	unsigned long membase, memsize;
 	extern char __dtb_k3_am625_beagleplay_start[];
+	unsigned int size;
 
 	fdt_find_mem(__dtb_k3_am625_beagleplay_start, &membase, &memsize);
+
+	if (blob_is_valid_fdt_ptr(dtb, membase, memsize, &size))
+		handoff_data_add(HANDOFF_DATA_EXTERNAL_DT, dtb, size);
 
 	barebox_arm_entry(membase, memsize, __dtb_k3_am625_beagleplay_start);
 }
 
-void beagleplay(void)
+void beagleplay(void *dtb)
 {
 	putc_ll('>');
 
@@ -29,5 +35,5 @@ void beagleplay(void)
 
 	setup_c();
 
-	beagleplay_continue();
+	beagleplay_continue(dtb);
 }

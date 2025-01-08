barebox/barebox

commit 3a478ff3e1e6c8973744c3bd427d5bdff92fa4cd

fs: support opening /
With open() on / no longer accessing uninitialized memory, it now always
fails with errno == ENOENT. This is because d_lookup only compares
children of /, but never / itself.

Fix this by comparing the parent and while at it, rework the existing
code to be more concise.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240304190038.3486881-42-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -1502,7 +1502,6 @@ void d_add(struct dentry *dentry, struct inode *inode)
 }
 
 static bool d_same_name(const struct dentry *dentry,
-			const struct dentry *parent,
 			const struct qstr *name)
 {
 	if (dentry->d_name.len != name->len)
@@ -1511,17 +1510,16 @@ static bool d_same_name(const struct dentry *dentry,
 	return strncmp(dentry->d_name.name, name->name, name->len) == 0;
 }
 
-static struct dentry *d_lookup(const struct dentry *parent, const struct qstr *name)
+static struct dentry *d_lookup(struct dentry *parent, const struct qstr *name)
 {
 	struct dentry *dentry;
 
+	if (d_same_name(parent, name))
+		return dget(parent);
+
 	list_for_each_entry(dentry, &parent->d_subdirs, d_child) {
-		if (!d_same_name(dentry, parent, name))
-			continue;
-
-		dget(dentry);
-
-		return dentry;
+		if (d_same_name(dentry, name))
+			return dget(dentry);
 	}
 
 	return NULL;

commit adbd003e2d65416ebf46099ea7c637d0517cd572

fs: fix resolving paths when directory name repeats
d_same_name() used to only check if the name exists in any of the
directory's children.

In order to fix lookup of /, the function was changed to also compare
the name against the parent. This is obviously wrong, when the parent
and child have the same name: Resolution of the child would always
return the parent dentry triggering unexpected behavior.

Fix this by effectively reverting the functional change in commit
3a478ff3e1e6 ("fs: support opening /"). This breaks opening /, but that
currently only affects dirfd usage and will be fixed separately.

Fixes: 3a478ff3e1e6 ("fs: support opening /")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.kernel.org/r/20250108091425.911629-2-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/fs.c
@@ -1530,9 +1530,6 @@ static struct dentry *d_lookup(struct dentry *parent, const struct qstr *name)
 {
 	struct dentry *dentry;
 
-	if (d_same_name(parent, name))
-		return dget(parent);
-
 	list_for_each_entry(dentry, &parent->d_subdirs, d_child) {
 		if (d_same_name(dentry, name))
 			return dget(dentry);

test/self/dirfd.c
@@ -100,8 +100,13 @@ static void test_dirfd(void)
 	int fd;
 
 	fd = open("/", O_PATH);
-	if (expect(fd < 0, false, "open(/, O_PATH) = %d", fd))
+	if (expect(fd < 0, false, "open(/, O_PATH) = %d", fd)) {
 		close(fd);
+	} else {
+		pr_info("\tIgnoring expected failure\n");
+		failed_tests--;
+		skipped_tests++;
+	}
 
 #define B(dot, dotdot, zero, dev) 0b##dev##zero##dotdot##dot
 	/* We do fiften tests for every configuration
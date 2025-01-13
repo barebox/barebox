c28e6b932a8c1fcb5b6a32e0f58ef2d1fe5a4da6
Sascha Hauer <s.hauer@pengutronix.de>
2025-01-13 12:27:06 +0100

    ARM: beagleplay: add Cortex-R5 boot support
    This adds a new image for the beagleplay board for running on the
    Cortex-R5 boot processor. It will setup the SDRAM and load and start
    the following stages from the boot medium.
    
    Link: https://lore.kernel.org/r/20250113-k3-r5-v3-19-065fcdcc28d3@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/beagleplay/ddr.h
@@ -0,0 +1,6 @@
+#ifndef __BEAGLEPLAY_DDR_H
+#define __BEAGLEPLAY_DDR_H
+
+void beagleplay_ddr_init(void);
+
+#endif /* __BEAGLEPLAY_DDR_H */
--- arch/arm/boards/beagleplay/lowlevel.c
+++ arch/arm/boards/beagleplay/lowlevel.c
@@ -3,10 +3,15 @@
 #include <common.h>
 #include <asm/barebox-arm-head.h>
 #include <asm/barebox-arm.h>
+#include <mach/k3/debug_ll.h>
 #include <debug_ll.h>
 #include <pbl.h>
 #include <pbl/handoff-data.h>
 #include <compressed-dtb.h>
+#include <cache.h>
+#include <mach/k3/r5.h>
+
+#include "ddr.h"
 
 /* Called from assembly */
 void beagleplay(void *dtb);
@@ -36,4 +41,37 @@ void beagleplay(void *dtb)
 	setup_c();
 
 	beagleplay_continue(dtb);
+}
+
+extern char __dtb_k3_am625_r5_beagleplay_start[];
+
+static noinline void beagleplay_r5_continue(void)
+{
+	pbl_set_putc((void *)debug_ll_ns16550_putc, (void *)AM62X_UART_UART0_BASE);
+
+	putc_ll('>');
+
+	k3_mpu_setup_regions();
+
+	am625_early_init();
+	beagleplay_ddr_init();
+
+	barebox_arm_entry(0x80000000, 0x80000000, __dtb_k3_am625_r5_beagleplay_start);
+}
+
+void beagleplay_r5_entry(void);
+
+void beagleplay_r5_entry(void)
+{
+	k3_ctrl_mmr_unlock();
+
+	writel(0x00050000, 0xf41c8);
+	writel(0x00010000, 0xf41cc);
+
+	k3_debug_ll_init((void *)AM62X_UART_UART0_BASE);
+
+	relocate_to_current_adr();
+	setup_c();
+
+	beagleplay_r5_continue();
 }

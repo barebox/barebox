9124e7c0b5638f71ef4442fcafab2987158d5a7f
Sascha Hauer <s.hauer@pengutronix.de>
2025-02-12 15:09:23 +0100

    ARM: k3: r5: add USB DFU and eMMC boot support
    This patch adds USB DFU and eMMC boot support for the k3 SoCs.
    
    DFU mode is entered when the SoC is booted from USB. For continuing the
    boot we need several binaries (bl31, OP-TEE, barebox, ti-dm firmware),
    these can be uploaded as distinct files or combined together as a FIP
    image. This patch also adds the possibility to put a FIP image on the
    SD/eMMC card which is then preferred over the distinct files which were
    previously expected on SD/eMMC cards.
    
    Also the K3 SoCs have a boot mode to boot from eMMC boot partitions for
    which this patch adds support. When this bootmode is selected the
    tiboot3.bin file is loaded from the active boot partition. We then need
    to load the FIP image from the same boot partition. This image is
    expected to be at offset 1MiB in the boot partition.
    
    Link: https://lore.kernel.org/r/20250212-k3-emmc-v2-10-8dd1bb0ce60a@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/mach/k3/common.h
+++ include/mach/k3/common.h
@@ -2,11 +2,18 @@
 #define __MACH_K3_COMMON_H
 
 #include <bootsource.h>
+#include <linux/sizes.h>
+#include <linux/uuid.h>
+
+#define UUID_TI_DM_FW \
+        UUID_INIT(0x9e8c2017, 0x8b94, 0x4e2b, 0xa7, 0xb3, 0xa0, 0xf8, 0x8e, 0xab, 0xb8, 0xae)
 
 void am625_get_bootsource(enum bootsource *src, int *instance);
 bool k3_boot_is_emmc(void);
 u64 am625_sdram_size(void);
 void am625_register_dram(void);
 void am625_enable_32k_crystal(void);
+
+#define K3_EMMC_BOOTPART_TIBOOT3_BIN_SIZE	SZ_1M
 
 #endif /* __MACH_K3_COMMON_H */

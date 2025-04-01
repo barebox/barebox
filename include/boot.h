226b9603368b140f4979a2959bf89ccfa60aef3f
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-04-01 12:47:59 +0200

    boot: split off bootarg API into new bootargs.h header
    In preparation for adding new functions to the flexible bootarg support
    that are not used at boot-time, factor out the bootargs support,
    so new code need not include the whole boot.h header.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20250401104806.3959859-10-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/boot.h
+++ include/boot.h
@@ -6,21 +6,7 @@
 #include <menu.h>
 #include <environment.h>
 #include <bootm-overrides.h>
-
-#ifdef CONFIG_FLEXIBLE_BOOTARGS
-const char *linux_bootargs_get(void);
-int linux_bootargs_overwrite(const char *bootargs);
-#else
-static inline const char *linux_bootargs_get(void)
-{
-	return getenv("bootargs");
-}
-
-static inline int linux_bootargs_overwrite(const char *bootargs)
-{
-	return setenv("bootargs", bootargs);
-}
-#endif
+#include <bootargs.h>
 
 struct bootentries {
 	struct list_head entries;

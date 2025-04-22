368b097ec5732a7a80640fb774d8ee77d58d8b83
Sascha Hauer <s.hauer@pengutronix.de>
2025-04-22 16:57:14 +0200

    Revert "ARM: k3: define debug_ll_write_reg only on CONFIG_DEBUG_AM62X_UART=y"
    This patch breaks compilation of beagleplay in multi_v8_defconfig:
    
    arch/arm/boards/beagleplay/lowlevel.c:44:30: error:  debug_ll_ns16550_putc' undeclared (first use in
    this function)
       44 |         pbl_set_putc((void *)debug_ll_ns16550_putc, (void *)AM62X_UART_UART0_BASE);
    
    debug_ll_ns16550_putc() shoudn't be used as pbl putc function directly.
    This should be fixed, but it is no material for master, so revert for
    now.
    
    This reverts commit 6b749354d11a475f54c51aa76f82e224b9b59ef3.

--- include/mach/k3/debug_ll.h
+++ include/mach/k3/debug_ll.h
@@ -2,20 +2,6 @@
 #define __MACH_K3_DEBUG_LL_H__
 #include <io.h>
 
-#define AM62X_UART_UART0_BASE	0x02800000
-#define AM62X_UART_UART1_BASE	0x02810000
-#define AM62X_UART_UART2_BASE	0x02820000
-#define AM62X_UART_UART3_BASE	0x02830000
-#define AM62X_UART_UART4_BASE	0x02840000
-#define AM62X_UART_UART5_BASE	0x02850000
-#define AM62X_UART_UART6_BASE	0x02860000
-
-#if defined CONFIG_DEBUG_AM62X_UART
-#define K3_DEBUG_SOC AM62X_UART
-
-#define __K3_UART_BASE(soc, num) soc##_UART##num##_BASE
-#define K3_UART_BASE(soc, num) __K3_UART_BASE(soc, num)
-
 static inline uint8_t debug_ll_read_reg(void __iomem *base, int reg)
 {
 	return readb(base + (reg << 2));
@@ -36,6 +22,25 @@ static inline void k3_debug_ll_init(void __iomem *base)
 	debug_ll_write_reg(base, 8, 0x00);
 }
 
+#define AM62X_UART_UART0_BASE	0x02800000
+#define AM62X_UART_UART1_BASE	0x02810000
+#define AM62X_UART_UART2_BASE	0x02820000
+#define AM62X_UART_UART3_BASE	0x02830000
+#define AM62X_UART_UART4_BASE	0x02840000
+#define AM62X_UART_UART5_BASE	0x02850000
+#define AM62X_UART_UART6_BASE	0x02860000
+
+#if defined CONFIG_DEBUG_AM62X_UART
+#define K3_DEBUG_SOC AM62X_UART
+
+#define __K3_UART_BASE(soc, num) soc##_UART##num##_BASE
+#define K3_UART_BASE(soc, num) __K3_UART_BASE(soc, num)
+
+static inline void debug_ll_init(void)
+{
+	/* already configured */
+}
+
 static inline void PUTC_LL(int c)
 {
 	void __iomem *base = (void *)K3_UART_BASE(K3_DEBUG_SOC,
@@ -44,10 +49,6 @@ static inline void PUTC_LL(int c)
 	debug_ll_ns16550_putc(base, c);
 }
 
-#else
-static inline void k3_debug_ll_init(void __iomem *base)
-{
-}
 #endif
 
 #endif /* __MACH_K3_DEBUG_LL_H__ */

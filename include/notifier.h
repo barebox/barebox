667e0e153125279499a87ec081207eaf36ca56c0
Ahmad Fatoum <a.fatoum@barebox.org>
2025-04-22 09:56:48 +0200

    include: add support for NOTIFY_BAD
    When notified, returning NOTIFY_BAD or NOTIFY_STOP can be used stop
    further calls and inform the original notification site.
    
    Provide the same in barebox.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
    Link: https://lore.barebox.org/20250422075648.220966-1-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/notifier.h
+++ include/notifier.h
@@ -37,5 +37,21 @@ int clock_notifier_call_chain(void);
 
 #define NOTIFY_DONE		0x0000		/* Don't care */
 #define NOTIFY_OK		0x0001		/* Suits me */
+#define NOTIFY_STOP_MASK	0x8000		/* Don't call further */
+#define NOTIFY_BAD		(NOTIFY_STOP_MASK|0x0002)
+						/* Bad/Veto action */
+/*
+ * Clean way to return from the notifier and stop further calls.
+ */
+#define NOTIFY_STOP		(NOTIFY_OK|NOTIFY_STOP_MASK)
+
+/* Encapsulate (negative) errno value (in particular, NOTIFY_BAD <=> EPERM). */
+static inline int notifier_from_errno(int err)
+{
+	if (err)
+		return NOTIFY_STOP_MASK | (NOTIFY_OK - err);
+
+	return NOTIFY_OK;
+}
 
 #endif /* __NOTIFIER_H */

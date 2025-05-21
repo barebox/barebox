barebox/barebox

commit f0d91d7ba6cb5b5f8d107bef211c68a4c1d7b0f6

of: partition: add Linux CONFIG_OF_PARTITION-compatible adaptive fixup mode
Linux supports OF partitions for MMC since Linux v6.13-rc1 with commit
2e3a191e89f9 ("block: add support for partition table defined in OF"),
but in a manner incompatible to barebox:

If CONFIG_OF_PARTITION is enabled and a fixed-partitions node exists,
the EFI/MBR partition table is completely ignored.
In barebox, they are allowed to coexist, as long as they don't conflict.

To resolve this conflict, we change the default fixup scheme to an
"adaptive" scheme that uses new-style fixups, but with the compatible
depending on the parent device:

  - If the partitions node in the kernel DT already has a compatible,
    use the same compatible as not to clobber new Linux OF partitions,
    which would be parsed by CONFIG_OF_PARTITION
  - If the device is an MTD device, use "fixed-partitions", so Linux
    sees the same partitions and to maintain backwards compatibility
  - For everything else (i.e. EEPROMs and SD/eMMC), use
    "barebox,fixed-partitions"

This scheme is fully backwards-compatible with older kernels and
userspace:

  - barebox,fixed-partitions replaces fixed-partitions only
    for nodes that Linux didn't parse before anyway

  - fixed-partitions will continue to be emitted for MTD,
    which Linux did parse before

  - barebox-state doesn't actually look for the "fixed-partitions"
    compatible to find out if a patition is in a container.
    Instead it checks the parent node name to be equal
    to "partitions"...

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.kernel.org/r/20250217180833.3955657-7-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/of/partition.c
@@ -16,8 +16,6 @@
 #include <init.h>
 #include <globalvar.h>
 
-static unsigned int of_partition_binding;
-
 /**
  * enum of_binding_name - Name of binding to use for OF partition fixup
  * @MTD_OF_BINDING_NEW:		Fix up new-style partition bindings
@@ -25,12 +23,19 @@ static unsigned int of_partition_binding;
  * @MTD_OF_BINDING_LEGACY:	Fix up legacy partition bindings
  *				directly into the parent node without container
  * @MTD_OF_BINDING_DONTTOUCH:	Don't touch partition nodes at all - no fixups
+ * @MTD_OF_BINDING_ADAPTIVE:	Do a new-style fixup with compatible being either:
+ *				- the same compatible as in the kernel DT if available
+ *				- "fixed-partitions" for MTD
+ *				- "barebox,fixed-partitions" otherwise
  */
 enum of_binding_name {
 	MTD_OF_BINDING_NEW,
 	MTD_OF_BINDING_LEGACY,
 	MTD_OF_BINDING_DONTTOUCH,
+	MTD_OF_BINDING_ADAPTIVE,
 };
+
+static unsigned int of_partition_binding = MTD_OF_BINDING_ADAPTIVE;
 
 struct cdev *of_parse_partition(struct cdev *cdev, struct device_node *node)
 {
@@ -172,6 +177,7 @@ int of_fixup_partitions(struct device_node *np, struct cdev *cdev)
 {
 	struct cdev *partcdev;
 	struct device_node *part, *partnode;
+	const char *compat = "fixed-partitions";
 	int ret;
 	int n_cells, n_parts = 0;
 
@@ -199,10 +205,16 @@ int of_fixup_partitions(struct device_node *np, struct cdev *cdev)
 		of_delete_node(partnode);
 		partnode = np;
 		break;
+	case MTD_OF_BINDING_ADAPTIVE:
+		/* If there's already a compatible property, leave it as-is */
+		if (of_property_present(partnode, "compatible"))
+			break;
+		if (!cdev->mtd)
+			compat = "barebox,fixed-partitions";
+		fallthrough;
 	case MTD_OF_BINDING_NEW:
 		partnode = partnode ?: of_new_node(np, "partitions");
-		ret = of_property_write_string(partnode, "compatible",
-					       "fixed-partitions");
+		ret = of_property_write_string(partnode, "compatible", compat);
 		if (ret)
 			return ret;
 		break;
@@ -302,7 +314,7 @@ int of_partitions_register_fixup(struct cdev *cdev)
 }
 
 static const char *of_binding_names[] = {
-	"new", "legacy", "donttouch"
+	"new", "legacy", "donttouch", "adaptive"
 };
 
 static int of_partition_init(void)

commit 1c9a72656f8828a8d495f0bfe7662ad81caf6ed4

of: partition: overwrite arm,arm-firmware-suite unless donttouch
barebox doesn't support arm,arm-firmware-suite partitioning and if the
barebox device tree uses a fixed-partitions binding for a flash, it will
overwrite a kernel partitions node that uses arm,arm-firmware-suite with
the fixed partition binding.

This broke in the default case with the addition of
global.of_partition_binding=adaptive, because the check for a compatible
was successful, but the compatible was neither fixed-partitions or
barebox,fixed-partitions.

Fix this by explicitly checking the compatibles we expect.
This fixes a failed of_get_reproducible_name in of_state_fixup, which
broke barebox-state usage on Vexpress in DistroKit.

Fixes: f0d91d7ba6cb ("of: partition: add Linux CONFIG_OF_PARTITION-compatible adaptive fixup mode")
Reported-by: Robert Schwebel <rsc@pengutronix.de>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250520124118.73783-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/of/partition.c
@@ -206,8 +206,8 @@ int of_fixup_partitions(struct device_node *np, struct cdev *cdev)
 		partnode = np;
 		break;
 	case MTD_OF_BINDING_ADAPTIVE:
-		/* If there's already a compatible property, leave it as-is */
-		if (of_property_present(partnode, "compatible"))
+		/* If there's already a fixed-partitions node, leave compatible as-is */
+		if (of_node_is_fixed_partitions(partnode))
 			break;
 		if (!cdev->mtd)
 			compat = "barebox,fixed-partitions";
3c1a4a3e48eb9d86f613f234199b657e99e0cd40
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-05-28 12:50:39 +0200

    ARM: lib32: disable ATAGS support by default
    We haven't had any new platforms with ATAGS for many years, so it's
    about time we make the support configurable and disable it by default.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20250528105046.3270397-4-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ Documentation/migration-guides/migration-2025.07.0.rst
@@ -0,0 +1,6 @@
+Configuration options
+---------------------
+
+* The legacy ARM 32-bit ATAGS mechanism is now disabled by default.
+  It can be re-enabled using the newly added ``CONFIG_BOOT_ATAGS`` option.
+  Use of ATAGS is deprecated. Users should migrate to OpenFirmware device trees.
--- arch/arm/include/asm/armlinux.h
+++ arch/arm/include/asm/armlinux.h
@@ -6,19 +6,36 @@
 #include <asm/memory.h>
 #include <asm/setup.h>
 #include <asm/secure.h>
+#include <linux/bug.h>
 
-#if defined CONFIG_ARM_LINUX && defined CONFIG_CPU_32
+struct tag;
+
+#if defined CONFIG_BOOT_ATAGS
 void armlinux_set_bootparams(void *params);
+struct tag *armlinux_get_bootparams(void);
 void armlinux_set_architecture(unsigned architecture);
+unsigned armlinux_get_architecture(void);
 void armlinux_set_revision(unsigned int);
 void armlinux_set_serial(u64);
+void armlinux_setup_tags(unsigned long initrd_address,
+			 unsigned long initrd_size, int swap);
 #else
 static inline void armlinux_set_bootparams(void *params)
 {
 }
 
+static inline struct tag *armlinux_get_bootparams(void)
+{
+	BUG();
+}
+
 static inline void armlinux_set_architecture(unsigned architecture)
 {
+}
+
+static inline unsigned armlinux_get_architecture(void)
+{
+	return 0;
 }
 
 static inline void armlinux_set_revision(unsigned int rev)
@@ -28,8 +45,12 @@ static inline void armlinux_set_revision(unsigned int rev)
 static inline void armlinux_set_serial(u64 serial)
 {
 }
-#endif
 
+static inline void armlinux_setup_tags(unsigned long initrd_address,
+				       unsigned long initrd_size, int swap)
+{
+}
+#endif
 #if defined CONFIG_ARM_BOARD_APPEND_ATAG
 void armlinux_set_atag_appender(struct tag *(*)(struct tag *));
 #else

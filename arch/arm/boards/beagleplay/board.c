b5883c42b29ca93c62f3f1ff4a67497887f5605d
Lars Schmidt <l.schmidt@pengutronix.de>
2025-06-11 11:36:22 +0200

    ARM: k3: beagleplay: Enable 32k crystal
    Signed-off-by: Lars Schmidt <l.schmidt@pengutronix.de>
    Link: https://lore.barebox.org/20250611093622.1309016-1-l.schmidt@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/beagleplay/board.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#define pr_fmt(fmt) "beagleplay: " fmt
+
+#include <linux/kernel.h>
+#include <mach/k3/common.h>
+#include <driver.h>
+#include <bbu.h>
+
+static int beagleplay_probe(struct device *dev)
+{
+	am625_enable_32k_crystal();
+
+	return 0;
+}
+
+static __maybe_unused struct of_device_id beagleplay_ids[] = {
+	{
+		.compatible = "beagle,am625-beagleplay",
+	}, {
+		/* sentinel */
+	}
+};
+
+static struct driver beagleplay_driver = {
+	.name = "beagleplay",
+	.probe = beagleplay_probe,
+	.of_compatible = beagleplay_ids,
+};
+coredevice_platform_driver(beagleplay_driver);

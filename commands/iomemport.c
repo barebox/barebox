fbc90b1e85bcd4fb6265edf2bc42362ea291aa49
Ahmad Fatoum <a.fatoum@barebox.org>
2025-06-16 09:16:32 +0200

    commands: iomemport: support showing info for particular argument
    Follow-up commit will start printing extra information on an address, so
    prepare for that by allowing printout of information related to a
    specific address.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
    Link: https://lore.barebox.org/20250616071634.811000-4-a.fatoum@barebox.org
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- commands/iomemport.c
+++ commands/iomemport.c
@@ -13,13 +13,16 @@
 #define	FLAG_IOPORT		BIT(1)
 
 static void __print_resources(struct resource *res, int indent,
-			      unsigned flags)
+			      ulong *addr, unsigned flags)
 {
 	const char *size_str;
 	char buf[64];
 	struct resource *r;
 	resource_size_t size = resource_size(res);
 	int i;
+
+	if (addr && !region_overlap_end(*addr, *addr, res->start, res->end))
+		return;
 
 	for (i = 0; i < indent; i++)
 		printf("  ");
@@ -37,19 +40,20 @@ static void __print_resources(struct resource *res, int indent,
 			res->name);
 
 	list_for_each_entry(r, &res->children, sibling) {
-		__print_resources(r, indent + 1, flags);
+		__print_resources(r, indent + 1, addr, flags);
 	}
 }
 
-static void print_resources(struct resource *res, unsigned flags)
+static void print_resources(struct resource *res, ulong *addr, unsigned flags)
 {
-	__print_resources(res, 0, flags);
+	__print_resources(res, 0, addr, flags);
 }
 
 static int do_iomem(int argc, char *argv[])
 {
+	ulong addr, *arg = NULL;
 	unsigned flags = 0;
-	int opt;
+	int opt, ret;
 
 	while((opt = getopt(argc, argv, "v")) > 0) {
 		switch(opt) {
@@ -64,25 +68,56 @@ static int do_iomem(int argc, char *argv[])
 	argv += optind;
 	argc -= optind;
 
-	if (argc > 0)
+	if (argc > 1)
 		return COMMAND_ERROR_USAGE;
 
-	print_resources(&iomem_resource, flags);
+	if (argv[0]) {
+		ret = kstrtoul(argv[0], 16, &addr);
+		if (ret)
+			return ret;
+		arg = &addr;
+	}
+
+	print_resources(&iomem_resource, arg, flags);
 
 	return 0;
 }
 
+BAREBOX_CMD_HELP_START(iomem)
+BAREBOX_CMD_HELP_TEXT("Print barebox view of the physical address space.")
+BAREBOX_CMD_HELP_TEXT("An optional ADDRESS can be specified to get information")
+BAREBOX_CMD_HELP_TEXT("about its region in particular")
+BAREBOX_CMD_HELP_END
+
 BAREBOX_CMD_START(iomem)
 	.cmd		= do_iomem,
 	BAREBOX_CMD_DESC("show IO memory usage")
-	BAREBOX_CMD_OPTS("[-v]")
+	BAREBOX_CMD_OPTS("[-v] [ADDRESS]")
 	BAREBOX_CMD_GROUP(CMD_GRP_INFO)
+	BAREBOX_CMD_HELP(cmd_iomem_help)
 BAREBOX_CMD_END
 
 #if IO_SPACE_LIMIT > 0
 static int do_ioport(int argc, char *argv[])
 {
-	print_resources(&ioport_resource, FLAG_IOPORT);
+	ulong addr, *arg = NULL;
+	int ret;
+
+	/* No options supported */
+	argv += 1;
+	argc -= 1;
+
+	if (argc > 1)
+		return COMMAND_ERROR_USAGE;
+
+	if (argv[0]) {
+		ret = kstrtoul(argv[0], 16, &addr);
+		if (ret)
+			return ret;
+		arg = &addr;
+	}
+
+	print_resources(&ioport_resource, arg, FLAG_IOPORT);
 
 	return 0;
 }
@@ -90,6 +125,7 @@ static int do_ioport(int argc, char *argv[])
 BAREBOX_CMD_START(ioport)
 	.cmd		= do_ioport,
 	BAREBOX_CMD_DESC("show IO port usage")
+	BAREBOX_CMD_OPTS("[PORT]")
 	BAREBOX_CMD_GROUP(CMD_GRP_INFO)
 BAREBOX_CMD_END
 #endif

barebox/barebox

commit 8f71bf6146acc3717b5656d9e7141d643914ad2d

bootm: create generic FIT image handler
So far every architecture registers its own image handler for handling
FIT images. FIT is a container format and as such we can create a
generic handler for it. To find the correct handler for the kernel we
then have to detect the OS file type from the kernel within the fit
image instead of the original image.

For the generic FIT handler repurpose the sandbox FIT image handler. The
code therein was bogus anyway as it tried to open the FIT image which
will already be opened by the generic code. Remove that stuff and just
print an error message.

Link: https://lore.barebox.org/20250407130729.2889508-2-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/lib32/bootm.c
@@ -729,12 +729,6 @@ BAREBOX_MAGICVAR(aimage_noverwrite_bootargs, "Disable overwrite of the bootargs 
 BAREBOX_MAGICVAR(aimage_noverwrite_tags, "Disable overwrite of the tags addr with the one present in aimage");
 #endif
 
-static struct image_handler arm_fit_handler = {
-        .name = "FIT image",
-        .bootm = do_bootm_linux,
-        .filetype = filetype_oftree,
-};
-
 static struct binfmt_hook binfmt_aimage_hook = {
 	.type = filetype_aimage,
 	.exec = "bootm",
@@ -766,8 +760,6 @@ static int armlinux_register_image_handler(void)
 		register_image_handler(&aimage_handler);
 		binfmt_register(&binfmt_aimage_hook);
 	}
-	if (IS_BUILTIN(CONFIG_FITIMAGE))
-	        register_image_handler(&arm_fit_handler);
 	binfmt_register(&binfmt_arm_zimage_hook);
 	binfmt_register(&binfmt_barebox_hook);
 

arch/kvx/lib/bootm.c
@@ -107,28 +107,16 @@ err_free_fdt:
 	return ret;
 }
 
-static int do_bootm_fit(struct image_data *data)
-{
-	int ret;
-	struct elf_image *elf;
-
-	elf = elf_open_binary((void *) data->fit_kernel);
-	if (IS_ERR(elf))
-		return PTR_ERR(data->elf);
-
-	ret = do_boot_elf(data, elf);
-
-	elf_close(elf);
-
-	return ret;
-}
-
 static int do_bootm_elf(struct image_data *data)
 {
 	struct elf_image *elf;
 	int ret;
 
-	elf = elf_open(data->os_file);
+	if (data->fit_kernel)
+		elf = elf_open_binary((void *) data->fit_kernel);
+	else
+		elf = elf_open(data->os_file);
+
 	if (IS_ERR(elf))
 		return PTR_ERR(elf);
 
@@ -145,12 +133,6 @@ static struct image_handler elf_handler = {
 	.filetype = filetype_elf,
 };
 
-static struct image_handler fit_handler = {
-	.name = "FIT",
-	.bootm = do_bootm_fit,
-	.filetype = filetype_oftree,
-};
-
 static struct binfmt_hook binfmt_elf_hook = {
 	.type = filetype_elf,
 	.exec = "bootm",
@@ -159,9 +141,6 @@ static struct binfmt_hook binfmt_elf_hook = {
 static int kvx_register_image_handler(void)
 {
 	register_image_handler(&elf_handler);
-
-	if (IS_ENABLED(CONFIG_FITIMAGE))
-		register_image_handler(&fit_handler);
 
 	binfmt_register(&binfmt_elf_hook);
 

arch/riscv/lib/bootm.c
@@ -42,12 +42,6 @@ static struct image_handler riscv_linux_efi_handler = {
         .filetype = filetype_riscv_efi_linux_image,
 };
 
-static struct image_handler riscv_fit_handler = {
-	.name = "FIT image",
-	.bootm = do_bootm_linux,
-	.filetype = filetype_oftree,
-};
-
 static struct image_handler riscv_barebox_handler = {
         .name = "RISC-V barebox image",
         .bootm = do_bootm_linux,
@@ -59,9 +53,6 @@ static int riscv_register_image_handler(void)
 	register_image_handler(&riscv_linux_handler);
 	register_image_handler(&riscv_linux_efi_handler);
 	register_image_handler(&riscv_barebox_handler);
-
-	if (IS_ENABLED(CONFIG_FITIMAGE))
-		register_image_handler(&riscv_fit_handler);
 
 	return 0;
 }

common/bootm.c
@@ -714,6 +714,7 @@ int bootm_boot(struct bootm_data *bootm_data)
 	int ret;
 	enum filetype os_type;
 	size_t size;
+	const char *os_type_str;
 
 	if (!bootm_data->os_file) {
 		pr_err("no image given\n");
@@ -769,9 +770,13 @@ int bootm_boot(struct bootm_data *bootm_data)
 		}
 	}
 
+	os_type_str = file_type_to_short_string(os_type);
+
 	switch (os_type) {
 	case filetype_oftree:
 		ret = bootm_open_fit(data);
+		os_type = file_detect_type(data->fit_kernel, data->fit_kernel_size);
+		os_type_str = "FIT";
 		break;
 	case filetype_uimage:
 		ret = bootm_open_os_uimage(data);
@@ -782,13 +787,6 @@ int bootm_boot(struct bootm_data *bootm_data)
 	}
 
 	if (ret) {
-		const char *os_type_str;
-
-		if (os_type == filetype_oftree)
-			os_type_str = "FIT";
-		else
-			os_type_str = file_type_to_short_string(os_type);
-
 		pr_err("Loading %s image failed with: %pe\n", os_type_str, ERR_PTR(ret));
 		goto err_out;
 	}

arch/arm/lib64/armlinux.c
@@ -44,12 +44,6 @@ static struct image_handler aarch64_linux_efi_handler = {
         .filetype = filetype_arm64_efi_linux_image,
 };
 
-static struct image_handler aarch64_fit_handler = {
-	.name = "FIT image",
-	.bootm = do_bootm_linux,
-	.filetype = filetype_oftree,
-};
-
 static int do_bootm_barebox(struct image_data *data)
 {
 	void (*fn)(unsigned long x0, unsigned long x1, unsigned long x2,
@@ -96,9 +90,6 @@ static int aarch64_register_image_handler(void)
 	register_image_handler(&aarch64_linux_efi_handler);
 	register_image_handler(&aarch64_linux_handler);
 	register_image_handler(&aarch64_barebox_handler);
-
-	if (IS_ENABLED(CONFIG_FITIMAGE))
-		register_image_handler(&aarch64_fit_handler);
 
 	return 0;
 }

common/image-fit.c
@@ -979,40 +979,21 @@ void fit_close(struct fit_handle *handle)
 	free(handle);
 }
 
-static int do_bootm_sandbox_fit(struct image_data *data)
+static int do_bootm_fit(struct image_data *data)
 {
-	struct fit_handle *handle;
-	void *config;
-	int ret;
+	pr_err("Cannot boot device tree binary blob\n");
 
-	handle = fit_open(data->os_file, data->verbose, BOOTM_VERIFY_NONE,
-			  FILESIZE_MAX);
-	if (IS_ERR(handle))
-		return PTR_ERR(handle);
-
-	config = fit_open_configuration(handle, data->os_part);
-	if (IS_ERR(config)) {
-		ret = PTR_ERR(config);
-		goto out;
-	}
-
-	ret = 0;
-out:
-	fit_close(handle);
-
-	return ret;
+	return -EINVAL;
 }
 
-static struct image_handler sandbox_fit_handler = {
+static struct image_handler fit_handler = {
 	.name = "FIT image",
-	.bootm = do_bootm_sandbox_fit,
+	.bootm = do_bootm_fit,
 	.filetype = filetype_oftree,
 };
 
-static __maybe_unused int sandbox_fit_register(void)
+static int bootm_fit_register(void)
 {
-	return register_image_handler(&sandbox_fit_handler);
+	return register_image_handler(&fit_handler);
 }
-#ifdef CONFIG_SANDBOX
-late_initcall(sandbox_fit_register);
-#endif
+late_initcall(bootm_fit_register);

commit c2ee359ef2e0e690c7f195ff048d87974e93d92e

ARM: ARM32: Fix FIT image booting
Commit 8f71bf6146acc3 ("bootm: create generic FIT image handler")
introduced a generic FIT image handler. With this ARM32 FIT images can
no longer be booted. The ARM32 FIT image handler used to call
do_bootm_linux(), but with the generic FIT image handler we select the
boot handler based on the filetype of the Kernel image which is a zImage
and consequently we call do_bootz_linux(). do_bootz_linux() operates on
data->os_file which points to the original FIT image, so booting fails
with:

invalid magic 0x28341b01

As a workaround check if we are booting a FIT image in do_bootz_linux()
and call do_bootm_linux() in that case.

Fixes: 8f71bf6146ac ("bootm: create generic FIT image handler")
Link: https://lore.barebox.org/20250701101641.549515-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/lib32/bootm.c
@@ -462,6 +462,9 @@ static int do_bootz_linux(struct image_data *data)
 	unsigned long mem_free;
 	void *fdt = NULL;
 
+	if (data->os_fit)
+		return do_bootm_linux(data);
+
 	fd = open(data->os_file, O_RDONLY);
 	if (fd < 0) {
 		perror("open");
barebox/barebox

commit 20d87123a63807770581c8e9cdf4ce9fd9223c6c

treewide: replace dev_get_drvdata with device_get_match_data
Both Linux and barebox define a dev_get_drvdata function, but
annoyingly, the barebox version accesses the private data set in the
probe function, while the Linux version retrieves the match data set by
the driver core instead.

In preparation for changing the function to have the Linux semantics,
start by replacing most instances in barebox with device_get_match_data.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250506063446.3133582-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/eeprom/at24.c
@@ -380,9 +380,9 @@ static int at24_probe(struct device *dev)
 		unsigned long magic;
 		u32 page_size;
 
-		err = dev_get_drvdata(dev, (const void **)&magic);
-		if (err)
-			return err;
+		magic = (uintptr_t)device_get_match_data(dev);
+		if (!magic)
+			return -ENODEV;
 
 		chip.byte_len = BIT(magic & AT24_BITMASK(AT24_SIZE_BYTELEN));
 		magic >>= AT24_SIZE_BYTELEN;

drivers/net/fsl-fman.c
@@ -1085,15 +1085,14 @@ static int fsl_fman_mdio_probe(struct device *dev)
 static int fsl_fman_port_probe(struct device *dev)
 {
 	struct resource *iores;
-	int ret;
 	struct fsl_fman_port *port;
 	unsigned long type;
 
 	dev_dbg(dev, "probe\n");
 
-	ret = dev_get_drvdata(dev, (const void **)&type);
-	if (ret)
-		return ret;
+	type = (uintptr_t)device_get_match_data(dev);
+	if (!type)
+		return -ENODEV;
 
 	iores = dev_request_mem_resource(dev, 0);
 	if (IS_ERR(iores))

drivers/rtc/rtc-ds1307.c
@@ -290,11 +290,11 @@ static int ds1307_probe(struct device *dev)
 	unsigned long driver_data;
 	const struct device_node *np = dev->of_node;
 
-	ds1307 = xzalloc(sizeof(struct ds1307));
+	driver_data = (uintptr_t)device_get_match_data(dev);
+	if (!driver_data)
+		return -ENODEV;
 
-	err = dev_get_drvdata(dev, (const void **)&driver_data);
-	if (err)
-		goto exit;
+	ds1307 = xzalloc(sizeof(struct ds1307));
 
 	ds1307->client = client;
 	ds1307->type = driver_data;

arch/arm/boards/stm32mp15xx-dkx/board.c
@@ -6,12 +6,13 @@
 
 static int dkx_probe(struct device *dev)
 {
-	const void *model;
+	const char *model;
 
 	stm32mp_bbu_mmc_fip_register("sd", "/dev/mmc0",
 				     BBU_HANDLER_FLAG_DEFAULT);
 
-	if (dev_get_drvdata(dev, &model) == 0)
+	model = device_get_match_data(dev);
+	if (model)
 		barebox_set_model(model);
 
 	barebox_set_hostname("stm32mp15xx-dkx");

arch/arm/cpu/psci-client.c
@@ -121,8 +121,8 @@ static int __init psci_probe(struct device *dev)
 	ulong of_version, actual_version;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&of_version);
-	if (ret)
+	of_version = (uintptr_t)device_get_match_data(dev);
+	if (!of_version)
 		return -ENODEV;
 
 	ret = of_property_read_string(dev->of_node, "method", &method);

arch/arm/mach-imx/iim.c
@@ -453,15 +453,17 @@ static int imx_iim_probe(struct device *dev)
 	struct resource *iores;
 	struct iim_priv *iim;
 	int i, ret;
-	struct imx_iim_drvdata *drvdata = NULL;
-	char *nregs = NULL;
+	const struct imx_iim_drvdata *drvdata;
+	const char *nregs = NULL;
 
 	if (imx_iim)
 		return -EBUSY;
 
 	iim = xzalloc(sizeof(*iim));
 
-	dev_get_drvdata(dev, (const void **)&drvdata);
+	drvdata = device_get_match_data(dev);
+	if (!drvdata)
+		return -ENODEV;
 
 	if (drvdata && drvdata->supply)
 		iim->supply = drvdata->supply;

drivers/ata/sata-imx.c
@@ -87,12 +87,12 @@ static int imx_sata_probe(struct device *dev)
 {
 	struct resource *iores;
 	struct imx_ahci *imx_ahci;
-	struct imx_sata_data *data;
+	const struct imx_sata_data *data;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&data);
-	if (ret)
-		return ret;
+	data = device_get_match_data(dev);
+	if (!data)
+		return -ENODEV;
 
 	imx_ahci = xzalloc(sizeof(*imx_ahci));
 

drivers/bus/imx-weim.c
@@ -69,14 +69,14 @@ MODULE_DEVICE_TABLE(of, weim_id_table);
 struct imx_weim {
 	struct device *dev;
 	void __iomem *base;
-	struct imx_weim_devtype *devtype;
+	const struct imx_weim_devtype *devtype;
 };
 
 /* Parse and set the timing for this device. */
 static int
 weim_timing_setup(struct imx_weim *weim, struct device_node *np)
 {
-	struct imx_weim_devtype *devtype = weim->devtype;
+	const struct imx_weim_devtype *devtype = weim->devtype;
 	u32 cs_idx, value[devtype->cs_regs_count];
 	int i, ret;
 
@@ -128,13 +128,13 @@ static int weim_parse_dt(struct imx_weim *weim)
 static int weim_probe(struct device *dev)
 {
 	struct resource *iores;
-	struct imx_weim_devtype *devtype;
+	const struct imx_weim_devtype *devtype;
 	struct imx_weim *weim;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&devtype);
-	if (ret)
-		return ret;
+	devtype = device_get_match_data(dev);
+	if (!devtype)
+		return -ENODEV;
 
 	weim = xzalloc(sizeof(*weim));
 

commit 964a91e4427f7e5b739863cf67288e7591f281f4

drivers: fix buggy comparisons of casted device_get_match_data to zero
device_get_match_data returns NULL when no match data is available,
which a cast to uintptr_t turns into a zero.

Most drivers either put valid pointers into the match data or they
reserve a literal value of zero to be "unknown".

Three drivers don't do that though and use 0 as valid value for the
match data and thus the check against zero breaks probing drivers with
the corresponding match data.

As device_get_match_data() would only fail anyway when manually
allocating platform devices that match by name or when adding new matches
without match data, it's perfectly fine to just skip the check and
assume the match data to be zero in that case to fix the breakage.

Fixes: 20d87123a638 ("treewide: replace dev_get_drvdata with device_get_match_data")
Reported-by: Renaud Barbier <Renaud.Barbier@ametek.com>
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250715201908.2515578-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/eeprom/at24.c
@@ -381,8 +381,6 @@ static int at24_probe(struct device *dev)
 		u32 page_size;
 
 		magic = (uintptr_t)device_get_match_data(dev);
-		if (!magic)
-			return -ENODEV;
 
 		chip.byte_len = BIT(magic & AT24_BITMASK(AT24_SIZE_BYTELEN));
 		magic >>= AT24_SIZE_BYTELEN;

drivers/net/fsl-fman.c
@@ -1091,8 +1091,6 @@ static int fsl_fman_port_probe(struct device *dev)
 	dev_dbg(dev, "probe\n");
 
 	type = (uintptr_t)device_get_match_data(dev);
-	if (!type)
-		return -ENODEV;
 
 	iores = dev_request_mem_resource(dev, 0);
 	if (IS_ERR(iores))

drivers/rtc/rtc-ds1307.c
@@ -291,8 +291,6 @@ static int ds1307_probe(struct device *dev)
 	const struct device_node *np = dev->of_node;
 
 	driver_data = (uintptr_t)device_get_match_data(dev);
-	if (!driver_data)
-		return -ENODEV;
 
 	ds1307 = xzalloc(sizeof(struct ds1307));
 
barebox/barebox

commit a6410cfa9aff182c45747e96b03c52956c77e556

block: mark underlying cdev with DEVFS_IS_BLOCK_DEV
When given a cdev, we detect whether it's a block device by testing that
its ops are those of a block device.

This has the downside that any block device must use the block caching layer.
ramdisks don't benefit from the caching though, so let's add a new flag
bit to mark block devices and use it in preparation for adding ramdisk
support.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250605113530.2076990-15-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/driver.h
@@ -581,6 +581,7 @@ extern struct list_head cdev_list;
 #define DEVFS_PARTITION_FIXED		(1U << 0)
 #define DEVFS_PARTITION_READONLY	(1U << 1)
 #define DEVFS_IS_CHARACTER_DEV		(1U << 3)
+#define DEVFS_IS_BLOCK_DEV		(1U << 4)
 #define DEVFS_PARTITION_FROM_OF		(1U << 5)
 #define DEVFS_PARTITION_FROM_TABLE	(1U << 6)
 #define DEVFS_IS_MBR_PARTITIONED	(1U << 7)

common/block.c
@@ -447,14 +447,6 @@ static struct cdev_operations block_ops = {
 	.discard_range = block_op_discard_range,
 };
 
-struct block_device *cdev_get_block_device(const struct cdev *cdev)
-{
-	if (!cdev || cdev->ops != &block_ops)
-		return NULL;
-
-	return cdev->priv;
-}
-
 int blockdevice_register(struct block_device *blk)
 {
 	loff_t size = (loff_t)blk->num_blocks * BLOCKSIZE(blk);
@@ -465,6 +457,7 @@ int blockdevice_register(struct block_device *blk)
 	blk->cdev.dev = blk->dev;
 	blk->cdev.ops = &block_ops;
 	blk->cdev.priv = blk;
+	blk->cdev.flags |= DEVFS_IS_BLOCK_DEV;
 	blk->rdbufsize = BUFSIZE >> blk->blockbits;
 
 	INIT_LIST_HEAD(&blk->buffered_blocks);

include/block.h
@@ -82,14 +82,9 @@ static inline int block_flush(struct block_device *blk)
 }
 
 #ifdef CONFIG_BLOCK
-struct block_device *cdev_get_block_device(const struct cdev *cdev);
 unsigned file_list_add_blockdevs(struct file_list *files);
 char *cdev_get_linux_rootarg(const struct cdev *partcdev);
 #else
-static inline struct block_device *cdev_get_block_device(const struct cdev *cdev)
-{
-	return NULL;
-}
 static inline unsigned file_list_add_blockdevs(struct file_list *files)
 {
 	return 0;
@@ -102,7 +97,8 @@ static inline char *cdev_get_linux_rootarg(const struct cdev *partcdev)
 
 static inline bool cdev_is_block_device(const struct cdev *cdev)
 {
-	return cdev_get_block_device(cdev) != NULL;
+	return IS_ENABLED(CONFIG_BLOCK) && cdev &&
+		(cdev->flags & DEVFS_IS_BLOCK_DEV);
 }
 
 static inline bool cdev_is_block_partition(const struct cdev *cdev)
@@ -114,6 +110,11 @@ static inline bool cdev_is_block_disk(const struct cdev *cdev)
 {
 	cdev = cdev_readlink(cdev);
 	return cdev_is_block_device(cdev) && !cdev_is_partition(cdev);
+}
+
+static inline struct block_device *cdev_get_block_device(const struct cdev *cdev)
+{
+	return cdev_is_block_device(cdev) ? cdev->priv : NULL;
 }
 
 #endif /* __BLOCK_H */

commit 71d8d678b39ea7ceec479881d6bafd7905735692

block: fix block device detection
Before commit a6410cfa9aff ("block: mark underlying cdev with
DEVFS_IS_BLOCK_DEV") the block device decision was made based on the
block_ops. Since each parition-cdev inherits the parent (master)
cdev-ops the parition-cdevs were detected as block device.

Commit a6410cfa9aff changed the detection mechanism to be based on cdev
flag but forgot to pass the flag to the partition (child) cdev.

Fix this by passing the DEVFS_IS_BLOCK_DEV to the partition (child) cdev
as well. Add a small helper function to query the inheritable devfs
flags instead of open coding it within __devfs_add_partition().

Fixes: a6410cfa9aff ("block: mark underlying cdev with DEVFS_IS_BLOCK_DEV")
Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250724091339.1091147-1-m.felsch@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

fs/devfs-core.c
@@ -599,7 +599,7 @@ static struct cdev *__devfs_add_partition(struct cdev *cdev,
 	loff_t _end = end ? *end : 0;
 	static struct cdev *new;
 	struct cdev *overlap;
-	unsigned inherited_flags = 0;
+	unsigned inherited_flags;
 
 	if (cdev_by_name(partinfo->name))
 		return ERR_PTR(-EEXIST);
@@ -643,7 +643,7 @@ static struct cdev *__devfs_add_partition(struct cdev *cdev,
 	}
 
 	/* Filter flags that we want to pass along to children */
-	inherited_flags |= cdev->flags & DEVFS_WRITE_AUTOERASE;
+	inherited_flags = get_inheritable_devfs_flags(cdev);
 
 	if (IS_ENABLED(CONFIG_MTD) && cdev->mtd) {
 		struct mtd_info *mtd;

include/driver.h
@@ -604,6 +604,16 @@ static inline bool cdev_is_gpt_partitioned(const struct cdev *master)
 	return master && (master->flags & DEVFS_IS_GPT_PARTITIONED);
 }
 
+#define DEVFS_INHERITABLE_FLAGS (DEVFS_WRITE_AUTOERASE | DEVFS_IS_BLOCK_DEV)
+
+static inline unsigned int
+get_inheritable_devfs_flags(const struct cdev *parent_cdev)
+{
+	if (!parent_cdev)
+		return 0;
+	return parent_cdev->flags & DEVFS_INHERITABLE_FLAGS;
+}
+
 struct cdev *
 cdev_find_child_by_gpt_typeuuid(struct cdev *cdev, const guid_t *typeuuid);
 
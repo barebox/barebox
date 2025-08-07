barebox/barebox

commit 926985a74ee043d4cbe1d7b6f9fefb30b3739e4e

bootm: don't clobber global.bootm. variables after script boot fails
While there is global.linux.bootargs.dyn to avoid boot script variables
contaminating later boot targets, there is no such thing for the
global.bootm. variables and as such it was up to the boot targets to
either ignore the variables or to override them.

The proper way would to expect the boot script boot callback to restore
the variables as they were before, so let's do that.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.kernel.org/r/20250214104817.2975052-6-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/bootm.c
@@ -68,6 +68,23 @@ void bootm_data_init_defaults(struct bootm_data *data)
 	data->dryrun = bootm_dryrun;
 }
 
+void bootm_data_restore_defaults(const struct bootm_data *data)
+{
+	globalvar_set("bootm.oftree", data->oftree_file);
+	globalvar_set("bootm.tee", data->tee_file);
+	globalvar_set("bootm.image", data->os_file);
+	pr_setenv("global.bootm.image.loadaddr", "0x%lx", data->os_address);
+	pr_setenv("global.bootm.initrd.loadaddr", "0x%lx", data->initrd_address);
+	globalvar_set("bootm.initrd", data->initrd_file);
+	globalvar_set("bootm.root_dev", data->root_dev);
+	bootm_set_verify_mode(data->verify);
+	bootm_appendroot = data->appendroot;
+	bootm_provide_machine_id = data->provide_machine_id;
+	bootm_provide_hostname = data->provide_hostname;
+	bootm_verbosity = data->verbose;
+	bootm_dryrun = data->dryrun;
+}
+
 static enum bootm_verify bootm_verify_mode = BOOTM_VERIFY_HASH;
 
 enum bootm_verify bootm_get_verify_mode(void)

common/boot.c
@@ -73,12 +73,14 @@ static int bootscript_boot(struct bootentry *entry, int verbose, int dryrun)
 	struct bootentry_script *bs = container_of(entry, struct bootentry_script, entry);
 	int ret;
 
-	struct bootm_data data = {};
+	struct bootm_data backup = {}, data = {};
 
 	if (dryrun == 1) {
 		printf("Would run %s\n", bs->scriptpath);
 		return 0;
 	}
+
+	bootm_data_init_defaults(&backup);
 
 	globalvar_add_simple("linux.bootargs.dyn.ip", NULL);
 	globalvar_add_simple("linux.bootargs.dyn.root", NULL);
@@ -86,7 +88,7 @@ static int bootscript_boot(struct bootentry *entry, int verbose, int dryrun)
 	ret = run_command(bs->scriptpath);
 	if (ret) {
 		pr_err("Running script '%s' failed: %s\n", bs->scriptpath, strerror(-ret));
-		return ret;
+		goto out;
 	}
 
 	bootm_data_init_defaults(&data);
@@ -96,7 +98,10 @@ static int bootscript_boot(struct bootentry *entry, int verbose, int dryrun)
 	if (dryrun >= 2)
 		data.dryrun = dryrun - 1;
 
-	return bootm_boot(&data);
+	ret = bootm_boot(&data);
+out:
+	bootm_data_restore_defaults(&backup);
+	return ret;
 }
 
 static unsigned int boot_watchdog_timeout;

include/bootm.h
@@ -138,6 +138,7 @@ static inline int bootm_verbose(struct image_data *data)
 #endif
 
 void bootm_data_init_defaults(struct bootm_data *data);
+void bootm_data_restore_defaults(const struct bootm_data *data);
 
 int bootm_load_os(struct image_data *data, unsigned long load_address);
 

commit a353f75968e07fcdf43c862e4e00856629c67c32

bootm: ignore global.bootm.initrd if unavailable
The bootm.initrd* variables are only available if CONFIG_BOOTM_INITRD is
enabled, but we were saving and attempting to restore them
unconditionally.

Fix that to avoid confusing warnings on systems without initrd support
compiled in.

Fixes: 926985a74ee0 ("bootm: don't clobber global.bootm. variables after script boot fails")
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250806135642.2864366-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/bootm.c
@@ -61,8 +61,10 @@ void bootm_data_init_defaults(struct bootm_data *data)
 	data->tee_file = getenv_nonempty("global.bootm.tee");
 	data->os_file = getenv_nonempty("global.bootm.image");
 	getenv_ul("global.bootm.image.loadaddr", &data->os_address);
-	getenv_ul("global.bootm.initrd.loadaddr", &data->initrd_address);
-	data->initrd_file = getenv_nonempty("global.bootm.initrd");
+	if (IS_ENABLED(CONFIG_BOOTM_INITRD)) {
+		getenv_ul("global.bootm.initrd.loadaddr", &data->initrd_address);
+		data->initrd_file = getenv_nonempty("global.bootm.initrd");
+	}
 	data->root_dev = getenv_nonempty("global.bootm.root_dev");
 	data->verify = bootm_get_verify_mode();
 	data->appendroot = bootm_appendroot;
@@ -78,8 +80,10 @@ void bootm_data_restore_defaults(const struct bootm_data *data)
 	globalvar_set("bootm.tee", data->tee_file);
 	globalvar_set("bootm.image", data->os_file);
 	pr_setenv("global.bootm.image.loadaddr", "0x%lx", data->os_address);
-	pr_setenv("global.bootm.initrd.loadaddr", "0x%lx", data->initrd_address);
-	globalvar_set("bootm.initrd", data->initrd_file);
+	if (IS_ENABLED(CONFIG_BOOTM_INITRD)) {
+		pr_setenv("global.bootm.initrd.loadaddr", "0x%lx", data->initrd_address);
+		globalvar_set("bootm.initrd", data->initrd_file);
+	}
 	globalvar_set("bootm.root_dev", data->root_dev);
 	bootm_set_verify_mode(data->verify);
 	bootm_appendroot = data->appendroot;
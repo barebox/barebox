barebox/barebox

commit 4a5a9b58b56825f69acc7c50a6b680b2f59f3f2c

drive: bus: make use of new bus_find_device helper
Instead of opencoding the iteration, let's make use of the new
helpers to lookup devices on a given bus.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250606055748.1990383-6-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/efi/efi-device.c
@@ -30,17 +30,20 @@ static int efi_locate_handle(enum efi_locate_search_type search_type,
 				   buffer);
 }
 
+static int efi_device_match_handle(struct device *dev, const void *handle)
+{
+	struct efi_device *efidev = container_of(dev, struct efi_device, dev);
+	return efidev->handle == handle;
+}
+
 static struct efi_device *efi_find_device(efi_handle_t handle)
 {
 	struct device *dev;
-	struct efi_device *efidev;
 
-	bus_for_each_device(&efi_bus, dev) {
-		efidev = container_of(dev, struct efi_device, dev);
-
-		if (efidev->handle == handle)
-			return efidev;
-	}
+	dev = bus_find_device(&efi_bus, NULL, efi_device_match_handle,
+			      efi_device_match_handle);
+	if (dev)
+		return container_of(dev, struct efi_device, dev);
 
 	return NULL;
 }

drivers/net/phy/phy.c
@@ -374,10 +374,9 @@ static struct phy_device *of_mdio_find_phy(struct eth_device *edev)
 		}
 	}
 
-	bus_for_each_device(&mdio_bus_type, dev) {
-		if (dev->of_node == phy_node)
-			return container_of(dev, struct phy_device, dev);
-	}
+	dev = bus_find_device_by_of_node(&mdio_bus_type, phy_node);
+	if (dev)
+		return container_of(dev, struct phy_device, dev);
 
 	return NULL;
 }

commit ec9e3a0a0356f10c271dde8e233ab06f63729018

efi: fix probing of EFI devices with parents
The refactor in the referenced commit sends in the match function as
the context data - instead of the handle we are looking for.

This meant that efi_find_device() always returned NULL, leading to all
EFI devices with parent handles not being probed.

Fix this by sending in the handle instead.

Fixes: 4a5a9b58b568 ("drive: bus: make use of new bus_find_device helper")
Signed-off-by: Tobias Waldekranz <tobias@waldekranz.com>
Tested-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250813214103.254178-1-tobias@waldekranz.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/efi/efi-device.c
@@ -40,8 +40,7 @@ static struct efi_device *efi_find_device(efi_handle_t handle)
 {
 	struct device *dev;
 
-	dev = bus_find_device(&efi_bus, NULL, efi_device_match_handle,
-			      efi_device_match_handle);
+	dev = bus_find_device(&efi_bus, NULL, handle, efi_device_match_handle);
 	if (dev)
 		return container_of(dev, struct efi_device, dev);
 
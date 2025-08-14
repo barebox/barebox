barebox/barebox

commit 4a5a9b58b56825f69acc7c50a6b680b2f59f3f2c

drive: bus: make use of new bus_find_device helper
Instead of opencoding the iteration, let's make use of the new
helpers to lookup devices on a given bus.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250606055748.1990383-6-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/efi/efi-device.c
@@ -30,17 +30,20 @@ static int efi_locate_handle(enum efi_locate_search_type search_type,
 				   buffer);
 }
 
+static int efi_device_match_handle(struct device *dev, const void *handle)
+{
+	struct efi_device *efidev = container_of(dev, struct efi_device, dev);
+	return efidev->handle == handle;
+}
+
 static struct efi_device *efi_find_device(efi_handle_t handle)
 {
 	struct device *dev;
-	struct efi_device *efidev;
 
-	bus_for_each_device(&efi_bus, dev) {
-		efidev = container_of(dev, struct efi_device, dev);
-
-		if (efidev->handle == handle)
-			return efidev;
-	}
+	dev = bus_find_device(&efi_bus, NULL, efi_device_match_handle,
+			      efi_device_match_handle);
+	if (dev)
+		return container_of(dev, struct efi_device, dev);
 
 	return NULL;
 }

drivers/net/phy/phy.c
@@ -374,10 +374,9 @@ static struct phy_device *of_mdio_find_phy(struct eth_device *edev)
 		}
 	}
 
-	bus_for_each_device(&mdio_bus_type, dev) {
-		if (dev->of_node == phy_node)
-			return container_of(dev, struct phy_device, dev);
-	}
+	dev = bus_find_device_by_of_node(&mdio_bus_type, phy_node);
+	if (dev)
+		return container_of(dev, struct phy_device, dev);
 
 	return NULL;
 }

commit 6e3b27f11772b42da310050d459fd104748867bc

test: py: add regression test for missing x86 EFI child devices
A bug introduced by commit 4a5a9b58b568 ("drive: bus: make use of new
bus_find_device helper") led to child devices of EFI handles not being
probed.

Let's assert existence of some devices to ensure such issues don't slip
through again. As handles don't have stable names, we don't hardcode
any, but instead look for their virtual child devices that we expect.

Cc: Tobias Waldekranz <tobias@waldekranz.com>
Link: https://lore.barebox.org/barebox/20250813214103.254178-1-tobias@waldekranz.com/T/#u
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250814071533.3917128-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

test/py/test_devices.py
@@ -1,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+import pytest
+
+
+def test_devices_present(env, barebox):
+    devices = env.config.data["targets"]["main"].get("devices")
+    if not devices:
+        pytest.skip("No devices listed in YAML to assert existence of")
+
+    for devname in devices.keys():
+        assert devices[devname] in barebox.run_check(f"devinfo {devname}")

test/x86/pc@efi_defconfig.yaml
@@ -14,6 +14,14 @@ targets:
       BareboxTestStrategy: {}
     features:
       - pci
+    devices:
+      fb0: 'Type: primary'
+      fbconsole0: 'Parent: fb0'
+      wdog0: 'Parent: efi-wdt'
+      efivarfs0: 'Driver: efivarfs'
+      'pci-8086:1237.0': 'Bus: pci'
+      efifs0: 'Driver: efifs'
+      acpi-APIC0: 'Signature: APIC (Multiple APIC Description Table)'
     runner:
       tuxmake_arch: x86_64
       kconfig_add:
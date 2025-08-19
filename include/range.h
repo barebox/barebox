barebox/barebox

commit 768fdb36f30e8c4fc1d91d5687464623ac1d79cf

partition: define new region_overlap_end_exclusive helper
Most ranges in barebox identified by start and end offsets are exclusive
at the end, for example, the linker symbols for the sections.

To make handling them a bit easier while avoiding off-by-one, let's add
a region_overlap_end_exclusive helper as well.

Unlike inclusive ranges, exclusive ranges can be empty, so add a check
for invalid ranges.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250805174541.2606267-3-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/range.h
@@ -23,6 +23,26 @@ static inline bool region_overlap_end_inclusive(u64 starta, u64 enda,
 }
 
 /**
+ * region_overlap_end_exclusive - check whether a pair of [start, end) ranges overlap
+ *
+ * @starta: start of the first range
+ * @enda:   end of the first range (exclusive)
+ * @startb: start of the second range
+ * @endb:   end of the second range (exclusive)
+ */
+static inline bool region_overlap_end_exclusive(u64 starta, u64 enda,
+						u64 startb, u64 endb)
+{
+	/* Empty ranges don't overlap */
+	if (starta >= enda || startb >= endb)
+		return false;
+
+	return region_overlap_end_inclusive(starta, enda - 1,
+					    startb, startb - 1);
+}
+
+
+/**
  * region_overlap_end - check whether a pair of [start, end] ranges overlap
  *
  * @starta: start of the first range
@@ -36,8 +56,8 @@ static inline bool region_overlap_size(u64 starta, u64 lena,
 	if (!lena || !lenb)
 		return false;
 
-	return region_overlap_end_inclusive(starta, starta + lena - 1,
-					    startb, startb + lenb - 1);
+	return region_overlap_end_exclusive(starta, starta + lena,
+					    startb, startb + lenb);
 }
 
 /**

commit f74cd4ff8a3167adf57b28eb1e9179068c2a5e50

partition: Fix wrong check
In region_overlap_end_exclusive() we pass startb - 1 as endb argument to
region_overlap_end_inclusive() which is obviously wrong. Fix this.

Fixes: 768fdb36f3 partition: define new region_overlap_end_exclusive helper
Link: https://lore.barebox.org/20250818083902.1536392-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/range.h
@@ -38,7 +38,7 @@ static inline bool region_overlap_end_exclusive(u64 starta, u64 enda,
 		return false;
 
 	return region_overlap_end_inclusive(starta, enda - 1,
-					    startb, startb - 1);
+					    startb, endb - 1);
 }
 
 
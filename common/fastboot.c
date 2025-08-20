barebox/barebox

commit dd377c937f8bd14842e8e1c119bd3323f85570f8

common: fastboot: send FAIL if variable does not exist
According to doc [1], new implementations should send FAIL if the
variable specified in the getvar command does not exist.

[1]: https://android.googlesource.com/platform/system/core/+show/refs/heads/main/fastboot/README.md#121

Signed-off-by: Andrei Lalaev <andrey.lalaev@gmail.com>
Link: https://lore.barebox.org/20250611175645.144025-1-andrey.lalaev@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/fastboot.c
@@ -348,8 +348,10 @@ static void cb_getvar(struct fastboot *fb, const char *cmd)
 	if (fastboot_tx_print_var(fb, &fb->variables, cmd))
 		goto out;
 
-	if (!all && !partition)
-		goto skip_partitions;
+	if (!all && !partition) {
+		fastboot_tx_print(fb, FASTBOOT_MSG_FAIL, "no such variable: %s", cmd);
+		goto out;
+	}
 
 	file_list_for_each_entry(fb->files, fentry) {
 		int ret;
@@ -371,8 +373,11 @@ static void cb_getvar(struct fastboot *fb, const char *cmd)
 	if (fastboot_tx_print_var(fb, &partition_list, cmd))
 		goto out;
 
-skip_partitions:
-	fastboot_tx_print(fb, FASTBOOT_MSG_OKAY, "");
+	if (all)
+		fastboot_tx_print(fb, FASTBOOT_MSG_OKAY, "");
+	else
+		fastboot_tx_print(fb, FASTBOOT_MSG_FAIL, "no such variable: %s", cmd);
+
 out:
 	fastboot_free_variables(&partition_list);
 }

commit 525ccfb5ac381c06898e6403e12ec8c34c42c0f8

fastboot: implement dummy has-slot:partition variables
Now that we enforce variables to be available, flashing can result in
following error message:

  ERROR: fastboot: no such variable: has-slot:rootfs-eMMC

Report the default no value to suppress this.

Fixes: dd377c937f8b ("common: fastboot: send FAIL if variable does not exist")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250820101702.1946026-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/fastboot.c
@@ -180,6 +180,8 @@ out:
 	fb_setvar(var, "%s", type);
 	var = fb_addvar(fb, list, "is-logical:%s", fentry->name);
 	fb_setvar(var, "%s", "no");
+	var = fb_addvar(fb, list, "has-slot:%s", fentry->name);
+	fb_setvar(var, "%s", "no");
 
 	return ret;
 }
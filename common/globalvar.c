barebox/barebox

commit feef2e053ea463cb36629f612979b43738bd513c

param: operate on bobjects instead of full devices
Previous commit associated the parameters with the bobject inside the
device. Double down and provide bobject API for creation and use of
parameters and use them to implement the device parameter API.

As each device starts with a bobject, let's make use of GCC's
transparent union feature (typedeffed as bobject_t) to have the compiler
automatically convert pointers.
This way the dev_ param functions become simple aliases to the bobject_
API, but the main upside is that we do not need to adjust callbacks
across the tree as they will just continue to work without type errors.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20250811122824.1667791-8-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/param.h
@@ -5,6 +5,7 @@
 #include <linux/err.h>
 #include <linux/types.h>
 #include <linux/list.h>
+#include <bobject.h>
 
 #define PARAM_FLAG_RO	(1 << 0)
 #define PARAM_GLOBALVAR_UNQUALIFIED	(1 << 1)
@@ -28,13 +29,13 @@ enum param_type {
 };
 
 struct param_d {
-	const char* (*get)(struct device *, struct param_d *param);
-	int (*set)(struct device *, struct param_d *param, const char *val);
+	const char* (*get)(bobject_t, struct param_d *param);
+	int (*set)(bobject_t, struct param_d *param, const char *val);
 	void (*info)(struct param_d *param);
 	unsigned int flags;
 	const char *name;
 	char *value;
-	struct device *dev;
+	struct bobject *bobj;
 	void *driver_priv;
 	struct list_head list;
 	enum param_type type;
@@ -44,98 +45,98 @@ enum param_tristate { PARAM_TRISTATE_UNKNOWN, PARAM_TRISTATE_TRUE, PARAM_TRISTAT
 
 #ifdef CONFIG_PARAMETER
 const char *get_param_type(struct param_d *param);
-const char *dev_get_param(struct device *dev, const char *name);
-int dev_set_param(struct device *dev, const char *name, const char *val);
-struct param_d *get_param_by_name(struct device *dev, const char *name);
+const char *bobject_get_param(bobject_t bobj, const char *name);
+int bobject_set_param(bobject_t bobj, const char *name, const char *val);
+struct param_d *get_param_by_name(bobject_t bobj, const char *name);
 
-struct param_d *dev_add_param(struct device *dev, const char *name,
-			      int (*set)(struct device *dev, struct param_d *p, const char *val),
-			      const char *(*get)(struct device *, struct param_d *p),
+struct param_d *bobject_add_param(bobject_t bobj, const char *name,
+			      int (*set)(bobject_t bobj, struct param_d *p, const char *val),
+			      const char *(*get)(bobject_t, struct param_d *p),
 			      unsigned long flags);
 
-struct param_d *dev_add_param_string(struct device *dev, const char *name,
+struct param_d *bobject_add_param_string(bobject_t bobj, const char *name,
 				     int (*set)(struct param_d *p, void *priv),
 				     int (*get)(struct param_d *p, void *priv),
 				     char **value, void *priv);
 
-struct param_d *__dev_add_param_int(struct device *dev, const char *name,
+struct param_d *__bobject_add_param_int(bobject_t bobj, const char *name,
 				    int (*set)(struct param_d *p, void *priv),
 				    int (*get)(struct param_d *p, void *priv),
 				    void *value, enum param_type type,
 				    const char *format, void *priv);
 
-struct param_d *dev_add_param_enum(struct device *dev, const char *name,
+struct param_d *bobject_add_param_enum(bobject_t bobj, const char *name,
 				   int (*set)(struct param_d *p, void *priv),
 				   int (*get)(struct param_d *p, void *priv),
 				   int *value, const char * const *names,
 				   int num_names, void *priv);
 
-struct param_d *dev_add_param_tristate(struct device *dev, const char *name,
+struct param_d *bobject_add_param_tristate(bobject_t bobj, const char *name,
 				       int (*set)(struct param_d *p, void *priv),
 				       int (*get)(struct param_d *p, void *priv),
 				       int *value, void *priv);
 
-struct param_d *dev_add_param_tristate_ro(struct device *dev,
+struct param_d *bobject_add_param_tristate_ro(bobject_t bobj,
 					  const char *name,
 					  int *value);
 
-struct param_d *dev_add_param_bitmask(struct device *dev, const char *name,
+struct param_d *bobject_add_param_bitmask(bobject_t bobj, const char *name,
 				      int (*set)(struct param_d *p, void *priv),
 				      int (*get)(struct param_d *p, void *priv),
 				      unsigned long *value,
 				      const char * const *names, int num_names,
 				      void *priv);
 
-struct param_d *dev_add_param_ip(struct device *dev, const char *name,
+struct param_d *bobject_add_param_ip(bobject_t bobj, const char *name,
 				 int (*set)(struct param_d *p, void *priv),
 				 int (*get)(struct param_d *p, void *priv),
 				 IPaddr_t *ip, void *priv);
 
-struct param_d *dev_add_param_mac(struct device *dev, const char *name,
+struct param_d *bobject_add_param_mac(bobject_t bobj, const char *name,
 				  int (*set)(struct param_d *p, void *priv),
 				  int (*get)(struct param_d *p, void *priv),
 				  u8 *mac, void *priv);
 
-struct param_d *dev_add_param_file_list(struct device *dev, const char *name,
+struct param_d *bobject_add_param_file_list(bobject_t bobj, const char *name,
 					int (*set)(struct param_d *p, void *priv),
 					int (*get)(struct param_d *p, void *priv),
 					struct file_list **file_list,
 					void *priv);
 
-struct param_d *dev_add_param_fixed(struct device *dev, const char *name,
+struct param_d *bobject_add_param_fixed(bobject_t bobj, const char *name,
 				    const char *value);
 
 void param_remove(struct param_d *p);
 
-int dev_param_set_generic(struct device *dev, struct param_d *p,
+int bobject_param_set_generic(bobject_t bobj, struct param_d *p,
 			  const char *val);
 
 #else
-static inline const char *dev_get_param(struct device *dev, const char *name)
+static inline const char *bobject_get_param(bobject_t bobj, const char *name)
 {
 	return NULL;
 }
-static inline int dev_set_param(struct device *dev, const char *name,
+static inline int bobject_set_param(bobject_t bobj, const char *name,
 				const char *val)
 {
 	return 0;
 }
-static inline struct param_d *get_param_by_name(struct device *dev,
+static inline struct param_d *get_param_by_name(bobject_t bobj,
 						const char *name)
 {
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param(struct device *dev,
+static inline struct param_d *bobject_add_param(bobject_t bobj,
 					    const char *name,
-					    int (*set)(struct device *dev, struct param_d *p, const char *val),
-					    const char *(*get)(struct device *, struct param_d *p),
+					    int (*set)(bobject_t bobj, struct param_d *p, const char *val),
+					    const char *(*get)(bobject_t, struct param_d *p),
 					    unsigned long flags)
 {
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_string(struct device *dev,
+static inline struct param_d *bobject_add_param_string(bobject_t bobj,
 						   const char *name,
 						   int (*set)(struct param_d *p, void *priv),
 						   int (*get)(struct param_d *p, void *priv),
@@ -144,7 +145,7 @@ static inline struct param_d *dev_add_param_string(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *__dev_add_param_int(struct device *dev,
+static inline struct param_d *__bobject_add_param_int(bobject_t bobj,
 						  const char *name,
 						  int (*set)(struct param_d *p, void *priv),
 						  int (*get)(struct param_d *p, void *priv),
@@ -156,7 +157,7 @@ static inline struct param_d *__dev_add_param_int(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_enum(struct device *dev,
+static inline struct param_d *bobject_add_param_enum(bobject_t bobj,
 						 const char *name,
 						 int (*set)(struct param_d *p, void *priv),
 						 int (*get)(struct param_d *p, void *priv),
@@ -168,7 +169,7 @@ static inline struct param_d *dev_add_param_enum(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_bitmask(struct device *dev,
+static inline struct param_d *bobject_add_param_bitmask(bobject_t bobj,
 						    const char *name,
 						    int (*set)(struct param_d *p, void *priv),
 						    int (*get)(struct param_d *p, void *priv),
@@ -179,7 +180,7 @@ static inline struct param_d *dev_add_param_bitmask(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_tristate(struct device *dev,
+static inline struct param_d *bobject_add_param_tristate(bobject_t bobj,
 						     const char *name,
 						     int (*set)(struct param_d *p, void *priv),
 						     int (*get)(struct param_d *p, void *priv),
@@ -188,14 +189,14 @@ static inline struct param_d *dev_add_param_tristate(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_tristate_ro(struct device *dev,
+static inline struct param_d *bobject_add_param_tristate_ro(bobject_t bobj,
 							const char *name,
 							int *value)
 {
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_ip(struct device *dev,
+static inline struct param_d *bobject_add_param_ip(bobject_t bobj,
 					       const char *name,
 					       int (*set)(struct param_d *p, void *priv),
 					       int (*get)(struct param_d *p, void *priv),
@@ -204,7 +205,7 @@ static inline struct param_d *dev_add_param_ip(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_mac(struct device *dev,
+static inline struct param_d *bobject_add_param_mac(bobject_t bobj,
 						const char *name,
 						int (*set)(struct param_d *p, void *priv),
 						int (*get)(struct param_d *p, void *priv),
@@ -213,7 +214,7 @@ static inline struct param_d *dev_add_param_mac(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_file_list(struct device *dev,
+static inline struct param_d *bobject_add_param_file_list(bobject_t bobj,
 						      const char *name,
 						      int (*set)(struct param_d *p, void *priv),
 						      int (*get)(struct param_d *p, void *priv),
@@ -223,7 +224,7 @@ static inline struct param_d *dev_add_param_file_list(struct device *dev,
 	return NULL;
 }
 
-static inline struct param_d *dev_add_param_fixed(struct device *dev,
+static inline struct param_d *bobject_add_param_fixed(bobject_t bobj,
 						  const char *name,
 						  const char *value)
 {
@@ -232,7 +233,7 @@ static inline struct param_d *dev_add_param_fixed(struct device *dev,
 
 static inline void param_remove(struct param_d *p) {}
 
-static inline int dev_param_set_generic(struct device *dev, struct param_d *p,
+static inline int bobject_param_set_generic(bobject_t bobj, struct param_d *p,
 					const char *val)
 {
 	return 0;
@@ -242,19 +243,19 @@ static inline int dev_param_set_generic(struct device *dev, struct param_d *p,
 int param_set_readonly(struct param_d *p, void *priv);
 
 /*
- * dev_add_param_int
- * dev_add_param_int32
- * dev_add_param_uint32
- * dev_add_param_int64
- * dev_add_param_uint64
+ * bobject_add_param_int
+ * bobject_add_param_int32
+ * bobject_add_param_uint32
+ * bobject_add_param_int64
+ * bobject_add_param_uint64
  */
 #define DECLARE_PARAM_INT(intname, inttype, paramtype) \
-	static inline struct param_d *dev_add_param_##intname(struct device *dev, const char *name,	\
+	static inline struct param_d *bobject_add_param_##intname(bobject_t bobj, const char *name,	\
 			int (*set)(struct param_d *p, void *priv),					\
 			int (*get)(struct param_d *p, void *priv),					\
 			inttype *value, const char *format, void *priv)					\
 	{												\
-		return __dev_add_param_int(dev, name, set, get, value, paramtype, format, priv);	\
+		return __bobject_add_param_int(bobj, name, set, get, value, paramtype, format, priv);	\
 	}
 
 DECLARE_PARAM_INT(int, int, PARAM_TYPE_INT32)
@@ -264,17 +265,17 @@ DECLARE_PARAM_INT(int64, int64_t, PARAM_TYPE_INT64)
 DECLARE_PARAM_INT(uint64, uint64_t, PARAM_TYPE_UINT64)
 
 /*
- * dev_add_param_int_fixed
- * dev_add_param_int32_fixed
- * dev_add_param_uint32_fixed
- * dev_add_param_int64_fixed
- * dev_add_param_uint64_fixed
+ * bobject_add_param_int_fixed
+ * bobject_add_param_int32_fixed
+ * bobject_add_param_uint32_fixed
+ * bobject_add_param_int64_fixed
+ * bobject_add_param_uint64_fixed
  */
 #define DECLARE_PARAM_INT_FIXED(intname, inttype, paramtype) \
-	static inline struct param_d *dev_add_param_##intname##_fixed(struct device *dev, const char *name,	\
+	static inline struct param_d *bobject_add_param_##intname##_fixed(bobject_t bobj, const char *name,	\
 			inttype value, const char *format)							\
 	{													\
-		return __dev_add_param_int(dev, name, ERR_PTR(-EROFS), NULL, &value, paramtype, format, NULL);	\
+		return __bobject_add_param_int(bobj, name, ERR_PTR(-EROFS), NULL, &value, paramtype, format, NULL);	\
 	}
 
 DECLARE_PARAM_INT_FIXED(int, int, PARAM_TYPE_INT32)
@@ -284,17 +285,17 @@ DECLARE_PARAM_INT_FIXED(int64, int64_t, PARAM_TYPE_INT64)
 DECLARE_PARAM_INT_FIXED(uint64, uint64_t, PARAM_TYPE_UINT64)
 
 /*
- * dev_add_param_int_ro
- * dev_add_param_int32_ro
- * dev_add_param_uint32_ro
- * dev_add_param_int64_ro
- * dev_add_param_uint64_ro
+ * bobject_add_param_int_ro
+ * bobject_add_param_int32_ro
+ * bobject_add_param_uint32_ro
+ * bobject_add_param_int64_ro
+ * bobject_add_param_uint64_ro
  */
 #define DECLARE_PARAM_INT_RO(intname, inttype, paramtype) \
-	static inline struct param_d *dev_add_param_##intname##_ro(struct device *dev, const char *name,		\
+	static inline struct param_d *bobject_add_param_##intname##_ro(bobject_t bobj, const char *name,		\
 			inttype *value, const char *format)								\
 	{														\
-		return __dev_add_param_int(dev, name, param_set_readonly, NULL, value, paramtype, format, NULL);	\
+		return __bobject_add_param_int(bobj, name, param_set_readonly, NULL, value, paramtype, format, NULL);	\
 	}
 
 DECLARE_PARAM_INT_RO(int, int, PARAM_TYPE_INT32)
@@ -303,64 +304,106 @@ DECLARE_PARAM_INT_RO(uint32, uint32_t, PARAM_TYPE_UINT32)
 DECLARE_PARAM_INT_RO(int64, int64_t, PARAM_TYPE_INT64)
 DECLARE_PARAM_INT_RO(uint64, uint64_t, PARAM_TYPE_UINT64)
 
-static inline struct param_d *dev_add_param_bool(struct device *dev,
+static inline struct param_d *bobject_add_param_bool(bobject_t bobj,
 						 const char *name,
 						 int (*set)(struct param_d *p, void *priv),
 						 int (*get)(struct param_d *p, void *priv),
 						 uint32_t *value, void *priv)
 {
-	return __dev_add_param_int(dev, name, set, get, value, PARAM_TYPE_BOOL, "%u", priv);
+	return __bobject_add_param_int(bobj, name, set, get, value, PARAM_TYPE_BOOL, "%u", priv);
 }
 
-static inline struct param_d *dev_add_param_bool_fixed(struct device *dev,
+static inline struct param_d *bobject_add_param_bool_fixed(bobject_t bobj,
 						       const char *name,
 						       uint32_t value)
 {
-	return __dev_add_param_int(dev, name, ERR_PTR(-EROFS), NULL, &value, PARAM_TYPE_BOOL,
+	return __bobject_add_param_int(bobj, name, ERR_PTR(-EROFS), NULL, &value, PARAM_TYPE_BOOL,
 				   "%u", NULL);
 }
 
-static inline struct param_d *dev_add_param_bool_ro(struct device *dev,
+static inline struct param_d *bobject_add_param_bool_ro(bobject_t bobj,
 						    const char *name,
 						    uint32_t *value)
 {
-	return __dev_add_param_int(dev, name, param_set_readonly, NULL, value, PARAM_TYPE_BOOL,
+	return __bobject_add_param_int(bobj, name, param_set_readonly, NULL, value, PARAM_TYPE_BOOL,
 				   "%u", NULL);
 }
 
-static inline struct param_d *dev_add_param_string_ro(struct device *dev,
+static inline struct param_d *bobject_add_param_string_ro(bobject_t bobj,
 						      const char *name,
 						      char **value)
 {
-	return dev_add_param_string(dev, name, param_set_readonly, NULL, value, NULL);
+	return bobject_add_param_string(bobj, name, param_set_readonly, NULL, value, NULL);
 }
 
-static inline struct param_d *dev_add_param_string_fixed(struct device *dev,
+static inline struct param_d *bobject_add_param_string_fixed(bobject_t bobj,
 							 const char *name,
 							 const char *value)
 {
-	return dev_add_param_fixed(dev, name, value);
+	return bobject_add_param_fixed(bobj, name, value);
 }
 
-static inline struct param_d *dev_add_param_enum_ro(struct device *dev,
+static inline struct param_d *bobject_add_param_enum_ro(bobject_t bobj,
 						    const char *name,
 						    int *value,
 						    const char * const *names,
 						    int num_names)
 {
-	return dev_add_param_enum(dev, name, param_set_readonly, NULL,
+	return bobject_add_param_enum(bobj, name, param_set_readonly, NULL,
 				  value, names, num_names, NULL);
 }
 
-static inline struct param_d *dev_add_param_bitmask_ro(struct device *dev,
+static inline struct param_d *bobject_add_param_bitmask_ro(bobject_t bobj,
 						       const char *name,
 						       unsigned long *value,
 						       const char * const *names,
 						       int num_names)
 {
-	return dev_add_param_bitmask(dev, name, param_set_readonly, NULL,
+	return bobject_add_param_bitmask(bobj, name, param_set_readonly, NULL,
 				     value, names, num_names, NULL);
 }
+
+#define dev_get_param			bobject_get_param
+#define dev_set_param			bobject_set_param
+#define dev_add_param			bobject_add_param
+#define dev_add_param_string		bobject_add_param_string
+#define __dev_add_param_int		__bobject_add_param_int
+#define dev_add_param_enum		bobject_add_param_enum
+#define dev_add_param_tristate		bobject_add_param_tristate
+#define dev_add_param_tristate_ro	bobject_add_param_tristate_ro
+#define dev_add_param_bitmask		bobject_add_param_bitmask
+#define dev_add_param_ip		bobject_add_param_ip
+#define dev_add_param_mac		bobject_add_param_mac
+#define dev_add_param_file_list		bobject_add_param_file_list
+#define dev_add_param_fixed		bobject_add_param_fixed
+#define dev_param_set_generic		bobject_param_set_generic
+
+#define dev_add_param_int		bobject_add_param_int
+#define dev_add_param_int32		bobject_add_param_int32
+#define dev_add_param_uint32		bobject_add_param_uint32
+#define dev_add_param_int64		bobject_add_param_int64
+#define dev_add_param_uint64		bobject_add_param_uint64
+
+#define dev_add_param_int_fixed		bobject_add_param_int_fixed
+#define dev_add_param_int32_fixed	bobject_add_param_int32_fixed
+#define dev_add_param_uint32_fixed	bobject_add_param_uint32_fixed
+#define dev_add_param_int64_fixed	bobject_add_param_int64_fixed
+#define dev_add_param_uint64_fixed	bobject_add_param_uint64_fixed
+
+
+#define dev_add_param_int_ro		bobject_add_param_int_ro
+#define dev_add_param_int32_ro		bobject_add_param_int32_ro
+#define dev_add_param_uint32_ro		bobject_add_param_uint32_ro
+#define dev_add_param_int64_ro		bobject_add_param_int64_ro
+#define dev_add_param_uint64_ro		bobject_add_param_uint64_ro
+
+#define dev_add_param_bool		bobject_add_param_bool
+#define dev_add_param_bool_fixed	bobject_add_param_bool_fixed
+#define dev_add_param_bool_ro		bobject_add_param_bool_ro
+#define dev_add_param_string_ro		bobject_add_param_string_ro
+#define dev_add_param_string_fixed	bobject_add_param_string_fixed
+#define dev_add_param_enum_ro		bobject_add_param_enum_ro
+#define dev_add_param_bitmask_ro	bobject_add_param_bitmask_ro
 
 /*
  * unimplemented:

commit fc5d59977d5405ca949cb908711900f97571450c

param: don't use bobject_t in function pointer signatures
UndefinedBehaviorSanitizier for clang-21 complains about calling
functions through function pointers with bobject_t in their signatures
when the original arguments use struct device * or struct bobject *.

Let's migrate all function pointers to use struct bobject * instead to
fix this.

Fixes: feef2e053ea4 ("param: operate on bobjects instead of full devices")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250901152940.2472959-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/globalvar.c
@@ -177,8 +177,7 @@ static int nvvar_device_dispatch(const char *name, struct device **dev,
 	return 1;
 }
 
-static int nv_set(struct device *dev, struct param_d *p, const char *name,
-		  const char *val)
+static int nv_set(struct param_d *p, const char *name, const char *val)
 {
 	int ret;
 
@@ -196,17 +195,17 @@ static int nv_set(struct device *dev, struct param_d *p, const char *name,
 	return 0;
 }
 
-static const char *nv_param_get(struct device *dev, struct param_d *p)
+static const char *nv_param_get(struct bobject *bobj, struct param_d *p)
 {
 	return p->value ? p->value : "";
 }
 
-static int nv_param_set(struct device *dev, struct param_d *p,
+static int nv_param_set(struct bobject *bobj, struct param_d *p,
 			const char *val)
 {
 	int ret;
 
-	ret = nv_set(dev, p, p->name, val);
+	ret = nv_set(p, p->name, val);
 	if (ret)
 		return ret;
 
@@ -237,7 +236,7 @@ static int __nvvar_add(const char *name, const char *value)
 		return ret;
 
 	if (value)
-		return nv_set(&nv_device, p, name, value);
+		return nv_set(p, name, value);
 
 	ret = nvvar_device_dispatch(name, &dev, &pname);
 	if (ret > 0)
@@ -455,7 +454,7 @@ void globalvar_set(const char *name, const char *val)
 	dev_set_param(&global_device, name, val);
 }
 
-static int globalvar_simple_set(struct device *dev, struct param_d *p,
+static int globalvar_simple_set(struct bobject *bobj, struct param_d *p,
 				const char *val)
 {
 	struct device *rdev;
@@ -474,7 +473,7 @@ static int globalvar_simple_set(struct device *dev, struct param_d *p,
 	}
 
 	/* Pass to the generic function we have overwritten */
-	return dev_param_set_generic(dev, p, val);
+	return dev_param_set_generic(bobj, p, val);
 }
 
 /*

include/param.h
@@ -30,8 +30,8 @@ enum param_type {
 };
 
 struct param_d {
-	const char* (*get)(bobject_t, struct param_d *param);
-	int (*set)(bobject_t, struct param_d *param, const char *val);
+	const char* (*get)(struct bobject *, struct param_d *param);
+	int (*set)(struct bobject *, struct param_d *param, const char *val);
 	void (*info)(struct param_d *param);
 	unsigned int flags;
 	const char *name;

lib/parameter.c
@@ -155,8 +155,8 @@ static int compare(struct list_head *a, struct list_head *b)
 
 static int __bobject_add_param(struct param_d *param, struct bobject *bobj,
 			   const char *name,
-			   int (*set)(bobject_t _bobj, struct param_d *p, const char *val),
-			   const char *(*get)(bobject_t _bobj, struct param_d *p),
+			   int (*set)(struct bobject *, struct param_d *p, const char *val),
+			   const char *(*get)(struct bobject *, struct param_d *p),
 			   unsigned long flags)
 {
 	if (get_param_by_name(bobj, name))
@@ -199,8 +199,8 @@ static int __bobject_add_param(struct param_d *param, struct bobject *bobj,
  * not use static arrays when using the generic functions.
  */
 struct param_d *bobject_add_param(bobject_t _bobj, const char *name,
-			      int (*set)(bobject_t _bobj, struct param_d *p, const char *val),
-			      const char *(*get)(bobject_t _bobj, struct param_d *param),
+			      int (*set)(struct bobject *, struct param_d *p, const char *val),
+			      const char *(*get)(struct bobject *, struct param_d *param),
 			      unsigned long flags)
 {
 	struct bobject *bobj = _bobj.bobj;
d2ca57fa4a86ecd76af7d6ca6bb9e328cb872f5e
Tobias Waldekranz <tobias@waldekranz.com>
2025-09-18 09:43:16 +0200

    ci: pytest: Open up testfs to more consumers than the FIT test
    With upcoming dm-verity tests, we want to store more test artifacts
    than the FIT image in the testfs.
    
    Since the dm tests might run on systems for which no ITS is available
    to build an FIT from, make FIT generation conditional on the ITS being
    available. This allows us to enable the testfs feature on all matrix
    cells that support 9p over virtio.
    
    Signed-off-by: Tobias Waldekranz <tobias@waldekranz.com>
    Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20250918074455.891780-7-tobias@waldekranz.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- scripts/generate_testfs.sh
+++ scripts/generate_testfs.sh
@@ -13,14 +13,21 @@ fi
 rm -rf "${KBUILD_OUTPUT}/testfs/"
 mkdir -p ${KBUILD_OUTPUT}/testfs
 
-cat ${KBUILD_OUTPUT}/images/barebox-dt-2nd.img | \
-  ${KGZIP} -n -f -9 >${KBUILD_OUTPUT}/barebox-dt-2nd.img.gz
+generate_fit()
+{
+    cat ${KBUILD_OUTPUT}/images/barebox-dt-2nd.img | \
+	${KGZIP} -n -f -9 >${KBUILD_OUTPUT}/barebox-dt-2nd.img.gz
 
-cp .github/testfs/${KBUILD_DEFCONFIG}-gzipped.its ${KBUILD_OUTPUT}/
+    cp .github/testfs/${KBUILD_DEFCONFIG}-gzipped.its ${KBUILD_OUTPUT}/
 
-find COPYING LICENSES/ | cpio -o -H newc | ${KGZIP} \
-  > ${KBUILD_OUTPUT}/ramdisk.cpio.gz
+    find COPYING LICENSES/ | cpio -o -H newc | ${KGZIP} \
+						   > ${KBUILD_OUTPUT}/ramdisk.cpio.gz
 
-${MKIMAGE} -G $PWD/test/self/development_rsa2048.pem -r \
-      -f ${KBUILD_OUTPUT}/${KBUILD_DEFCONFIG}-gzipped.its \
-	 ${KBUILD_OUTPUT}/testfs/barebox-gzipped.fit
+    ${MKIMAGE} -G $PWD/test/self/development_rsa2048.pem -r \
+	       -f ${KBUILD_OUTPUT}/${KBUILD_DEFCONFIG}-gzipped.its \
+	       ${KBUILD_OUTPUT}/testfs/barebox-gzipped.fit
+}
+
+if [ -f .github/testfs/${KBUILD_DEFCONFIG}-gzipped.its ]; then
+	generate_fit
+fi

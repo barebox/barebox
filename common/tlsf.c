d63902176f9a60861aff56c429ba9dd962c58f21
Chali Anis <chalianis1@gmail.com>
2025-10-06 20:59:48 -0400

    tlsf: support on-demand requesting memory from external store
    barebox running under an operating system (or nearly one like UEFI) does
    not have control over all the memory and should not hog too much memory
    upfront as that can interfere with other applications, e.g. EFI drivers
    or the EFI firmware itself.
    
    To accommodate this use case, allow TLSF to dynamically request more
    memory from an external store via tlsf_register_store().
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
    Signed-off-by: Chali Anis <chalianis1@gmail.com>
    Link: https://lore.barebox.org/20251007005955.1427990-3-chalianis1@gmail.com
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- common/tlsf.c
+++ common/tlsf.c
@@ -159,10 +159,11 @@ typedef struct control_t
 	/* Empty lists point at this block to indicate they are free. */
 	block_header_t block_null;
 
+	void (*request_store)(tlsf_t, size_t);
+
 	/* Bitmaps for free lists. */
 	unsigned int fl_bitmap;
 	unsigned int sl_bitmap[FL_INDEX_COUNT];
-	u32 : BYTES_TO_BITS(ALIGN_SIZE - sizeof(size_t));
 
 	/* Head of free lists. */
 	block_header_t* blocks[FL_INDEX_COUNT][SL_INDEX_COUNT];
@@ -568,7 +569,7 @@ static block_header_t* block_trim_free_leading(control_t* control, block_header_
 	return remaining_block;
 }
 
-static block_header_t* block_locate_free(control_t* control, size_t size)
+static block_header_t* __block_locate_free(control_t* control, size_t size)
 {
 	int fl = 0, sl = 0;
 	block_header_t* block = 0;
@@ -596,6 +597,23 @@ static block_header_t* block_locate_free(control_t* control, size_t size)
 	}
 
 	return block;
+}
+
+static block_header_t* block_locate_free(control_t* control, size_t size)
+{
+	block_header_t *block;
+
+	block = __block_locate_free(control, size);
+	if (block)
+		return block;
+
+	if (!control->request_store || !size)
+		return NULL;
+
+	control->request_store(tlsf_cast(tlsf_t, control),
+			       size + tlsf_pool_overhead());
+
+	return __block_locate_free(control, size);
 }
 
 static void* block_prepare_used(control_t* control, block_header_t* block,
@@ -636,6 +654,7 @@ static void control_construct(control_t* control)
 	control->block_null.next_free = &control->block_null;
 	control->block_null.prev_free = &control->block_null;
 
+	control->request_store = NULL;
 	control->fl_bitmap = 0;
 	for (i = 0; i < FL_INDEX_COUNT; ++i)
 	{
@@ -932,6 +951,13 @@ tlsf_t tlsf_create_with_pool(void* mem, size_t bytes)
 	tlsf_add_pool(tlsf, (char*)mem + tlsf_size(), bytes - tlsf_size());
 	kasan_poison_shadow(mem, tlsf_size(), KASAN_TAG_INVALID);
 	return tlsf;
+}
+
+void tlsf_register_store(tlsf_t tlsf, void (*cb)(tlsf_t tlsf, size_t bytes))
+{
+	control_t* control = tlsf_cast(control_t*, tlsf);
+
+	control->request_store = cb;
 }
 
 void tlsf_destroy(tlsf_t tlsf)

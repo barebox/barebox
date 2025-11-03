barebox/barebox

commit 7f357a8d26b7ac0dbcc9e2dcb207e06da6e9a509

public-keys: ecdsa: remove list in ecdsa keys
We have a function to iterate over ecdsa keys. This is unused and
shouldn't be necessary as we have a function to iterate over public
keys. Remove the iterator and the list entry which gives us the
possibility to put ecdsa keys into the RO data section.

Link: https://lore.barebox.org/20251020-public-keys-const-v1-1-301de7918b06@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

crypto/ecdsa.c
@@ -104,15 +104,6 @@ int ecdsa_verify(const struct ecdsa_public_key *key, const uint8_t *sig,
 	return _ecdsa_verify(ctx, (void *)mhash, rh, sh);
 }
 
-static LIST_HEAD(ecdsa_keys);
-
-int ecdsa_key_add(struct ecdsa_public_key *key)
-{
-	list_add_tail(&key->list, &ecdsa_keys);
-
-	return 0;
-}
-
 struct ecdsa_public_key *ecdsa_key_dup(const struct ecdsa_public_key *key)
 {
 	struct ecdsa_public_key *new;
@@ -128,13 +119,4 @@ struct ecdsa_public_key *ecdsa_key_dup(const struct ecdsa_public_key *key)
 	new->size_bits = key_size_bits;
 
 	return new;
-}
-
-const struct ecdsa_public_key *ecdsa_key_next(const struct ecdsa_public_key *prev)
-{
-	prev = list_prepare_entry(prev, &ecdsa_keys, list);
-	list_for_each_entry_continue(prev, &ecdsa_keys, list)
-		return prev;
-
-	return NULL;
 }

include/crypto/ecdsa.h
@@ -11,27 +11,15 @@ struct ecdsa_public_key {
 	const uint64_t *x;	/* x coordinate of public key */
 	const uint64_t *y;	/* y coordinate of public key */
 	unsigned int size_bits;	/* key size in bits, derived from curve name */
-	struct list_head list;
 };
-
-const struct ecdsa_public_key *ecdsa_key_next(const struct ecdsa_public_key *prev);
-
-#define for_each_ecdsa_key(key) \
-	for (key = ecdsa_key_next(NULL); key; key = ecdsa_key_next(key))
 
 #ifdef CONFIG_CRYPTO_ECDSA
 int ecdsa_verify(const struct ecdsa_public_key *key, const uint8_t *sig,
 		 const uint32_t sig_len, const uint8_t *hash);
-int ecdsa_key_add(struct ecdsa_public_key *key);
 struct ecdsa_public_key *ecdsa_key_dup(const struct ecdsa_public_key *key);
 #else
 static inline int ecdsa_verify(const struct ecdsa_public_key *key, const uint8_t *sig,
 		 const uint32_t sig_len, const uint8_t *hash)
-{
-	return -ENOSYS;
-}
-
-static inline int ecdsa_key_add(struct ecdsa_public_key *key)
 {
 	return -ENOSYS;
 }

commit 01aa57fe9009c8bf93c8f9436c705c165a260464

public-keys: fix key->size for ECDSA keys
After not invoking ecdsa_key_dup() on keys before first use anymore,
key->size bits remains uninitialized, breaking ecdsa_verify() and
thereby ECDSA at large.

 - Drop the unused ecdsa_key_dup
 - Drop the uninitialized size_bits field in struct ecdsa_public_key
 - Invoke ecdsa_key_size() in ecdsa_verify() instead

Fixes: 7f357a8d26 ("public-keys: ecdsa: remove list in ecdsa keys")
Signed-off-by: Jonas Rebmann <jre@pengutronix.de>
Link: https://lore.barebox.org/20251028-fixup-ecdsa-v1-1-aca221d4672b@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

crypto/ecdsa.c
@@ -78,7 +78,10 @@ int ecdsa_verify(const struct ecdsa_public_key *key, const uint8_t *sig,
 	const void *r, *s;
 	u64 rh[4], sh[4];
 	u64 mhash[ECC_MAX_DIGITS];
-	int key_size_bytes = key->size_bits / 8;
+	int key_size_bits, key_size_bytes;
+
+	key_size_bits = ecdsa_key_size(key->curve_name);
+	key_size_bytes = key_size_bits / 8;
 
 	ctx->curve_id = curve_id;
 	ctx->curve = ecc_get_curve(curve_id);
@@ -102,21 +105,4 @@ int ecdsa_verify(const struct ecdsa_public_key *key, const uint8_t *sig,
 	ecc_swap_digits((u64 *)hash, mhash, ctx->curve->g.ndigits);
 
 	return _ecdsa_verify(ctx, (void *)mhash, rh, sh);
-}
-
-struct ecdsa_public_key *ecdsa_key_dup(const struct ecdsa_public_key *key)
-{
-	struct ecdsa_public_key *new;
-	int key_size_bits;
-
-	key_size_bits = ecdsa_key_size(key->curve_name);
-	if (!key_size_bits)
-		return NULL;
-
-	new = xmemdup(key, sizeof(*key));
-	new->x = xmemdup(key->x, key_size_bits / 8);
-	new->y = xmemdup(key->y, key_size_bits / 8);
-	new->size_bits = key_size_bits;
-
-	return new;
 }

include/crypto/ecdsa.h
@@ -10,7 +10,6 @@ struct ecdsa_public_key {
 	const char *curve_name;	/* Name of curve, e.g. "prime256v1" */
 	const uint64_t *x;	/* x coordinate of public key */
 	const uint64_t *y;	/* y coordinate of public key */
-	unsigned int size_bits;	/* key size in bits, derived from curve name */
 };
 
 #ifdef CONFIG_CRYPTO_ECDSA
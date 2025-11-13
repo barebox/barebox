barebox/barebox

commit 1e91e857bf1b703dab838750612bf481d4683825

bobject: free object name in bobject_del()
We have bobject_free() which removes all parameters and frees the
bobject. This works for bobjects which have been allocated with
bobject_alloc(). struct device has a bobject embedded and is freed
along with the device, so bobject_free() is inappropriate and
bobject_del() is called instead. This however doesn't free the bobjects
name which then leaks when the device is freed.

To fix this move the freeing of the bobject name to bobject_del(). To
make this work with CONFIG_PARAMETER disabled drop the static inline
wrapper. As the bobject name is now freed in bobject_del() we have to
remove the freeing of struct device::name which is the same object.

While at it fix the function name in the bobject_del() function
description.

Link: https://lore.barebox.org/20251107-memleaks-v1-3-c7b74a2ae9db@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

lib/bobject.c
@@ -51,15 +51,13 @@ void bobject_free(struct bobject *bobj)
 	if (!bobj)
 		return;
 
-	free(bobj->name);
 	bobject_del(bobj);
 	free(bobj);
 }
 EXPORT_SYMBOL_GPL(bobject_free);
 
-#ifdef CONFIG_PARAMETER
 /**
- * bobject_remove_parameters - remove all parameters from a bobject and free their
+ * bobject_del - remove all parameters from a bobject and free their
  * memory
  * @param bobject	The barebox object
  */
@@ -69,6 +67,7 @@ void bobject_del(struct bobject *bobj)
 
 	list_for_each_entry_safe(p, n, &bobj->parameters, list)
 		param_remove(p);
+
+	free(bobj->name);
 }
 EXPORT_SYMBOL(bobject_del);
-#endif

drivers/base/driver.c
@@ -344,8 +344,6 @@ EXPORT_SYMBOL(unregister_device);
  */
 void free_device_res(struct device *dev)
 {
-	free(dev->name);
-	dev->name = NULL;
 	free(dev->unique_name);
 	dev->unique_name = NULL;
 	free(dev->deferred_probe_reason);

include/bobject.h
@@ -39,10 +39,6 @@ static inline void bobject_init(struct bobject *bobj)
 struct bobject *bobject_alloc(const char *name);
 void bobject_free(struct bobject *bobj);
 
-#ifdef CONFIG_PARAMETER
 void bobject_del(struct bobject *bobj);
-#else
-static inline void bobject_del(struct bobject *bobj) { }
-#endif
 
 #endif

commit 6c6f7301c4f31d990cf0c4bd788a6978ef0a3492

device: use free_const to free device/bobject name
A device name (and thus a bobject name) may be statically allocated. Use
free_const() to handle this gracefully.

bobject_set_name() called from dev_set_name() also assumes that
dev->name is allocated and frees the existing name. Use free_const()
here as well.

Fixes: 1e91e857bf ("bobject: free object name in bobject_del()")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

lib/bobject.c
@@ -29,7 +29,7 @@ int bobject_set_name(bobject_t bobj, const char *fmt, ...)
 	 * Free old pointer, we do this after vasprintf call in case
 	 * old device name was in one of vargs
 	 */
-	free(oldname);
+	free_const(oldname);
 
 	return WARN_ON(err < 0) ? err : 0;
 }
@@ -68,6 +68,6 @@ void bobject_del(struct bobject *bobj)
 	list_for_each_entry_safe(p, n, &bobj->parameters, list)
 		param_remove(p);
 
-	free(bobj->name);
+	free_const(bobj->name);
 }
 EXPORT_SYMBOL(bobject_del);
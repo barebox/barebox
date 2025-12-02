barebox/barebox

commit 44c6bedbd43926d404214c3504f4e0a557e821ae

pmdomain: allow callback for when devices are attached
This will be needed for the incoming Rockchip pmdomain drivers.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20251027192938.2586566-4-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/base/power.c
@@ -331,14 +331,22 @@ int pm_runtime_resume_and_get_genpd(struct device *dev)
 	return genpd_power_on(genpd);
 }
 
-static void genpd_add_device(struct generic_pm_domain *genpd, struct device *dev)
+static int genpd_add_device(struct generic_pm_domain *genpd, struct device *dev)
 {
 	dev->pm_domain = genpd;
+
+	if (genpd->attach_dev)
+		return genpd->attach_dev(genpd, dev);
+
+	return 0;
 }
 
 static void genpd_remove_device(struct generic_pm_domain *genpd,
 			       struct device *dev)
 {
+	if (genpd->detach_dev)
+		genpd->detach_dev(genpd, dev);
+
 	dev->pm_domain = NULL;
 }
 

include/pm_domain.h
@@ -20,6 +20,9 @@ struct generic_pm_domain {
 
 	int (*power_off)(struct generic_pm_domain *domain);
 	int (*power_on)(struct generic_pm_domain *domain);
+
+	int (*attach_dev)(struct generic_pm_domain *genpd, struct device *dev);
+	void (*detach_dev)(struct generic_pm_domain *genpd, struct device *dev);
 };
 
 typedef struct generic_pm_domain *(*genpd_xlate_t)(struct of_phandle_args *args,

commit 87e50b97ca3ca13b8248fa48bdd20d2196f14ba3

pmdomain: fix NULL pointer derference with dummy power domains
With the addition of commit 4aca68990d04 ("drivers: power: ignore power
domains with barebox,allow-dummy"), it's possible for genpd to be NULL
when there is no driver and the device tree node has barebox,allow-dummy
in it.

Allow for this possibility in genpd_add_device and genpd_remove_device
as well.

Fixes: 44c6bedbd439 ("pmdomain: allow callback for when devices are attached")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20251201133832.3303902-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/base/power.c
@@ -335,7 +335,7 @@ static int genpd_add_device(struct generic_pm_domain *genpd, struct device *dev)
 {
 	dev->pm_domain = genpd;
 
-	if (genpd->attach_dev)
+	if (genpd && genpd->attach_dev)
 		return genpd->attach_dev(genpd, dev);
 
 	return 0;
@@ -344,7 +344,7 @@ static int genpd_add_device(struct generic_pm_domain *genpd, struct device *dev)
 static void genpd_remove_device(struct generic_pm_domain *genpd,
 			       struct device *dev)
 {
-	if (genpd->detach_dev)
+	if (genpd && genpd->detach_dev)
 		genpd->detach_dev(genpd, dev);
 
 	dev->pm_domain = NULL;
45a84ffb43047c10a5597574fc14a999b4f60e6f
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-12-11 21:29:54 +0100

    efi: mode: add efi_get_runtime_services helper
    Similarly to the existing efi_get_boot_services() helper that replaces
    hardcoded BS use to allow code to be usable from both payload and
    upcoming loader support, efi_get_runtime_services() can replace RT use.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20251211203056.2014710-4-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/efi/efi-mode.h
+++ include/efi/efi-mode.h
@@ -7,7 +7,9 @@
 #include <linux/types.h>
 
 struct efi_boot_services;
+struct efi_runtime_services;
 extern struct efi_boot_services *BS;
+extern struct efi_runtime_services *RT;
 
 static inline bool efi_is_payload(void)
 {
@@ -23,6 +25,14 @@ static inline struct efi_boot_services *efi_get_boot_services(void)
 {
 	if (efi_is_payload())
 		return BS;
+
+	return NULL;
+}
+
+static inline struct efi_runtime_services *efi_get_runtime_services(void)
+{
+	if (efi_is_payload())
+		return RT;
 
 	return NULL;
 }

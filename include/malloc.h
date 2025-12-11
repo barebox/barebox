2a0cb4bac83247d5a0b3285c39ae630f8cd287ae
Ahmad Fatoum <a.fatoum@pengutronix.de>
2025-12-11 21:24:28 +0100

    malloc: define malloc_add_pool outside of TLSF
    This function is currently being used by the EFI payload init ode.
    Define a stub to allow allyesconfig to build it.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Link: https://lore.barebox.org/20251211202435.1626910-2-a.fatoum@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/malloc.h
+++ include/malloc.h
@@ -26,6 +26,9 @@ void *malloc_add_pool(void *mem, size_t bytes);
 void malloc_register_store(void (*cb)(size_t bytes));
 bool malloc_store_is_registered(void);
 #else
+#include <linux/bug.h>
+static inline void *malloc_add_pool(void *mem, size_t bytes) { BUG(); }
+static inline void malloc_register_store(void (*cb)(size_t bytes)) { BUG(); }
 static inline bool malloc_store_is_registered(void) { return false; }
 #endif
 

barebox/barebox

commit 20d87123a63807770581c8e9cdf4ce9fd9223c6c

treewide: replace dev_get_drvdata with device_get_match_data
Both Linux and barebox define a dev_get_drvdata function, but
annoyingly, the barebox version accesses the private data set in the
probe function, while the Linux version retrieves the match data set by
the driver core instead.

In preparation for changing the function to have the Linux semantics,
start by replacing most instances in barebox with device_get_match_data.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20250506063446.3133582-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/i2c/busses/i2c-omap.c
@@ -144,7 +144,7 @@
 struct omap_i2c_struct {
 	void 			*base;
 	u8			reg_shift;
-	struct omap_i2c_driver_data	*data;
+	const struct omap_i2c_driver_data	*data;
 	struct resource		*ioarea;
 	u32			speed;		/* Speed of bus in Khz */
 	u16			scheme;
@@ -345,7 +345,7 @@ static int omap_i2c_init(struct omap_i2c_struct *i2c_omap)
 	u16 fsscll = 0, fssclh = 0, hsscll = 0, hssclh = 0;
 	uint64_t start;
 	unsigned long internal_clk = 0;
-	struct omap_i2c_driver_data *i2c_data = i2c_omap->data;
+	const struct omap_i2c_driver_data *i2c_data = i2c_omap->data;
 
 	if (i2c_omap->rev >= OMAP_I2C_OMAP1_REV_2) {
 		/* Disable I2C controller before soft reset */
@@ -1063,7 +1063,7 @@ i2c_omap_probe(struct device *pdev)
 {
 	struct resource *iores;
 	struct omap_i2c_struct	*i2c_omap;
-	struct omap_i2c_driver_data *i2c_data;
+	const struct omap_i2c_driver_data *i2c_data;
 	int r;
 	u32 speed = 0;
 	u32 rev;
@@ -1075,9 +1075,9 @@ i2c_omap_probe(struct device *pdev)
 		goto err_free_mem;
 	}
 
-	r = dev_get_drvdata(pdev, (const void **)&i2c_data);
-	if (r)
-		return r;
+	i2c_data = device_get_match_data(pdev);
+	if (!i2c_data)
+		return -ENODEV;
 
 	if (of_machine_is_compatible("ti,am33xx"))
 		i2c_data = &am33xx_data;

arch/arm/boards/stm32mp15xx-dkx/board.c
@@ -6,12 +6,13 @@
 
 static int dkx_probe(struct device *dev)
 {
-	const void *model;
+	const char *model;
 
 	stm32mp_bbu_mmc_fip_register("sd", "/dev/mmc0",
 				     BBU_HANDLER_FLAG_DEFAULT);
 
-	if (dev_get_drvdata(dev, &model) == 0)
+	model = device_get_match_data(dev);
+	if (model)
 		barebox_set_model(model);
 
 	barebox_set_hostname("stm32mp15xx-dkx");

arch/arm/cpu/psci-client.c
@@ -121,8 +121,8 @@ static int __init psci_probe(struct device *dev)
 	ulong of_version, actual_version;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&of_version);
-	if (ret)
+	of_version = (uintptr_t)device_get_match_data(dev);
+	if (!of_version)
 		return -ENODEV;
 
 	ret = of_property_read_string(dev->of_node, "method", &method);

arch/arm/mach-imx/iim.c
@@ -453,15 +453,17 @@ static int imx_iim_probe(struct device *dev)
 	struct resource *iores;
 	struct iim_priv *iim;
 	int i, ret;
-	struct imx_iim_drvdata *drvdata = NULL;
-	char *nregs = NULL;
+	const struct imx_iim_drvdata *drvdata;
+	const char *nregs = NULL;
 
 	if (imx_iim)
 		return -EBUSY;
 
 	iim = xzalloc(sizeof(*iim));
 
-	dev_get_drvdata(dev, (const void **)&drvdata);
+	drvdata = device_get_match_data(dev);
+	if (!drvdata)
+		return -ENODEV;
 
 	if (drvdata && drvdata->supply)
 		iim->supply = drvdata->supply;

drivers/ata/sata-imx.c
@@ -87,12 +87,12 @@ static int imx_sata_probe(struct device *dev)
 {
 	struct resource *iores;
 	struct imx_ahci *imx_ahci;
-	struct imx_sata_data *data;
+	const struct imx_sata_data *data;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&data);
-	if (ret)
-		return ret;
+	data = device_get_match_data(dev);
+	if (!data)
+		return -ENODEV;
 
 	imx_ahci = xzalloc(sizeof(*imx_ahci));
 

drivers/bus/imx-weim.c
@@ -69,14 +69,14 @@ MODULE_DEVICE_TABLE(of, weim_id_table);
 struct imx_weim {
 	struct device *dev;
 	void __iomem *base;
-	struct imx_weim_devtype *devtype;
+	const struct imx_weim_devtype *devtype;
 };
 
 /* Parse and set the timing for this device. */
 static int
 weim_timing_setup(struct imx_weim *weim, struct device_node *np)
 {
-	struct imx_weim_devtype *devtype = weim->devtype;
+	const struct imx_weim_devtype *devtype = weim->devtype;
 	u32 cs_idx, value[devtype->cs_regs_count];
 	int i, ret;
 
@@ -128,13 +128,13 @@ static int weim_parse_dt(struct imx_weim *weim)
 static int weim_probe(struct device *dev)
 {
 	struct resource *iores;
-	struct imx_weim_devtype *devtype;
+	const struct imx_weim_devtype *devtype;
 	struct imx_weim *weim;
 	int ret;
 
-	ret = dev_get_drvdata(dev, (const void **)&devtype);
-	if (ret)
-		return ret;
+	devtype = device_get_match_data(dev);
+	if (!devtype)
+		return -ENODEV;
 
 	weim = xzalloc(sizeof(*weim));
 

drivers/clocksource/timer-imx-gpt.c
@@ -52,7 +52,7 @@ static struct imx_gpt_regs regs_imx31 = {
 	.tctl_val = IMX31_TCTL_FRR | IMX31_TCTL_CLKSOURCE_PER | TCTL_TEN,
 };
 
-static struct imx_gpt_regs *regs;
+static const struct imx_gpt_regs *regs;
 static void __iomem *timer_base;
 
 static uint64_t imx_clocksource_read(void)
@@ -81,16 +81,15 @@ static int imx_gpt_probe(struct device *dev)
 {
 	struct resource *iores;
 	int i;
-	int ret;
 	unsigned long rate;
 
 	/* one timer is enough */
 	if (timer_base)
 		return 0;
 
-	ret = dev_get_drvdata(dev, (const void **)&regs);
-	if (ret)
-		return ret;
+	regs = device_get_match_data(dev);
+	if (!regs)
+		return -ENODEV;
 
 	iores = dev_request_mem_resource(dev, 0);
 	if (IS_ERR(iores))

drivers/eeprom/at24.c
@@ -380,9 +380,9 @@ static int at24_probe(struct device *dev)
 		unsigned long magic;
 		u32 page_size;
 
-		err = dev_get_drvdata(dev, (const void **)&magic);
-		if (err)
-			return err;
+		magic = (uintptr_t)device_get_match_data(dev);
+		if (!magic)
+			return -ENODEV;
 
 		chip.byte_len = BIT(magic & AT24_BITMASK(AT24_SIZE_BYTELEN));
 		magic >>= AT24_SIZE_BYTELEN;

commit b3c4f9a12b8c127f0ae48a5eab082090a60dfdf2

i2c: omap: fix probe on am335x
The AM335x uses ti,omap4-i2c as compatible for the I2C device tree
nodes, but that compatible unlike its brethern has no driver data as
it's determined using the machine compatible in the probe function
instead.

device_get_match_data() returning NULL is thus expected and should not
lead to an immediate error.

Fixes: 20d87123a638 ("treewide: replace dev_get_drvdata with device_get_match_data")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20251212153649.365215-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/i2c/busses/i2c-omap.c
@@ -1076,13 +1076,14 @@ i2c_omap_probe(struct device *pdev)
 	}
 
 	i2c_data = device_get_match_data(pdev);
-	if (!i2c_data)
-		return -ENODEV;
 
 	if (of_machine_is_compatible("ti,am33xx"))
 		i2c_data = &am33xx_data;
 	if (of_machine_is_compatible("ti,omap4"))
 		i2c_data = &omap4_data;
+
+	if (!i2c_data)
+		return -EINVAL;
 
 	i2c_omap->data = i2c_data;
 	i2c_omap->reg_shift = (i2c_data->flags >>
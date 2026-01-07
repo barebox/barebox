barebox/barebox

commit a382be0b4f234acad4ae7bd589dbef7162a2be60

mci: add option to detect non-removable cards during startup
We can optionally detect all MMC/SD cards during startup which might
cost some boot time, so this is best disabled. This adds another option
to detect only cards that are marked non-removable in the device tree
during startup. The rationale is that we will likely boot from that
device anyway, so detecting it during startup does not add to the boot
time.

We are aiming to find a good place for the barebox environment without
having to explicitly add it to the device tree. Putting the environment
into a regular partition is a good start, but the device that has the
environment needs to be available when we are searching for it. Enabling
this new option is one way to it.

Link: https://lore.barebox.org/20250602-createnv-v1-5-c3239ff875d5@pengutronix.de
Link: https://lore.barebox.org/20250620-createnv-v2-5-7fd3cc286b9a@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mci.h
@@ -58,6 +58,7 @@
 #define MMC_CAP_1_2V_DDR		(1 << 9)	/* Host supports eMMC DDR 1.2V */
 #define MMC_CAP_DDR			(MMC_CAP_3_3V_DDR | MMC_CAP_1_8V_DDR | \
 					 MMC_CAP_1_2V_DDR)
+#define MMC_CAP_NONREMOVABLE	(1 << 8)	/* Nonremovable e.g. eMMC */
 #define MMC_CAP_UHS_SDR12	(1 << 16)	/* Host supports UHS SDR12 mode */
 #define MMC_CAP_UHS_SDR25	(1 << 17)	/* Host supports UHS SDR25 mode */
 #define MMC_CAP_UHS_SDR50	(1 << 18)	/* Host supports UHS SDR50 mode */

drivers/mci/Kconfig
@@ -19,13 +19,30 @@ config MCI_TUNING
 	  higher clock speeds than 52 MHz SDR. MMC only; SD-Card max
 	  frequency is 50MHz SDR at present.
 
-config MCI_STARTUP
-	bool "Force probe on system start"
+choice
+	prompt "MMC detection on startup"
+
+config MCI_STARTUP_NONE
+	bool "do not detect cards during system start"
 	help
-	  Say 'y' here if the MCI framework should always probe for all attached
+	  Choose this if the MCI framework should never detect cards during system
+	  start
+
+config MCI_STARTUP_NONREMOVABLE
+	bool "detect non removable cards on system start"
+	help
+	  Choose this if the MCI framework should detect non removable cards (i.e.
+	  devices which have the non-removable device tree property).
+
+config MCI_STARTUP
+	bool "detect all cards on system start"
+	help
+	  Choose this if the MCI framework should always probe for all attached
 	  MCI cards on system start up. This may required for some legacy boards.
 	  When this is 'n', probing happens on demand either with "mci*.probe=1"
 	  or with driver/board code calling device_detect.
+
+endchoice
 
 config MCI_INFO
 	bool "MCI Info"

drivers/mci/mci-core.c
@@ -3101,7 +3101,9 @@ int mci_register(struct mci_host *host)
 		devinfo_add(&mci->dev, mci_info);
 
 	/* if enabled, probe the attached card immediately */
-	if (IS_ENABLED(CONFIG_MCI_STARTUP))
+	if (IS_ENABLED(CONFIG_MCI_STARTUP) ||
+	   (IS_ENABLED(CONFIG_MCI_STARTUP_NONREMOVABLE) &&
+	    (host->host_caps & MMC_CAP_NONREMOVABLE)))
 		mci_card_probe(mci);
 
 	if (!(host->caps2 & MMC_CAP2_NO_SD) && dev_of_node(host->hw_dev)) {
@@ -3195,6 +3197,8 @@ void mci_of_parse_node(struct mci_host *host,
 		host->caps2 |= MMC_CAP2_NO_SD;
 	if (of_property_read_bool(np, "no-mmc"))
 		host->caps2 |= MMC_CAP2_NO_MMC;
+	if (of_property_read_bool(np, "non-removable"))
+                host->host_caps |= MMC_CAP_NONREMOVABLE;
 	if (IS_ENABLED(CONFIG_MCI_TUNING)) {
 		u32 drv_type;
 

commit cf4a68050d630d63723280c50b207217577b6ef0

mmc: resolve conflict between MMC_CAP_NONREMOVABLE and MMC_CAP_1_8V_DDR
Resolve conflict between MMC_CAP_NONREMOVABLE and MMC_CAP_1_8V_DDR.

Fixes: a382be0b4f ("mci: add option to detect non-removable cards during startup")
Signed-off-by: zhengxiaojun <zheng.xiaojun at foxmail.com>
Link: https://lore.barebox.org/tencent_C5E0959753A838615BF48C3792E12BC74A08@qq.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mci.h
@@ -58,7 +58,7 @@
 #define MMC_CAP_1_2V_DDR		(1 << 9)	/* Host supports eMMC DDR 1.2V */
 #define MMC_CAP_DDR			(MMC_CAP_3_3V_DDR | MMC_CAP_1_8V_DDR | \
 					 MMC_CAP_1_2V_DDR)
-#define MMC_CAP_NONREMOVABLE	(1 << 8)	/* Nonremovable e.g. eMMC */
+#define MMC_CAP_NONREMOVABLE	(1 << 10)	/* Nonremovable e.g. eMMC */
 #define MMC_CAP_UHS_SDR12	(1 << 16)	/* Host supports UHS SDR12 mode */
 #define MMC_CAP_UHS_SDR25	(1 << 17)	/* Host supports UHS SDR25 mode */
 #define MMC_CAP_UHS_SDR50	(1 << 18)	/* Host supports UHS SDR50 mode */
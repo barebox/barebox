d419b47a05cfad46f4c09d391246933d17ec72ae
Ahmad Fatoum <a.fatoum@barebox.org>
2026-01-09 17:57:30 +0100

    test: py: add more kernel boot tests
    We have one boot test for a Debian kernel via GRUB. The kernel in this
    same image can be EFI booted directly thanks to its EFI stub and it can
    be even booted without EFI at all via the normal boot protocol.
    
    Add a test exercising both.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
    Link: https://lore.barebox.org/20260109170007.805177-5-a.fatoum@barebox.org
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- test/py/test_linux_efiloader.py
+++ test/py/test_linux_efiloader.py
@@ -16,6 +16,70 @@ def get_journalctl(shell, kernel=True, grep=None):
 
 
 @pytest.mark.lg_feature(['bootable', 'efi'])
+@pytest.mark.parametrize('efiloader', [False, True])
+def test_boot_manual_with_initrd(strategy, barebox, env, efiloader):
+    """Test booting Debian kernel directly without GRUB"""
+
+    barebox.run_check(f"global.bootm.efi={'required' if efiloader else 'disabled'}")
+
+    def get_option(strategy, opt):
+        config = strategy.target.env.config
+        return config.get_target_option(strategy.target.name, opt)
+
+    try:
+        root_dev = get_option(strategy, "root_dev")
+        kernel_path = get_option(strategy, "bootm.image")
+    except KeyError:
+        pytest.fail("Feature bootable enabled, but root_dev/bootm.image option missing.")  # noqa
+
+    # Detect block devices
+    barebox.run_check("detect -a")
+    barebox.run_check(f"ls /mnt/{root_dev}/")
+
+    [kernel_path] = barebox.run_check(f"ls /mnt/{root_dev}/{kernel_path}")
+
+    try:
+        initrd_path = get_option(strategy, "bootm.initrd")
+        [initrd_path] = barebox.run_check(f"ls /mnt/{root_dev}/{initrd_path}")
+        barebox.run_check(f"global.bootm.initrd={initrd_path}")
+    except KeyError:
+        pass
+
+    barebox.run_check(f"global.bootm.image={kernel_path}")
+    barebox.run_check(f"global.bootm.root_dev=/dev/{root_dev}")
+    barebox.run_check("global.bootm.appendroot=1")
+    # Speed up subsequent runs a bit
+    barebox.run_check("global linux.bootargs.noapparmor=apparmor=0")
+
+    # Boot the kernel - it should use EFI stub by default
+    with strategy.boot_kernel(bootm=True) as shell:
+        shell.run_check("grep -q apparmor=0 /proc/cmdline")
+
+        initrd_freed = any("Freeing initrd memory"
+                           in line for line in get_journalctl(shell, 'initrd'))
+        assert initrd_freed, "initrd was not loaded or freed"
+
+        # Verify we booted to shell
+        dmesg = get_journalctl(shell, 'efi')
+
+        uefi_not_found = re.search("efi: UEFI not found.",
+                                   "\n".join(dmesg)) is not None
+
+        if efiloader:
+            test_efi_kernel_no_warn(shell)
+            test_expected_efi_messages(shell, env)
+            test_efi_systab(shell, env)
+            test_efivars_filesystem_not_empty(shell)
+
+            assert not uefi_not_found, \
+                   "EFI stub was not used despite global.bootm.efi=required"
+        else:
+            # Verify that EFI was NOT used
+            assert uefi_not_found, \
+                   "EFI stub was used despite global.bootm.efi=disabled"
+
+
+@pytest.mark.lg_feature(['bootable', 'efi'])
 def test_efi_kernel_no_warn(shell):
     stdout, stderr, ret = shell.run("journalctl -k --no-pager --grep efi -o cat -p warning")
     assert stdout == []

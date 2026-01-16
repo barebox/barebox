barebox/barebox

commit ae175d4748b12292216d6cd24cd890d8816d5fb6

PBL: fdt: add fdt_fixup_mem to fixup memory nodes
Board code in the PBL may use fdt_fixup_mem() to write the base
addresses and sizes of detected SDRAM to the fdt before passing the fdt
to other software like the TF-A and OP-TEE.

This is implemented on the fdt to avoid that the PBL needs to unpack the
device tree, use the of function for manipulation and repack the device
tree.

Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>
Link: https://lore.barebox.org/20250528-rk3588-optee-v2-5-63070238dd13@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

pbl/fdt.c
@@ -2,6 +2,7 @@
 #include <linux/libfdt.h>
 #include <pbl.h>
 #include <linux/printk.h>
+#include <stdio.h>
 
 static const __be32 *fdt_parse_reg(const __be32 *reg, uint32_t n,
 				   uint64_t *val)
@@ -71,6 +72,83 @@ void fdt_find_mem(const void *fdt, unsigned long *membase, unsigned long *memsiz
 err:
 	pr_err("No memory, cannot continue\n");
 	while (1);
+}
+
+static int fdt_find_or_add_memory(void *fdt, int parentoffset, const char *name)
+{
+	int err;
+	int node;
+
+	node = fdt_subnode_offset(fdt, parentoffset, name);
+	if (node != -FDT_ERR_NOTFOUND)
+		return node;
+
+	/* Create new memory node */
+	node = fdt_add_subnode(fdt, parentoffset, name);
+	if (node < 0)
+		return node;
+	err = fdt_setprop(fdt, node, "device_type", "memory", sizeof("memory"));
+	if (err < 0)
+		return err;
+
+	return node;
+}
+
+int fdt_fixup_mem(void *fdt, unsigned long membase[], unsigned long memsize[],
+		  size_t num)
+{
+	int node, root;
+	int err;
+	int i;
+
+	err = fdt_check_header(fdt);
+	if (err != 0) {
+		pr_err("Invalid device tree blob: %s\n", fdt_strerror(err));
+		return err;
+	}
+
+	root = fdt_path_offset(fdt, "/");
+	if (root < 0) {
+		pr_err("Cannot find root node: %s\n", fdt_strerror(root));
+		return root;
+	}
+
+	/* Delete memory node without @address postfix */
+	node = fdt_subnode_offset(fdt, root, "memory");
+	if (node >= 0)
+		fdt_del_node(fdt, node);
+
+	for (i = 0; i < num; i++) {
+		unsigned long base = membase[i];
+		unsigned long size = memsize[i];
+		char name[32];
+
+		if (size == 0)
+			continue;
+
+		snprintf(name, sizeof(name), "memory@%lx", base);
+		node = fdt_find_or_add_memory(fdt, root, name);
+		if (!node) {
+			pr_warn("%s: Failed to get node: %s\n",
+				name, fdt_strerror(err));
+			continue;
+		}
+
+		/* Add or rewrite the reg property */
+		fdt_delprop(fdt, node, "reg");
+		err = fdt_appendprop_addrrange(fdt, root, node, "reg",
+					       base, size);
+		if (err < 0) {
+			pr_warn("%s: Failed to set reg property %lx %lx: %s\n",
+				name, base, size, fdt_strerror(err));
+			continue;
+		}
+
+		/* Remove status property to ensure the node is enabled */
+		fdt_delprop(fdt, node, "status");
+	}
+
+	return err;
 }
 
 const void *fdt_device_get_match_data(const void *fdt, const char *nodepath,

include/pbl.h
@@ -17,6 +17,7 @@ extern unsigned long free_mem_end_ptr;
 void pbl_barebox_uncompress(void *dest, void *compressed_start, unsigned int len);
 
 void fdt_find_mem(const void *fdt, unsigned long *membase, unsigned long *memsize);
+int fdt_fixup_mem(void *fdt, unsigned long membase[], unsigned long memsize[], size_t num);
 
 struct fdt_device_id {
 	const char *compatible;

commit 7c266f0ed1c50f7372ef6dbd9e447023980e1db4

pbl: fdt: fix fdt_fixup_mem error handling
fdt_find_or_add_memory() returns >= 0 on success, so the return must be
checked to be < 0. Also fix the following pr_warn() which used the wrong
variable.

Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
Fixes: ae175d4748b1 ("PBL: fdt: add fdt_fixup_mem to fixup memory nodes")
Cc: Michael Tretter <m.tretter@pengutronix.de>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260114120221.941836-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

pbl/fdt.c
@@ -128,9 +128,9 @@ int fdt_fixup_mem(void *fdt, unsigned long membase[], unsigned long memsize[],
 
 		snprintf(name, sizeof(name), "memory@%lx", base);
 		node = fdt_find_or_add_memory(fdt, root, name);
-		if (!node) {
+		if (node < 0) {
 			pr_warn("%s: Failed to get node: %s\n",
-				name, fdt_strerror(err));
+				name, fdt_strerror(node));
 			continue;
 		}
 
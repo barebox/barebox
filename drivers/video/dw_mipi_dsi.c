barebox/barebox

commit 5115bff30153315223ecc3426bfeecc606194259

video: dw_mipi_dsi: activate pattern generator during enable as a hack
There's still something fishy with the initialization sequence of the
video pipeline with respect to the MIPI-DSI bridge.

As a workaround, jump start the pipeline by briefly enabling the video
pattern generator and disabling it again.

This will lead to a very brief flash of colored bars until that issue is
better understood and properly resolved.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20251201085927.1306364-3-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/video/dw_mipi_dsi.c
@@ -906,9 +906,20 @@ static int dw_mipi_dsi_ioctl(struct vpl *vpl, unsigned int port,
 {
 	struct dw_mipi_dsi *dsi = container_of(vpl, struct dw_mipi_dsi, vpl);
 	struct drm_display_mode mode = {};
+	bool vpg_enable_hack;
+	int ret;
+
+	/*
+	 * HACK: If Video Pattern Generator is not running here, the panel
+	 * initialization on the Sitronix ST7703 (RGB30-Panel) times out...
+	 * FIXME: figure out why Linux doesn't suffer from this
+	 */
+	vpg_enable_hack = true;
 
 	switch (cmd) {
 	case VPL_ENABLE:
+		if (vpg_enable_hack)
+			dsi->vpg_mode = VPG_COLORS_V;
 		/* Switch to video mode for panel-bridge enable & panel enable */
 		dw_mipi_dsi_set_mode(dsi, MIPI_DSI_MODE_VIDEO);
 		break;
@@ -934,7 +945,14 @@ static int dw_mipi_dsi_ioctl(struct vpl *vpl, unsigned int port,
 	/* Probes should have occured by now */
 	WARN_ON(!dsi->panel_bridge);
 
-	return vpl_bridge_ioctl(dsi->panel_bridge, cmd, data);
+	ret = vpl_bridge_ioctl(dsi->panel_bridge, cmd, data);
+
+	if (cmd == VPL_ENABLE && vpg_enable_hack) {
+		dsi->vpg_mode = VPG_DISABLED;
+		vpg_param_set(NULL, dsi);
+	}
+
+	return ret;
 }
 
 static const char *vpg_modes[] = {

commit fde027dd650e70b578f7c8f1ef181542278e85b8

video: dw_mipi_dsi: remove VPG workaround for panel initialization
The Video Pattern Generator was being temporarily enabled during
VPL_ENABLE to work around panel initialization timeouts on the Sitronix
ST7703 of the Powkiddy RGB30. The root cause was a sync polarity
that has been fixed in the previous commit, so this hack can now go away.

This fixes a regression for the STM32MP157C-DK2: Enabling the VPG at the
point we did, caused the panel to synchronize to a wrong start offset and
led to a funny effect with the barebox logo that's shown by default:

On subsequent reboots, the display output would start at different
locations and so the logo would wander around and because the color
format is RGB888, the hexagon inside the barebox logo b would keep
switching between red, green and blue.

Fixes: 5115bff30153 ("video: dw_mipi_dsi: activate pattern generator during enable as a hack")
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20260213092906.3960051-2-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/video/dw_mipi_dsi.c
@@ -906,20 +906,9 @@ static int dw_mipi_dsi_ioctl(struct vpl *vpl, unsigned int port,
 {
 	struct dw_mipi_dsi *dsi = container_of(vpl, struct dw_mipi_dsi, vpl);
 	struct drm_display_mode mode = {};
-	bool vpg_enable_hack;
-	int ret;
-
-	/*
-	 * HACK: If Video Pattern Generator is not running here, the panel
-	 * initialization on the Sitronix ST7703 (RGB30-Panel) times out...
-	 * FIXME: figure out why Linux doesn't suffer from this
-	 */
-	vpg_enable_hack = true;
 
 	switch (cmd) {
 	case VPL_ENABLE:
-		if (vpg_enable_hack)
-			dsi->vpg_mode = VPG_COLORS_V;
 		/* Switch to video mode for panel-bridge enable & panel enable */
 		dw_mipi_dsi_set_mode(dsi, MIPI_DSI_MODE_VIDEO);
 		break;
@@ -945,14 +934,7 @@ static int dw_mipi_dsi_ioctl(struct vpl *vpl, unsigned int port,
 	/* Probes should have occured by now */
 	WARN_ON(!dsi->panel_bridge);
 
-	ret = vpl_bridge_ioctl(dsi->panel_bridge, cmd, data);
-
-	if (cmd == VPL_ENABLE && vpg_enable_hack) {
-		dsi->vpg_mode = VPG_DISABLED;
-		vpg_param_set(NULL, dsi);
-	}
-
-	return ret;
+	return vpl_bridge_ioctl(dsi->panel_bridge, cmd, data);
 }
 
 static const char *vpg_modes[] = {
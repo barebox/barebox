barebox/barebox

commit 3ee4d8927379eb8f55f52bbddd01a8fe74239c55

regulator: bcm2835: register only on Raspberry Pi
The device was registered whenever the driver was compiled, including in
multi-platform builds. Guard behind raspberrypi,bcm2835-firmware, until
this is turned into a "real" driver one day.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20260112085617.2952747-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/regulator/bcm2835.c
@@ -138,8 +138,10 @@ postcore_platform_driver(regulator_bcm2835_driver);
 
 static int regulator_bcm2835_init(void)
 {
-	add_generic_device("regulator-bcm2835", DEVICE_ID_SINGLE, NULL,
-			   0, 0, 0, NULL);
+	if (of_get_compatible_child(of_get_root_node(),
+				    "raspberrypi,bcm2835-firmware"))
+		add_generic_device("regulator-bcm2835", DEVICE_ID_SINGLE, NULL,
+				   0, 0, 0, NULL);
 
 	return 0;
 }

commit 5d66ead5495919e012ca2df1f35fd8e05716c749

regulator: bcm2835: fix USB-breaking regression
For use in multi-platform configurations, regulator_bcm2835_init() was
gated behind a lookup for "raspberrypi,bcm2835-firmware"-compatible nodes
in the live barebox DT.

The lookup using of_get_compatible_child is erroneous though, because it
only looks at immediate children and doesn't recurse down.

This is unfortunate, when the firmware node is at /soc/firmware.

Fix this by using the correct API.

Fixes: 3ee4d8927379 ("regulator: bcm2835: register only on Raspberry Pi")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260225115713.2047529-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/regulator/bcm2835.c
@@ -138,7 +138,7 @@ postcore_platform_driver(regulator_bcm2835_driver);
 
 static int regulator_bcm2835_init(void)
 {
-	if (of_get_compatible_child(of_get_root_node(),
+	if (of_find_compatible_node(NULL, NULL,
 				    "raspberrypi,bcm2835-firmware"))
 		add_generic_device("regulator-bcm2835", DEVICE_ID_SINGLE, NULL,
 				   0, 0, 0, NULL);
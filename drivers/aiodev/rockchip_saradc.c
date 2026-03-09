barebox/barebox

commit 6e46ee2f2e443bd0fc6bbe0919ea859b91938159

aiodev: rockchip_saradc: Add RK3562 support
The RK3562 has a SARADC v2 with 8 channels and 12 bit resolution.
compile-tested Only.

Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>
Reviewed-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260130-barebox-kickpi-v2-8-d27cf94a39b6@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/aiodev/rockchip_saradc.c
@@ -256,6 +256,12 @@ fail_data:
 	return ret;
 }
 
+static const struct rockchip_saradc_cfg rk3562_saradc_cfg = {
+	.num_bits = 12,
+	.num_channels = 8,
+	.read = rockchip_saradc_read_v2,
+};
+
 static const struct rockchip_saradc_cfg rk3568_saradc_cfg = {
 	.num_bits = 10,
 	.num_channels = 8,
@@ -276,6 +282,7 @@ static const struct rockchip_saradc_cfg rk3588_saradc_cfg = {
 };
 
 static const struct of_device_id of_rockchip_saradc_match[] = {
+	{ .compatible = "rockchip,rk3562-saradc", .data = &rk3562_saradc_cfg },
 	{ .compatible = "rockchip,rk3568-saradc", .data = &rk3568_saradc_cfg },
 	{ .compatible = "rockchip,rk3576-saradc", .data = &rk3576_saradc_cfg },
 	{ .compatible = "rockchip,rk3588-saradc", .data = &rk3588_saradc_cfg },

commit 9d93c1aece25d7962ee5e23d489d3876cf4d05fe

aiodev: rockchip_saradc: Fix ADC resolution for RK3562
The RK3562 has a 10 bit A/D converter.

Fixes: 6e46ee2f (aiodev: rockchip_saradc: Add RK3562 support)

Signed-off-by: Robin van der Gracht <robin.van.der.gracht@protonic.nl>
Link: https://lore.barebox.org/20260304124606.549402-1-robin.van.der.gracht@protonic.nl
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/aiodev/rockchip_saradc.c
@@ -257,7 +257,7 @@ fail_data:
 }
 
 static const struct rockchip_saradc_cfg rk3562_saradc_cfg = {
-	.num_bits = 12,
+	.num_bits = 10,
 	.num_channels = 8,
 	.read = rockchip_saradc_read_v2,
 };
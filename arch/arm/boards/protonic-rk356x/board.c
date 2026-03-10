577a9815e27f866ba231510b431534ea73af220e
Robin van der Gracht <robin.van.der.gracht@protonic.nl>
2026-03-10 16:24:26 +0100

    ARM: boards: Rename protonic-mecsbc to protonic-rk356x
    A more generic name which allows us to add more boards in the future.
    
    Signed-off-by: Robin van der Gracht <robin.van.der.gracht@protonic.nl>
    Link: https://lore.barebox.org/20260310152433.786340-2-robin.van.der.gracht@protonic.nl
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/protonic-rk356x/board.c
+++ arch/arm/boards/protonic-rk356x/board.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
-#define pr_fmt(fmt) "MECSBC: " fmt
+#define pr_fmt(fmt) "Protonic RK356x: " fmt
 
 #include <bootsource.h>
 #include <common.h>
@@ -13,17 +13,17 @@
 #include <aiodev.h>
 #include <globalvar.h>
 
-struct mecsbc_model {
+struct prt_rk356x_model {
 	const char *name;
 	const char *shortname;
 };
 
-struct mecsbc_priv {
+struct prt_rk356x_priv {
 	int hw_id;
 	int hw_rev;
 };
 
-static struct mecsbc_priv mecsbc_data;
+static struct prt_rk356x_priv prt_priv;
 
 static int saradc_get_value(const char *chan)
 {
@@ -38,56 +38,56 @@ static int saradc_get_value(const char *chan)
 	return voltage;
 }
 
-static int mecsbc_get_vin_mv(void)
+static int prt_rk356x_get_vin_mv(void)
 {
 	return saradc_get_value("aiodev0.in_value2_mV") * 22;
 }
 
-static bool mecsbc_get_usb_boot(void)
+static bool prt_rk356x_get_usb_boot(void)
 {
 	return saradc_get_value("aiodev0.in_value0_mV") < 74;
 }
 
-static int mecsbc_adc_id_values[] = {
+static int prt_rk356x_adc_id_values[] = {
 	1800, 1662, 1521, 1354, 1214, 1059, 900, 742, 335, 589, 278, 137, 0
 };
 
-static int mecsbc_get_adc_id(const char *chan)
+static int prt_rk356x_get_adc_id(const char *chan)
 {
 	int val;
 	unsigned int t;
 
 	val = saradc_get_value(chan) + 74;
 
-	for (t = 0; t < ARRAY_SIZE(mecsbc_adc_id_values); t++) {
-		if (val > mecsbc_adc_id_values[t])
+	for (t = 0; t < ARRAY_SIZE(prt_rk356x_adc_id_values); t++) {
+		if (val > prt_rk356x_adc_id_values[t])
 			return t;
 	}
 
 	return t;
 }
 
-static void mecsbc_process_adc(struct device *dev)
+static void prt_rk356x_process_adc(struct device *dev)
 {
-	mecsbc_data.hw_id = mecsbc_get_adc_id("aiodev0.in_value1_mV");
-	mecsbc_data.hw_rev = mecsbc_get_adc_id("aiodev0.in_value3_mV");
+	prt_priv.hw_id = prt_rk356x_get_adc_id("aiodev0.in_value1_mV");
+	prt_priv.hw_rev = prt_rk356x_get_adc_id("aiodev0.in_value3_mV");
 
-	dev_add_param_uint32_ro(dev, "boardrev", &mecsbc_data.hw_rev, "%u");
-	dev_add_param_uint32_ro(dev, "boardid", &mecsbc_data.hw_id, "%u");
+	dev_add_param_uint32_ro(dev, "boardrev", &prt_priv.hw_rev, "%u");
+	dev_add_param_uint32_ro(dev, "boardid", &prt_priv.hw_id, "%u");
 
 	/* Check if we need to enable the USB gadget instead of booting */
-	if (mecsbc_get_usb_boot()) {
+	if (prt_rk356x_get_usb_boot()) {
 		globalvar_add_simple("boot.default", "net");
 		globalvar_add_simple("usbgadget.acm", "1");
 		globalvar_add_simple("usbgadget.autostart", "1");
 		globalvar_add_simple("system.partitions", "/dev/mmc0(mmc0)");
-		pr_info("MECSBC: Enter USB recovery\n");
+		pr_info("Enter USB recovery\n");
 	} else {
 		globalvar_add_simple("boot.default", "bootchooser");
 	}
 
-	pr_info("Board id: %d, revision %d\n", mecsbc_data.hw_id, mecsbc_data.hw_rev);
-	pr_info("VIN = %d V\n", mecsbc_get_vin_mv() / 1000);
+	pr_info("Board id: %d, revision %d\n", prt_priv.hw_id, prt_priv.hw_rev);
+	pr_info("VIN = %d V\n", prt_rk356x_get_vin_mv() / 1000);
 }
 
 static int mecsbc_sd_of_fixup(struct device_node *root, void *context)
@@ -108,31 +108,31 @@ static int mecsbc_sd_of_fixup(struct device_node *root, void *context)
 	return 0;
 }
 
-static int mecsbc_of_fixup_hwrev(struct device *dev)
+static int prt_rk356x_of_fixup_hwrev(struct device *dev)
 {
 	const char *compat;
 	char *buf;
 
 	compat = of_device_get_match_compatible(dev);
 
-	buf = xasprintf("%s-m%u-r%u", compat, mecsbc_data.hw_id,
-			mecsbc_data.hw_rev);
+	buf = xasprintf("%s-m%u-r%u", compat, prt_priv.hw_id,
+			prt_priv.hw_rev);
 	barebox_set_of_machine_compatible(buf);
 
 	free(buf);
 
-	if (mecsbc_data.hw_id == 0 && mecsbc_data.hw_rev == 0)
+	if (prt_priv.hw_id == 0 && prt_priv.hw_rev == 0)
 		of_register_fixup(mecsbc_sd_of_fixup, dev);
 
 	return 0;
 }
 
-static int mecsbc_probe(struct device *dev)
+static int prt_rk356x_probe(struct device *dev)
 {
 	int ret = 0;
 	enum bootsource bootsource = bootsource_get();
 	int instance = bootsource_get_instance();
-	const struct mecsbc_model *model;
+	const struct prt_rk356x_model *model;
 	struct device_node *np;
 
 	np = of_find_node_by_name_address(NULL, "saradc@fe720000");
@@ -151,18 +151,18 @@ static int mecsbc_probe(struct device *dev)
 	rk3568_bbu_mmc_register("emmc", BBU_HANDLER_FLAG_DEFAULT, "/dev/mmc0");
 	rk3568_bbu_mmc_register("sd", 0, "/dev/mmc1");
 
-	mecsbc_process_adc(dev);
-	mecsbc_of_fixup_hwrev(dev);
+	prt_rk356x_process_adc(dev);
+	prt_rk356x_of_fixup_hwrev(dev);
 
 	return ret;
 }
 
-static const struct mecsbc_model mecsbc = {
+static const struct prt_rk356x_model mecsbc = {
 	.name = "Protonic MECSBC board",
 	.shortname = "mecsbc",
 };
 
-static const struct of_device_id mecsbc_of_match[] = {
+static const struct of_device_id prt_rk356x_of_match[] = {
 	{
 		.compatible = "prt,mecsbc",
 		.data = &mecsbc,
@@ -170,11 +170,11 @@ static const struct of_device_id mecsbc_of_match[] = {
 	{ /* sentinel */ },
 };
 
-static struct driver mecsbc_board_driver = {
-	.name = "board-mecsbc",
-	.probe = mecsbc_probe,
-	.of_compatible = mecsbc_of_match,
+static struct driver prt_rk356x_board_driver = {
+	.name = "board-protonic-rk356x",
+	.probe = prt_rk356x_probe,
+	.of_compatible = prt_rk356x_of_match,
 };
-coredevice_platform_driver(mecsbc_board_driver);
+coredevice_platform_driver(prt_rk356x_board_driver);
 
-BAREBOX_DEEP_PROBE_ENABLE(mecsbc_of_match);
+BAREBOX_DEEP_PROBE_ENABLE(prt_rk356x_of_match);

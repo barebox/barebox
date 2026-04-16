0c29366bb4237099d60b926f2415490d5fbe2b35
Michael Tretter <m.tretter@pengutronix.de>
2026-04-16 11:48:05 +0200

    arm: socfpga: agilex5: cleanup TF-A loading
    Use local variables for the bl31 and bl33 address to be able to change
    these more easily and consistently.
    
    Print the TF-A memory locations to the logs.
    
    Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>
    Link: https://lore.barebox.org/20260416-socfpga-agilex5-sdram-v1-1-07556aa7219f@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-socfpga/atf.c
+++ arch/arm/mach-socfpga/atf.c
@@ -11,24 +11,26 @@
 
 void __noreturn agilex5_load_and_start_image_via_tfa(unsigned long memsize)
 {
-	unsigned long atf_dest = AGILEX5_ATF_BL31_BASE_ADDR;
-	void __noreturn (*bl31)(void) = (void *)atf_dest;
+	void *bl31 = (void *)AGILEX5_ATF_BL31_BASE_ADDR;
+	void *bl33 = (void *)AGILEX5_ATF_BL33_BASE_ADDR;
 	const void *tfa;
 	size_t tfa_size;
 
-	pr_debug("Load TFA\n");
-
-	memcpy((void *)AGILEX5_ATF_BL33_BASE_ADDR, __image_start, barebox_image_size);
+	pr_debug("copy bl33 to %p from %p (%zu bytes)\n",
+		 bl33, __image_start, barebox_image_size);
+	memcpy(bl33, __image_start, barebox_image_size);
 
 	get_builtin_firmware(agilex5_bl31_bin, &tfa, &tfa_size);
 
+	pr_debug("copy bl31 to %p from %p (%zu bytes)\n",
+		 bl31, tfa, tfa_size);
 	memcpy(bl31, tfa, tfa_size);
 
 	asm volatile("msr sp_el2, %0" : :
-		     "r" (AGILEX5_ATF_BL33_BASE_ADDR - 16) :
+		     "r" (bl33 - 16) :
 		     "cc");
 
-	pr_debug("Jumping to @0x%08lx\n", atf_dest);
-	bl31_entry((uintptr_t)bl31, 0, AGILEX5_ATF_BL33_BASE_ADDR, 0);
+	pr_debug("Jump to bl31 0x%p\n", bl31);
+	bl31_entry((uintptr_t)bl31, 0, (uintptr_t)bl33, 0);
 	__builtin_unreachable();
 }

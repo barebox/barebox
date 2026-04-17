barebox/barebox

commit 65077d20782b4fb619d9425f265fe36fe98ef043

mci: treat unknown MMC version as greater than known versions
Currently, unknown MMC revisions are assumed to be v4.0-compatible.

Instead let's assume newer eMMC revisions to be compatible with the
newest that's explicitly supported by assigning 0xFFF as versions, so
comparison for being greater than some version always evaluate true.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260302163748.577552-2-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/mci-core.c
@@ -1155,6 +1155,8 @@ static void mci_correct_version_from_ext_csd(struct mci *mci)
 {
 	if (!IS_SD(mci) && (mci->version >= MMC_VERSION_4) && mci->ext_csd) {
 		switch (mci->ext_csd[EXT_CSD_REV]) {
+		case 0:
+			mci->version = MMC_VERSION_4;
 		case 1:
 			mci->version = MMC_VERSION_4_1;
 			break;
@@ -1178,6 +1180,9 @@ static void mci_correct_version_from_ext_csd(struct mci *mci)
 			break;
 		case 9:
 			mci->version = MMC_VERSION_5_1B;
+			break;
+		default:
+			mci->version = MMC_VERSION_MAX;
 			break;
 		}
 	}

include/mci.h
@@ -47,6 +47,7 @@
 #define MMC_VERSION_5_0		(MMC_VERSION_MMC | 0x500)
 #define MMC_VERSION_5_1		(MMC_VERSION_MMC | 0x510)
 #define MMC_VERSION_5_1B	(MMC_VERSION_MMC | 0x51B)
+#define MMC_VERSION_MAX		(MMC_VERSION_MMC | 0xFFF)
 
 #define MMC_CAP_SPI			(1 << 0)
 #define MMC_CAP_4_BIT_DATA		(1 << 1)

commit 41295e7cd5e16e8999b8ad7d103e87ca54896593

mci: add missing break
65077d2078 adds a new case to a switch/case which lacks a break, so it
falls through and EXT_CSD_REV = 0 is detected as MMC_VERSION_4_1 instead
of MMC_VERSION_4. Add the missing break.

Fixes: 65077d2078 ("mci: treat unknown MMC version as greater than known versions")
Link: https://lore.barebox.org/20260415081357.450925-1-s.hauer@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/mci-core.c
@@ -1160,6 +1160,7 @@ static void mci_correct_version_from_ext_csd(struct mci *mci)
 		switch (mci->ext_csd[EXT_CSD_REV]) {
 		case 0:
 			mci->version = MMC_VERSION_4;
+			break;
 		case 1:
 			mci->version = MMC_VERSION_4_1;
 			break;
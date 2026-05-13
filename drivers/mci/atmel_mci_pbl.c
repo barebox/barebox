barebox/barebox

commit 28974435d0853d696b19dee5f022cd4aa04fd24c

mci: add data segment to mci_cmd
In Linux the struct mmc_command includes the mmc_data field and doesn't
hand it around in the function calls.

Do the same here and remove the mci_data parameter from the send_cmd
callback. The mci-core sets up the mci_cmd->data field and the drivers
can get the mmc_data from the mci_cmd struct.

A lot of the calls in mci-core don't use data in the mci_send_cmd call.
Explicitly initialize the mci_cmd struct with 0 (and therefore the
mci_data struct with NULL) on declaration.

When data is used and set up before mci_send_cmd is called the pointer
is xzalloc'd where it previously was setup on the stack.

Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>
Link: https://lore.barebox.org/20260309-v2025-11-0-topic-socfpga-agilex5-sdhci-v2-1-fcc1f327acf8@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/atmel_mci_pbl.c
@@ -12,7 +12,7 @@ static int pbl_atmci_common_request(struct pbl_mci *mci,
 				    struct mci_cmd *cmd,
 				    struct mci_data *data)
 {
-	return atmci_common_request(mci->priv, cmd, data);
+	return atmci_common_request(mci->priv, cmd, cmd->data);
 }
 
 static struct atmel_mci atmci_host;

drivers/mci/am654-sdhci.c
@@ -454,13 +454,13 @@ static void print_error(struct am654_sdhci_plat *host, int cmdidx)
 		sdhci_read32(&host->sdhci, SDHCI_INT_NORMAL_STATUS));
 }
 
-static int am654_sdhci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-				struct mci_data *data)
+static int am654_sdhci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	struct am654_sdhci_plat *host = container_of(mci, struct am654_sdhci_plat, mci);
 	u32 mask, command, xfer;
 	int ret;
 	dma_addr_t dma;
+	struct mci_data *data = cmd->data;
 
 	ret = sdhci_wait_idle(&host->sdhci, cmd, data);
 	if (ret)

drivers/mci/arasan-sdhci.c
@@ -244,13 +244,13 @@ static void print_error(struct arasan_sdhci_host *host, int cmdidx, int ret)
 		sdhci_read32(&host->sdhci, SDHCI_INT_NORMAL_STATUS));
 }
 
-static int arasan_sdhci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-				 struct mci_data *data)
+static int arasan_sdhci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	struct arasan_sdhci_host *host = to_arasan_sdhci_host(mci);
 	u32 mask, command, xfer;
 	dma_addr_t dma;
 	int ret;
+	struct mci_data *data = cmd->data;
 
 	ret = sdhci_wait_idle(&host->sdhci, cmd, data);
 	if (ret)

drivers/mci/atmel-sdhci.c
@@ -29,10 +29,9 @@ struct at91_sdhci_priv {
 
 #define to_priv(h) container_of(h, struct at91_sdhci_priv, mci)
 
-static int at91_sdhci_mci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-				   struct mci_data *data)
+static int at91_sdhci_mci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
-	return at91_sdhci_send_command(&to_priv(mci)->host, cmd, data);
+	return at91_sdhci_send_command(&to_priv(mci)->host, cmd, cmd->data);
 }
 
 static void at91_sdhci_mci_set_ios(struct mci_host *mci, struct mci_ios *ios)

drivers/mci/atmel_mci.c
@@ -23,12 +23,11 @@ static void atmci_set_ios(struct mci_host *mci, struct mci_ios *ios)
 	atmci_common_set_ios(host, ios);
 }
 
-static int atmci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-		  struct mci_data *data)
+static int atmci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	struct atmel_mci *host = to_mci_host(mci);
 
-	return atmci_common_request(host, cmd, data);
+	return atmci_common_request(host, cmd, cmd->data);
 }
 
 static void atmci_info(struct device *mci_dev)

drivers/mci/bcm2835-sdhost.c
@@ -521,10 +521,10 @@ static void bcm2835_set_clock(struct bcm2835_host *host, unsigned int clock)
 	writel(clock / 2, host->regs + SDTOUT);
 }
 
-static int bcm2835_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-			    struct mci_data *data)
+static int bcm2835_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	struct bcm2835_host *host = to_bcm2835_host(mci);
+	struct mci_data *data = cmd->data;
 	u32 edm, fsm;
 	int ret = 0;
 

drivers/mci/dove-sdhci.c
@@ -54,13 +54,13 @@ static int dove_sdhci_wait_for_done(struct dove_sdhci *host, u16 mask)
 	return 0;
 }
 
-static int dove_sdhci_mci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd,
-				struct mci_data *data)
+static int dove_sdhci_mci_send_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	u32 command, xfer;
 	int ret;
 	unsigned int num_bytes = 0;
 	struct dove_sdhci *host = priv_from_mci_host(mci);
+	struct mci_data *data = cmd->data;
 
 	ret = sdhci_wait_idle_data(&host->sdhci, cmd);
 	if (ret)

drivers/mci/dw_mmc.c
@@ -263,7 +263,7 @@ static int dwmci_write_data_pio(struct dwmci_host *host, struct mci_data *data)
 }
 
 static int
-dwmci_cmd(struct mci_host *mci, struct mci_cmd *cmd, struct mci_data *data)
+dwmci_cmd(struct mci_host *mci, struct mci_cmd *cmd)
 {
 	struct dwmci_host *host = to_dwmci_host(mci);
 	int flags = 0;
@@ -273,6 +273,7 @@ dwmci_cmd(struct mci_host *mci, struct mci_cmd *cmd, struct mci_data *data)
 	int ret;
 	unsigned int num_bytes = 0;
 	dma_addr_t dma = 0;
+	struct mci_data *data = cmd->data;
 
 	start = get_time_ns();
 	while (1) {

commit 26c3f0823a56e241c9aae08f18e2d7b0240bc27c

mci: atmel_mci: fix mci_pbl breakage after mci_cmd::data change
To align with Linux, mci_cmd was extended by a data pointer and the
send_cmd in barebox proper was adapted to drop the mci_data member.

The PBL drivers were not adapted and just ignore mci_data, except for
the Atmel MCI driver, which now discards the actually populated data and
passes cmd->data, which is always NULL.

Correct the argument to fix BOOT.BIN boot up on the KSZ9477-EVB.
A patch that switches pbl_mci as a whole to the new API will follow
separately.

Fixes: 28974435d085 ("mci: add data segment to mci_cmd")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260513133859.3877113-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/mci/atmel_mci_pbl.c
@@ -12,7 +12,7 @@ static int pbl_atmci_common_request(struct pbl_mci *mci,
 				    struct mci_cmd *cmd,
 				    struct mci_data *data)
 {
-	return atmci_common_request(mci->priv, cmd, cmd->data);
+	return atmci_common_request(mci->priv, cmd, data);
 }
 
 static struct atmel_mci atmci_host;
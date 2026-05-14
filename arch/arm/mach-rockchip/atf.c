barebox/barebox

commit f2ae1a4a85d03cb351dfaac649f10bdfc42be9b0

ARM: rockchip: atf: enable MMU in PBL
Using compressed binaries for TF-A and OP-TEE comes with the penalty of
additional startup time. Enable the MMU before uncompressing to speed
that up significantly.

Link: https://lore.barebox.org/20260320-compressed-firmware-rockchip-v1-6-7d03c7e39d2f@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/atf.c
@@ -7,6 +7,7 @@
 #include <tee/optee.h>
 #include <asm/atf_common.h>
 #include <asm/barebox-arm.h>
+#include <asm/mmu.h>
 #include <asm-generic/memory_layout.h>
 #include <asm-generic/sections.h>
 #include <mach/rockchip/dmc.h>
@@ -144,7 +145,6 @@ static struct fwobj bl32; /* OP-TEE in barebox image */
 			get_builtin_firmware_compressed(tee_bin, &bl32);	\
 	} while (0)
 
-
 static int rockchip_create_optee_fdt(void *buf, int bufsize)
 {
 	unsigned long base[ROCKCHIP_MAX_DRAM_RESOURCES];
@@ -171,6 +171,8 @@ static void rockchip_atf_load_bl31(void *fdt)
 {
 	unsigned long bl31_ep;
 
+	mmu_early_enable(membase[0], membase[0] + memsize[0]);
+
 	bl31_ep = load_elf64_image_phdr(&bl31);
 
 	if (IS_ENABLED(CONFIG_ARCH_ROCKCHIP_OPTEE))
@@ -180,6 +182,8 @@ static void rockchip_atf_load_bl31(void *fdt)
 	asm volatile("msr sp_el2, %0" : :
 			"r" ((ulong)barebox_load_address - 16) :
 			"cc");
+
+	mmu_disable();
 
 	bl31_entry(bl31_ep, optee_load_address,
 		   barebox_load_address, (uintptr_t)fdt);

commit a0ef3a1b5cb6801976fcef943ce1ef7dd2a08cd9

ARM: rockchip: atf: pass correct memsize to mmu_early_enable()
Second argument of mmu_early_enable() is the usable memory size. Pass
memsize[0] there which contains the memory size as the name suggests.
Without this the TTB ends up outside the DRAM.

Fixes: f2ae1a4a85 ("ARM: rockchip: atf: enable MMU in PBL")
Link: https://lore.barebox.org/20260513-rockchip-fixes-v1-3-a4055849a014@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/atf.c
@@ -172,7 +172,7 @@ static void rockchip_atf_load_bl31(void *fdt)
 {
 	unsigned long bl31_ep;
 
-	mmu_early_enable(membase[0], membase[0] + memsize[0]);
+	mmu_early_enable(membase[0], memsize[0]);
 
 	bl31_ep = load_elf64_image_phdr(&bl31);
 
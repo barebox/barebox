barebox/barebox

commit 516447c05cf05d41975b96e643cad378864b0e44

ARM: rockchip: separate physical DRAM start from usable start
The RK3xxx_DRAM_BOTTOM macros define the first usable DRAM address, that
is the physical DRAM start plus the offset occupied by the TF-A. With
upcoming PBL mmu support we need both addresses, so separate them.
We use RK3xxx_DRAM_START for the physical start and add
ROCKCHIP_DRAM_TFA_CARVE_OUT where needed.

Link: https://lore.barebox.org/20260320-compressed-firmware-rockchip-v1-5-7d03c7e39d2f@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/dmc.c
@@ -160,13 +160,13 @@ static size_t rockchip_ram(phys_addr_t membase, resource_size_t memsize,
 		 * RK3576 has internal registers below the DRAM start and thus
 		 * doesn't need any gaps in the DRAM space.
 		 */
-		base[0] = membase;
+		base[0] = membase + ROCKCHIP_DRAM_TFA_CARVE_OUT;
 		size[0] = memsize;
 
 		return 1;
 	}
 
-	base[i] = membase;
+	base[i] = membase + ROCKCHIP_DRAM_TFA_CARVE_OUT;
 	size[i] = min_t(resource_size_t, RK3588_INT_REG_START, memsize) - membase;
 	i++;
 
@@ -202,7 +202,7 @@ size_t rk3399_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
 
 	pr_debug("%s() = %llu\n", __func__, (u64)size);
 
-	return rockchip_ram(RK3399_DRAM_BOTTOM, memsize, RK3399_INT_REG_START, base, size, n);
+	return rockchip_ram(RK3399_DRAM_START, memsize, RK3399_INT_REG_START, base, size, n);
 }
 
 size_t rk3562_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
@@ -218,7 +218,7 @@ size_t rk3562_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
 
 	pr_debug("%s() = %llu\n", __func__, (u64)memsize);
 
-	return rockchip_ram(RK3562_DRAM_BOTTOM, memsize, RK3562_INT_REG_START, base, size, n);
+	return rockchip_ram(RK3562_DRAM_START, memsize, RK3562_INT_REG_START, base, size, n);
 }
 
 size_t rk3568_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
@@ -234,7 +234,7 @@ size_t rk3568_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
 
 	pr_debug("%s() = %llu\n", __func__, (u64)memsize);
 
-	return rockchip_ram(RK3568_DRAM_BOTTOM, memsize, RK3568_INT_REG_START, base, size, n);
+	return rockchip_ram(RK3568_DRAM_START, memsize, RK3568_INT_REG_START, base, size, n);
 }
 
 size_t rk3576_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
@@ -250,7 +250,7 @@ size_t rk3576_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
 
 	pr_debug("%s() = %llu\n", __func__, (u64)size);
 
-	return rockchip_ram(RK3576_DRAM_BOTTOM, memsize, RK3576_INT_REG_START, base, size, n);
+	return rockchip_ram(RK3576_DRAM_START, memsize, RK3576_INT_REG_START, base, size, n);
 }
 
 #define RK3588_PMUGRF_BASE 0xfd58a000
@@ -277,7 +277,7 @@ size_t rk3588_ram_sizes(phys_addr_t *base, resource_size_t *size, size_t n)
 
 	memsize = size1 + size2;
 
-	return rockchip_ram(RK3588_DRAM_BOTTOM, memsize, RK3588_INT_REG_START, base, size, n);
+	return rockchip_ram(RK3588_DRAM_START, memsize, RK3588_INT_REG_START, base, size, n);
 }
 
 static int rockchip_dmc_probe(struct device *dev)
@@ -335,28 +335,28 @@ static const struct rockchip_dmc_drvdata rk3399_drvdata = {
 	.os_reg2 = RK3399_PMUGRF_OS_REG2,
 	.os_reg3 = RK3399_PMUGRF_OS_REG3,
 	.internal_registers_start = RK3399_INT_REG_START,
-	.membase = RK3399_DRAM_BOTTOM,
+	.membase = RK3399_DRAM_START,
 };
 
 static const struct rockchip_dmc_drvdata rk3562_drvdata = {
 	.os_reg2 = RK3562_PMUGRF_OS_REG2,
 	.os_reg3 = RK3562_PMUGRF_OS_REG3,
 	.internal_registers_start = RK3562_INT_REG_START,
-	.membase = RK3562_DRAM_BOTTOM,
+	.membase = RK3562_DRAM_START,
 };
 
 static const struct rockchip_dmc_drvdata rk3568_drvdata = {
 	.os_reg2 = RK3568_PMUGRF_OS_REG2,
 	.os_reg3 = RK3568_PMUGRF_OS_REG3,
 	.internal_registers_start = RK3568_INT_REG_START,
-	.membase = RK3568_DRAM_BOTTOM,
+	.membase = RK3568_DRAM_START,
 };
 
 static const struct rockchip_dmc_drvdata rk3576_drvdata = {
 	.os_reg2 = RK3576_PMUGRF_OS_REG2,
 	.os_reg3 = RK3576_PMUGRF_OS_REG3,
 	.internal_registers_start = RK3576_INT_REG_START,
-	.membase = RK3576_DRAM_BOTTOM,
+	.membase = RK3576_DRAM_START,
 };
 
 static const struct rockchip_dmc_drvdata rk3588_drvdata = {
@@ -365,7 +365,7 @@ static const struct rockchip_dmc_drvdata rk3588_drvdata = {
 	.os_reg4 = RK3588_PMUGRF_OS_REG4,
 	.os_reg5 = RK3588_PMUGRF_OS_REG5,
 	.internal_registers_start = RK3588_INT_REG_START,
-	.membase = RK3588_DRAM_BOTTOM,
+	.membase = RK3588_DRAM_START,
 };
 
 static struct of_device_id rockchip_dmc_dt_ids[] = {

include/mach/rockchip/atf.h
@@ -3,12 +3,14 @@
 #ifndef __MACH_ATF_H
 #define __MACH_ATF_H
 
-/* First usable DRAM address. Lower mem is used for ATF and OP-TEE */
-#define RK3399_DRAM_BOTTOM		0xa00000
-#define RK3562_DRAM_BOTTOM		0xa00000
-#define RK3568_DRAM_BOTTOM		0xa00000
-#define RK3576_DRAM_BOTTOM		0x40a00000
-#define RK3588_DRAM_BOTTOM		0xa00000
+/* The first 10MiB of DRAM are used by the TF-A */
+#define ROCKCHIP_DRAM_TFA_CARVE_OUT	0xa00000
+
+#define RK3399_DRAM_START		0x0
+#define RK3562_DRAM_START		0x0
+#define RK3568_DRAM_START		0x0
+#define RK3576_DRAM_START		0x40000000
+#define RK3588_DRAM_START		0x0
 
 /*
  * The tee.bin image has an OP-TEE specific header that describes the
@@ -36,11 +38,11 @@
  * board lowlevel code should relocate barebox here. This is where
  * OP-TEE jumps to after initialization.
  */
-#define RK3399_BAREBOX_LOAD_ADDRESS	(RK3399_DRAM_BOTTOM + 1024*1024)
-#define RK3562_BAREBOX_LOAD_ADDRESS	(RK3562_DRAM_BOTTOM + 1024*1024)
-#define RK3568_BAREBOX_LOAD_ADDRESS	(RK3568_DRAM_BOTTOM + 1024*1024)
-#define RK3576_BAREBOX_LOAD_ADDRESS	(RK3576_DRAM_BOTTOM + 1024*1024)
-#define RK3588_BAREBOX_LOAD_ADDRESS	(RK3588_DRAM_BOTTOM + 1024*1024)
+#define RK3399_BAREBOX_LOAD_ADDRESS	(RK3399_DRAM_START + ROCKCHIP_DRAM_TFA_CARVE_OUT + 1024*1024)
+#define RK3562_BAREBOX_LOAD_ADDRESS	(RK3562_DRAM_START + ROCKCHIP_DRAM_TFA_CARVE_OUT + 1024*1024)
+#define RK3568_BAREBOX_LOAD_ADDRESS	(RK3568_DRAM_START + ROCKCHIP_DRAM_TFA_CARVE_OUT + 1024*1024)
+#define RK3576_BAREBOX_LOAD_ADDRESS	(RK3576_DRAM_START + ROCKCHIP_DRAM_TFA_CARVE_OUT + 1024*1024)
+#define RK3588_BAREBOX_LOAD_ADDRESS	(RK3588_DRAM_START + ROCKCHIP_DRAM_TFA_CARVE_OUT + 1024*1024)
 
 void __noreturn rk3562_barebox_entry(void *fdt);
 void __noreturn rk3568_barebox_entry(void *fdt);

commit dd691689bc9cb8e3f85a6daeeec34362f6199d21

ARM: rockchip: dmc: substract ROCKCHIP_DRAM_TFA_CARVE_OUT from memory regions
With 516447c05c rockchip_ram() gets the physical memory base/size
instead of ROCKCHIP_DRAM_TFA_CARVE_OUT already honored. With this we
not only have to add ROCKCHIP_DRAM_TFA_CARVE_OUT to membase, but also
substract it from memsize. The latter was forgotten, add it now.

Fixes: 516447c05c ("ARM: rockchip: separate physical DRAM start from usable start")
Link: https://lore.barebox.org/20260513-rockchip-fixes-v1-2-a4055849a014@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/dmc.c
@@ -161,13 +161,13 @@ static size_t rockchip_ram(phys_addr_t membase, resource_size_t memsize,
 		 * doesn't need any gaps in the DRAM space.
 		 */
 		base[0] = membase + ROCKCHIP_DRAM_TFA_CARVE_OUT;
-		size[0] = memsize;
+		size[0] = memsize - ROCKCHIP_DRAM_TFA_CARVE_OUT;
 
 		return 1;
 	}
 
 	base[i] = membase + ROCKCHIP_DRAM_TFA_CARVE_OUT;
-	size[i] = min_t(resource_size_t, RK3588_INT_REG_START, memsize) - membase;
+	size[i] = min_t(resource_size_t, RK3588_INT_REG_START, memsize) - membase - ROCKCHIP_DRAM_TFA_CARVE_OUT;
 	i++;
 
 	if (i < n && memsize > SZ_4G) {
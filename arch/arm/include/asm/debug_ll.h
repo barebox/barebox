b804a356447f927b0bf7a8a7c19bcd34c12bd245
Sascha Hauer <s.hauer@pengutronix.de>
2026-06-01 14:29:54 +0200

    debug_ll: Add Kconfig options for debug uarts that have none
    Some SoCs include its debug_ll.h header file only by testing if
    support for that SoC is compiled in. That doesn't work when barebox is
    built for multiple SoCs as this leads to multiple definitions of
    PUTC_LL.
    
    Add a debug UART Kconfig option for all SoCs that do not yet have one.
    The debug UART selection is a choice which makes sure only a single
    debug UART is selected which also means we can include the SoC specific
    debug_ll.h files using #if defined / elif defined.
    
    Link: https://lore.barebox.org/20260601-debug-ll-conflicts-v1-2-bf17f9f0e23a@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/include/asm/debug_ll.h
+++ arch/arm/include/asm/debug_ll.h
@@ -3,66 +3,45 @@
 #ifndef __ASM_DEBUG_LL_H__
 #define __ASM_DEBUG_LL_H__
 
-#ifdef CONFIG_DEBUG_IMX_UART
+#if defined CONFIG_DEBUG_IMX_UART
 #include <mach/imx/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_ROCKCHIP_UART
+#elif defined CONFIG_DEBUG_ROCKCHIP_UART
 #include <mach/rockchip/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_OMAP_UART
+#elif defined CONFIG_DEBUG_OMAP_UART
 #include <mach/omap/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_ZYNQMP_UART
+#elif defined CONFIG_DEBUG_ZYNQMP_UART
 #include <mach/zynqmp/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_STM32MP_UART
+#elif defined CONFIG_DEBUG_STM32MP_UART
 #include <mach/stm32mp/debug_ll.h>
-#endif
-
-#if defined(CONFIG_DEBUG_VEXPRESS_UART) || \
-    defined(CONFIG_DEBUG_QEMU_ARM32_VIRT)
+#elif defined CONFIG_DEBUG_VEXPRESS_UART || defined CONFIG_DEBUG_QEMU_ARM32_VIRT
 #include <mach/vexpress/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_BCM283X_UART
+#elif defined CONFIG_DEBUG_BCM283X_UART
 #include <mach/bcm283x/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_LAYERSCAPE_UART
+#elif defined CONFIG_DEBUG_LAYERSCAPE_UART
 #include <mach/layerscape/debug_ll.h>
-#endif
-
-#ifdef CONFIG_DEBUG_SEMIHOSTING
+#elif defined CONFIG_DEBUG_SEMIHOSTING
 #include <debug_ll/semihosting.h>
-#endif
-
-#ifdef CONFIG_DEBUG_QEMU_ARM64_VIRT
+#elif defined CONFIG_DEBUG_AT91_UART
+#include <mach/at91/debug_ll.h>
+#elif defined CONFIG_DEBUG_QEMU_ARM64_VIRT
 #define DEBUG_LL_UART_ADDR		0x9000000
 #include <debug_ll/pl011.h>
-#elif defined CONFIG_ARCH_MVEBU
-#include <mach/mvebu/debug_ll.h>
-#elif defined CONFIG_ARCH_ZYNQ
-#include <mach/zynq/debug_ll.h>
-#elif defined CONFIG_ARCH_VERSATILE
-#include <mach/versatile/debug_ll.h>
-#elif defined CONFIG_ARCH_TEGRA
-#include <mach/tegra/debug_ll.h>
-#elif defined CONFIG_ARCH_SOCFPGA
-#include <mach/socfpga/debug_ll.h>
-#elif defined CONFIG_ARCH_PXA
-#include <mach/pxa/debug_ll.h>
-#elif defined CONFIG_ARCH_MXS
-#include <mach/mxs/debug_ll.h>
-#elif defined CONFIG_ARCH_CLPS711X
-#include <mach/clps711x/debug_ll.h>
-#elif defined CONFIG_ARCH_AT91
-#include <mach/at91/debug_ll.h>
-#elif defined CONFIG_ARCH_K3
+#elif defined CONFIG_DEBUG_AM62X_UART
 #include <mach/k3/debug_ll.h>
+#elif defined CONFIG_DEBUG_MVEBU_UART
+#include <mach/mvebu/debug_ll.h>
+#elif defined CONFIG_DEBUG_CLPS711X_UART
+#include <mach/clps711x/debug_ll.h>
+#elif defined CONFIG_DEBUG_MXS_UART
+#include <mach/mxs/debug_ll.h>
+#elif defined CONFIG_DEBUG_VERSATILE_UART
+#include <mach/versatile/debug_ll.h>
+#elif defined CONFIG_DEBUG_TEGRA_UART
+#include <mach/tegra/debug_ll.h>
+#elif defined CONFIG_DEBUG_ZYNQ_UART
+#include <mach/zynq/debug_ll.h>
+#elif defined CONFIG_DEBUG_SOCFPGA_UART
+#include <mach/socfpga/debug_ll.h>
 #endif
 
 #endif

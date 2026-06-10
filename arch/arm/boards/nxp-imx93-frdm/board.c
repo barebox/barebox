6b557db8a0890d95e90c0aba45f654a66769418a
Bastian Krause <bst@pengutronix.de>
2026-06-10 19:49:19 +0200

    ARM: nxp-imx93-frdm: add bbu handler for eMMC
    This allows updating barebox via barebox_update as well as via fastboot.
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>
    Reviewed-by: Marco Felsch <m.felsch@pengutronix.de>
    Link: https://lore.barebox.org/20260610174919.3813577-2-bst@pengutronix.de
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/nxp-imx93-frdm/board.c
+++ arch/arm/boards/nxp-imx93-frdm/board.c
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#include <common.h>
 #include <deep-probe.h>
+#include <mach/imx/bbu.h>
+
+static int imx93_frdm_probe(struct device *dev)
+{
+	imx9_bbu_internal_mmcboot_register_handler("eMMC", "/dev/mmc0", 0);
+
+	return 0;
+}
 
 static const struct of_device_id frdm_imx93_of_match[] = {
 	{
@@ -10,3 +19,10 @@ static const struct of_device_id frdm_imx93_of_match[] = {
 };
 
 BAREBOX_DEEP_PROBE_ENABLE(frdm_imx93_of_match);
+
+static struct driver frdm_imx93_board_driver = {
+	.name = "board-imx93-frdm",
+	.probe = imx93_frdm_probe,
+	.of_compatible = frdm_imx93_of_match,
+};
+coredevice_platform_driver(frdm_imx93_board_driver);

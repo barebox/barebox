barebox/barebox

commit 8dfab4e2aafa41dede3b9e74a53c9223d0a3d8ee

ARM: rockchip: pass device tree to TF-A
The upstream OP-TEE expects a device tree to be able to initialize the
dynamic shared memory. Therefore, barebox should pass a device tree that
contains memory nodes through the TF-A to OP-TEE.

OP-TEE may modify the passed fdt. Thus, barebox copies the fdt from the
fixed size rodata area to the rk_scratch area and allocates a
configurable memory area for the fdt to allow fdt modification.

OP-TEE has the CFG_DTB_MAX_SIZE config option for the maximum size of
the fdt. barebox must reserve at least that much memory for the fdt to
avoid out of bounds accesses from OP-TEE.

Unfortunately, the downstream TF-A fails to start if barebox passes its
device tree and it is not possible to detect if the loaded TF-A is able
to handle the device tree. Add a config option to pass the device tree
if it is possible.

Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>
Reviewed-by: Marco Felsch <m.felsch@pengutronix.de>
Link: https://lore.barebox.org/20250528-rk3588-optee-v2-8-63070238dd13@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/Kconfig
@@ -141,6 +141,29 @@ config ARCH_ROCKCHIP_ATF
 	  useful for debugging early startup, but for all other cases,
 	  say y here.
 
+config ARCH_ROCKCHIP_ATF_PASS_FDT
+	bool "Pass device tree to TF-A"
+	depends on ARCH_ROCKCHIP_ATF
+	help
+	  Enable this option if you are using an upstream OP-TEE that uses the
+	  device tree to initialize dynamic shared memory, which is passed
+	  through the upstream TF-A.
+
+	  Disable the option if you are using a downstream TF-A since it
+	  doesn't always cope with device trees. Supposedly this happens if the
+	  device tree is too large, for example if CONFIG_OF_OVERLAY_LIVE is
+	  enabled.
+
+config ARCH_ROCKCHIP_ATF_FDT_SIZE
+	hex
+	default 0x0
+	default 0x60000 if ARCH_ROCKCHIP_ATF_PASS_FDT
+	prompt "Reserved size for the FDT blob passed to the TF-A"
+	help
+	  Set this size to CFG_DTB_MAX_SIZE in the OP-TEE configuration. OP-TEE
+	  may modify the passed device tree and increase it's size. This
+	  ensures that barebox reserves enough memory for the modifications.
+
 config ARCH_ROCKCHIP_OPTEE
 	bool "Build rockchip OP-TEE binary into barebox"
 	depends on ARCH_ROCKCHIP_ATF

include/mach/rockchip/bootrom.h
@@ -13,7 +13,10 @@
 struct rockchip_scratch_space {
 	u32 irom[16];
 	struct optee_header optee_hdr;
+	/* FDT needs an 8 byte alignment */
+	u8 fdt[CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE] __aligned(8);
 };
+static_assert(sizeof(struct rockchip_scratch_space) <= CONFIG_SCRATCH_SIZE);
 
 extern struct rockchip_scratch_space *rk_scratch;
 

arch/arm/mach-rockchip/atf.c
@@ -183,21 +183,21 @@ void __noreturn rk3588_barebox_entry(void *fdt)
 	rk_scratch = (void *)arm_mem_scratch(endmem);
 
 	if (current_el() == 3) {
+		void *fdt_scratch = NULL;
+
 		rk3588_lowlevel_init();
 		rockchip_store_bootrom_iram(IOMEM(RK3588_IRAM_BASE));
 
-		/*
-		 * The downstream TF-A doesn't cope with our device tree when
-		 * CONFIG_OF_OVERLAY_LIVE is enabled, supposedly because it is
-		 * too big for some reason. Otherwise it doesn't have any visible
-		 * effect if we pass a device tree or not, except that the TF-A
-		 * fills in the ethernet MAC address into the device tree.
-		 * The upstream TF-A doesn't use the device tree at all.
-		 *
-		 * Pass NULL for now until we have a good reason to pass a real
-		 * device tree.
-		 */
-		rk3588_atf_load_bl31(NULL);
+		if (IS_ENABLED(CONFIG_ARCH_ROCKCHIP_ATF_PASS_FDT)) {
+			pr_debug("Copy fdt to scratch area 0x%p (%zu bytes)\n",
+				 rk_scratch->fdt, sizeof(rk_scratch->fdt));
+			if (fdt_open_into(fdt, rk_scratch->fdt, sizeof(rk_scratch->fdt)) == 0)
+				fdt_scratch = rk_scratch->fdt;
+			else
+				pr_warn("Failed to copy fdt to scratch: Continue without fdt\n");
+		}
+
+		rk3588_atf_load_bl31(fdt_scratch);
 		/* not reached when CONFIG_ARCH_ROCKCHIP_ATF */
 	}
 

commit da91ab6cb505fb09415bf8df3fae552e7b37c5e1

ARM: rockchip: hide CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE behind ATF_PASS_FDT
If a user had configured barebox before the addition of
CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE, updating barebox to a newer version
would set the variable to its default of 0.

If they then enable CONFIG_ARCH_ROCKCHIP_ATF_PASS_FDT, the default were
not be reapplied thus ending up with
CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE=0, which is guaranteed to fail.

In older barebox versions, this had a chance of hanging during BL31
setup, because an uninitialized FDT pointer was passed to TF-A.

This is fixed now, but still the default settings for
CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE are likely to confuse users interested
in passing along the FDT.

Improve the situation a bit by hiding CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE
behind CONFIG_ARCH_ROCKCHIP_ATF_PASS_FDT and having only a single default.

This is not the neatest solution, because now users will likely get a
build error, because CONFIG_SCRATCH_SIZE will need to be increased in
accordance, but at least it's a build-time failure rather than runtime..

Reported-by: Lucie L. Hartmann <lucie@mntre.com>
Fixes: 8dfab4e2aafa ("ARM: rockchip: pass device tree to TF-A")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260618151700.671088-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

arch/arm/mach-rockchip/Kconfig
@@ -187,10 +187,9 @@ config ARCH_ROCKCHIP_ATF_PASS_FDT
 	  enabled.
 
 config ARCH_ROCKCHIP_ATF_FDT_SIZE
-	hex
-	default 0x0
-	default 0x60000 if ARCH_ROCKCHIP_ATF_PASS_FDT
-	prompt "Reserved size for the FDT blob passed to the TF-A"
+	hex "Reserved size for the FDT blob passed to the TF-A"
+	depends on ARCH_ROCKCHIP_ATF_PASS_FDT
+	default 0x60000
 	help
 	  Set this size to CFG_DTB_MAX_SIZE in the OP-TEE configuration. OP-TEE
 	  may modify the passed device tree and increase it's size. This

include/mach/rockchip/bootrom.h
@@ -10,11 +10,17 @@
 #include <tee/optee.h>
 #include <pbl.h>
 
+#ifdef CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE
+#define ROCKCHIP_ATF_FDT_SIZE CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE
+#else
+#define ROCKCHIP_ATF_FDT_SIZE 0
+#endif
+
 struct rockchip_scratch_space {
 	u32 iram[16];
 	struct optee_header optee_hdr;
 	/* FDT needs an 8 byte alignment */
-	u8 fdt[CONFIG_ARCH_ROCKCHIP_ATF_FDT_SIZE] __aligned(8);
+	u8 fdt[ROCKCHIP_ATF_FDT_SIZE] __aligned(8);
 };
 static_assert(sizeof(struct rockchip_scratch_space) <= CONFIG_SCRATCH_SIZE);
 
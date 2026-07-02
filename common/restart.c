barebox/barebox

commit d20a9a455d8d8e341c349bbe12fc9cb7598f42c6

restart: allow selecting restart handler by device name
Many SoCs feature multiple instances of the same watchdog IP. For
example, the i.MX8MP has three watchdogs, which would be called wdog0,
wdog1, wdog2 in barebox, but they would all have imxwd as restart
handler name.

To be able to differentiate between them for restart purposes, add an
optional device member that's used as fallback when no exact restart
handler name was found.

To make this visible to the user, also change reset -l to list the
device name if it exists. Once we assign i.MX watchdog restart handlers
a device pointer, the reset -l output would look like this:

  watchdog-restart                              10
  imxwd-warm                                    90 W
  imxwd                wdog0                   100
  imxwd-warm                                    90 W
  imxwd                wdog1                   100
  imxwd-warm                                    90 W
  imxwd                wdog2                   100

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240729143007.1558035-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/restart.c
@@ -82,8 +82,12 @@ struct restart_handler *restart_handler_get_by_name(const char *name, int flags)
 	unsigned int priority = 0;
 
 	list_for_each_entry(tmp, &restart_handler_list, list) {
-		if (name && tmp->name && strcmp(name, tmp->name))
-			continue;
+		if (name) {
+			if ((tmp->name && strcmp(name, tmp->name)) &&
+			    (tmp->dev && strcmp(name, dev_name(tmp->dev))))
+				continue;
+		}
+
 		if (flags && (tmp->flags & flags) != flags)
 			continue;
 		if (tmp->priority > priority) {
@@ -120,7 +124,8 @@ void restart_handlers_print(void)
 	struct restart_handler *tmp;
 
 	list_for_each_entry(tmp, &restart_handler_list, list) {
-		printf("%-20s %6d ", tmp->name, tmp->priority);
+		printf("%-20s %-20s %6d ",
+		       tmp->name, tmp->dev ? dev_name(tmp->dev) : "", tmp->priority);
 		if (tmp->flags & RESTART_FLAG_WARM_BOOTROM)
 			putchar('W');
 		putchar('\n');

include/restart.h
@@ -21,6 +21,7 @@ struct restart_handler {
 	int flags;
 	struct device_node *of_node;
 	const char *name;
+	struct device *dev;
 	struct list_head list;
 };
 

commit 8bd9cab774e9c2216081f8f7757f9c1edadb0c20

restart: fix restart handler by name selection
Commit d20a9a455d8d ("restart: allow selecting restart handler by device
name") introduce a bug, while adding the support to select a restart
handler by device name.

The list_for_each_entry() loop should sort out all restart handler which
don't match the user requested restart handler. Instead all restart
handlers which don't have a device associated are honored as well
because the if condition is always 0 in this case.

Split the if to fix this and add a helper variable to make the code more
readable.

Fixes: d20a9a455d8d ("restart: allow selecting restart handler by device name")
Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
Link: https://lore.barebox.org/20260622172121.1736295-1-m.felsch@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/restart.c
@@ -117,8 +117,13 @@ struct restart_handler *restart_handler_get_by_name(const char *name, int flags)
 
 	list_for_each_entry(tmp, &restart_handler_list, list) {
 		if (name) {
-			if ((tmp->name && strcmp(name, tmp->name)) &&
-			    (tmp->dev && strcmp(name, dev_name(tmp->dev))))
+			bool found = false;
+
+			if (tmp->name && !strcmp(name, tmp->name))
+				found = true;
+			if (tmp->dev && !strcmp(name, dev_name(tmp->dev)))
+				found = true;
+			if (!found)
 				continue;
 		}
 
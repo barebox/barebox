barebox/barebox

commit 086d01264d9aff24651d931344835ab183ec560e

ARM: i.MX: add support for fast authentication
Integrate support for fast authentication on i.MX8 SoCs.
Fast authentication is only supported for HAB version >= 4.1.2

>From the NXP manual it says:

"It provides the option to use the SRK to verify the CSF data and Image data directly,
instead of using the CSF and IMG keys.
This reduces the number of key pair authentications that must occur during the ROM/HAB boot stage.
The typical boot time for an image smaller than 1MB can be reduced from 25 ms to 12 ms."

Introduce a variable called `HABV4_FAST_AUTH` that ignores the CSF and IMG
certificates during the CSF file generation and only considers one of the SRK instead.

Link: https://lore.barebox.org/20260518-fpf-fast-auth-v1-1-eb795aedf999@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mach/imx/habv4-imx8-gencsf.h
@@ -8,6 +8,7 @@
  * CONFIG_HABV4_TABLE_BIN
  * CONFIG_HABV4_CSF_CRT_PEM
  * CONFIG_HABV4_IMG_CRT_PEM
+ * CONFIG_HABV4_SRK_CRT_PEM
  */
 #if defined(CONFIG_HABV4) && defined(CONFIG_CPU_64)
 #if defined(CONFIG_HABV4_QSPI)
@@ -26,9 +27,14 @@ hab File = CONFIG_HABV4_TABLE_BIN
 hab # SRK index within SRK-Table 0..3
 hab Source index = CONFIG_HABV4_SRK_INDEX
 
+#if defined(CONFIG_HABV4_FAST_AUTH)
+hab [Install NOCAK]
+hab File = CONFIG_HABV4_SRK_CRT_PEM
+#else
 hab [Install CSFK]
 /* target key index in keystore 1 */
 hab File = CONFIG_HABV4_CSF_CRT_PEM
+#endif
 
 hab [Authenticate CSF]
 
@@ -49,12 +55,14 @@ hab Features = FIELD RETURN
 hab UID = CONFIG_HABV4_CSF_UNLOCK_UID
 #endif
 
+#if !defined(CONFIG_HABV4_FAST_AUTH)
 hab [Install Key]
 /* verification key index in key store (0, 2...4) */
 hab Verification index = 0
 /* target key index in key store (2...4) */
 hab Target index = 2
 hab File = CONFIG_HABV4_IMG_CRT_PEM
+#endif
 
 hab [Authenticate Data]
 /* verification key index in key store (2...4) */

arch/arm/mach-imx/Kconfig
@@ -968,6 +968,21 @@ config HAB_CERTS_ENV
 	  CONFIG_HABV4_CSF_CRT_PEM
 	  CONFIG_HABV4_IMG_CRT_PEM
 
+	  For HABv4 with fast authentication:
+
+	  CONFIG_HABV4_TABLE_BIN
+	  CONFIG_HABV4_SRK_CRT_PEM
+
+config HABV4_FAST_AUTH
+	depends on HABV4
+	bool "Fast authentication"
+	help
+	  Use the SRK to verify the CSF and image data.
+	  This reduces the number of key pair authentications
+	  that occur during the ROM/HAB boot state, resulting in a decreased boot time.
+
+	  Requires HAB version >= 4.1.2
+
 config HABV4_SRK_INDEX
 	depends on HABV4
 	int "SRK index"

scripts/Makefile.lib
@@ -594,6 +594,7 @@ imxcfg_cpp_flags  = -Wp,-MD,$(depfile) -nostdinc -x assembler-with-cpp \
       $(call overwrite-hab-env,CONFIG_HABV4_CSF_CRT_PEM) \
       -DCONFIG_HABV4_CSF_UNLOCK_UID=$(CONFIG_HABV4_CSF_UNLOCK_UID) \
       $(call overwrite-hab-env,CONFIG_HABV4_IMG_CRT_PEM) \
+      $(call overwrite-hab-env,CONFIG_HABV4_SRK_CRT_PEM) \
 
 dcd-tmp = $(subst $(comma),_,$(dot-target).dcd.tmp)
 

commit deffe6346d7bb5b36e87e08709928666e23a1209

ARM: i.MX: fix fast auth wrong CSF file
The commit [1], which integrates fast authentication support on
i.MX8 SoCs does not work as expected. The resulting CSF cannot be
verified correctly by the boot rom, leading to an invalid signature.

The CST user guide documentation states in `Table 10. Authenticate Data arguments`,
that the `Verification Index` must also be set to 0 when we use fast
auth. This commit fixes that.

With this change, the bootrom seems to validate the signature properly
now.

[1] 086d01264d ("ARM: i.MX: add support for fast authentication")

Signed-off-by: Fabian Pfitzner <f.pfitzner@pengutronix.de>
Fixes: 086d01264d ("ARM: i.MX: add support for fast authentication")
Reviewed-by: Marco Felsch <m.felsch@pengutronix.de>
Link: https://lore.barebox.org/20260710-fpf-fast-auth-v1-1-5a2709d29ea4@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

include/mach/imx/habv4-imx8-gencsf.h
@@ -66,7 +66,11 @@ hab File = CONFIG_HABV4_IMG_CRT_PEM
 
 hab [Authenticate Data]
 /* verification key index in key store (2...4) */
+#if defined(CONFIG_HABV4_FAST_AUTH)
+hab Verification index = 0
+#else
 hab Verification index = 2
+#endif
 
 hab_blocks
 
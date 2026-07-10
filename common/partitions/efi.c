barebox/barebox

commit a576f2f7e5afaf3078dd816c6d5b178f93b8b4d2

partitions: efi: fix overflow issues while allocating gpt entries
while parsting the GPT header in alloc_read_gpt_entries() the number
of partitions can be large that multiplying it with the size of a single
partition overflows 32-bit multiplication.

we already enforce a MAX_PARTITION limit of 128 partitions in efi_partition(),
so allowing any bigger value in alloc_read_gpt_entries() would fail,
even if we fix the overflow.

Therefore, we can enforce the limit strictly and treat any overflow as
a failing condition.

Signed-off-by: Abdelrahman Youssef <abdelrahmanyossef12@gmail.com>
Link: https://lore.kernel.org/r/20241031124854.625174-1-abdelrahmanyossef12@gmail.com
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/partitions/efi.c
@@ -36,6 +36,28 @@ struct efi_partition {
 static const int force_gpt = IS_ENABLED(CONFIG_PARTITION_DISK_EFI_GPT_NO_FORCE);
 
 /**
+* compute_partitions_entries_size() - return the size of all partitions
+* @gpt: GPT header
+*
+* Description: return size of all partitions, 0 on error
+*
+* This is a helper function that compute the size of all partitions
+* by multiplying the size of a single partition by the number of partitions
+*/
+static u32 compute_partitions_entries_size(const gpt_header *gpt)
+{
+	u32 nb_parts, sz_parts, total_size;
+
+	nb_parts = min_t(u32, MAX_PARTITION, le32_to_cpu(gpt->num_partition_entries));
+	sz_parts = le32_to_cpu(gpt->sizeof_partition_entry);
+
+	if (check_mul_overflow(nb_parts, sz_parts, &total_size))
+		return 0;
+
+	return total_size;
+}
+
+/**
  * efi_crc32() - EFI version of crc32 function
  * @buf: buffer to calculate crc32 of
  * @len - length of buf
@@ -81,14 +103,12 @@ static u64 last_lba(struct block_device *bdev)
 static gpt_entry *alloc_read_gpt_entries(struct block_device *blk,
 					 gpt_header * pgpt_head)
 {
-	size_t count = 0;
+	u32 count = 0;
 	gpt_entry *pte = NULL;
 	unsigned long from, size;
 	int ret;
 
-	count = le32_to_cpu(pgpt_head->num_partition_entries) *
-		le32_to_cpu(pgpt_head->sizeof_partition_entry);
-
+	count = compute_partitions_entries_size(pgpt_head);
 	if (!count)
 		return NULL;
 
@@ -156,7 +176,7 @@ static gpt_header *alloc_read_gpt_header(struct block_device *blk,
 static int is_gpt_valid(struct block_device *blk, u64 lba,
 			gpt_header **gpt, gpt_entry **ptes)
 {
-	u32 crc, origcrc;
+	u32 crc, origcrc, count;
 	u64 lastlba;
 
 	if (!ptes)
@@ -215,10 +235,13 @@ static int is_gpt_valid(struct block_device *blk, u64 lba,
 	if (!(*ptes = alloc_read_gpt_entries(blk, *gpt)))
 		goto fail;
 
+	/* Check the size of all partitions */
+	count = compute_partitions_entries_size(*gpt);
+	if (!count)
+		goto fail_ptes;
+
 	/* Check the GUID Partition Table Entry Array CRC */
-	crc = efi_crc32((const unsigned char *)*ptes,
-		le32_to_cpu((*gpt)->num_partition_entries) *
-		le32_to_cpu((*gpt)->sizeof_partition_entry));
+	crc = efi_crc32((const unsigned char *)*ptes, count);
 
 	if (crc != le32_to_cpu((*gpt)->partition_entry_array_crc32)) {
 		dev_dbg(blk->dev, "GUID Partitition Entry Array CRC check failed: 0x%08x 0x%08x\n",

commit 9317ffc12b61a1f06397aa130a8e26b6209ccf9e

partitions: efi: validate complete GPT entry array
The Fedora-Workstation-Live-44-1.7.aarch64.iso image advertises 248
slots, which exceeds the 128 partition limit that the barebox EFI
partition parser enforced because of the MAX_PARTITION macro.

The enforcement is not only for creating actual partitions, but also
for computing the CRC, so valid GPTs with a larger entry array are
rejected, even though they have fewer actual partitions.

Read the complete entry array for validation while keeping the
existing export limit when partitions are registered.

Fixes: a576f2f7e5af ("partitions: efi: fix overflow issues while allocating gpt entries")
Assisted-by: Codex:gpt-5.5
Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20260709073316.522048-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

common/partitions/efi.c
@@ -142,7 +142,7 @@ static u32 compute_partitions_entries_size(const gpt_header *gpt)
 {
 	u32 nb_parts, sz_parts, total_size;
 
-	nb_parts = min_t(u32, MAX_PARTITION, le32_to_cpu(gpt->num_partition_entries));
+	nb_parts = le32_to_cpu(gpt->num_partition_entries);
 	sz_parts = le32_to_cpu(gpt->sizeof_partition_entry);
 
 	if (check_mul_overflow(nb_parts, sz_parts, &total_size))
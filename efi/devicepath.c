barebox/barebox

commit e0e30f6362429f2e4510f90f7a677d797aa6087b

efi: payload: protect against buggy EFI implementations
barebox' EFI loaded image handle should have a device path, but on the
off-case that it doesn't, we should just return an error code instead of
dereferencing a null pointer.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20240304190038.3486881-96-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

efi/payload/init.c
@@ -328,6 +328,9 @@ static int efi_postcore_init(void)
 	loaded_image_dp = device_path_from_handle(efi_loaded_image->device_handle);
 	pr_debug("loaded-image: %pD\n", loaded_image_dp);
 
+	if (!loaded_image_dp)
+		return -EINVAL;
+
 	uuid = device_path_to_partuuid(loaded_image_dp);
 	if (uuid) {
 		wchar_t *uuid16 = xstrdup_char_to_wchar(uuid);

commit da47299449f34466b09e97806473d1bc32a99604

efi: payload: don't fail postcore initcall when image has no device path
When barebox was loaded from memory or from a device without a device
path, e.g. via QEMU's fw_cfg on top of the barebox EFI loader, the loaded
image has no device handle. efi_postcore_init() currently bails out with
-EINVAL in that case, printing an initcall error and skipping the
registration of the barebox update handler.

There is no partition UUID to export as LoaderDevicePartUUID then, but
that's no reason to abort the initcall. The parent image handle may lack a
device path as well, and device_path_to_filepath() would then crash on the
NULL pointer. Make device_path_next_compatible_node() tolerate a NULL
device path, so both helpers just return NULL and the initcall proceeds
with the defaults.

Fixes: e0e30f636242 ("efi: payload: protect against buggy EFI implementations")
Assisted-by: Claude:fable-5
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.barebox.org/20260826121510.2935211-1-a.fatoum@pengutronix.de
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

efi/devicepath.c
@@ -788,7 +788,7 @@ static const struct efi_device_path *
 device_path_next_compatible_node(const struct efi_device_path *dev_path,
 				 u8 type, u8 subtype)
 {
-	for (; !is_device_path_end(dev_path);
+	for (; dev_path && !is_device_path_end(dev_path);
 	     dev_path = next_device_path_node(dev_path)) {
 
 		if (device_path_type(dev_path) != type)

efi/payload/init.c
@@ -246,11 +246,12 @@ static int efi_postcore_init(void)
 	efi_set_variable_uint64_le("LoaderFeatures", &efi_systemd_vendor_guid,
 				   loader_features);
 
+	/*
+	 * The image may have been loaded from memory, e.g. QEMU's fw_cfg.
+	 * There is no partition to report then, but the rest should still run.
+	 */
 	loaded_image_dp = device_path_from_handle(efi_loaded_image->device_handle);
 	pr_debug("loaded-image: %pD\n", loaded_image_dp);
-
-	if (!loaded_image_dp)
-		return -EINVAL;
 
 	uuid = device_path_to_partuuid(loaded_image_dp);
 	if (uuid) {
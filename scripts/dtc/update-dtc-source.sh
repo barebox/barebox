c96cf638ddd68b86566e28d426c48250caa10a64
Ahmad Fatoum <a.fatoum@barebox.org>
2026-09-20 13:58:52 +0200

    scripts/dtc: import fdtoverlay
    The build is about to apply overlays at compile time like Linux does
    in some instances, so import the current fdt_overlay.c and adjust the
    Makefile to build it.
    
    Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>

--- scripts/dtc/update-dtc-source.sh
+++ scripts/dtc/update-dtc-source.sh
@@ -32,7 +32,7 @@ DTC_LINUX_PATH=`pwd`/scripts/dtc
 
 DTC_SOURCE="checks.c data.c dtc.c dtc.h flattree.c fstree.c livetree.c srcpos.c \
 		srcpos.h treesource.c util.c util.h version_gen.h Makefile.dtc \
-		dtc-lexer.l dtc-parser.y fdtget.c"
+		dtc-lexer.l dtc-parser.y fdtget.c fdtoverlay.c"
 LIBFDT_SOURCE="Makefile.libfdt fdt.c fdt.h fdt_addresses.c fdt_empty_tree.c \
 		fdt_overlay.c fdt_ro.c fdt_rw.c fdt_strerror.c fdt_sw.c \
 		fdt_wip.c libfdt.h libfdt_env.h libfdt_internal.h"

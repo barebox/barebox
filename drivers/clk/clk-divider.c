barebox/barebox

commit 08a50cd3064ad367bd23c217d26856cc1ac47d57

clk: clk-divider: fix _get_maxdiv for table based divider
The divider lacks the code for calculating the maximum divider for table
based dividers. Add it.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/clk/clk-divider.c
@@ -23,12 +23,25 @@
 
 #define div_mask(d)	((1 << ((d)->width)) - 1)
 
+static unsigned int _get_table_maxdiv(const struct clk_div_table *table)
+{
+	unsigned int maxdiv = 0;
+	const struct clk_div_table *clkt;
+
+	for (clkt = table; clkt->div; clkt++)
+		if (clkt->div > maxdiv)
+			maxdiv = clkt->div;
+	return maxdiv;
+}
+
 static unsigned int _get_maxdiv(struct clk_divider *divider)
 {
 	if (divider->flags & CLK_DIVIDER_ONE_BASED)
 		return div_mask(divider);
 	if (divider->flags & CLK_DIVIDER_POWER_OF_TWO)
 		return 1 << div_mask(divider);
+	if (divider->table)
+		return _get_table_maxdiv(divider->table);
 	return div_mask(divider) + 1;
 }
 

commit 11e354a96c0aedf3fe13068f0876123eedf0d021

clk: divider: cap table divider values to 'width' member
This is an adoption of the corresponding Linux commit:

| commit fab88ca788dcacf2fbb006d5663456cbd390ee18
| Author: Stephen Boyd <sboyd@codeaurora.org>
| Date:   Mon Nov 30 17:31:38 2015 -0800
|
|     clk: divider: Cap table divider values to 'width' member
|
|     When we use a clk divider with a divider table, we limit the maximum
|     divider value in divider_get_val() to the div_mask(width), but when we
|     calculate the divider in divider_round_rate() we don't consider that
|     the maximum divider may be limited by the width. Pass the width along
|     to _get_table_maxdiv() so that we only return the maximum divider that
|     is valid. This is useful for clocks that want to share the same divider
|     table while limiting the available dividers to some subset of the table
|     depending on the width of the bitfield.

Without this, clk_divider_bestdiv() may settle on a divider whose table
value does not fit the register field. divider_get_val() then clamps the
value with min_t(), so clk_set_rate() programs a divider other than the
one determine_rate() promised.

Fixes: 08a50cd3064a ("clk: clk-divider: fix _get_maxdiv for table based divider")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

drivers/clk/clk-divider.c
@@ -12,13 +12,14 @@
 #include <linux/log2.h>
 #include <linux/math64.h>
 
-static unsigned int _get_table_maxdiv(const struct clk_div_table *table)
+static unsigned int _get_table_maxdiv(const struct clk_div_table *table,
+				      u8 width)
 {
-	unsigned int maxdiv = 0;
+	unsigned int maxdiv = 0, mask = clk_div_mask(width);
 	const struct clk_div_table *clkt;
 
 	for (clkt = table; clkt->div; clkt++)
-		if (clkt->div > maxdiv)
+		if (clkt->div > maxdiv && clkt->val <= mask)
 			maxdiv = clkt->div;
 	return maxdiv;
 }
@@ -31,7 +32,7 @@ static unsigned int _get_maxdiv(const struct clk_div_table *table, u8 width,
 	if (flags & CLK_DIVIDER_POWER_OF_TWO)
 		return 1 << clk_div_mask(width);
 	if (table)
-		return _get_table_maxdiv(table);
+		return _get_table_maxdiv(table, width);
 	return clk_div_mask(width) + 1;
 }
 
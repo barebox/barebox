barebox/barebox

commit b75025a39c77d4a7f408e56d44863bb98f1201f1

clk: divider: Fix best div calculation for power-of-two and table dividers
commit dd23c2cd38da2c64af381b19795d2c4f115e8ecb Linux upstream.

  clk: divider: Fix best div calculation for power-of-two and table dividers

  The divider returned by clk_divider_bestdiv() is likely to be invalid in case
  of power-of-two and table dividers when CLK_SET_RATE_PARENT flag isn't set.

  Fixes boot on STiH416 platform.

  Signed-off-by: Maxime Coquelin <maxime.coquelin@st.com>
  Signed-off-by: Mike Turquette <mturquette@linaro.org>
  [mturquette@linaro.org: trivial merge conflict & updated changelog]

Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/clk/clk-divider.c
@@ -141,6 +141,37 @@ static bool _is_valid_div(struct clk_divider *divider, unsigned int div)
 	return true;
 }
 
+static int _round_up_table(const struct clk_div_table *table, int div)
+{
+	const struct clk_div_table *clkt;
+	int up = _get_table_maxdiv(table);
+
+	for (clkt = table; clkt->div; clkt++) {
+		if (clkt->div == div)
+			return clkt->div;
+		else if (clkt->div < div)
+			continue;
+
+		if ((clkt->div - div) < (up - div))
+			up = clkt->div;
+	}
+
+	return up;
+}
+
+static int _div_round_up(struct clk_divider *divider,
+		unsigned long parent_rate, unsigned long rate)
+{
+	int div = DIV_ROUND_UP(parent_rate, rate);
+
+	if (divider->flags & CLK_DIVIDER_POWER_OF_TWO)
+		div = __roundup_pow_of_two(div);
+	if (divider->table)
+		div = _round_up_table(divider->table, div);
+
+	return div;
+}
+
 static int clk_divider_bestdiv(struct clk *clk, unsigned long rate,
 		unsigned long *best_parent_rate)
 {
@@ -156,7 +187,7 @@ static int clk_divider_bestdiv(struct clk *clk, unsigned long rate,
 
 	if (!(clk->flags & CLK_SET_RATE_PARENT)) {
 		parent_rate = *best_parent_rate;
-		bestdiv = DIV_ROUND_UP(parent_rate, rate);
+		bestdiv = _div_round_up(divider, parent_rate, rate);
 		bestdiv = bestdiv == 0 ? 1 : bestdiv;
 		bestdiv = bestdiv > maxdiv ? maxdiv : bestdiv;
 		return bestdiv;
@@ -229,6 +260,9 @@ static int clk_divider_set_rate(struct clk *clk, unsigned long rate,
 	} else {
 		div = DIV_ROUND_UP(parent_rate, rate);
 	}
+
+	if (!_is_valid_div(divider, div))
+		return -EINVAL;
 
 	value = _get_val(divider, div);
 

commit 309ff28510d818426e305a08fb31f00976c76815

clk: divider: fix table round up function
This is an adoption of the corresponding Linux commit:

| commit fe52e7505f8bf365d5ab0eeee19ababe406cbaaf
| Author: Maxime COQUELIN <maxime.coquelin@st.com>
| Date:   Wed May 7 18:48:52 2014 +0200
|
|     clk: divider: Fix table round up function
|
|     Commit 1d9fe6b97 ("clk: divider: Fix best div calculation for
|     power-of-two and table dividers") introduces a regression in its
|     _table_round_up function.
|
|     When the divider passed to this function is greater than the max
|     divider available in the table, this function returns table's max
|     divider.  Problem is that it causes an infinite loop in
|     clk_divider_bestdiv() because _next_div() will never return a value
|     greater than maxdiv.
|
|     Instead of returning table's max divider, this patch returns INT_MAX.

barebox does not have _next_div(), so there is no infinite loop to fix
here: clk_divider_bestdiv() clamps the result against maxdiv itself. Take
the change anyway to keep the file diffable against Linux, and because
the round-to-closest support added in the next commits needs a divider
that is genuinely out of range to compare against.

Fixes: b75025a39c77 ("clk: divider: Fix best div calculation for power-of-two and table dividers")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

drivers/clk/clk-divider.c
@@ -136,7 +136,7 @@ static bool _is_valid_div(const struct clk_div_table *table, unsigned int div,
 static int _round_up_table(const struct clk_div_table *table, int div)
 {
 	const struct clk_div_table *clkt;
-	int up = _get_table_maxdiv(table);
+	int up = INT_MAX;
 
 	for (clkt = table; clkt->div; clkt++) {
 		if (clkt->div == div)
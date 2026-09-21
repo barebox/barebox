barebox/barebox

commit 88ab8499365bd7aef2ec7282fd17469ba42824b2

ci: pytest: test against Debian 13.7.0 image
The Debian 13.6.0 CDROM ISO is no longer available, bump the minor revision
as a minimal fix to get CI going again.

Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

scripts/fetch-os.sh
@@ -8,7 +8,7 @@ fi
 set -euo pipefail
 
 declare -A images=(
-	["debian-13.6.0-arm64-netinst.iso"]="https://cloud.debian.org/images/release/current/arm64/iso-cd/debian-13.6.0-arm64-netinst.iso"
+	["debian-13.7.0-arm64-netinst.iso"]="https://cloud.debian.org/images/release/current/arm64/iso-cd/debian-13.7.0-arm64-netinst.iso"
 )
 
 found=0

.github/workflows/test-labgrid-pytest.yml
@@ -35,7 +35,7 @@ jobs:
           - ARCH: arm
             lgenv: test/arm/multi_v8_efiloader_defconfig.yaml
             defconfig: multi_v8_efiloader_defconfig
-            osimg: debian-13.6.0-arm64-netinst.iso
+            osimg: debian-13.7.0-arm64-netinst.iso
 
           - ARCH: mips
             lgenv: test/mips/qemu-malta_defconfig.yaml

commit c387edb3e09d7df20fd278e7b347ff6e4fc18959

ci: pytest: hardcode Debian netinstall ISO version at single place
Instead of hardcoding the version of the Debian ISO we test against at
multiple places and missing some, rename it to skip the minor versions
and have scripts/fetch-os.sh be the only place where the version
matters.

Fixes: 88ab8499365b ("ci: pytest: test against Debian 13.7.0 image")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

scripts/fetch-os.sh
@@ -8,7 +8,7 @@ fi
 set -euo pipefail
 
 declare -A images=(
-	["debian-13.7.0-arm64-netinst.iso"]="https://cloud.debian.org/images/release/current/arm64/iso-cd/debian-13.7.0-arm64-netinst.iso"
+	["debian-13.x.y-arm64-netinst.iso"]="https://cloud.debian.org/images/release/current/arm64/iso-cd/debian-13.7.0-arm64-netinst.iso"
 )
 
 found=0

.github/workflows/test-labgrid-pytest.yml
@@ -35,7 +35,7 @@ jobs:
           - ARCH: arm
             lgenv: test/arm/multi_v8_efiloader_defconfig.yaml
             defconfig: multi_v8_efiloader_defconfig
-            osimg: debian-13.7.0-arm64-netinst.iso
+            osimg: debian-13.x.y-arm64-netinst.iso
 
           - ARCH: mips
             lgenv: test/mips/qemu-malta_defconfig.yaml

test/arm/multi_v8_efiloader_defconfig.yaml
@@ -9,7 +9,7 @@ targets:
         kernel: barebox-dt-2nd.img
         display: qemu-default
         extra_args: >
-          -drive if=virtio,snapshot=on,file=debian-13.6.0-arm64-netinst.iso
+          -drive if=virtio,snapshot=on,file=debian-13.x.y-arm64-netinst.iso
           -device virtio-rng
       BareboxDriver:
         prompt: 'barebox@[^:]+:[^ ]+ '
@@ -35,6 +35,6 @@ targets:
 
 images:
   barebox-dt-2nd.img: !template "$LG_BUILDDIR/images/barebox-dt-2nd.img"
-  debian_iso: !template "../../debian-13.6.0-arm64-netinst.iso"
+  debian_iso: !template "../../debian-13.x.y-arm64-netinst.iso"
 imports:
   -  ../strategy.py